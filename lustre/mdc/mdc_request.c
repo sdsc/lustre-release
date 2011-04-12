d91dfdbd41dee5cf096778d5b7eef0186321388c
Lai Siyao <laisiyao@whamcloud.com>
2011-04-12 02:39:32 -0700

    LU-106 unable to handle kernel paging request in lprocfs_stats_collect()
    Some in memory data may be released/uninitialized at the time
    of proc entry creation/removal, this patch includes the following
    fixes:
    * initialize data before proc entry creation
    * free data after proc entry removal
    * free proc entries in obd_precleanup() because
      obd_uuid/nid/nid_stats_hash are released in class_cleanup().
    * free proc entries after obd_zombie_barrier() because obd_export
      hold one refcound of nid_stat.
    
    Signed-off-by: Lai Siyao <laisiyao@whamcloud.com>
    Change-Id: I03cb977e1be0747032a70f6a39fec804f81d70cc
    Reviewed-on:  http://review.whamcloud.com/326

--- lustre/mdc/mdc_request.c
+++ lustre/mdc/mdc_request.c
@@ -2041,6 +2041,11 @@ static int mdc_precleanup(struct obd_device *obd, enum obd_cleanup_stage stage)
                         cfs_up_write(&obd->u.cli.cl_sem);
                         obd->u.cli.cl_import = NULL;
                 }
+
+                ptlrpc_lprocfs_unregister_obd(obd);
+                lprocfs_obd_cleanup(obd);
+                ptlrpcd_decref();
+
                 rc = obd_llog_finish(obd, 0);
                 if (rc != 0)
                         CERROR("failed to cleanup llogging subsystems\n");
@@ -2056,10 +2061,6 @@ static int mdc_cleanup(struct obd_device *obd)
         OBD_FREE(cli->cl_rpc_lock, sizeof (*cli->cl_rpc_lock));
         OBD_FREE(cli->cl_setattr_lock, sizeof (*cli->cl_setattr_lock));
         OBD_FREE(cli->cl_close_lock, sizeof (*cli->cl_close_lock));
-
-        ptlrpc_lprocfs_unregister_obd(obd);
-        lprocfs_obd_cleanup(obd);
-        ptlrpcd_decref();
 
         return client_obd_cleanup(obd);
 }
