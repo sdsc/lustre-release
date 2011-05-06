ba663a03cff53c99c686acbd610105b80fe54286
yangsheng <ys@whamcloud.com>
2011-05-06 23:03:56 +0800

    LU-286 racer: general protection fault.
    Fixed previous patch hasn't cleanup appropriate may cause
    invalid reference.
    
    Change-Id: Ib9b6375c9a7bc57ee5ad17da514848ce8ca87acd
    Signed-off-by: Yang Sheng <ys@whamcloud.com>

--- lustre/mdc/mdc_locks.c
+++ lustre/mdc/mdc_locks.c
@@ -641,11 +641,12 @@ int mdc_enqueue(struct obd_export *exp, struct ldlm_enqueue_info *einfo,
         if (rc == 0) {
                 rc = ldlm_cli_enqueue(exp, &req, einfo, res_id, &policy, &flags,
                                       NULL, 0, NULL, lockh, 0);
+                if (rc < 0)
+                        CERROR("ldlm_cli_enqueue: %d\n", rc);
                 mdc_exit_request(&obddev->u.cli);
         }
         mdc_put_rpc_lock(obddev->u.cli.cl_rpc_lock, it);
         if (rc < 0) {
-                CERROR("ldlm_cli_enqueue: %d\n", rc);
                 mdc_clear_replay_flag(req, rc);
                 ptlrpc_req_finished(req);
                 RETURN(rc);
