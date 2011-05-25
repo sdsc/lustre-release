e51993edf395dfc6c141855266b7327128d71e8f
nasf <yong.fan@whamcloud.com>
2011-05-25 20:31:02 +0800

    LU-163 MDS returns 32/64-bit dir name hash according to client type
    1) liblustre client, 1.8.5 or older client do not support 64-bit dir name hash.
    2) register Lustre version 'get_name()' for NFS processing.
    3) 'll_dir_seek()' processes name hash-based dir seek operation.
    4) re-define "DIR_END_OFF" as "0x7fffffffffffffffULL" to always return positive
       dir name hash for successful seek.
    
    Signed-off-by: nasf <yong.fan@whamcloud.com>
    Change-Id: I0e3177c98c6e8814c39551ab54edfa3f861cbc8c

--- lustre/include/linux/lustre_compat25.h
+++ lustre/include/linux/lustre_compat25.h
@@ -48,6 +48,12 @@
 
 #include <linux/lustre_patchless_compat.h>
 
+#ifndef SEEK_SET
+ #define SEEK_SET 0
+ #define SEEK_CUR 1
+ #define SEEK_END 2
+#endif
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,14)
 struct ll_iattr {
         struct iattr    iattr;
