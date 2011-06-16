sdsc/lustre-release

commit c81b47a724dd4414b8bd79eb99b9737a70fd5855

LU-384 land bz22281 on b1_8
To facilitate distinctive naming, we need the work in bugzilla
22281 landed on b1_8.
Add an lbuild option to set the downstream-release.  Normally
this should not be exposed to/through lbuild as this was orig-
inally intended for downstream packages to add their own stamp
to the release string.

Include e4189c54470deff4a98f0b2bb03ac6be7ed85ce9:
    b=24095 don't really need configure --with-linux

    Don't really need to have run configure --with-linux for
    version-tag.pl to work.

    i=wangyb
    i=cliff

Expand the list of debian/* files that should be included in the
dist tarball.
Don't try to [un]patch the local-env.sh.  It should never really
be included in a landed patch.

Include most of 086ff8c46009479bfdac9f570331a6c93b7d52ac:

    b=24094 add kver; update kdir, kobjdir

    Add a new macro to allow the caller to simply define which kernel
    he wants to build for.
    Refactor the kdir and kobjdir to function more properly.
    Remove krequires.  Nothing is using it anymore.

    i=wangyb
    i=cliff

Include c1a61e3e9129d0e2062eed617d922e6dfa9ede5a:

    LU-284 strip .$arch off of kern vers for requires

    RHEL6 appends .arch to the end of the version in the utsrelease.h file.
    The kernel RPM however provides that value with the .arch stripped off,
    so do the same stripping in the lustre-modules RPM for it's Requires:.

    Signed-off-by: Brian J. Murrell <brian@whamcloud.com>
    Change-Id: Ib6ff32c006f184edceb5d667b649bb106d503faf
    Reviewed-on: http://review.whamcloud.com/502
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

Signed-off-by: Brian J. Murrell <brian@whamcloud.com>
Change-Id: I82f4f9a556fd1d1fc51ef8930e2017798ff88706
Reviewed-on: http://review.whamcloud.com/884
Reviewed-by: Johann Lombardi <johann@whamcloud.com>
Tested-by: Hudson

build/autoconf/lustre-build-linux.m4
@@ -95,7 +95,15 @@ AC_SUBST(modulenetdir)
 
 # ------------ RELEASE --------------------------------
 AC_MSG_CHECKING([for Lustre release])
-RELEASE="`echo ${LINUXRELEASE} | tr '-' '_'`_`date +%Y%m%d%H%M`"
+AC_ARG_WITH([release],
+	AC_HELP_STRING([--with-release=string],
+		       [set the release string (default=$kvers_YYYYMMDDhhmm)]),
+	[RELEASE=$withval],
+	RELEASE=""
+	if test -n "$DOWNSTREAM_RELEASE"; then
+		RELEASE="${DOWNSTREAM_RELEASE}_"
+	fi
+	RELEASE="$RELEASE`echo ${LINUXRELEASE} | tr '-' '_'`_$BUILDID")
 AC_MSG_RESULT($RELEASE)
 AC_SUBST(RELEASE)
 
@@ -159,12 +167,28 @@ AC_DEFUN([LB_ARG_CANON_PATH], [
 # Find paths for linux, handling kernel-source rpms
 #
 AC_DEFUN([LB_LINUX_PATH],
-[AC_MSG_CHECKING([for Linux sources])
+[# prep some default values
+for DEFAULT_LINUX in /lib/modules/$(uname -r)/{source,build} /usr/src/linux; do
+	if readlink -q -e $DEFAULT_LINUX; then
+		break
+	fi
+done
+if test "$DEFAULT_LINUX" = "/lib/modules/$(uname -r)/source"; then
+	PATHS="/lib/modules/$(uname -r)/build"
+fi
+PATHS+="$DEFAULT_LINUX"
+for DEFAULT_LINUX_OBJ in $PATHS; do
+	if readlink -q -e $DEFAULT_LINUX_OBJ; then
+		break
+	fi
+done
+AC_MSG_CHECKING([for Linux sources])
 AC_ARG_WITH([linux],
 	AC_HELP_STRING([--with-linux=path],
-		       [set path to Linux source (default=/usr/src/linux)]),
-	[LB_ARG_CANON_PATH([linux], [LINUX])],
-	[LINUX=/usr/src/linux])
+		       [set path to Linux source (default=/lib/modules/$(uname -r)/{source,build},/usr/src/linux)]),
+	[LB_ARG_CANON_PATH([linux], [LINUX])
+	DEFAULT_LINUX_OBJ=$LINUX],
+	[LINUX=$DEFAULT_LINUX])
 AC_MSG_RESULT([$LINUX])
 AC_SUBST(LINUX)
 
@@ -176,9 +200,10 @@ LB_CHECK_FILE([$LINUX],[],
 AC_MSG_CHECKING([for Linux objects dir])
 AC_ARG_WITH([linux-obj],
 	AC_HELP_STRING([--with-linux-obj=path],
-			[set path to Linux objects dir (default=$LINUX)]),
+			[set path to Linux objects dir (default=/lib/modules/$(uname -r)/build,/usr/src/linux)]),
 	[LB_ARG_CANON_PATH([linux-obj], [LINUX_OBJ])],
-	[LINUX_OBJ=$LINUX])
+	[LINUX_OBJ=$DEFAULT_LINUX_OBJ])
+
 AC_MSG_RESULT([$LINUX_OBJ])
 AC_SUBST(LINUX_OBJ)
 

autoMakefile.am
@@ -16,7 +16,18 @@ include build/autoMakefile.am.toplevel
 EXTRA_DIST += config.h.in
 
 if USES_DPKG
-EXTRA_DIST += debian/*
+EXTRA_DIST += debian/control.modules.in debian/control.main \
+	      debian/linux-patch-lustre.kpatches \
+	      debian/lustre-utils.examples debian/copyright debian/source \
+	      debian/source/format debian/lustre-source.docs \
+	      debian/lustre-utils.docs debian/lustre-utils.manpages \
+	      debian/lustre-client-modules.install debian/README.Debian \
+	      debian/.gitignore debian/linux-patch-lustre.dirs \
+	      debian/linux-patch-lustre.unpatch debian/lustre-dev.install \
+	      debian/lustre-source.dirs debian/liblustre.install debian/rules \
+	      debian/lustre-dev.dirs debian/linux-patch-lustre.apply \
+	      debian/control debian/changelog debian/lustre-tests.install \
+	      debian/lustre-utils.install
 endif
 
 if LDISKFS_ENABLED
@@ -46,11 +57,14 @@ checkstack-clean:
 
 module-dist-hook:
 	if [ -d CVS -o -d .git ]; then \
-	    perl lustre/scripts/tree_status.pl > $(distdir)/tree_status; \
-	elif [ -f tree_status ]; then \
-	    cp tree_status $(distdir)/tree_status; \
+	    perl lustre/scripts/make_META.pl > $(distdir)/META; \
+	    cp $(distdir)/META $(distdir)/ldiskfs/META; \
+	elif [ -f META ]; then \
+	    cp META $(distdir)/META; \
+	    cp $(distdir)/META $(distdir)/ldiskfs/META; \
 	else \
-	    echo -e "I have no idea how to create a tree_status file in $(distdir).\nPlease file a bug at http://bugzilla.lustre.org/"; \
+	    echo -e "I have no idea how to create a META file in $(distdir).\nPlease file a bug at http://bugzilla.lustre.org/"; \
+	    exit 1; \
 	fi
 	if [ -d .git ]; then \
 		build/extract_patches $(distdir); \

build/autoMakefile.am.toplevel
@@ -112,10 +112,20 @@ EXTRA_DIST = @PACKAGE_TARNAME@.spec					\
 
 rpms-real: @PACKAGE_TARNAME@.spec dist Makefile
 	CONFIGURE_ARGS=$$(echo $$(eval echo $(ac_configure_args)) | sed -re 's/--(en|dis)able-tests//'); \
-	RPMARGS="--define \"configure_args $$CONFIGURE_ARGS\""; \
+	if [ -n "@LINUX@" ]; then \
+		CONFIGURE_ARGS=$$(echo $$(eval echo $$CONFIGURE_ARGS) | sed -re 's/--with-linux=[^ ][^ ]*//'); \
+		RPMARGS="--define \"kdir @LINUX@\""; \
+		CONFIGURE_ARGS=$$(echo $$(eval echo $$CONFIGURE_ARGS) | sed -re 's/--with-linux-obj=[^ ][^ ]*//'); \
+		if [ -n "@LINUX_OBJ@" -a "@LINUX_OBJ@" != "@LINUX@" ]; then \
+			RPMARGS="$$RPMARGS --define \"kobjdir @LINUX_OBJ@\""; \
+		fi; \
+	fi; \
+	CONFIGURE_ARGS=$$(echo $$(eval echo $$CONFIGURE_ARGS) | sed -re 's/--with-release=[^ ][^ ]*//'); \
+	RPMARGS="$$RPMARGS --define \"configure_args $$CONFIGURE_ARGS\""; \
 	if ! $(BUILD_TESTS); then \
 		RPMARGS="$$RPMARGS --define \"build_lustre_tests 0\""; \
 	fi; \
+	echo "Building Lustre RPM with $$RPMARGS"; \
 	eval rpmbuild $$RPMARGS -ta $(distdir).tar.gz
 
 srpm-real: @PACKAGE_TARNAME@.spec dist Makefile

build/autoconf/lustre-build.m4
@@ -29,6 +29,86 @@ AC_SUBST(lb_target_os)
 ])
 
 #
+# LB_DOWNSTREAM_RELEASE
+#
+AC_DEFUN([LB_DOWNSTREAM_RELEASE],
+[AC_ARG_WITH([downstream-release],
+	AC_HELP_STRING([--with-downstream-release=string],
+		       [set a string in the BUILD_VERSION and RPM Release: (default is nothing)]),
+	[DOWNSTREAM_RELEASE=$with_downstream_release],
+	[
+	# if not specified, see if it's in the META file
+	if test -f META; then
+		DOWNSTREAM_RELEASE=$(sed -ne '/^LOCAL_VERSION =/s/.*= *//p' META)
+	fi
+	])
+AC_SUBST(DOWNSTREAM_RELEASE)
+])
+
+#
+# LB_BUILDID
+#
+# Check if the source is a GA release and if not, set a "BUILDID"
+#
+# Currently there are at least two ways/modes of/for doing this.  One
+# is if we are in a valid git repository, the other is if we are in a
+# non-git source tree of some form.  Building the latter from the former
+# will be handled here.
+AC_DEFUN([LB_BUILDID],
+[
+AC_MSG_CHECKING([for buildid])
+BUILDID=""
+if git branch >/dev/null 2>&1; then
+	ffw=0
+	hash=""
+	ver=$(git describe --match [[0-9v]]\* --tags)
+	if [[[ $ver = *-*-* ]]]; then
+		hash=${ver##*-}
+		ffw=${ver#*-}
+		ffw=${ffw%-*}
+		ver=${ver%%-*}
+	fi
+	# it's tempting to use [[ $ver =~ ^v([0-9]+_)+([0-9]+|RC[0-9]+)$ ]]
+	# here but the portability of the regex on the right is dismal
+	# (thanx suse)
+	if echo "$ver" | egrep -q "^v([0-9]+_)+([0-9]+|RC[0-9]+)$"; then
+		ver=$(echo $ver | sed -e 's/^v\(.*\)/\1/' \
+				      -e 's/_RC[[0-9]].*$//' -e 's/_/./g')
+	fi
+
+	# a "lustre fix" value of .0 should be truncated
+	if [[[ $ver = *.*.*.0 ]]]; then
+		ver=${ver%.0}
+	fi
+	# ditto for a "lustre fix" value of _0
+	if [[[ $ver = v*_*_*_0 ]]]; then
+		ver=${ver%_0}
+	fi
+
+
+	# only do this test for lustre (not ldiskfs)
+	if test "$PACKAGE" = "lustre" -a "$ver" != "$VERSION"; then
+		AC_MSG_ERROR([most recent tag found: $ver does not match current version $VERSION.])
+	fi
+
+	if test "$ffw" != "0"; then
+		BUILDID="$hash"
+		msg="$BUILDID (ahead by $ffw commits)"
+		AC_MSG_RESULT([$msg])
+	else
+		AC_MSG_RESULT([none... congratulations, you must be on a tag])
+	fi
+elif test -f META; then
+	BUILDID=$(sed -ne '/^BUILDID =/s/.*= *//p' META)
+	msg="$BUILDID (from META file)"
+	AC_MSG_RESULT([$msg])
+else
+	AC_MSG_WARN([FIXME: I don't know how to deal with source trees outside of git that don't have a META file.  Not setting a buildid.])
+fi
+AC_SUBST(BUILDID)
+])
+
+#
 # LB_CHECK_FILE
 #
 # Check for file existance even when cross compiling
@@ -227,7 +307,7 @@ case x$with_ldiskfs in
 		;;
 	*)
 		AC_MSG_RESULT([$with_ldiskfs])
-		LB_CHECK_FILE([$with_ldiskfs/ldiskfs/linux/ldiskfs_fs.h],[],[
+		LB_CHECK_FILE([$with_ldiskfs/ldiskfs/inode.c],[],[
 			AC_MSG_ERROR([A complete (built) external ldiskfs was not found.])
 		])
 		LDISKFS_DIR=$with_ldiskfs
@@ -622,6 +702,30 @@ fi
 ])
 
 #
+# LB_CONFIG_DIST
+#
+# Just enough configure so that "make dist" is useful
+#
+# this simply re-adjusts some defaults, which of course can be overridden
+# on the configure line after the --for-dist option
+#
+AC_DEFUN([LB_CONFIG_DIST],
+[AC_MSG_CHECKING([whether to configure just enough for make dist])
+AC_ARG_ENABLE([dist],
+	AC_HELP_STRING([--enable-dist],
+			[only configure enough for make dist]),
+	[enable_dist='yes'],[enable_dist='no'])
+AC_MSG_RESULT([$enable_dist])
+if test x$enable_dist != xno; then
+	enable_modules='no'
+	enable_utils='no'
+        enable_liblustre='no'
+        enable_doc='no'
+        enable_tests='no'
+fi
+])
+
+#
 # LB_CONFIG_DOCS
 #
 # Build docs?
@@ -836,6 +940,9 @@ AC_DEFUN([LB_CONFIGURE],
 
 LB_CONFIG_DIST
 
+LB_DOWNSTREAM_RELEASE
+LB_BUILDID
+
 LB_USES_DPKG
 
 LB_LIBCFS_DIR
@@ -856,7 +963,7 @@ LB_CONFIG_UTILS
 LB_CONFIG_TESTS
 LC_CONFIG_CLIENT_SERVER
 
-# two macros for cmd3 
+# two macros for cmd3
 m4_ifdef([LC_CONFIG_SPLIT], [LC_CONFIG_SPLIT])
 LN_CONFIG_CDEBUG
 LC_QUOTA

build/extract_patches
@@ -34,6 +34,18 @@ if [ $ffw -gt 0 ]; then
 	rm -f 00list *.dpatch
 	for file in [0-9][0-9][0-9][0-9]-*.patch; do
 		if [ -s $file ] && ! grep -q "^--- a/debian/" $file; then
+			# remove patching of files that the debian packaging
+			# should not be touching
+			ed <<EOF $file
+/diff --git a\/local-env.sh b\/local-env.sh
+/diff --git a\//i
+endofrangemarker
+.
+?diff --git a\/
+/diff --git a\/local-env.sh b\/local-env.sh/,/endofrangemarker/d
+w
+q
+EOF
 			desc=$(cat $file | sed -e '1,/^$/d' \
 						 -e '/^---$/,$d')
 			dpatch_file=${file/.patch/.dpatch}

build/lbuild
@@ -50,7 +50,6 @@ TARGET_ARCHS_ALL="$TARGET_ARCH"
 CONFIGURE_FLAGS=
 EXTERNAL_PATCHES=
 EXTRA_VERSION=
-LUSTRE_EXTRA_VERSION=
 STAGEDIR=
 TMPDIR=${TMPDIR:-"/var/tmp"}
 TIMESTAMP=
@@ -131,6 +130,9 @@ Usage: ${0##*/} [OPTION]... [-- <lustre configure options>]
     Specifies the CVS Root to use when pulling files from CVS.  The
     environment variable \$CVSROOT is used if this option is not
     present.
+
+  --downstream-release
+    Set the downstream version.
 
   --external-patches=EXTERNAL_PATCHES
     Directory similar to lustre/lustre/kernel_patches/ that lbuild should
@@ -248,7 +250,22 @@ EOF
     fatal "$1" "$2"
 }
 
-# canonicalize a relative path
+# canonicalize a relative path to a file
+canon_filepath() {
+    local PATH="$1"
+
+    if [ ! -f "$PATH" ]; then
+        return 1
+    fi
+
+    local FILE=${PATH##*/}
+    local DIR=${PATH%/*}
+
+    echo $(canon_path "$DIR")/$FILE
+    return 0
+}
+
+# canonicalize a relative path to a dir
 canon_path() {
     local PATH="$1"
 
@@ -632,7 +649,9 @@ load_target() {
         if $PATCHLESS || [ -n "$SERIES" ]; then
             EXTRA_VERSION=$(echo $EXTRA_VERSION | sed -e "s/\(.*_lustre\)\..*/\1/")
 #            EXTRA_VERSION="${EXTRA_VERSION}-${TAG}.${TIMESTAMP}"
-            EXTRA_VERSION="${EXTRA_VERSION}.${TIMESTAMP}"
+            if ! $PATCHLESS && [ -n "$BUILDID" ]; then
+                EXTRA_VERSION="${EXTRA_VERSION}.${BUILDID}"
+            fi
         fi
     fi
     # EXTRA_VERSION=${EXTRA_VERSION//-/_}
@@ -728,7 +747,8 @@ unpack_lustre() {
             fatal 1 "There was an error checking out Lustre/Portals/Build from CVS."
         echo "Creating lustre tarball..."
         sh autogen.sh || fatal 1 "There was an error running autogen.sh."
-        ./configure --disable-{modules,utils,liblustre,tests,doc} || \
+        ./configure --enable-dist \
+                    --with-downstream-release "$DOWNSTREAM_RELEASE" || \
             fatal 1 "There was an error running ./configure to create makefiles."
         make dist || fatal 1 "There was an error running 'make dist'."
         LUSTRE=$PWD/lustre-*.tar.gz
@@ -791,46 +811,70 @@ build_lustre() {
         targets="--target $arch $targets"
     done
 
-    local confoptions="--with-linux=${linux}"
+    local confoptions=""
+
     if $PATCHLESS; then
-        confoptions="--with-linux=${linux} --disable-server"
-    fi
-    if [ "$linuxobj" != "" ]; then
-        confoptions="$confoptions --with-linux-obj=${linuxobj}"
+        confoptions="$confoptions --disable-server"
     fi
 
-    ./configure $confoptions ${CONFIGURE_FLAGS} 2>&1
-    if [ "$?" != "0" ]; then
-        local saved_config="../config.log.$(date +%s)"
-        cp config.log $saved_config
-        chmod a+r $saved_config
-        echo "Saved config.log is at $saved_config"
-        cat /proc/mounts
-        ls -l /proc/$$
-        pwd
-        echo "config.log contents:"
-        cat config.log
-        popd
-        return 255
-    fi
-
-    gen_lustre_version
-
-    # hack. Somebody move build/lustre.spec to lustre.spec for b1_6
-    local lustre_spec
-    [ -f lustre.spec ] && lustre_spec=lustre.spec
-    [ -f build/lustre.spec ] && lustre_spec=build/lustre.spec
-
-    [ -f "$lustre_spec" ] && sed \
-        -e "s^Release: .*$^Release: $LUSTRE_EXTRA_VERSION^" \
-        < $lustre_spec \
-        > ../lustre.spec
-
-    local rpmbuildopt='-bb'
+    local rpmbuildopt='-tb'
     if $NORPM; then
-        rpmbuildopt='-bc'
+        rpmbuildopt='-tc'
         echo NORPM mode. Only compiling.
     fi
+
+    ( $(skeep_ldiskfs_rpm $TAG) ) || {
+
+        pushd ldiskfs > /dev/null || return 255
+
+        if !  ./configure --enable-dist; then
+            echo "failed to configure in ldiskfs"
+            popd >/dev/null # pushd ldiskfs
+            popd >/dev/null # pushd lustre
+            return 255
+        fi
+
+        if ! make dist 2>&1; then
+            popd >/dev/null # pushd ldiskfs
+            popd >/dev/null # pushd lustre
+            return 255
+        fi
+
+        #cp lustre-ldiskfs*.tar.gz $TOPDIR/SOURCES || \
+        #    fatal 1 "Could not copy lustre-ldiskfs*.tar.gz to $TOPDIR/SOURCES"
+
+        if ! $RPMBUILD $targets $rpmbuildopt lustre-ldiskfs*.tar.gz \
+            --define "configure_args $confoptions ${CONFIGURE_FLAGS}" \
+            --define "kdir $linux" \
+            ${linuxobj:+--define "kobjdir $linuxobj"} \
+            --define "_tmppath /var/tmp" \
+            --define "_topdir $TOPDIR" 2>&1; then
+            popd >/dev/null # pushd ldiskfs
+            popd >/dev/null # pushd lustre
+            return 255
+        fi
+
+        if $DO_SRC; then
+            if ! $RPMBUILD -ts lustre-ldiskfs*.tar.gz \
+                --define "configure_args $confoptions ${CONFIGURE_FLAGS}" \
+                --define "kdir $linux" \
+                ${linuxobj:+--define "kobjdir $linuxobj"} \
+                --define "_tmppath /var/tmp" \
+                --define "_topdir $TOPDIR" 2>&1; then
+                popd >/dev/null # pushd ldiskfs
+                popd >/dev/null # pushd lustre
+                return 255
+            fi
+        fi
+        popd >/dev/null # pushd ldiskfs
+
+        # tell lustre where ldiskfs is
+        # XXX - pointing to the RPM BUILD dir is a hack.  we need to flesh
+        #       out the ldiskfs RPM build so that it builds a
+        #       lustre-ldiskfs-devel RPM and install that and point lustre
+        #       to that instead
+        confoptions="$confoptions --with-ldiskfs=$(ls -d $TOPDIR/BUILD/lustre-ldiskfs-*)"
+    }
 
     # convert the $PATCHLESS boolean to an empty/not-empty boolean
     # as silly as this seems, it makes the syntax of the rpmbuild command
@@ -846,50 +890,18 @@ build_lustre() {
         lustre_tests="no"
     fi
 
-    $RPMBUILD $targets $rpmbuildopt ../lustre.spec \
+    $RPMBUILD $targets $rpmbuildopt "$LUSTRE" \
         ${is_patchless:+--define "lustre_name lustre-client"} \
         ${lustre_tests:+--define "build_lustre_tests 0"} \
         ${FIND_REQUIRES:+--define "__find_requires $FIND_REQUIRES"} \
         --define "configure_args $confoptions ${CONFIGURE_FLAGS}" \
+        --define "kdir $linux" \
+        ${linuxobj:+--define "kobjdir $linuxobj"} \
         --define "_tmppath $TMPDIR" \
         --define "_topdir $TOPDIR" 2>&1 || \
         fatal 1 "Error building rpms for $BUILD_ARCHS."
 
     popd >/dev/null
-    ( $(skeep_ldiskfs_rpm $TAG) ) && return
-
-    pushd lustre/ldiskfs || return 255
-    make dist 2>&1
-    if [ "$?" != "0" ]; then
-        popd
-        return 255
-    fi
-    cp lustre-ldiskfs*.tar.gz $TOPDIR/SOURCES
-
-    gen_lustre_version
-
-    local ldiskfs_spec=lustre-ldiskfs.spec
-    [ -f "$ldiskfs_spec" ] && sed \
-    -e "s^Release: .*$^Release: $LUSTRE_EXTRA_VERSION^" \
-    < $ldiskfs_spec \
-    > ../lustre-ldiskfs.spec
-
-    if ! $RPMBUILD $targets $rpmbuildopt ../lustre-ldiskfs.spec \
-        --define "_tmppath /var/tmp" \
-        --define "_topdir $TOPDIR" 2>&1; then
-        popd
-        return 255
-    fi
-
-    if $DO_SRC; then
-        if ! $RPMBUILD -bs ../lustre-ldiskfs.spec \
-            --define "_tmppath /var/tmp" \
-            --define "_topdir $TOPDIR" 2>&1; then
-            popd
-            return 255
-        fi
-    fi
-    popd
 
 }
 
@@ -913,33 +925,21 @@ stage() {
 
 #check if we need to build separate ldiskfs RPM
 skeep_ldiskfs_rpm() {
-        local tag="$1"
+    local tag="$1"
 
-        local skip=false
+    local skip=false
 
-        if ! $LDISKFSRPM; then
-            skip=true
-        elif $PATCHLESS; then
-            skip=true
-        else
-            for skiptag in $SKIPLDISKFSRPM; do
-                [[ $tag == $skiptag ]] && skip=true && break
-            done
-        fi
+    if ! $LDISKFSRPM; then
+        skip=true
+    elif $PATCHLESS; then
+        skip=true
+    else
+        for skiptag in $SKIPLDISKFSRPM; do
+            [[ $tag == $skiptag ]] && skip=true && break
+        done
+    fi
 
-        pushd $TOPDIR/BUILD/lustre-[1-9]* >/dev/null
-        grep -q '^SERVER_TRUE[ \t]=[ \t]#$' autoMakefile && skip=true
-        popd >/dev/null
-
-        echo $skip
-
-}
-
-#generate LUSTRE_EXTRA_VERSION from EXTRA_VERSION
-gen_lustre_version() {
-
-    LUSTRE_EXTRA_VERSION="${lnxmaj}-${EXTRA_VERSION}${FLAVOR_DELIMITER}${RPMSMPTYPE}"
-    LUSTRE_EXTRA_VERSION=${LUSTRE_EXTRA_VERSION//-/_}
+    echo $skip
 
 }
 
@@ -1454,7 +1454,7 @@ build_ofed() {
             return 0
         fi
     fi
-    
+
     # build kernel-ib
     if $USE_BUILD_CACHE && [ -n "$REUSEBUILD" ]; then
         local REUSE_SIGNATURE=$({ echo "$ofed_version";
@@ -1760,7 +1760,7 @@ set -E
 
 [ -r ~/.lbuildrc ] && . ~/.lbuildrc
 
-options=$(getopt -o d:D:h -l kerneltree:,distro:,kernelrpm:,reusebuild:,patchless,ldiskfs,ccache,reuse:,norpm,disable-datestamp,external-patches:,timestamp:,extraversion:,kerneldir:,linux:,lustre:,nodownload,nosrc,ofed-version:,publish,release,set-value:,src,stage:,tag:,target:,target-archs:,with-linux:,xen,enable-mptlinux,enable-rdac -- "$@")
+options=$(getopt -o d:D:h -l kerneltree:,distro:,downstream-release:,kernelrpm:,reusebuild:,patchless,ldiskfs,ccache,reuse:,norpm,disable-datestamp,external-patches:,timestamp:,extraversion:,kerneldir:,linux:,lustre:,nodownload,nosrc,ofed-version:,publish,release,set-value:,src,stage:,tag:,target:,target-archs:,with-linux:,xen,enable-mptlinux,enable-rdac -- "$@")
 
 if [ $? != 0 ]; then
     usage 1
@@ -1783,6 +1783,10 @@ while [ "$1" ]; do
             ;;
         -D)
             DATE=$2
+            shift 2
+            ;;
+        --downstream-release)
+            DOWNSTREAM_RELEASE=$2
             shift 2
             ;;
         --external-patches)
@@ -1849,7 +1853,9 @@ while [ "$1" ]; do
             shift 2
             ;;
         --lustre)
-            LUSTRE=$2
+            if ! LUSTRE=$(canon_filepath "$2"); then
+                fatal 1 "Could not determine the canonical location of $2"
+            fi
             shift 2
             ;;
         --nodownload)
@@ -1931,6 +1937,10 @@ done
 check_options
 
 unpack_lustre
+
+# XXX - should we _always_ get the buildid from the META file?  what are the
+# other (i.e. non-lustre-tarball use cases of lbuild)?
+BUILDID=$(sed -ne '/^BUILDID =/s/.*= *//p' lustre/META)
 
 load_target
 

build/lbuild-oel5
@@ -3,8 +3,6 @@
 source ${0%/*}/lbuild-rhel5
 
 # increment this if you have made a change that should force a new kernel
-# to build built
-#BUILD_GEN=1
-#BUILD_GEN=2	# bz19952: remove -lustre tag from kernel RPM names
-BUILD_GEN=3	# bz19975 enable the building of src.rpms by default
-
+# to build built for this distribution (only -- if you want to force a kernel
+# build on all distributions, update the BUILD_GEN variable in build/lbuild)
+BUILD_GEN+=".0"

build/lbuild-sles10
@@ -1,10 +1,9 @@
 # vim:expandtab:shiftwidth=4:softtabstop=4:tabstop=4:
 
 # increment this if you have made a change that should force a new kernel
-# to build built
-#BUILD_GEN=1
-#BUILD_GEN=2	# bz19952: remove -lustre tag from kernel RPM names
-BUILD_GEN=3	# bz19975 enable the building of src.rpms by default
+# to build built for this distribution (only -- if you want to force a kernel
+# build on all distributions, update the BUILD_GEN variable in build/lbuild)
+BUILD_GEN+=".0"
 
 source ${0%/*}/lbuild-sles
 

commit c1a5e7979a53619229e08b84c0bb66e19ec0d422

LU-384 Add space to default PATHS
The update in c81b47a724dd4414b8bd79eb99b9737a70fd5855 broke
configure if --with-linux was not specified, causing build
failures because the .config file could not be found.  Fix this.

Update some ancient documentation that is referenced if the
.config file cannot be found to better describe what is needed
to build Lustre with modern kernel packages (for RHEL at least).

Change-Id: I5b373e3ee3ef42c68257c27a4b194c9e00cf023f
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>

README
@@ -1,3 +1,3 @@
 Instructions for building, configuring and running Lustre can be found at:
-    http://wiki.lustre.org/index.php/Use
+    http://wiki.whamcloud.com/
 

build/README.kernel-source
@@ -1,82 +1,37 @@
-Lustre can currently build against Red Hat 2.4 and SuSE 2.6
-kernel-source RPMs.  All other kernel-source RPMs are *unsupported* at
-this time.
+Lustre can be build against current Red Hat and SuSE kernels.  The
+specific  kernel versions are listed in the lustre/ChangeLog file.
 
-Note that a Lustre-patched kernel is required for building Lustre; in
-most cases a kernel-source RPM from your Linux vendor will not contain
-the necessary patches.
+To build client packages it is enough to have the kernel-devel packages
+installed with only the .config and header files.  To build server
+packages, the full kernel-source package with Lustre patches applied
+needs to be available.
 
 ##
-## Instructions for Red Hat 2.4.x kernel-source RPMs
+## Instructions for Red Hat 2.6 kernel-devel RPMs
 ##
 
-1.  kernel.h
+1. Kernel-devel package
 
-Building against a kernel-source RPM requires a special header.  On
-Red Hat systems, this file should be automatically created at boot
-time, and saved in /boot/kernel.h.
+The kernel-devel RPM should be installed, and referenced appropriately
+by /lib/modules/$(uname -r)/{build,source} with a pre-existing .config
+file for the built kernel.  This .config file should NOT be modified.
 
- *** If you are not running Red Hat Linux, or are not booted into the
- *** kernel you are trying to build against, you need to create this
- *** file manually.
+2.  A Client Example
 
- *** If you do not, the Lustre build may fail, or may fail to build
- *** modules that work with your kernel.
+To configure Lustre for the currently-running kernel:
 
-Here is an example /boot/kernel.h file.  If you are building on
-x86_64, the first defines should be __MODULE_KERNEL_x86_64, etc.  The
-other defines should be simple to figure out.
+./configure
 
-/* This file is automatically generated at boot time. */
-#ifndef __BOOT_KERNEL_H_
-#define __BOOT_KERNEL_H_
+or to build against a different kernel:
 
-/* Kernel type i686-smp */
+./configure --with-linux=/lib/modules/{other kernel version/build
 
-#ifndef __MODULE_KERNEL_i686
-#define __MODULE_KERNEL_i686 1
-#endif
+3. Another Example
 
-#ifndef __BOOT_KERNEL_ENTERPRISE
-#define __BOOT_KERNEL_ENTERPRISE 0
-#endif
+To configure Lustre for building server packages, with the full kernel source:
 
-#ifndef __BOOT_KERNEL_BIGMEM
-#define __BOOT_KERNEL_BIGMEM 0
-#endif
+./configure --with-linux=/usr/src/linux
 
-#ifndef __BOOT_KERNEL_HUGEMEM
-#define __BOOT_KERNEL_HUGEMEM 0
-#endif
-
-#ifndef __BOOT_KERNEL_SMP
-#define __BOOT_KERNEL_SMP 1
-#endif
-
-#ifndef __BOOT_KERNEL_UP
-#define __BOOT_KERNEL_UP 0
-#endif
-
-#endif
-
-You should save this somewhere, and pass the location of this file to
-./configure using the --with-kernel-source-header option.
-
-2.  .config
-
-You will also need to tell Lustre about the .config file for your
-kernel.  The two likely locations of this file are
-/boot/config-$(uname -r), and /usr/src/linux-2.4/configs/.  You should
-pass the location of this file to Lustre using the --with-linux-config
-option.
-
-3.  An Example
-
-Here is an example for configuring Lustre:
-
-./configure --with-linux=/usr/src/linux-2.4.20-28.9_lustre.1.0.3 \
---with-kernel-source-header=/boot/kernel.h \
---with-linux-config=/boot/config-2.4.20-28.9_lustre.1.0.3smp
 
 ##
 ## Instructions for SuSE 2.6.x kernel-source RPMs

build/autoconf/lustre-build-linux.m4
@@ -176,12 +176,15 @@ done
 if test "$DEFAULT_LINUX" = "/lib/modules/$(uname -r)/source"; then
 	PATHS="/lib/modules/$(uname -r)/build"
 fi
-PATHS+="$DEFAULT_LINUX"
+echo "DEFAULT_LINUX=$DEFAULT_LINUX"
+PATHS+=" $DEFAULT_LINUX"
+echo "PATHS=$PATHS"
 for DEFAULT_LINUX_OBJ in $PATHS; do
 	if readlink -q -e $DEFAULT_LINUX_OBJ; then
 		break
 	fi
 done
+echo "DEFAULT_LINUX_OBJ=$DEFAULT_LINUX_OBJ"
 AC_MSG_CHECKING([for Linux sources])
 AC_ARG_WITH([linux],
 	AC_HELP_STRING([--with-linux=path],

lustre/BUILDING
@@ -1,17 +1,18 @@
 BUILDING LUSTRE
 ---------------
 
-You must already have a Lustre-patched kernel, which is outside of the
-scope of this document.  For more information on this process, see the
-web sites below.  Also consider downloading a pre-packaged Lustre
-kernel and utilities from http://downloads.lustre.org/
+To build the server components, you must already have a Lustre-patched kernel,
+which is outside of the scope of this document.  It is possible to build
+Lustre clients against unpatched kernels.  For more information on this
+process, see the web sites below.  Also consider downloading a pre-packaged
+Lustre kernel and utilities from http://downloads.whamcloud.com/
 
 To build:
   sh autogen.sh
-  ./configure --with-linux=/usr/src/lustre_patched_kernel_tree
+  ./configure [--with-linux=/usr/src/kernel_tree if not in a "normal" location]
   make
 
-To play with Lustre:
+To play with Lustre (will format a simple test filesystem using loop devices):
   cd tests
   sh llmount.sh
 
@@ -19,7 +20,7 @@ To clean up:
   sh llmountcleanup.sh
 
 More information about Lustre:
-  http://www.lustre.org/
+  http://wiki.whamcloud.com/
 
 Feedback: 
   lustre-discuss@lists.lustre.org