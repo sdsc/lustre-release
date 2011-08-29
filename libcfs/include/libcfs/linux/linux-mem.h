cf0906684e0b910cbefdd3a96de0698d284e8bb3
yangsheng <ys@whamcloud.com>
2011-08-29 12:32:18 +0800

    LU-506 FC15 patchless client support.
    The changes to adapt the 'struct shrink_control'. v3.0
    use shrink_control as shrinker callback argument instead
    of the 'int nr_to_scan' and 'gfp_mask'. Redhat backport
    it to FC15.
    
    Signed-off-by: Yang Sheng <ys@whamcloud.com>
    Change-Id: Id9f6a9e10efe785d2837d1ad73098d2808a2f076

--- libcfs/include/libcfs/linux/linux-mem.h
+++ libcfs/include/libcfs/linux/linux-mem.h
@@ -161,14 +161,22 @@ extern int cfs_mem_is_in_cache(const void *addr, const cfs_mem_cache_t *kmem);
  */
 #define cfs_shrinker    shrinker
 
-#ifdef HAVE_SHRINKER_WANT_SHRINK_PTR
+#if defined(HAVE_SHRINKER_WANT_SHRINK_PTR) || defined(HAVE_SHRINK_CONTROL)
 #define SHRINKER_FIRST_ARG struct shrinker *shrinker, 
 #else
 #define SHRINKER_FIRST_ARG 
 #endif
 
+
 #ifdef HAVE_REGISTER_SHRINKER
-typedef int (*cfs_shrinker_t)(SHRINKER_FIRST_ARG int nr_to_scan, gfp_t gfp_mask);
+
+#ifdef HAVE_SHRINK_CONTROL
+typedef int (*cfs_shrinker_t)(SHRINKER_FIRST_ARG
+                              struct shrink_control *sc);
+#else
+typedef int (*cfs_shrinker_t)(SHRINKER_FIRST_ARG
+                              int nr_to_scan, gfp_t gfp_mask);
+#endif
 
 static inline
 struct cfs_shrinker *cfs_set_shrinker(int seek, cfs_shrinker_t func)
