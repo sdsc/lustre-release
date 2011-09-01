79f831f48ecedaf94986c74daf73e87d2f5846ea
Lai Siyao <laisiyao@whamcloud.com>
2011-08-31 19:35:23 -0700

    LU-581 1.8 <-> 2.1 interop: sanity test 120 fail
    In 1.8 <-> 2.1 upgrade case, LMV device doesn't exist, but some
    ELC logic is implemented in LMV layer.
    * replace MF_MDC_CANCEL_FID? with MF_REMOTE_FID? which means the
      specific fid is on remote MDT, LMV has sent async cancel request,
      and mdc_create/link/unlink/rename/setattr doesn't need cancel
      lock synchronously.
    
    Change-Id: Ia0018b45758d7584096d226419265b35747fe7c8
    Signed-off-by: Lai Siyao <laisiyao@whamcloud.com>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -371,9 +371,9 @@ static int ll_intent_file_open(struct file *file, void *lmm,
                         opc = LUSTRE_OPC_CREATE;
         }
 
-        op_data  = ll_prep_md_op_data(NULL, parent->d_inode,
-                                      file->f_dentry->d_inode, name, len,
-                                      O_RDWR, opc, NULL);
+        op_data = ll_prep_md_op_data(NULL, parent->d_inode,
+                                     file->f_dentry->d_inode, name, len,
+                                     O_RDWR, opc, NULL);
         if (IS_ERR(op_data))
                 RETURN(PTR_ERR(op_data));
 
