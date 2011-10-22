b0a5c5fa3eabb6de38900b4931c851f2984ae47b
Niu Yawei <niu@whamcloud.com>
2011-10-22 16:16:55 +0800

    LU-753 obdfilter: improper LASSERT in filter_commitrw_write()
    In rare cases fsfilt_commit_wait() will wake up and return after the
    transaction has finished its work and updated j_commit_sequence but
    the commit callbacks have not been run yet. Which will trigger the
    LASSERT(oti->oti_transno <= obd->obd_last_committed) improperly.
    
    We should just wait for the commit callback finished instead of put
    an improper LASSERT here.
    
    Signed-off-by: Niu Yawei <niu@whamcloud.com>
    Change-Id: Ibd5add8d352d2e7598be49b0bf8fa37d40ce6e1f

--- lustre/obdfilter/filter_io_26.c
+++ lustre/obdfilter/filter_io_26.c
@@ -785,10 +785,21 @@ retry:
                         rc = err;
         }
 
-        if (obd->obd_replayable && !rc && wait_handle)
-                LASSERTF(oti->oti_transno <= obd->obd_last_committed,
-                         "oti_transno "LPU64" last_committed "LPU64"\n",
-                         oti->oti_transno, obd->obd_last_committed);
+        /* In rare cases fsfilt_commit_wait() will wake up and return after
+         * the transaction has finished its work and updated j_commit_sequence
+         * but the commit callbacks have not been run yet.  Wait here until
+         * that is finished so that clients requesting sync IO don't see the
+         * reply transno < last_committed.  LU-753 */
+        if (unlikely(obd->obd_replayable && !rc && wait_handle &&
+                     oti->oti_transno > obd->obd_last_committed)) {
+                cfs_waitq_t waitq;
+                struct l_wait_info lwi = { 0 };
+
+                cfs_waitq_init(&waitq);
+                l_wait_event(waitq,
+                             oti->oti_transno <= obd->obd_last_committed,
+                             &lwi);
+        }
 
         fsfilt_check_slow(obd, now, "commitrw commit");
 
