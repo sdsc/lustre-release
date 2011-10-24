574970851b8d0129d9e7675a24fa16e7d7cd8740
Niu Yawei <niu@whamcloud.com>
2011-10-24 08:14:09 -0700

    LU-694 ptlrpc: Job Stats
    This feature is to collect filesystem operation stats for the jobs
    running on Lustre.
    
    When some job scheduler (SLURM, for instance) is running on lustre
    client, the lustre client will pack the job id into each request
    (open, unlink, write...), and server will collect those information
    then expose them via procfs.
    
    - A 'pb_jobid' is added in 'ptlrpc_body' for storing the jobID;
    - The job stats can be accessed from the proc file 'job_stats' in
      each mdt & obdfilter proc entries;
    - 'job_cleanup_interval' is created in each mdt & obdfilter proc
      entries for tuning stats cleanup interval;
    - 'jobid_var' is created in llite proc entry for configuring the
      jobID environment variable name;
    
    Signed-off-by: Niu Yawei <niu@whamcloud.com>
    Change-Id: I35c21f93d7ce1ce648504ce437fcfd374f891453

--- lustre/include/cl_object.h
+++ lustre/include/cl_object.h
@@ -2384,6 +2384,8 @@ struct cl_req_attr {
         struct obdo     *cra_oa;
         /** Capability. */
         struct obd_capa *cra_capa;
+        /** Jobid */
+        char             cra_jobid[JOBSTATS_JOBID_SIZE];
 };
 
 /**
