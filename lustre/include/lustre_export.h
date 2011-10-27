438ef97f18f0e308314cf914d21e86704a3af98e
Oleg Drokin <green@whamcloud.com>
2011-10-26 20:52:27 -0400

    LU-793 ptlrpc: Allow clients to reconnect with RPCs in process
    Since not letting clients with pending RPCs to reconnect caused a lot
    of problems, this patch allows such a reconnection and would only
    refuse to process requests with the same xids that are already being
    processed.
    
    Change-Id: I629bbcef766901797ffcea80a218d4482b1a2d93
    Signed-off-by: Oleg Drokin <green@whamcloud.com>

--- lustre/include/lustre_export.h
+++ lustre/include/lustre_export.h
@@ -246,6 +246,8 @@ struct obd_export {
                                   /* client timed out and tried to reconnect,
                                    * but couldn't because of active rpcs */
                                   exp_abort_active_req:1;
+        cfs_list_t                exp_rpcs_in_progress;  /* RPC being handled */
+        __u64                     exp_max_xid_seen;
         /* also protected by exp_lock */
         enum lustre_sec_part      exp_sp_peer;
         struct sptlrpc_flavor     exp_flvr;             /* current */
