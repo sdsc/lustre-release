b4a91f57eec8e9f63e7e314707a9dd247733e47f
Fan Yong <yong.fan@whamcloud.com>
2011-11-07 22:46:56 +0800

    ORNL-3 consider low-layer options for MDT ACL flags
    Currently, MDT layer enables ACL support by default without checking
    whether low-layer (ldiskfs) enables ACL support or not, then causes
    unnecessary data traffic on network and through MDS stack for ACL.
    
    So MDT should communicate with low-layer before setting ACL flags.
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: I804f10bf486745ddd3b23b89e959dfd585589ac0

--- lustre/include/darwin/lustre_lib.h
+++ lustre/include/darwin/lustre_lib.h
@@ -1,3 +1,4 @@
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
  * vim:expandtab:shiftwidth=8:tabstop=8:
  *
  * GPL HEADER START
@@ -22,12 +23,13 @@
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
  * have any questions.
- *
- * GPL HEADER END
  */
 /*
  * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
+ */
+/*
+ * Copyright (c) 2011 Whamcloud, Inc.
  */
 /*
  * This file is part of Lustre, http://www.lustre.org/
