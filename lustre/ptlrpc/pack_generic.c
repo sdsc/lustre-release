e3b5791eeaff6448ccf4f04d7bf1bc66445fcf35
Alex Zhuravlev <bzzz@whamcloud.com>
2011-11-22 10:59:11 +0300

    ORI-377 debug: changes to help debug logging infrastructure to save stack
     - libcfs_debug_vmsg2() to accept libcfs_debug_msg_data structure
       to replace SUBSYSTEM, __FILE__, __FUNCTION__, __LINE__ and cdls
       on the stack
     - libcfs_debug_vmsg1() for regular users like CDEBUG() which don't
       need 2 format strings
     - CDEBUG, RETURN, EXIT, GOTO, CHECK_STACK to init libcfs_debug_msg_data
    
    Change-Id: Id663188f18adbe5059010929fc0b40927e714771
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>

--- lustre/ptlrpc/pack_generic.c
+++ lustre/ptlrpc/pack_generic.c
@@ -2262,7 +2262,7 @@ static inline int rep_ptlrpc_body_swabbed(struct ptlrpc_request *req)
         }
 }
 
-void _debug_req(struct ptlrpc_request *req, __u32 mask,
+void _debug_req(struct ptlrpc_request *req,
                 struct libcfs_debug_msg_data *data, const char *fmt, ... )
 {
         int req_ok = req->rq_reqmsg != NULL;
@@ -2275,8 +2275,7 @@ void _debug_req(struct ptlrpc_request *req, __u32 mask,
         }
 
         va_start(args, fmt);
-        libcfs_debug_vmsg2(data->msg_cdls, data->msg_subsys,mask,data->msg_file,
-                           data->msg_fn, data->msg_line, fmt, args,
+        libcfs_debug_vmsg2(data, fmt, args,
                            " req@%p x"LPU64"/t"LPD64"("LPD64") o%d->%s@%s:%d/%d"
                            " lens %d/%d e %d to %d dl "CFS_TIME_T" ref %d "
                            "fl "REQ_FLAGS_FMT"/%x/%x rc %d/%d\n",
