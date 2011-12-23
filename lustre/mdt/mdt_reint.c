1fcfeb9d1434e4be750b88fa7c0f164cb3348b6a
Yu Jian <yujian@whamcloud.com>
2011-12-23 19:52:59 +0800

    LU-80 lustre: wide striping support
    This is a combination of the following patches for supporting
    wide striping:
    
    * http://review.whamcloud.com/1708 for the ldiskfs changes
    * http://review.whamcloud.com/1194 for the utils changes
    * http://review.whamcloud.com/1111 for the lov changes
    * http://review.whamcloud.com/1808 for the mds changes
    * http://review.whamcloud.com/1880 for the test changes
    
    This combined patch set is intended to be built by Jenkins
    and tested by autotest system.
    
    Signed-off-by: Yu Jian <yujian@whamcloud.com>
    Change-Id: I18d872c8d87db5ec13aa62a899388b73a7c43490

--- lustre/mdt/mdt_reint.c
+++ lustre/mdt/mdt_reint.c
@@ -485,7 +485,7 @@ static int mdt_reint_setattr(struct mdt_thread_info *info,
         struct mdt_object       *mo;
         struct md_object        *next;
         struct mdt_body         *repbody;
-        int                      som_au, rc;
+        int                      som_au, rc, rc2;
         ENTRY;
 
         DEBUG_REQ(D_INODE, req, "setattr "DFID" %x", PFID(rr->rr_fid1),
@@ -604,7 +604,9 @@ out:
         if (rc == 0)
                 mdt_counter_incr(req->rq_export, LPROC_MDT_SETATTR);
 
-        mdt_shrink_reply(info);
+        rc2 = mdt_fix_reply(info);
+        if (rc == 0)
+                rc = rc2;
         return rc;
 }
 
