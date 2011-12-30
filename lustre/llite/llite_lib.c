bc713f35b7143fc6dacbbf73861475314c392325
Hongchao Zhang <hongchao.zhang@whamcloud.com>
2011-12-30 15:55:47 +0800

    LU-921 llite: generate warings in case of discarding dirty page
    when a client is evicted, dirty pages may get silently discarded,
    the caller of successful write(2) will not know that the data he
    wrote have been discarded due to eviction before it can be flushed
    to the OSS.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@whamcloud.com>
    Change-Id: Iecfbf096548ff08cdd6064d53ad8c688343fcddc

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -2235,3 +2235,38 @@ int ll_show_options(struct seq_file *seq, struct vfsmount *vfs)
 
         RETURN(0);
 }
+
+void ll_dirty_page_discard_warn(cfs_page_t *page, int ioret)
+{
+        char *buf, *path = NULL;
+        struct dentry *dentry = NULL;
+
+        buf = (char*)__get_free_page(GFP_KERNEL);
+        if (buf != NULL) {
+                dentry = d_find_alias(page->mapping->host);
+                if (dentry) {
+#ifdef HAVE_FS_STRUCT_USE_PATH
+                        struct path p;
+                        p.dentry = dentry;
+                        p.mnt = current->fs->root.mnt;
+                        path_get(&p);
+                        path = d_path(&p, buf, PAGE_SIZE);
+                        path_put(&p);
+#else
+                        path = d_path(dentry, current->fs->rootmnt, buf,
+                                      PAGE_SIZE);
+#endif
+                }
+        }
+
+        CWARN("dirty page discard: %s/[ino:%lu]/%s may get corrupted (rc %d)\n",
+              s2lsi(page->mapping->host->i_sb)->lsi_lmd->lmd_dev,
+              page->mapping->host->i_ino, (path && !IS_ERR(path)) ? path : "",
+              ioret);
+
+        if (dentry)
+                dput(dentry);
+
+        if (buf)
+                free_page((unsigned long)buf);
+}
