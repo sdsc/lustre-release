dc78a1403c4421045c27296b42ddb8d24ad1cb99
Niu Yawei <niu@whamcloud.com>
2012-01-13 00:33:22 -0800

    LU-904 ptlrpc: redo io on -EINPROGRESS
    When server return -EINPROGRESS for a write RPC, the client should keep
    resending the RPC until server return other error code or the client is
    evicted.
    
    This is required by the new quota design: when a write on OST can't
    acquire quota from master for broken network, it should return
    -EINPROGRESS to inform the client to retry write infinitely.
    
    This patch also fixed the defect of redo io RPC can't be aborted during
    eviction, in a lightweight manner.
    
    Signed-off-by: Niu Yawei <niu@whamcloud.com>
    Change-Id: Iea393cb1ea55e9d006f52dbfc39a2b9a3670d682

--- lustre/utils/wiretest.c
+++ lustre/utils/wiretest.c
@@ -984,6 +984,8 @@ void lustre_assert_wire_constants(void)
                  OBD_CONNECT_MAXBYTES);
         LASSERTF(OBD_CONNECT_IMP_RECOV == 0x10000000000ULL, "found 0x%.16llxULL\n",
                  OBD_CONNECT_IMP_RECOV);
+        LASSERTF(OBD_CONNECT_EINPROGRESS == 0x80000000000ULL, "found 0x%.16llxULL\n",
+                 OBD_CONNECT_EINPROGRESS);
         LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
                  (unsigned)OBD_CKSUM_CRC32);
         LASSERTF(OBD_CKSUM_ADLER == 0x00000002UL, "found 0x%.8xUL\n",
