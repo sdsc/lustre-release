cd12b830925b3ecde09fa66f567dc083dcc8b8bb
Fan Yong <yong.fan@whamcloud.com>
2012-01-30 19:41:30 +0800

    LU-957 mdd: proc interfaces for OI Scrub
    To allow administrator to show/adjust OI Scrub
    parameters after MDT mounts up, in spite of OI
    Scrub running or not.
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: I4a1dc96ae913aa77f7603bc75b04484c5f1c7c1a

--- lustre/mdd/mdd_lproc.c
+++ lustre/mdd/mdd_lproc.c
@@ -299,6 +299,260 @@ static int lprocfs_wr_sync_perm(struct file *file, const char *buffer,
         return count;
 }
 
+static int lprocfs_rd_noscrub(char *page, char **start, off_t off,
+                                int count, int *eof, void *data)
+{
+        struct mdd_device *mdd = data;
+
+        LASSERT(mdd != NULL);
+        return snprintf(page, count, "%d\n", mdd->mdd_noscrub);
+}
+
+static int lprocfs_wr_noscrub(struct file *file, const char *buffer,
+                              unsigned long count, void *data)
+{
+        struct mdd_device *mdd = data;
+        int val, rc;
+
+        LASSERT(mdd != NULL);
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        cfs_down(&mdd->mdd_scrub_header_sem);
+        if (!!val != mdd->mdd_noscrub) {
+                mdd->mdd_noscrub = !!val;
+                /* Notify MDT layer */
+                rc = md_do_upcall(NULL, &mdd->mdd_md_dev, MD_NOSCRUB, &val);
+                if (rc != 0)
+                        mdd->mdd_noscrub = !val;
+        }
+        cfs_up(&mdd->mdd_scrub_header_sem);
+        return rc != 0 ? rc : count;
+}
+
+static int lprocfs_rd_scrub_speed_limit(char *page, char **start, off_t off,
+                                        int count, int *eof, void *data)
+{
+        struct mdd_device *mdd = data;
+
+        LASSERT(mdd != NULL);
+        return snprintf(page, count, "%u\n",
+                        mdd->mdd_scrub_header_mem.sh_param.sp_speed_limit);
+}
+
+static int lprocfs_wr_scrub_speed_limit(struct file *file, const char *buffer,
+                                        unsigned long count, void *data)
+{
+        struct mdd_device *mdd = data;
+        struct scrub_param *sp_mem = &mdd->mdd_scrub_header_mem.sh_param;
+        struct scrub_param *sp_disk = &mdd->mdd_scrub_header_disk.sh_param;
+        __u32 val, old;
+        int rc;
+
+        LASSERT(mdd != NULL);
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        cfs_down(&mdd->mdd_scrub_header_sem);
+        if (val != sp_mem->sp_speed_limit) {
+                old = sp_mem->sp_speed_limit;
+                sp_mem->sp_speed_limit = val;
+                rc = mdd_scrub_store_param(mdd->mdd_scrub_env, mdd,
+                                           &mdd->mdd_scrub_header_mem,
+                                           &mdd->mdd_scrub_header_disk);
+                if (rc != 0) {
+                        sp_mem->sp_speed_limit = old;
+                        sp_disk->sp_speed_limit = cpu_to_be32(old);
+                } else {
+                        cfs_spin_lock(&mdd->mdd_scrub_lock);
+                        if (mdd->mdd_scrub_it != NULL)
+                                mdd_scrub_msi_set_speed(sp_mem,
+                                                        mdd->mdd_scrub_it);
+                        cfs_spin_unlock(&mdd->mdd_scrub_lock);
+                }
+        }
+        cfs_up(&mdd->mdd_scrub_header_sem);
+        return rc != 0 ? rc : count;
+}
+
+static int lprocfs_rd_scrub_error_handle(char *page, char **start, off_t off,
+                                         int count, int *eof, void *data)
+{
+        struct mdd_device *mdd = data;
+        char *str;
+
+        LASSERT(mdd != NULL);
+        if (mdd->mdd_scrub_header_mem.sh_param.sp_flags & SPF_FAILOUT)
+                str = "abort";
+        else
+                str = "continue";
+        return snprintf(page, count, "%s\n", str);
+}
+
+static int lprocfs_wr_scrub_error_handle(struct file *file, const char *buffer,
+                                         unsigned long count, void *data)
+{
+        struct mdd_device *mdd = data;
+        struct scrub_param *sp_mem = &mdd->mdd_scrub_header_mem.sh_param;
+        struct scrub_param *sp_disk = &mdd->mdd_scrub_header_disk.sh_param;
+        char dummy[12];
+        int val, old, rc = 0;
+
+        LASSERT(mdd != NULL);
+        if (cfs_copy_from_user(dummy, buffer, 8))
+                return -EFAULT;
+
+        if (strcmp(dummy, "abort") == 0) {
+                val = 1;
+        } else if (strcmp(dummy, "continue") == 0) {
+                val = 0;
+        } else {
+                CWARN("Invalid value for scrub error handle.\n"
+                      "The valid value should be: abort|continue\n");
+                return -EINVAL;
+        }
+
+        cfs_down(&mdd->mdd_scrub_header_sem);
+        if (sp_mem->sp_flags & SPF_FAILOUT)
+                old = 1;
+        else
+                old = 0;
+
+        if (val != old) {
+                if (val != 0)
+                        sp_mem->sp_flags |= SPF_FAILOUT;
+                else
+                        sp_mem->sp_flags &= ~SPF_FAILOUT;
+                rc = mdd_scrub_store_param(mdd->mdd_scrub_env, mdd,
+                                           &mdd->mdd_scrub_header_mem,
+                                           &mdd->mdd_scrub_header_disk);
+                if (rc != 0) {
+                        if (old != 0)
+                                sp_mem->sp_flags |= SPF_FAILOUT;
+                        else
+                                sp_mem->sp_flags &= ~SPF_FAILOUT;
+                        sp_disk->sp_flags = cpu_to_be16(sp_mem->sp_flags);
+                }
+        }
+        cfs_up(&mdd->mdd_scrub_header_sem);
+        return rc != 0 ? rc : count;
+}
+
+static int lprocfs_rd_scrub_pipeline_window(char *page, char **start,
+                                            off_t off, int count,
+                                            int *eof, void *data)
+{
+        struct mdd_device *mdd = data;
+
+        LASSERT(mdd != NULL);
+        return snprintf(page, count, "%u\n",
+                        mdd->mdd_scrub_header_mem.sh_param.sp_pipeline_window);
+}
+
+static int lprocfs_wr_scrub_pipeline_window(struct file *file,
+                                            const char *buffer,
+                                            unsigned long count, void *data)
+{
+        struct mdd_device *mdd = data;
+        struct scrub_param *sp_mem = &mdd->mdd_scrub_header_mem.sh_param;
+        struct scrub_param *sp_disk = &mdd->mdd_scrub_header_disk.sh_param;
+        struct dt_scrub_set_param dssp;
+        __u32 val, old;
+        int rc;
+
+        LASSERT(mdd != NULL);
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        cfs_down(&mdd->mdd_scrub_header_sem);
+        if (val != sp_mem->sp_pipeline_window) {
+                old = sp_mem->sp_pipeline_window;
+                sp_mem->sp_pipeline_window = val;
+                rc = mdd_scrub_store_param(mdd->mdd_scrub_env, mdd,
+                                           &mdd->mdd_scrub_header_mem,
+                                           &mdd->mdd_scrub_header_disk);
+                if (rc != 0) {
+                        sp_mem->sp_pipeline_window = old;
+                        sp_disk->sp_pipeline_window = cpu_to_be32(old);
+                } else {
+                        dssp.dssp_flags = DSSF_ADJUST_WINDOW;
+                        dssp.dssp_window = sp_mem->sp_pipeline_window;
+                        cfs_spin_lock(&mdd->mdd_scrub_lock);
+                        if (mdd->mdd_scrub_it != NULL) {
+                                struct mdd_scrub_it *msi = mdd->mdd_scrub_it;
+
+                                if (msi->msi_it != NULL)
+                                        mdd->mdd_scrub_obj->do_index_ops->
+                                        dio_it.set(NULL, msi->msi_it, &dssp);
+                        }
+                        cfs_spin_unlock(&mdd->mdd_scrub_lock);
+                }
+
+        }
+        cfs_up(&mdd->mdd_scrub_header_sem);
+        return rc != 0 ? rc : count;
+}
+
+static int lprocfs_rd_scrub_checkpoint_interval(char *page, char **start,
+                                                off_t off, int count,
+                                                int *eof, void *data)
+{
+        struct mdd_device *mdd = data;
+
+        LASSERT(mdd != NULL);
+        return snprintf(page, count, "%u\n",
+                        mdd->mdd_scrub_header_mem.
+                        sh_param.sp_checkpoint_interval);
+}
+
+static int lprocfs_wr_scrub_checkpoint_interval(struct file *file,
+                                                const char *buffer,
+                                                unsigned long count, void *data)
+{
+        struct mdd_device *mdd = data;
+        struct scrub_param *sp_mem = &mdd->mdd_scrub_header_mem.sh_param;
+        struct scrub_param *sp_disk = &mdd->mdd_scrub_header_disk.sh_param;
+        __u32 val, old;
+        int rc;
+
+        LASSERT(mdd != NULL);
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        if (val < 1 || val > SPM_CHECKPOING_INTERVAL) {
+                CWARN("Invalid value for scrub checkpoint interval: %d\n"
+                      "The valid value should be [1 - %d]\n",
+                      val, SPM_CHECKPOING_INTERVAL);
+                return -EINVAL;
+        }
+
+        cfs_down(&mdd->mdd_scrub_header_sem);
+        if (val != sp_mem->sp_checkpoint_interval) {
+                old = sp_mem->sp_checkpoint_interval;
+                sp_mem->sp_checkpoint_interval = val;
+                rc = mdd_scrub_store_param(mdd->mdd_scrub_env, mdd,
+                                           &mdd->mdd_scrub_header_mem,
+                                           &mdd->mdd_scrub_header_disk);
+                if (rc != 0) {
+                        sp_mem->sp_checkpoint_interval = old;
+                        sp_disk->sp_checkpoint_interval = cpu_to_be32(old);
+                } else {
+                        cfs_spin_lock(&mdd->mdd_scrub_lock);
+                        if (mdd->mdd_scrub_it != NULL)
+                                mdd->mdd_scrub_it->msi_checkpoint_next -=
+                                                cfs_time_seconds(old -
+                                                sp_mem->sp_checkpoint_interval);
+                        cfs_spin_unlock(&mdd->mdd_scrub_lock);
+                }
+        }
+        cfs_up(&mdd->mdd_scrub_header_sem);
+        return rc != 0 ? rc : count;
+}
+
 static struct lprocfs_vars lprocfs_mdd_obd_vars[] = {
         { "atime_diff",      lprocfs_rd_atime_diff, lprocfs_wr_atime_diff, 0 },
         { "changelog_mask",  lprocfs_rd_changelog_mask,
@@ -309,6 +563,15 @@ static struct lprocfs_vars lprocfs_mdd_obd_vars[] = {
                              mdd_lprocfs_quota_wr_type, 0 },
 #endif
         { "sync_permission", lprocfs_rd_sync_perm, lprocfs_wr_sync_perm, 0 },
+        { "noscrub",         lprocfs_rd_noscrub, lprocfs_wr_noscrub, 0 },
+        { "scrub_speed_limit", lprocfs_rd_scrub_speed_limit,
+                               lprocfs_wr_scrub_speed_limit, 0 },
+        { "scrub_error_handle", lprocfs_rd_scrub_error_handle,
+                                lprocfs_wr_scrub_error_handle, 0 },
+        { "scrub_pipeline_window", lprocfs_rd_scrub_pipeline_window,
+                                   lprocfs_wr_scrub_pipeline_window, 0 },
+        { "scrub_checkpoint_interval", lprocfs_rd_scrub_checkpoint_interval,
+                                       lprocfs_wr_scrub_checkpoint_interval, 0},
         { 0 }
 };
 
