2d69cc724fcb1f9a6fd3df8e0f9f6023949a1ffe
Alexander.Boyko <alexander_boyko@xyratex.com>
2012-02-06 14:45:51 +0400

    LU-1071 lnet: add config file support for routes and ip2nets params.
    Large network requires many config line for routes, but the module
    param is limited to 1024 bytes. This patch resolves this limitation.
    The routes and ip2nets module parameters accept absolute path names,
    the old syntax is supported as well. Example:
    routes="/etc/lnet_routes.conf"
    
    Signed-off-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Reviewd-by: Alexey Lyashkov <alexey_lyashkow@xyratex.com>
    Reveiwed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Reviewed-by: Isaac Huang <isaac_huang@xyratex.com>
    Xyratex-bug-id: MRP-342
    Change-Id: I55be1aa053f001ebe5bfc5613ce7c19231935234

--- lnet/lnet/api-ni.c
+++ lnet/lnet/api-ni.c
@@ -59,17 +59,79 @@ static char *routes = "";
 CFS_MODULE_PARM(routes, "s", charp, 0444,
                 "routes to non-local networks");
 
+static int lnet_read_file(const char *name, char *config, size_t size)
+{
+        cfs_file_t      *filp;
+        int             rc;
+        loff_t          pos = 0;
+
+        filp = cfs_filp_open(name, O_RDONLY, 0600, &rc);
+        if (!filp) {
+                CERROR("Can't open file %s: rc %d\n", name, rc);
+                return rc;
+        }
+
+        rc = cfs_user_read(filp, config, size, &pos);
+        cfs_filp_close(filp);
+        if (rc < 0)
+                CERROR("Can't read file %s: rc %d\n", name, rc);
+
+        return rc;
+}
+
+static char *lnet_read_file_mem(const char *name, size_t max_size)
+{
+        char            *file;
+        int             rc;
+
+        file = cfs_alloc_large(max_size);
+        if (file == NULL) {
+                CERROR("Failed to allocate memory for "
+                       "file, size = %lu\n", (long unsigned)max_size);
+                return ERR_PTR(-ENOMEM);
+        }
+
+        memset(file, 0, max_size);
+
+        rc = lnet_read_file(name, file, max_size);
+        if (rc < 0) {
+                cfs_free_large(file);
+                return ERR_PTR(rc);
+        }
+
+        return file;
+}
+
+#define CFS_CONF_FILE_SIZE      (1024 * 1024)
+/* Caller should free memory */
 char *
 lnet_get_routes(void)
 {
-        return routes;
+        if (*routes == '/') {
+                /* Read routes config file to memory */
+                return lnet_read_file_mem(routes, CFS_CONF_FILE_SIZE);
+        } else {
+                return routes;
+        }
+}
+
+void
+lnet_put_routes(char *rbuf)
+{
+        if (rbuf != NULL && rbuf != routes)
+                cfs_free_large(rbuf);
 }
 
 char *
 lnet_get_networks(void)
 {
-        char   *nets;
+        char    *nets;
+        char    *config = NULL;
         int     rc;
+
+        if (*networks == 0 && *ip2nets == 0)
+                /* default to "tcp" when nothing specified */
+                return "tcp";
 
         if (*networks != 0 && *ip2nets != 0) {
                 LCONSOLE_ERROR_MSG(0x101, "Please specify EITHER 'networks' or "
@@ -77,15 +139,25 @@ lnet_get_networks(void)
                 return NULL;
         }
 
-        if (*ip2nets != 0) {
-                rc = lnet_parse_ip2nets(&nets, ip2nets);
-                return (rc == 0) ? nets : NULL;
-        }
-
         if (*networks != 0)
                 return networks;
 
-        return "tcp";
+        LASSERT(*ip2nets != 0);
+
+        if (*ip2nets == '/') {
+                /* read config from file in ip2nets */
+                config = lnet_read_file_mem(ip2nets, CFS_CONF_FILE_SIZE);
+                if (IS_ERR(config))
+                        return NULL;
+        } else if (*ip2nets != 0) {
+                config = ip2nets;
+        }
+
+        rc = lnet_parse_ip2nets(&nets, config);
+        if (config != ip2nets)
+                cfs_free_large(config);
+
+        return (rc == 0) ? nets : NULL;
 }
 
 void
@@ -112,6 +184,11 @@ lnet_get_routes(void)
         return (str == NULL) ? "" : str;
 }
 
+void
+lnet_put_routes(char *rbufs)
+{
+        return;
+}
 char *
 lnet_get_networks (void)
 {
@@ -1250,6 +1327,7 @@ LNetNIInit(lnet_pid_t requested_pid)
 {
         int         im_a_router = 0;
         int         rc;
+        char        *routes_cfg;
 
         LNET_MUTEX_DOWN(&the_lnet.ln_api_mutex);
 
@@ -1277,7 +1355,15 @@ LNetNIInit(lnet_pid_t requested_pid)
         if (rc != 0)
                 goto failed1;
 
-        rc = lnet_parse_routes(lnet_get_routes(), &im_a_router);
+        routes_cfg = lnet_get_routes();
+        if (IS_ERR(routes_cfg)) {
+                rc = PTR_ERR(routes_cfg);
+                goto failed1_5;
+        }
+
+        rc = lnet_parse_routes(routes_cfg, &im_a_router);
+        lnet_put_routes(routes_cfg);
+
         if (rc != 0)
                 goto failed2;
 
@@ -1316,6 +1402,7 @@ LNetNIInit(lnet_pid_t requested_pid)
         lnet_acceptor_stop();
  failed2:
         lnet_destroy_routes();
+ failed1_5:
         lnet_shutdown_lndnis();
  failed1:
         lnet_unprepare();
