eceac33b01a45ef064f66e512f03568a824d46c7
Lai Siyao <laisiyao@whamcloud.com>
2012-02-29 12:44:54 +0800

    LU-1110 fid: add full support for open-by-fid
    LU-1110 fid: add full support for open-by-fid
    
    Op-by-fid which involves parent change are not permitted:
            link/unlink/rename/create/open(CREATE)
    return -EPERM directly in case dt operations are triggered, but obf
    object doesn't have disk object, which will panic.
    
    Add more sanity tests for op-by-fid.
    
    Signed-off-by: Lai Siyao <laisiyao@whamcloud.com>
    Change-Id: I10869923e861ac0165886a48880f58e1269554ec

--- lustre/mdt/mdt_reint.c
+++ lustre/mdt/mdt_reint.c
@@ -295,6 +295,9 @@ static int mdt_md_create(struct mdt_thread_info *info)
         if (IS_ERR(parent))
                 RETURN(PTR_ERR(parent));
 
+        if (mdt_object_obf(parent))
+                GOTO(out_put_parent, rc = -EPERM);
+
         rc = mdt_version_get_check_save(info, parent, 0);
         if (rc)
                 GOTO(out_put_parent, rc);
@@ -699,6 +702,9 @@ static int mdt_reint_unlink(struct mdt_thread_info *info,
                 GOTO(out, rc);
         }
 
+        if (mdt_object_obf(mp))
+                GOTO(out_unlock_parent, rc = -EPERM);
+
         rc = mdt_version_get_check_save(info, mp, 0);
         if (rc)
                 GOTO(out_unlock_parent, rc);
@@ -844,6 +850,9 @@ static int mdt_reint_link(struct mdt_thread_info *info,
                                   MDS_INODELOCK_UPDATE);
         if (IS_ERR(mp))
                 RETURN(PTR_ERR(mp));
+
+        if (mdt_object_obf(mp))
+                GOTO(out_unlock_parent, rc = -EPERM);
 
         rc = mdt_version_get_check_save(info, mp, 0);
         if (rc)
@@ -1148,6 +1157,9 @@ static int mdt_reint_rename(struct mdt_thread_info *info,
         if (IS_ERR(msrcdir))
                 GOTO(out_rename_lock, rc = PTR_ERR(msrcdir));
 
+        if (mdt_object_obf(msrcdir))
+                GOTO(out_unlock_source, rc = -EPERM);
+
         rc = mdt_version_get_check_save(info, msrcdir, 0);
         if (rc)
                 GOTO(out_unlock_source, rc);
@@ -1171,6 +1183,9 @@ static int mdt_reint_rename(struct mdt_thread_info *info,
                                           rr->rr_fid2);
                 if (IS_ERR(mtgtdir))
                         GOTO(out_unlock_source, rc = PTR_ERR(mtgtdir));
+
+                if (mdt_object_obf(mtgtdir))
+                        GOTO(out_put_target, rc = -EPERM);
 
                 /* check early, the real version will be saved after locking */
                 rc = mdt_version_get_check(info, mtgtdir, 1);
