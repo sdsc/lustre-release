f1479baed696d9aa63e120c808a2c98856c041b2
Liu Xuezhao <xuezhao.liu@emc.com>
2012-03-16 12:37:02 +0800

    LU-1214 quota: don't compile quota module for client
    1. Only and always compile lquota module for server
    2. HAVE_QUOTA_SUPPORT macro is no more needed in quota source files
    3. Removes several un-used function declarations in quota_internal.h
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Change-Id: Ibf94ba6f37b43721cc53c2d7bde017b039581bbd

--- lustre/quota/lproc_quota.c
+++ lustre/quota/lproc_quota.c
@@ -46,8 +46,6 @@
 
 #include "quota_internal.h"
 
-#ifdef HAVE_QUOTA_SUPPORT
-
 #ifdef LPROCFS
 int lprocfs_quota_rd_bunit(char *page, char **start, off_t off, int count,
                            int *eof, void *data)
@@ -683,4 +681,3 @@ int lquota_proc_cleanup(struct lustre_quota_ctxt *qctxt)
 }
 
 #endif  /* LPROCFS */
-#endif
