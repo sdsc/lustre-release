84b3bb5769f5067af54e01ab164c59f8d5698b4a
Yu Jian <yujian@whamcloud.com>
2012-04-13 14:33:48 +0800

    LU-425 tests: fix the issue of using "grep -w"
    This patch fixes the following issue while using "grep -w"
    to do exact match:
    
    $ echo /mnt/nbp0-2 | grep -w /mnt/nbp0
    /mnt/nbp0-2
    
    Per the description of "-w" option:
    -w, --word-regexp
    Select only those lines containing matches that form whole words.
    The test is that the matching substring must either be at the
    beginning of the line, or preceded by a non-word constituent
    character. Similarly, it must be either at the end of the line
    or followed by a non-word constituent character. Word-constituent
    characters are letters, digits, and the underscore.
    
    So, the hyphen "-" character is a non-word constituent character
    and "grep -w" does not do exact match on strings which contain it.
    
    Signed-off-by: Yu Jian <yujian@whamcloud.com>
    Change-Id: I53d306d38b05408c351db10dc545120f2f9dabce

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -866,7 +866,7 @@ sanity_mount_check_nodes () {
     local rc=0
     for mnt in $mnts ; do
         do_nodes $nodes "running=\\\$(grep -c $mnt' ' /proc/mounts);
-mpts=\\\$(mount | grep -w -c $mnt);
+mpts=\\\$(mount | grep -c $mnt' ');
 if [ \\\$running -ne \\\$mpts ]; then
     echo \\\$(hostname) env are INSANE!;
     exit 1;
@@ -936,7 +936,7 @@ fi;
 exit \\\$rc" || return ${PIPESTATUS[0]}
 
     echo "Started clients $clients: "
-    do_nodes $clients "mount | grep -w $mnt"
+    do_nodes $clients "mount | grep $mnt' '"
 
     set_default_debug_nodes $clients
 
