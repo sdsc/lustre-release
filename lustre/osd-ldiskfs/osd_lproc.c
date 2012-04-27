52393eb0741e0073ec05ecda2479eb8f34b2ff3d
Fan Yong <yong.fan@whamcloud.com>
2012-04-27 09:52:33 +0800

    LU-957 scrub: support MDT file-level backup/restore
    Lustre-2.x uses Object Index files for mapping global FID to MDT local
    identifier, which is different from Lustre-1.6/1.8. Such OI file wills
    become invalid after MDT file-level backup/restore, because of objects
    re-allocated on MDT.
    
    The patch rebuilds the OI files after MDT file-level backup/restore to
    make it workable as Lustre-1.6/1.8 series do.
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: I94eb6a4a9368d8c83a5cc53c3577939de4b7bf28

--- lustre/osd-ldiskfs/osd_lproc.c
+++ lustre/osd-ldiskfs/osd_lproc.c
@@ -483,6 +483,87 @@ static int lprocfs_osd_wr_pdo(struct file *file, const char *buffer,
 }
 #endif
 
+static int lprocfs_osd_rd_verify_oi(char *page, char **start, off_t off,
+                                    int count, int *eof, void *data)
+{
+        struct osd_device *dev = data;
+
+        LASSERT(dev != NULL);
+        if (unlikely(dev->od_mount == NULL))
+                return -EINPROGRESS;
+
+        *eof = 1;
+        return snprintf(page, count, "%d\n", dev->od_verify_oi);
+}
+
+static int lprocfs_osd_wr_verify_oi(struct file *file, const char *buffer,
+                                    unsigned long count, void *data)
+{
+        struct osd_device *dev = data;
+        int val, rc;
+
+        LASSERT(dev != NULL);
+        if (unlikely(dev->od_mount == NULL))
+                return -EINPROGRESS;
+
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        if (val == 0 && dev->od_scrub.os_file.sf_status != SS_COMPLETED) {
+                CERROR("OI scrub has never scanned the device completely, "
+                       "cannot disable OI verification under such case!\n");
+                return -EPERM;
+        }
+
+        dev->od_verify_oi = !!val;
+        return count;
+}
+
+static int lprocfs_osd_rd_auto_scrub(char *page, char **start, off_t off,
+                                     int count, int *eof, void *data)
+{
+        struct osd_device *dev = data;
+
+        LASSERT(dev != NULL);
+        if (unlikely(dev->od_mount == NULL))
+                return -EINPROGRESS;
+
+        *eof = 1;
+        return snprintf(page, count, "%d\n", !dev->od_scrub.os_noauto_scrub);
+}
+
+static int lprocfs_osd_wr_auto_scrub(struct file *file, const char *buffer,
+                                     unsigned long count, void *data)
+{
+        struct osd_device *dev = data;
+        int val, rc;
+
+        LASSERT(dev != NULL);
+        if (unlikely(dev->od_mount == NULL))
+                return -EINPROGRESS;
+
+        rc = lprocfs_write_helper(buffer, count, &val);
+        if (rc)
+                return rc;
+
+        dev->od_scrub.os_noauto_scrub = !val;
+        return count;
+}
+
+static int lprocfs_osd_rd_oi_scrub(char *page, char **start, off_t off,
+                                   int count, int *eof, void *data)
+{
+        struct osd_device *dev = data;
+
+        LASSERT(dev != NULL);
+        if (unlikely(dev->od_mount == NULL))
+                return -EINPROGRESS;
+
+        *eof = 1;
+        return osd_scrub_dump(dev, page, count);
+}
+
 struct lprocfs_vars lprocfs_osd_obd_vars[] = {
         { "blocksize",       lprocfs_osd_rd_blksize,     0, 0 },
         { "kbytestotal",     lprocfs_osd_rd_kbytestotal, 0, 0 },
@@ -495,6 +576,11 @@ struct lprocfs_vars lprocfs_osd_obd_vars[] = {
 #ifdef HAVE_LDISKFS_PDO
         { "pdo",             lprocfs_osd_rd_pdo, lprocfs_osd_wr_pdo, 0 },
 #endif
+        { "verify_oi",       lprocfs_osd_rd_verify_oi,
+                             lprocfs_osd_wr_verify_oi,   0 },
+        { "auto_scrub",      lprocfs_osd_rd_auto_scrub,
+                             lprocfs_osd_wr_auto_scrub,  0 },
+        { "oi_scrub",        lprocfs_osd_rd_oi_scrub,    0, 0 },
         { 0 }
 };
 
