26315787f7a26444c84c71534d5fb32fc58dcc37
Fan Yong <yong.fan@whamcloud.com>
2012-04-30 11:42:04 +0800

    LU-957 scrub: OSD layer cleanup for OI scrub
    1) Abstract some common functions in OSD layer.
    2) Call lu_object_put() in osd_object_find() under failure case.
    3) Do not release buffer_head until all the needed contents have
       been accessed in osd_oi_index_create_one().
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: I3e6880271ee73ec2c4638b02c7e9c55fc827bdf3

--- lustre/osd-ldiskfs/osd_oi.h
+++ lustre/osd-ldiskfs/osd_oi.h
@@ -59,6 +59,8 @@
 #include <lu_object.h>
 #include <md_object.h>
 
+#define OSD_OII_NOGEN (0)
+
 struct lu_fid;
 struct osd_thread_info;
 struct lu_site;
@@ -80,6 +82,42 @@ struct osd_inode_id {
         __u32 oii_gen; /* inode generation */
 };
 
+static inline void osd_id_pack(struct osd_inode_id *des,
+                               const struct osd_inode_id *src)
+{
+        des->oii_ino = cpu_to_be32(src->oii_ino);
+        des->oii_gen = cpu_to_be32(src->oii_gen);
+}
+
+static inline void osd_id_unpack(struct osd_inode_id *des,
+                                 struct osd_inode_id *src)
+{
+        des->oii_ino = be32_to_cpu(src->oii_ino);
+        des->oii_gen = be32_to_cpu(src->oii_gen);
+}
+
+static inline void osd_id_gen(struct osd_inode_id *id, __u32 ino, __u32 gen)
+{
+        id->oii_ino = ino;
+        id->oii_gen = gen;
+}
+
+static inline void osd_id_to_inode(struct inode *inode,
+                                   const struct osd_inode_id *id)
+{
+        inode->i_ino        = id->oii_ino;
+        inode->i_generation = id->oii_gen;
+}
+
+static inline int osd_id_eq(const struct osd_inode_id *id0,
+                            const struct osd_inode_id *id1)
+{
+        return (id0->oii_ino == id1->oii_ino) &&
+               (id0->oii_gen == id1->oii_gen ||
+                id0->oii_gen == OSD_OII_NOGEN ||
+                id1->oii_gen == OSD_OII_NOGEN);
+}
+
 int osd_oi_mod_init(void);
 int osd_oi_init(struct osd_thread_info *info, struct osd_device *osd);
 void osd_oi_fini(struct osd_thread_info *info, struct osd_device *osd);
@@ -87,7 +125,7 @@ int  osd_oi_lookup(struct osd_thread_info *info, struct osd_device *osd,
                    const struct lu_fid *fid, struct osd_inode_id *id);
 int  osd_oi_insert(struct osd_thread_info *info, struct osd_device *osd,
                    const struct lu_fid *fid, const struct osd_inode_id *id,
-                   struct thandle *th, int ingore_quota);
+                   struct thandle *th);
 int  osd_oi_delete(struct osd_thread_info *info,
                    struct osd_device *osd, const struct lu_fid *fid,
                    struct thandle *th);
