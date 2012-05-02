ce7263a82317c26532f0fd5e1383a079e98a7375
Artem Blagodarenko <artem_blagodarenko@xyratex.com>
2012-05-02 18:05:33 +0400

    LU-903 ldlm: inode references moved to resource
    There is a race condition while get_attr after cancel_lru_locks
    and sysctl drop_caches. ll_clear_inode clears l_ast_data for
    ldlm lock and this lock can't be canceled because "inode == NULL".
    ll_mdc_blocking_ast finds such lock. As result DCACHE_LUSTRE_INVALID
    is not set and lookup returns wrong inode.
    
    This patch moves inode structure reference to
    "ldlm_lock::l_resource::lr_lvb_inode". This prevents from different
    inode references for same resource's lock.
    
    Xyratex-bug-id: MRP-338
    
    Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    
    Signed-off-by: Artem Blagodarenko <artem_blagodarenko@xyratex.com>
    Change-Id: I4105b2aec38c90d3f5a20d1498a563192a74de55

--- lustre/liblustre/super.c
+++ lustre/liblustre/super.c
@@ -519,7 +519,7 @@ void llu_clear_inode(struct inode *inode)
 
         lli->lli_flags &= ~LLIF_MDS_SIZE_LOCK;
         md_change_cbdata(sbi->ll_md_exp, ll_inode2fid(inode),
-                         null_if_equal, inode);
+                         NULL, inode);
 
         if (lli->lli_smd)
                 obd_change_cbdata(sbi->ll_dt_exp, lli->lli_smd,
