b5b16e7f27674eb22bc070252cb4e2be00c23332
Jinshan Xiong <jinshan.xiong@whamcloud.com>
2012-05-10 13:50:48 -0700

    LU-744 osc: add lru pages management - new RPC
    Add a cache management at osc layer, this way we can control how much
    memory can be used to cache lustre pages and avoid complex solution
    as what we did in b1_8.
    
    Right now, admins can set how much memory an OSC can cache lustre
    pages. This is not perfect because an application may only access
    limited number of OSCs in a burst read. A self-adapative algorithm
    will be worked out to solve this issue.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
    Change-Id: I76c840aef5ca9a3a4619f06fcaee7de7f95b05f5

--- lustre/obdclass/cl_page.c
+++ lustre/obdclass/cl_page.c
@@ -636,7 +636,7 @@ static void cl_page_state_set(const struct lu_env *env,
 void cl_page_get(struct cl_page *page)
 {
         ENTRY;
-        LASSERT(page->cp_state != CPS_FREEING);
+        /* LASSERT(page->cp_state != CPS_FREEING); */
         cl_page_get_trust(page);
         EXIT;
 }
