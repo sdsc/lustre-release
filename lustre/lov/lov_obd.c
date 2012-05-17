908a4d4564041d07f3f4352e207f28e7a1c3e2a1
Liang Zhen <liang@whamcloud.com>
2012-05-17 14:47:15 +0800

    LU-56 obdclass: SMP improvement for lu_key
    ptlrpc service threads need to call lu_context_init/fini in each
    loop (for each RPC), this could be a big performance issue on
    fat SMP matchie because these operations on lu_context are serialized
    by one spinlock and we need to lock/unlock it for multiple times
    for each RPC.
    
    This can be resolved by using percpt lock, threads only need to
    take a local lock for lu_context_int/fini, which can significantly
    decrease contention between threads.
    
    Signed-off-by: Liang Zhen <liang@whamcloud.com>
    Change-Id: I5875a90365a103707526483047ec7628f6964a56

--- lustre/lov/lov_obd.c
+++ lustre/lov/lov_obd.c
@@ -3082,15 +3082,21 @@ static void /*__exit*/ lov_exit(void)
 {
         int rc;
 
-        lu_device_type_fini(&lov_device_type);
-        lu_kmem_fini(lov_caches);
-
+	/*
+	 * NB: I comment out lu_device_type_fini because it's a bug and
+	 * will crash this patch, it can be removed after landing
+	 * patch for LU-1417
+	 *
+	 * lu_device_type_fini(&lov_device_type);
+	 */
         if (quota_interface)
                 PORTAL_SYMBOL_PUT(lov_quota_interface);
 
         class_unregister_type(LUSTRE_LOV_NAME);
         rc = cfs_mem_cache_destroy(lov_oinfo_slab);
         LASSERT(rc == 0);
+
+	lu_kmem_fini(lov_caches);
 }
 
 MODULE_AUTHOR("Sun Microsystems, Inc. <http://www.lustre.org/>");
