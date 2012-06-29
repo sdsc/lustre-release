c952e4d32791ec1d6a8df1dc44624e0ca9eccfaa
Fan Yong <yong.fan@whamcloud.com>
2012-06-29 15:45:32 +0800

    LU-957 lfsck: LFSCK main engine
    Implement the main engine for Lustre online fsck. The kernel
    thread "lfsck" scans the system object table by low layer DT
    iteration APIs, and will drives registered scrub component(s)
    to check/repair Lustre system.
    
    It is the controller for the whole LFSCK, and controls the
    speed, including the low layer OI scrub (for osd-ldiskfs).
    
    For urgent mode, like MDT is restored from file-level backup
    against ldiskfs, the OI files are invalided. Under such case,
    we need to rebuild the OI files ASAP, then low layer OI scrub
    inside osd-ldiskfs ignores the main engine speed control, and
    runs with full speed.
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: If1bd3cfac1f299e964c029e5e9c4cce6432edfa5

--- lustre/osd-ldiskfs/osd_compat.c
+++ lustre/osd-ldiskfs/osd_compat.c
@@ -485,20 +485,22 @@ struct named_oid {
 };
 
 static const struct named_oid oids[] = {
-        { FLD_INDEX_OID,        "" /* "fld" */ },
-        { FID_SEQ_CTL_OID,      "" /* "seq_ctl" */ },
-        { FID_SEQ_SRV_OID,      "" /* "seq_srv" */ },
-        { MDD_ROOT_INDEX_OID,   "" /* "ROOT" */ },
-        { MDD_ORPHAN_OID,       "" /* "PENDING" */ },
-        { MDD_LOV_OBJ_OID,      "" /* LOV_OBJID */ },
-        { MDD_CAPA_KEYS_OID,    "" /* CAPA_KEYS */ },
+	{ FLD_INDEX_OID,        "" /* "fld" */ },
+	{ FID_SEQ_CTL_OID,      "" /* "seq_ctl" */ },
+	{ FID_SEQ_SRV_OID,      "" /* "seq_srv" */ },
+	{ MDD_ROOT_INDEX_OID,   "" /* "ROOT" */ },
+	{ MDD_ORPHAN_OID,       "" /* "PENDING" */ },
+	{ MDD_LOV_OBJ_OID,      "" /* LOV_OBJID */ },
+	{ MDD_CAPA_KEYS_OID,    "" /* CAPA_KEYS */ },
 	{ MDT_LAST_RECV_OID,    LAST_RCVD },
-        { OFD_LAST_RECV_OID,    "" /* LAST_RCVD */ },
+	{ LFSCK_BOOKMARK_OID,   "" /* "lfsck_bookmark" */ },
+	{ LFSCK_IT_OID,		"" /* "lfsck_it"*/},
+	{ OFD_LAST_RECV_OID,    "" /* LAST_RCVD */ },
 	{ OFD_LAST_GROUP_OID,   "LAST_GROUP" },
-        { LLOG_CATALOGS_OID,    "" /* "CATALOGS" */ },
-        { MGS_CONFIGS_OID,      "" /* MOUNT_CONFIGS_DIR */ },
+	{ LLOG_CATALOGS_OID,    "" /* "CATALOGS" */ },
+	{ MGS_CONFIGS_OID,      "" /* MOUNT_CONFIGS_DIR */ },
 	{ OFD_HEALTH_CHECK_OID, HEALTH_CHECK },
-        { 0,                    NULL }
+	{ 0,                    NULL }
 };
 
 static char *oid2name(const unsigned long oid)
