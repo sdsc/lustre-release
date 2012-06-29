9241933665e312d255dedb0760638dc0f17e9248
Hongchao Zhang <hongchao.zhang@whamcloud.com>
2012-06-29 13:06:41 +0800

    LU-1436 debug: return the original type in RETURN
    in RETURN, the bit length of rc could be 32 or 64, and it also could
    be pointer type, the conversion betweens pointer and integer type
    will trigger compile warning related to type casting, then define an
    static variable of typeof(rc), and avoid the stack usage ATM.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@whamcloud.com>
    Change-Id: I7f3a54438dfe1f294168691718c4db8116f68de4

--- libcfs/include/libcfs/libcfs_debug.h
+++ libcfs/include/libcfs/libcfs_debug.h
@@ -299,17 +299,23 @@ do {                                                                    \
  */
 #if defined(__GNUC__)
 
-long libcfs_log_return(struct libcfs_debug_msg_data *, long rc);
+void libcfs_log_return(struct libcfs_debug_msg_data *, long long rc);
 #define RETURN(rc)                                                      \
 do {                                                                    \
-        EXIT_NESTING;                                                   \
-        if (cfs_cdebug_show(D_TRACE, DEBUG_SUBSYSTEM)) {                \
-                LIBCFS_DEBUG_MSG_DATA_DECL(msgdata, D_TRACE, NULL);     \
-                return (typeof(rc))libcfs_log_return(&msgdata,          \
-                                                     (long)(rc));       \
-        }                                                               \
+	static typeof(rc) RETURN__ret;                                  \
+	RETURN__ret = (rc);                                             \
+	EXIT_NESTING;                                                   \
+	if (cfs_cdebug_show(D_TRACE, DEBUG_SUBSYSTEM)) {                \
+		LIBCFS_DEBUG_MSG_DATA_DECL(msgdata, D_TRACE, NULL);     \
+		if (sizeof(RETURN__ret) == 32)                          \
+			libcfs_log_return(&msgdata,                     \
+					  (long)(RETURN__ret));         \
+		else                                                    \
+			libcfs_log_return(&msgdata,                     \
+					  (long long)(RETURN__ret));    \
+	}                                                               \
                                                                         \
-        return (rc);                                                    \
+        return RETURN__ret;                                             \
 } while (0)
 #elif defined(_MSC_VER)
 #define RETURN(rc)                                                      \
