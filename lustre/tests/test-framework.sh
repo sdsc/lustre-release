80bb682269f346a0c484115cf65e55927fac4bfa
Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
2012-06-29 21:59:43 +0300

    LU-1562 test: recovery-small tests 2,3,5 failed
    1. The code that executed between tests has been cleaned up
    and moved to corresponding tests which use it to avoid its
    execution all the time, even if it is not required when
    running using ONLY.
    2. The test_2, and test_3 has been joined with test_1,
    and the test_7 has been joined with test_6, because
    they are in fact single scenario.
    3. Random file creation code moved to utility function into
    the test-framework.sh
    4. Added bash stack trace printing code to error_noexit that
    make it easier to analyze logs.
    
    Xyratex-bug-id: MRP-411
    Signed-off-by: Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
    Reviewed-by: Andrew Perepechko <andrew_perepechko@xyratex.com>
    Reviewed-by: Alexander Zarochentsev <alexander_zarochentsev@xyratex.com>
    Change-Id: Iea417c99edb001e4f5ad0d0980c7c10012e2514a

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -2196,6 +2196,23 @@ do_facet() {
     do_node $HOST "$@"
 }
 
+# Function: do_facet_random_file $FACET $FILE $SIZE
+# Creates FILE with random content on the given FACET of given SIZE
+
+do_facet_random_file() {
+	local facet=$1
+	local fpath=$2
+	local fsize=$3
+	do_facet $facet "dd if=/dev/urandom of=$fpath bs=$fsize count=1 2>/dev/null"
+}
+
+do_facet_create_file() {
+	local facet=$1
+	local fpath=$2
+	local fsize=$3
+	do_facet $facet "dd if=/dev/zero of=$fpath bs=$fsize count=1 2>/dev/null"
+}
+
 do_nodesv() {
     do_nodes --verbose "$@"
 }
@@ -3429,6 +3446,14 @@ stop_full_debug_logging() {
     debugrestore
 }
 
+# prints bash call stack
+log_trace_dump() {
+	echo "  Trace dump:"
+	for (( i=1; i < ${#BASH_LINENO[*]} ; i++ )) ; do
+		echo "  = ${BASH_SOURCE[$i]}:${BASH_LINENO[$i-1]}:${FUNCNAME[$i]}()"
+	done
+}
+
 ##################################
 # Test interface
 ##################################
@@ -3444,6 +3469,7 @@ error_noexit() {
     fi
 
     log " ${TESTSUITE} ${TESTNAME}: @@@@@@ ${TYPE}: $@ "
+    log_trace_dump
 
     mkdir -p $LOGDIR
     # We need to dump the logs on all nodes
@@ -3691,7 +3717,7 @@ banner() {
 run_one() {
     local testnum=$1
     local message=$2
-    tfile=f${testnum}
+    tfile=f.${TESTSUITE}.${testnum}
     export tdir=d0.${TESTSUITE}/d${base}
     export TESTNAME=test_$testnum
     local SAVE_UMASK=`umask`
