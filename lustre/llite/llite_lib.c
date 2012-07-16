692a1f9de211ced763a35acbe1a10cb50ccda472
Liu Xuezhao <xuezhao.liu@emc.com>
2012-07-16 17:05:04 +0800

    LU-1337 vfs: kernel 3.4 converts d_alloc_root to d_make_root
    kernel 3.4 converts d_alloc_root to d_make_root.
    (kernel commit 32991ab305ace7017c62f8eecbe5eb36dc32e13b)
    
    Add LC_HAVE_D_MAKE_ROOT/HAVE_D_MAKE_ROOT to ckeck it.
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Change-Id: I7ff1f0eff25495b655e650997016377ca0a200aa

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -539,7 +539,12 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
                                  NULL);
         cl_sb_init(sb);
 
-        sb->s_root = d_alloc_root(root);
+	sb->s_root = d_make_root(root);
+	if (sb->s_root == NULL) {
+		CERROR("Can't make root dentry\n");
+		GOTO(out_lock_cn_cb, err = -ENOMEM);
+	}
+
 #ifdef HAVE_DCACHE_LOCK
 	sb->s_root->d_op = &ll_d_root_ops;
 #else
