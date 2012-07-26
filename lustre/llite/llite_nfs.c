7af04c495ae78665b6afb9384a48108423689cb2
Alexander.Boyko <alexander_boyko@xyratex.com>
2012-07-26 10:16:51 +0400

    LU-1675: fix fid for ll_get_parent
    When nfs reexport occured between 1.8 client and 2.0 server,
    nfs client do mkdir a, cd a, and get -521 error, ll_get_parent()
    fail with -22 error, and mds print
    "mdt_body_unpack()) Invalid fid: [0x2010e0901000001:0x0:0x4000]".
    This patch fix fid translation to the ptlrpc request/reply.
    
    Signed-off-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Change-Id: Ib1a79e36ead478ebb874e7e04761bc43f33a410f

--- lustre/llite/llite_nfs.c
+++ lustre/llite/llite_nfs.c
@@ -266,21 +266,19 @@ struct dentry *ll_get_parent(struct dentry *dchild)
 
         sbi = ll_s2sbi(dir->i_sb);
 
-        fid.id = (__u64)dir->i_ino;
-        fid.generation = dir->i_generation;
-        fid.f_type = S_IFDIR;
+        ll_inode2fid(&fid, dir);
 
         rc = mdc_getattr_name(sbi->ll_mdc_exp, &fid, dotdot, strlen(dotdot) + 1,
-                              0, 0, &req);
+                              OBD_MD_FLID, 0, &req);
         if (rc) {
                 CERROR("failure %d inode %lu get parent\n", rc, dir->i_ino);
                 return ERR_PTR(rc);
         }
         body = lustre_msg_buf(req->rq_repmsg, REPLY_REC_OFF, sizeof (*body));
 
-        LASSERT((body->valid & OBD_MD_FLGENER) && (body->valid & OBD_MD_FLID));
-        fid.id = body->ino;
-        fid.generation = body->generation;
+        LASSERT(body->valid & OBD_MD_FLID);
+
+        fid = body->fid1;
         result = ll_iget_for_nfs(dir->i_sb, &fid);
 
         if (IS_ERR(result))
