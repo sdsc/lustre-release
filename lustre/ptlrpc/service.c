sdsc/lustre-release

commit 07b8db220e48782369f48d86213c5d404a628ded

LU-56 ptlrpc: Reduce at_lock dance
Some lock dances of service at_lock are unnecessary,
we made some cleanup in this patch to reduce useless lock/unlock.

Signed-off-by: Liang Zhen <liang@whamcloud.com>
Change-Id: I004729692254d1200a8b18c0c4494ff437233caf
Reviewed-on: http://review.whamcloud.com/2911
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: wangdi <di.wang@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/service.c
@@ -715,10 +715,11 @@ void ptlrpc_server_drop_request(struct ptlrpc_request *req)
         if (!cfs_atomic_dec_and_test(&req->rq_refcount))
                 return;
 
-	cfs_spin_lock(&svcpt->scp_at_lock);
 	if (req->rq_at_linked) {
 		struct ptlrpc_at_array *array = &svcpt->scp_at_array;
                 __u32 index = req->rq_at_index;
+
+		cfs_spin_lock(&svcpt->scp_at_lock);
 
                 LASSERT(!cfs_list_empty(&req->rq_timed_list));
                 cfs_list_del_init(&req->rq_timed_list);
@@ -727,10 +728,11 @@ void ptlrpc_server_drop_request(struct ptlrpc_request *req)
                 cfs_spin_unlock(&req->rq_lock);
                 array->paa_reqs_count[index]--;
                 array->paa_count--;
-        } else
-                LASSERT(cfs_list_empty(&req->rq_timed_list));
 
-	cfs_spin_unlock(&svcpt->scp_at_lock);
+		cfs_spin_unlock(&svcpt->scp_at_lock);
+	} else {
+		LASSERT(cfs_list_empty(&req->rq_timed_list));
+	}
 
         /* finalize request */
         if (req->rq_export) {
@@ -962,10 +964,8 @@ static void ptlrpc_at_set_timer(struct ptlrpc_service_part *svcpt)
 	struct ptlrpc_at_array *array = &svcpt->scp_at_array;
 	__s32 next;
 
-	cfs_spin_lock(&svcpt->scp_at_lock);
 	if (array->paa_count == 0) {
 		cfs_timer_disarm(&svcpt->scp_at_timer);
-		cfs_spin_unlock(&svcpt->scp_at_lock);
 		return;
 	}
 
@@ -979,7 +979,6 @@ static void ptlrpc_at_set_timer(struct ptlrpc_service_part *svcpt)
 		CDEBUG(D_INFO, "armed %s at %+ds\n",
 		       svcpt->scp_service->srv_name, next);
 	}
-	cfs_spin_unlock(&svcpt->scp_at_lock);
 }
 
 /* Add rpc to early reply check list */
@@ -989,7 +988,6 @@ static int ptlrpc_at_add_timed(struct ptlrpc_request *req)
 	struct ptlrpc_at_array *array = &svcpt->scp_at_array;
         struct ptlrpc_request *rq = NULL;
         __u32 index;
-        int found = 0;
 
         if (AT_OFF)
                 return(0);
@@ -1031,12 +1029,9 @@ static int ptlrpc_at_add_timed(struct ptlrpc_request *req)
         array->paa_count++;
         if (array->paa_count == 1 || array->paa_deadline > req->rq_deadline) {
                 array->paa_deadline = req->rq_deadline;
-                found = 1;
-        }
-	cfs_spin_unlock(&svcpt->scp_at_lock);
-
-	if (found)
 		ptlrpc_at_set_timer(svcpt);
+	}
+	cfs_spin_unlock(&svcpt->scp_at_lock);
 
 	return 0;
 }
@@ -1208,10 +1203,10 @@ static int ptlrpc_at_check_timed(struct ptlrpc_service_part *svcpt)
 	first = array->paa_deadline - now;
 	if (first > at_early_margin) {
 		/* We've still got plenty of time.  Reset the timer. */
-		cfs_spin_unlock(&svcpt->scp_at_lock);
 		ptlrpc_at_set_timer(svcpt);
-                RETURN(0);
-        }
+		cfs_spin_unlock(&svcpt->scp_at_lock);
+		RETURN(0);
+	}
 
         /* We're close to a timeout, and we don't know how much longer the
            server will take. Send early replies to everyone expiring soon. */
@@ -1253,10 +1248,10 @@ static int ptlrpc_at_check_timed(struct ptlrpc_service_part *svcpt)
                         index = 0;
         }
         array->paa_deadline = deadline;
-	cfs_spin_unlock(&svcpt->scp_at_lock);
-
 	/* we have a new earliest deadline, restart the timer */
 	ptlrpc_at_set_timer(svcpt);
+
+	cfs_spin_unlock(&svcpt->scp_at_lock);
 
         CDEBUG(D_ADAPTTO, "timeout in %+ds, asking for %d secs on %d early "
                "replies\n", first, at_extra, counter);

commit a7be51b6d5cfb66d2d866a47b06bd7106526095c

LU-1721 ptlrpc: AT race in drop request
It's introduced in by commit 07b8db220e48782369f48d86213c5d404a628ded
which makes ptlrpc_server_drop_request() not to hold at_lock for
checking req::rq_at_linked.
This change might race with ptlrpc_at_check_timed() if:

1) thread-1: call ptlrpc_at_check_timed() and remove the request from
   paa_reqs_array, before it set req::rq_at_linked to zero...
2) thread-2: call ptlrpc_server_drop_request() to release the last
   refcount, and it found req::rq_at_linked is non-zero, so it
   entered the condition "if (req->rq_at_linked) {...}"
3) thread-1: set req::rq_at_linked to zero
4) thread-2: take at_lock, and hit the assetion
   LASSERT(!cfs_list_empty(&req->rq_timed_list)) because thread-1 has
   already removed req::rq_at_linked from paa_reqs_array in step 1)

This patch fixed this issue and did some code cleanup.

Signed-off-by: Liang Zhen <liang@whamcloud.com>
Change-Id: If4469637a5f9d63c9253a9f4c4cac0bcd7f8b46e

lustre/ptlrpc/service.c
@@ -65,6 +65,7 @@ CFS_MODULE_PARM(at_extra, "i", int, 0644,
 /* forward ref */
 static int ptlrpc_server_post_idle_rqbds(struct ptlrpc_service_part *svcpt);
 static void ptlrpc_hpreq_fini(struct ptlrpc_request *req);
+static void ptlrpc_at_remove_timed(struct ptlrpc_request *req);
 
 static CFS_LIST_HEAD(ptlrpc_all_services);
 cfs_spinlock_t ptlrpc_all_services_lock;
@@ -863,23 +864,18 @@ void ptlrpc_server_drop_request(struct ptlrpc_request *req)
                 return;
 
 	if (req->rq_at_linked) {
-		struct ptlrpc_at_array *array = &svcpt->scp_at_array;
-                __u32 index = req->rq_at_index;
-
 		cfs_spin_lock(&svcpt->scp_at_lock);
-
-                LASSERT(!cfs_list_empty(&req->rq_timed_list));
-                cfs_list_del_init(&req->rq_timed_list);
-                cfs_spin_lock(&req->rq_lock);
-                req->rq_at_linked = 0;
-                cfs_spin_unlock(&req->rq_lock);
-                array->paa_reqs_count[index]--;
-                array->paa_count--;
-
+		/* NB: it's unnecessary to re-check because implicit barriers
+		 * in ptlrpc_at_check_timed() can guarantee order of changing
+		 * req::rq_refcount and req::rq_at_linked, but we still check
+		 * again in case people want to change ptlrpc_at_check_timed()
+		 * in the future, please see LU-1721 */
+		if (likely(req->rq_at_linked))
+			ptlrpc_at_remove_timed(req);
 		cfs_spin_unlock(&svcpt->scp_at_lock);
-	} else {
-		LASSERT(cfs_list_empty(&req->rq_timed_list));
 	}
+
+	LASSERT(cfs_list_empty(&req->rq_timed_list));
 
         /* finalize request */
         if (req->rq_export) {
@@ -1183,6 +1179,25 @@ static int ptlrpc_at_add_timed(struct ptlrpc_request *req)
 	return 0;
 }
 
+static void
+ptlrpc_at_remove_timed(struct ptlrpc_request *req)
+{
+	struct ptlrpc_at_array *array;
+
+	array = &req->rq_rqbd->rqbd_svcpt->scp_at_array;
+
+	/* NB: must call with hold svcpt::scp_at_lock */
+	LASSERT(!cfs_list_empty(&req->rq_timed_list));
+	cfs_list_del_init(&req->rq_timed_list);
+
+	cfs_spin_lock(&req->rq_lock);
+	req->rq_at_linked = 0;
+	cfs_spin_unlock(&req->rq_lock);
+
+	array->paa_reqs_count[req->rq_at_index]--;
+	array->paa_count--;
+}
+
 static int ptlrpc_at_send_early_reply(struct ptlrpc_request *req)
 {
 	struct ptlrpc_service_part *svcpt = req->rq_rqbd->rqbd_svcpt;
@@ -1366,29 +1381,26 @@ static int ptlrpc_at_check_timed(struct ptlrpc_service_part *svcpt)
                 cfs_list_for_each_entry_safe(rq, n,
                                              &array->paa_reqs_array[index],
                                              rq_timed_list) {
-                        if (rq->rq_deadline <= now + at_early_margin) {
-                                cfs_list_del_init(&rq->rq_timed_list);
-                                /**
-                                 * ptlrpc_server_drop_request() may drop
-                                 * refcount to 0 already. Let's check this and
-                                 * don't add entry to work_list
-                                 */
-                                if (likely(cfs_atomic_inc_not_zero(&rq->rq_refcount)))
-                                        cfs_list_add(&rq->rq_timed_list, &work_list);
-                                counter++;
-                                array->paa_reqs_count[index]--;
-                                array->paa_count--;
-                                cfs_spin_lock(&rq->rq_lock);
-                                rq->rq_at_linked = 0;
-                                cfs_spin_unlock(&rq->rq_lock);
-                                continue;
-                        }
+			if (rq->rq_deadline > now + at_early_margin) {
+				/* update the earliest deadline */
+				if (deadline == -1 ||
+				    rq->rq_deadline < deadline)
+					deadline = rq->rq_deadline;
+				break;
+			}
 
-                        /* update the earliest deadline */
-                        if (deadline == -1 || rq->rq_deadline < deadline)
-                                deadline = rq->rq_deadline;
-
-                        break;
+			ptlrpc_at_remove_timed(rq);
+			/**
+			 * ptlrpc_server_drop_request() may drop
+			 * refcount to 0 already. Let's check this and
+			 * don't add entry to work_list
+			 */
+			/* NB: do this after ptlrpc_at_remove_timed() to
+			 * make sure ptlrpc_server_drop_request() can see
+			 * changes in order */
+			if (likely(cfs_atomic_inc_not_zero(&rq->rq_refcount)))
+				cfs_list_add(&rq->rq_timed_list, &work_list);
+			counter++;
                 }
 
                 if (++index >= array->paa_size)