2a681fe9fe7a37a8f614ffa5926ad7c17512c7aa
Liu Xuezhao <xuezhao.liu@emc.com>
2012-08-09 10:31:10 +0800

    LU-1337 vfs: kernel 3.2 protects inode->i_nlink
    Kernel 3.2 protects inode->i_nlink from direct modification.
    Filesystems may only read i_nlink directly. They shall use the
    (set|clear|inc|drop)_nlink for modification.
    See kernel commit a78ef704a8dd430225955f0709b22d4a6ba21deb.
    
    This patch adds LC_HAVE_PROTECT_I_NLINK checking and implements
    set_nlink for old kernel, clear/inc/drop_nlink exists after 2.6.18
    so need not to be re-implemented.
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Change-Id: Ie958cb308291ecc48d409a1282fed7ea3549a561

--- lustre/include/linux/lustre_compat25.h
+++ lustre/include/linux/lustre_compat25.h
@@ -808,5 +808,14 @@ static inline bool selinux_is_enabled(void)
 }
 #endif
 
+#include <linux/fs.h>
+
+#ifndef HAVE_PROTECT_I_NLINK
+static inline void set_nlink(struct inode *inode, unsigned int nlink)
+{
+	inode->i_nlink = nlink;
+}
+#endif
+
 #endif /* __KERNEL__ */
 #endif /* _COMPAT25_H */
