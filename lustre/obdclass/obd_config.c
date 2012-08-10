19790db0075b8c546c434af2a790d8e47e105358
Andreas Dilger <adilger@whamcloud.com>
2012-08-10 16:24:00 -0600

    LU-1735 ptlrpc: cleanup jobid code
    Some cleanups for the jobid code:
    - if obd_jobid_var is too large, only print an error message once
    - in linux cfs_get_environ():
    -- cache strlen(key) since it doesn't change and is used often
    -- remove unnecessary typecasts of void pointers
    -- use "rc" instead of "ret"
    -- balance ENTRY and RETURN/GOTO calls
    - add cfs_get_environ() for liblustre and remove inline #ifdef
    - use strcmp() to compare strings that are known NUL-terminated
    - use strlcpy() to ensure NUL-terminated strings in target buffer
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I22c3d5c1755c1d6aab666a769df38218b954cab0

--- lustre/obdclass/obd_config.c
+++ lustre/obdclass/obd_config.c
@@ -44,6 +44,7 @@
 #include <linux/string.h>
 #else
 #include <liblustre.h>
+#include <string.h>
 #include <obd_class.h>
 #include <obd.h>
 #endif
@@ -902,8 +903,8 @@ static int class_set_global(char *ptr, int val, struct lustre_cfg *lcfg)
 		at_history = val;
 	else if (class_match_param(ptr, PARAM_JOBID_VAR, NULL) == 0) {
 		memset(obd_jobid_var, 0, JOBSTATS_JOBID_VAR_MAX_LEN + 1);
-		memcpy(obd_jobid_var, lustre_cfg_string(lcfg, 2),
-		       JOBSTATS_JOBID_VAR_MAX_LEN + 1);
+		strlcpy(obd_jobid_var, lustre_cfg_string(lcfg, 2),
+			JOBSTATS_JOBID_VAR_MAX_LEN + 1);
 	} else
 		RETURN(-EINVAL);
 
