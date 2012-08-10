93624bf9b611d54177171b0b176cbf8430a8e1d1
Andreas Dilger <adilger@whamcloud.com>
2012-08-10 16:24:00 -0600

    LU-1735 ptlrpc: cleanup jobid code
    Some cleanups for the jobid code:
    - if obd_jobid_var is too large, only print an error message once
    - in linux cfs_get_environ():
    -- cache strlen(key) since it doesn't change and is used often
    -- remove unnecessary typecasts of void pointers
    -- use "rc" instead of "ret"
    -- balance ENTRY and RETURN/GOTO calls
    - add cfs_get_environ() for liblustre and remove inline #ifdef
    - use strcmp() to compare strings that are known NUL-terminated
    - use strlcpy() to ensure NUL-terminated strings in target buffer
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I22c3d5c1755c1d6aab666a769df38218b954cab0

--- libcfs/libcfs/user-prim.c
+++ libcfs/libcfs/user-prim.c
@@ -46,6 +46,7 @@
 
 #ifndef __KERNEL__
 
+#include <string.h>
 #include <libcfs/libcfs.h>
 
 /*
@@ -261,6 +262,23 @@ uid_t cfs_curproc_fsuid(void)
 gid_t cfs_curproc_fsgid(void)
 {
         return getgid();
+}
+
+/* Read the environment variable of current process specified by @key. */
+int cfs_get_environ(const char *key, char *value, int *val_len)
+{
+	char *entry;
+	int len;
+
+	entry = getenv(key);
+	if (entry == NULL)
+		return -ENOENT;
+
+	len = strlcpy(value, entry, *val_len);
+	if (len >= *val_len)
+		return -EOVERFLOW;
+
+	return 0;
 }
 
 void cfs_enter_debugger(void)
