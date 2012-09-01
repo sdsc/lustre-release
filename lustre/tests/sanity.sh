6103b55a81f99a797626389f184d768308a9d95b
Andreas Dilger <adilger@whamcloud.com>
2012-08-31 19:15:42 -0600

    LU-1798 tests: don't set jobid if not changing
    Don't set jobid_var conf_param if the value would not change.  This
    avoids setting the same parameter in the config log multiple times,
    and avoids the extra delay on each startup waiting for the conf_param
    to propagate from the MGS to the client each time.
    
    Fix the console error printing if the jobid_env does not exist.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I36ed60451a875bcd46fd0d6f3d7068d1b1398df5

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -9123,6 +9123,13 @@ verify_jobstats() {
 	fi
 }
 
+jobstats_set() {
+	trap 0
+	NEW_JOBENV=${0:-$OLD_JOBENV}
+	do_facet mgs $LCTL conf_param $FSNAME.sys.jobid_var=$NEW_JOBENV
+	wait_update $HOSTNAME "$LCTL get_param -n jobid_var" $NEW_JOBENV
+}
+
 test_205() { # Job stats
 	[ -z "$(lctl get_param -n mdc.*.connect_flags | grep jobstats)" ] &&
 		skip "Server doesn't support jobstats" && return 0
@@ -9130,9 +9137,8 @@ test_205() { # Job stats
 	local cmd
 	OLD_JOBENV=`$LCTL get_param -n jobid_var`
 	if [ $OLD_JOBENV != $JOBENV ]; then
-		do_facet mgs $LCTL conf_param $FSNAME.sys.jobid_var=$JOBENV
-		wait_update $HOSTNAME "$LCTL get_param -n jobid_var" \
-			$JOBENV || return 1
+		jobstats_set $JOBENV
+		trap jobstats_set EXIT
 	fi
 
 	# mkdir
@@ -9169,11 +9175,7 @@ test_205() { # Job stats
 	# cleanup
 	rm -f $DIR/jobstats_test_rename
 
-	if [ $OLD_JOBENV != $JOBENV ]; then
-		do_facet mgs $LCTL conf_param $FSNAME.sys.jobid_var=$OLD_JOBENV
-		wait_update $HOSTNAME "$LCTL get_param -n jobid_var" \
-			$OLD_JOBENV || return 1
-	fi
+	[ $OLD_JOBENV != $JOBENV ] && jobstats_set $OLD_JOBENV
 }
 run_test 205 "Verify job stats"
 
