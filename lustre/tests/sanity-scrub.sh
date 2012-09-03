4bf57b450c39dc1fb1e1436f9a1ecd3e98d5a6ca
Fan Yong <yong.fan@whamcloud.com>
2012-09-03 16:49:22 +0800

    LU-1816 scrub: OI scrub skips new created objects for once
    For accelerating OI scrub, it will skip the objects which are new
    created after the latest MDT mount and marked as I_LUSTRE_NOSCRUB.
    But if the object with I_LUSTRE_NOSCRUB is used/cached in RAM for
    very long time, then OI scrub has no chance to process it even if
    there has been inconsistency happened until the MDT remount.
    
    So as compromise, the OI scrub will skip the new created objects
    for once only when the first accessing.
    
    Signed-off-by: Fan Yong <yong.fan@whamcloud.com>
    Change-Id: I560d7d851e3430326d24ec74154bb2a99f80b737

--- lustre/tests/sanity-scrub.sh
+++ lustre/tests/sanity-scrub.sh
@@ -670,6 +670,40 @@ test_10b() {
 }
 run_test 10b "non-stopped OI scrub should auto restarts after MDS remount (2)"
 
+test_11() {
+	echo "stopall"
+	stopall > /dev/null
+	echo "setupall"
+	setupall > /dev/null
+	mkdir -p $DIR/$tdir
+	createmany -o $DIR/$tdir/`date +%s` 100 || error "(1) Fail to create!"
+
+	# reset OI scrub start point by force
+	$START_SCRUB -r || error "(2) Fail to start OI scrub!"
+	sleep 3
+	local STATUS=$($SHOW_SCRUB | awk '/^status/ { print $2 }')
+	[ "$STATUS" == "completed" ] ||
+		error "(3) Expect 'completed', but got '$STATUS'"
+
+	# OI scrub should skip the new created objects for the first accessing
+	local SKIPPED=$($SHOW_SCRUB | awk '/^noscrub/ { print $2 }')
+	[ $SKIPPED -eq 102 ] ||
+		error "(4) Expect 102 objects skipped, but got $SKIPPED"
+
+	# reset OI scrub start point by force
+	$START_SCRUB -r || error "(5) Fail to start OI scrub!"
+	sleep 3
+	STATUS=$($SHOW_SCRUB | awk '/^status/ { print $2 }')
+	[ "$STATUS" == "completed" ] ||
+		error "(6) Expect 'completed', but got '$STATUS'"
+
+	# OI scrub should skip the new created object only once
+	SKIPPED=$($SHOW_SCRUB | awk '/^noscrub/ { print $2 }')
+	[ $SKIPPED -eq 0 ] ||
+		error "(7) Expect 0 objects skipped, but got $SKIPPED"
+}
+run_test 11 "OI scrub skips the new created objects only once"
+
 # restore the ${facet}_MKFS_OPTS variables
 for facet in MGS MDS OST; do
 	opts=SAVED_${facet}_MKFS_OPTS
