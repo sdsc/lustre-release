40d636bd3364903b5ca119208b5e0c035c0677f0
Alexander.Boyko <alexander_boyko@xyratex.com>
2012-09-06 18:51:50 +0400

    LU-1757 brw: added OBD short io connect flag
    To prevent collisions with any future flags needed in features written
    against this branch.
    
    Signed-off-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Change-Id: I2c9dee20ed8c9d5267f130738428621fb3f92344

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -823,6 +823,10 @@ static const char *obd_connect_names[] = {
         "umask",
         "einprogress",
         "grant_param",
+        "nanosec_time",
+        "variable_lvb",
+        "unknown",
+        "short_io",
         NULL
 };
 
