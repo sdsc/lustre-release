42bfd80b6d9a14263aedcde87491c1b693f90b9f
Keith Mannthey <keith@whamcloud.com>
2012-09-17 18:56:13 -0700

    LU-1839 build:  Update Whamcloud copyright messages for Intel
    Update script to assign all Whamcloud copyrights to Intel and add
    copyrights to files that have been edited by Whamcloud since
    2010-06-01.  Add ability to exclude some commits.
    
    Signed-off-by: Keith Mannthey <keith@whamcloud.com>
    Change-Id: I8c817e6564ea51b88223f954af8b838d713c9a44

--- build/updatecw.sh
+++ build/updatecw.sh
@@ -3,40 +3,54 @@
 # Does not add copyright notices to files that are missing them
 #
 # Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
-# Copyright (c) 2012, Whamcloud, Inc.
+# Copyright (c) 2012, Intel Corporation.
 #
 
 TMP=${TMP:-/tmp}
 TMPFILE=$(mktemp $TMP/updatecopy.XXXXXX)
+EXCFILE=$(mktemp $TMP/excludecopy.XXXXXX)
 NOW=$(date +%Y)
 DIRS=${DIRS:-"build ldiskfs libcfs lnet lustre snmp lustre-iokit"}
+EXCLUDE=${EXCLUDE:-'e3a7c58aebafce40323db54bf6056029e5af4a70\n
+65701b4a30efdb695776bcf690a2b3cabc928da1'}
 
 ORACOPY1="Copyright.*Oracle.*"
 ORACOPY2="Use is subject to license terms."
-ADDCOPY=${ADDCOPY:-"Copyright.*Whamcloud, Inc."}
+WHAMCOPY=${WHAMCOPY:-"Copyright.*Whamcloud, Inc."}
+INTCOPY=${INTCOPY:-"Copyright.*Intel Corporation."}
 AUTHOR=${AUTHOR:-".*@whamcloud.com"}
 START=${START:-"2010-06-01"}
 ECHOE=${ECHOE:-"echo -e"}
 [ "$($ECHOE foo)" = "-e foo" ] && ECHOE=echo
 
+echo -e $EXCLUDE > $EXCFILE
+
 git ls-files $DIRS | grep -v ${0##*/} | while read FILE; do
 	NEEDCOPY=false
 	# Pick only files that have changed since START
-	git log --follow --since=$START --author=$AUTHOR --pretty=format:%ci \
-		$FILE | cut -d- -f1 > $TMPFILE
+	git log --follow --since=$START --author=$AUTHOR \
+		--pretty=format:"%ci %H" $FILE | grep -v -f $EXCFILE \
+		| cut -d- -f1 > $TMPFILE
 
 	# Skip files not modified by $AUTHOR
 	[ -s "$TMPFILE" ] || continue
 
-	OLDCOPY="$(egrep "$ORACOPY1|$ORACOPY2|$ADDCOPY" $FILE|tail -n1|tr / .)"
+	OLDCOPY="$(egrep "$ORACOPY1|$ORACOPY2|$WHAMCOPY|$INTCOPY" \
+						 $FILE|tail -n1|tr / .)"
 	if [ -z "$OLDCOPY" ]; then
 		case $FILE in
 			*.[ch]) echo "$FILE: ** NO COPYRIGHT **" && continue ;;
 			*) continue ;;
 		esac
-	elif [ -z "$(echo "$OLDCOPY" | grep "$ADDCOPY")" ]; then
+	fi 
+
+	OLDCOPY="$(egrep "$WHAMCOPY|$INTCOPY" $FILE|tail -n1|tr / .)"
+	if [ -z "$(echo "$OLDCOPY" | grep -e "$INTCOPY" -e "$WHAMCOPY")" ];
+	then
 		NEEDCOPY=true
-	else
+		OLDCOPY="$(egrep "$ORACOPY1|$ORACOPY2|$WHAMCOPY|$INTCOPY" \
+                                                 $FILE|tail -n1|tr / .)"
+	elif [ ! -z "$(echo "$OLDCOPY" | grep -e "$INTCOPY")" ]; then
 		# Skip files that already have a copyright for this year
 		echo "$OLDCOPY" | grep -q "$NOW" && continue
 	fi
@@ -47,8 +61,13 @@ git ls-files $DIRS | grep -v ${0##*/} | while read FILE; do
 
 	[ $NEWYEAR == $OLDYEAR ] && YEAR="$NEWYEAR" || YEAR="$OLDYEAR, $NEWYEAR"
 	COMMENT=$(echo "$OLDCOPY" | sed -e "s/^\( *[^A-Z]*\) [A-Z].*/\1/")
-	NEWCOPY=$(sed -e "s/^/$COMMENT /" -e "s/\.\*/ (c) $YEAR, /" <<<$ADDCOPY)
+	NEWCOPY=$(sed -e "s/^/$COMMENT /" -e "s/\.\*/ (c) $YEAR, /" <<<$INTCOPY)
 
+	# '.\"' as a COMMENT isn't escaped correctly
+	if [ "$COMMENT" == ".\\\"" ]; then # add " to fix vim
+		echo "$FILE: *** EDIT MANUALLY ***"
+		continue
+	fi
 	# If copyright is unchanged (excluding whitespace), we're done
 	[ "$OLDCOPY" == "$NEWCOPY" ] && continue
 
@@ -64,7 +83,8 @@ git ls-files $DIRS | grep -v ${0##*/} | while read FILE; do
 		sed -e "/$OLDCOPY/r $TMPFILE" $FILE > $FILE.tmp
 	else
 		# Replace the old copyright line with a new copyright
-		sed -e "s#.*$ADDCOPY#$NEWCOPY#" $FILE > $FILE.tmp
+		sed -e "s/.*$INTCOPY/$NEWCOPY/" -e "s/.*$WHAMCOPY/$NEWCOPY/" \
+							$FILE > $FILE.tmp
 	fi
 	[ -s $FILE.tmp ] && cp $FILE.tmp $FILE
 	rm $FILE.tmp
