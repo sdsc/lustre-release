e7e817d1d5f26f52e469e3ad8c160d03f1034a29
Liu Xuezhao <xuezhao.liu@emc.com>
2012-09-27 14:04:33 +0800

    LU-1347 ldlm: makes EXPORT_SYMBOL follows function body
    Makes EXPORT_SYMBOL macros immediately follow the function body,
    to follow normal Linux kernel coding style.
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Change-Id: I5644d908c652b2e34d81e923367af4b5728399e1

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -131,6 +131,7 @@ int client_import_add_conn(struct obd_import *imp, struct obd_uuid *uuid,
 {
         return import_set_conn(imp, uuid, priority, 1);
 }
+EXPORT_SYMBOL(client_import_add_conn);
 
 int client_import_del_conn(struct obd_import *imp, struct obd_uuid *uuid)
 {
@@ -186,6 +187,7 @@ out:
                 CERROR("connection %s not found\n", uuid->uuid);
         RETURN(rc);
 }
+EXPORT_SYMBOL(client_import_del_conn);
 
 /**
  * Find conn uuid by peer nid. @peer is a server nid. This function is used
@@ -443,6 +445,7 @@ err:
         RETURN(rc);
 
 }
+EXPORT_SYMBOL(client_obd_setup);
 
 int client_obd_cleanup(struct obd_device *obddev)
 {
@@ -456,6 +459,7 @@ int client_obd_cleanup(struct obd_device *obddev)
         ldlm_put_ref();
         RETURN(0);
 }
+EXPORT_SYMBOL(client_obd_cleanup);
 
 /* ->o_connect() method for client side (OSC and MDC and MGC) */
 int client_connect_import(const struct lu_env *env,
@@ -524,6 +528,7 @@ out_sem:
 
         return rc;
 }
+EXPORT_SYMBOL(client_connect_import);
 
 int client_disconnect_export(struct obd_export *exp)
 {
@@ -598,6 +603,7 @@ int client_disconnect_export(struct obd_export *exp)
 
         RETURN(rc);
 }
+EXPORT_SYMBOL(client_disconnect_export);
 
 #ifdef HAVE_SERVER_SUPPORT
 int server_disconnect_export(struct obd_export *exp)
@@ -635,6 +641,7 @@ int server_disconnect_export(struct obd_export *exp)
 
 	RETURN(rc);
 }
+EXPORT_SYMBOL(server_disconnect_export);
 
 /* --------------------------------------------------------------------------
  * from old lib/target.c
@@ -1257,6 +1264,7 @@ out:
                 req->rq_status = rc;
         RETURN(rc);
 }
+EXPORT_SYMBOL(target_handle_connect);
 
 int target_handle_disconnect(struct ptlrpc_request *req)
 {
@@ -1272,6 +1280,7 @@ int target_handle_disconnect(struct ptlrpc_request *req)
 
         RETURN(0);
 }
+EXPORT_SYMBOL(target_handle_disconnect);
 
 void target_destroy_export(struct obd_export *exp)
 {
@@ -1292,6 +1301,7 @@ void target_destroy_export(struct obd_export *exp)
 	LASSERT_ATOMIC_ZERO(&exp->exp_cb_count);
 	LASSERT_ATOMIC_ZERO(&exp->exp_replay_count);
 }
+EXPORT_SYMBOL(target_destroy_export);
 
 /*
  * Recovery functions
@@ -1494,6 +1504,7 @@ void target_cleanup_recovery(struct obd_device *obd)
 
         EXIT;
 }
+EXPORT_SYMBOL(target_cleanup_recovery);
 
 /* obd_recovery_task_lock should be held */
 void target_cancel_recovery_timer(struct obd_device *obd)
@@ -1501,6 +1512,7 @@ void target_cancel_recovery_timer(struct obd_device *obd)
         CDEBUG(D_HA, "%s: cancel recovery timer\n", obd->obd_name);
         cfs_timer_disarm(&obd->obd_recovery_timer);
 }
+EXPORT_SYMBOL(target_cancel_recovery_timer);
 
 static void target_start_recovery_timer(struct obd_device *obd)
 {
@@ -2112,6 +2124,7 @@ void target_stop_recovery_thread(struct obd_device *obd)
                 cfs_wait_for_completion(&trd->trd_finishing);
         }
 }
+EXPORT_SYMBOL(target_stop_recovery_thread);
 
 void target_recovery_fini(struct obd_device *obd)
 {
@@ -2325,12 +2338,14 @@ int target_queue_recovery_request(struct ptlrpc_request *req,
         cfs_waitq_signal(&obd->obd_next_transno_waitq);
         RETURN(0);
 }
+EXPORT_SYMBOL(target_queue_recovery_request);
 
 int target_handle_ping(struct ptlrpc_request *req)
 {
         obd_ping(req->rq_svc_thread->t_env, req->rq_export);
         return req_capsule_server_pack(&req->rq_pill);
 }
+EXPORT_SYMBOL(target_handle_ping);
 
 void target_committed_to_req(struct ptlrpc_request *req)
 {
@@ -2382,6 +2397,7 @@ int target_pack_pool_reply(struct ptlrpc_request *req)
 
         RETURN(0);
 }
+EXPORT_SYMBOL(target_pack_pool_reply);
 
 int target_send_reply_msg(struct ptlrpc_request *req, int rc, int fail_id)
 {
@@ -2492,6 +2508,7 @@ void target_send_reply(struct ptlrpc_request *req, int rc, int fail_id)
 	cfs_spin_unlock(&svcpt->scp_rep_lock);
 	EXIT;
 }
+EXPORT_SYMBOL(target_send_reply);
 
 ldlm_mode_t lck_compat_array[] = {
         [LCK_EX] LCK_COMPAT_EX,
