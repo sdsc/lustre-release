b436b4582a11163ea86afe97ad935a376a3dabff
Johann Lombardi <johann.lombardi@intel.com>
2012-10-03 15:02:42 +0200

    LU-1842 quota: add quota locks support on QMT
    This patch adds methods for the quota lock lvb management and
    implements an intent policy for quota locks.
    It also introduces the rebalance thread which is in charge of
    sending glimpse callbacks to slave to notify them of new quota
    settings.
    
    This patch also enables sanity-quota.sh test 36 which should now
    pass.
    
    Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>
    Change-Id: I0cf7f86d480296f7e8fe774ed8937aae27736f88

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -5090,8 +5090,6 @@ static void mdt_fini(const struct lu_env *env, struct mdt_device *m)
 
         mdt_procfs_fini(m);
 
-	mdt_quota_fini(env, m);
-
         lut_fini(env, &m->mdt_lut);
         mdt_fs_cleanup(env, m);
         upcall_cache_cleanup(m->mdt_identity_cache);
@@ -5102,6 +5100,8 @@ static void mdt_fini(const struct lu_env *env, struct mdt_device *m)
                                     d->ld_obd->obd_force);
                 d->ld_obd->obd_namespace = m->mdt_namespace = NULL;
         }
+
+	mdt_quota_fini(env, m);
 
         cfs_free_nidlist(&m->mdt_nosquash_nids);
         if (m->mdt_nosquash_str) {
