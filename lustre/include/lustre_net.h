cb3f082165e4b47958edefe8e3541f7607f25d89
Prakash Surya <surya1@llnl.gov>
2012-10-09 18:02:01 -0700

    LU-2139 osc: Track number of "unstable" pages on client
    This change adds per file system counters to track the number of
    "unstable" pages held by a client. An "unstable" page is defined as
    page which is sent to the server as part of a bulk request, but is
    uncommitted to stable storage.
    
    In addition to simply tracking the unstable pages, a limit can be
    imposed to prevent a client from holding "too many" unstable pages; and
    any unmount operation will block until these unstable pages are
    committed to stable storage.
    
    The current number of, and maximum allowed, pages are stored in the
    ll_sb_info structure. When the current number of unstable pages reaches
    the maximum, calls to osc_page_init will sleep until pages are committed
    on the server, and the last_committed value reaches the client.
    
    In addition to tracking the number of pages in the ll_sb_info structure,
    the NR_UNSTABLE_NFS memory zone is also incremented and decremented
    for easy monitoring using the "NFS_Unstable:" field in /proc/meminfo.
    This field is also used internally by the kernel to limit the total
    amount of unstable pages on the system.
    
    The motivation for this change is twofold. First, the client must not
    allow itself to disconnect from an OST while still holding unstable
    pages. Otherwise, these unstable pages can get lost due to an OST
    failure, and replay is not possible due to the disconnect via unmount.
    
    Secondly, The client needs a mechanism to prevent it from allocating too
    much of its available RAM to unreclaimable pages pinned by the ptlrpc
    layer. If this case occurs, out of memory events can trigger as a side
    effect, which we need to avoid. By default, this per filesystem limit is
    disabled, and the kernel's global limit of unstable pages is used.
    
    The number of unstable pages tracked in the ll_sb_info structure are
    exported by the max_unstable_mb proc file, contained under each
    filesystem's llite namespace. This file can also be written to, allowing
    the upper bound of Lustre's unstable pages on the system to be modified
    dynamically.
    
    For example:
    
    	# Set unstable page limit to 512 MB
    	lctl set_param llite.*.max_unstable_mb=512
    
    	# Disable the unstable page limit
    	lctl set_param llite.*.max_unstable_mb=disabled
    
    	# Read unstable page limit in MB
    	lctl get_param llite.*.max_unstable_mb
    
    	# Read unstable limit, number of unstable pages, and number of waiters
    	lctl get_param llite.*.unstable_stats
    
    Signed-off-by: Prakash Surya <surya1@llnl.gov>
    Change-Id: Ic43d34bfa39da9ac4fe159000c7db5908467fd7b

--- lustre/include/lustre_net.h
+++ lustre/include/lustre_net.h
@@ -723,6 +723,8 @@ struct ptlrpc_request {
 		/* allow the req to be sent if the import is in recovery
 		 * status */
 		rq_allow_replay:1;
+		/* bulk request, sent to server, but uncommitted */
+		rq_unstable:1;
 
 	unsigned int rq_nr_resend;
 
