add29841bd6fafe52f6653d8b15abb17691c4a2a
Doug Oucharek <doug.s.oucharek@intel.com>
2012-10-11 15:08:32 -0700

    LU-2456 lnet: Basic support for sysfs (user and kernel space)
    This is an infrastructure addition to support the use of
    sysfs as a part of the Dynamic LNet Config project.
    
    Sysfs is going to be the main way to communicate
    configuration changes to the LNet module.
    
    Note: the format of sysfs comamnds is: <pid>:<command>.
    The "<pid>:" is put before the reply when you read
    the sysfs file.  This wil help user-space code to
    watch for collisions with other processes when writing
    to sysfs files (<pid> should match your own pid value).
    
    Signed-off-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Change-Id: Ifc16ab6b9b7b7e98e46cda2f23ef072f45d612c5

--- libcfs/include/libcfs/libcfsutil.h
+++ libcfs/include/libcfs/libcfsutil.h
@@ -26,6 +26,8 @@
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
+ *
+ * Copyright (c) 2011, 2012, Intel Corporation.
  */
 /*
  * This file is part of Lustre, http://www.lustre.org/
@@ -50,9 +52,35 @@
 #include <libcfs/util/parser.h>
 #include <libcfs/util/libcfsutil_ioctl.h>
 
+/* message severity level */
+enum libcfs_message_level {
+	CFS_MSG_OFF    = 0,
+	CFS_MSG_FATAL  = 1,
+	CFS_MSG_ERROR  = 2,
+	CFS_MSG_WARN   = 3,
+	CFS_MSG_NORMAL = 4,
+	CFS_MSG_INFO   = 5,
+	CFS_MSG_DEBUG  = 6,
+	CFS_MSG_MAX
+};
+
+/* the bottom three bits reserved for libcfs_message_level */
+#define CFS_MSG_MASK		0x00000007
+#define CFS_MSG_NO_ERRNO	0x00000010
+
 /*
  * Defined by libcfs/libcfs/util/util.c
  */
-int libcfs_tcd_type_max(void);
+extern int libcfs_tcd_type_max(void);
+extern void cfs_msg_set_level(int level);
+extern void cfs_error(int level, int _rc, char *fmt, ...);
+#define cfs_err_noerrno(level, fmt, a...)                             \
+	cfs_error((level) | CFS_MSG_NO_ERRNO, 0, fmt, ## a)
+extern void cfs_printf(int level, char *fmt, ...);
+extern int cfs_first_match(char *pattern, char *buffer);
+extern int cfs_get_param(const char *param_path, char *result,
+			    unsigned int result_size);
+extern int cfs_set_param(const char *param_path, char *buffer,
+			    unsigned int buffer_size);
 
 #endif	/* __LIBCFSUTIL_H__ */
---
+++ lnet/lnet/lib-sysfs.c
@@ -0,0 +1,220 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Portals; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2012, Intel Corporation.
+ */
+
+#define DEBUG_SUBSYSTEM S_LNET
+
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <libcfs/libcfs.h>
+#include <lnet/lib-lnet.h>
+
+#if defined(__linux__) && defined(__KERNEL__)
+
+#include <linux/device.h>
+
+#define LNET_SYSFS_MAX_BUF (PAGE_SIZE)
+#define MAXARGS 10
+#define LNET_SYSFS_MAX_PID 10
+#define LNET_SYSFS_MESSAGE_FORMAT "%s:%s"
+
+static char *lnet_sysfs_read_route;
+static char *lnet_sysfs_read_ni;
+
+static char lnet_sysfs_route_pid[LNET_SYSFS_MAX_PID];
+static char lnet_sysfs_ni_pid[LNET_SYSFS_MAX_PID];
+
+/* This routine will be used for parsing out the command arguments.  ifdef'ing
+   it out now to prevent compilations errors. */
+#if 0
+static int
+line2args(char *line, char **argv, int maxargs)
+{
+	char *arg;
+	int i = 0;
+
+	while ((arg = strsep(&line, " \t")) &&
+	       (i <= maxargs)) {
+		argv[i] = arg;
+		i++;
+	}
+	return i;
+}
+#endif
+
+static ssize_t
+lnet_sysfs_route_write(struct device *dev, struct device_attribute *attr,
+		       const char *buf, size_t count)
+{
+	int i;
+
+	/* Parse out the pid from the beginning of message. */
+	for (i = 0; (i < sizeof(lnet_sysfs_route_pid)) && (i < count) &&
+	     (buf[i] != ':'); i++)
+		lnet_sysfs_route_pid[i] = buf[i];
+	if ((i >= sizeof(lnet_sysfs_route_pid)) || (buf[i] != ':')) {
+		/* We did not hit a colon.  Message not formatted properly. */
+		strcpy(lnet_sysfs_read_route, "Fail: Bad message format\n");
+		strcpy(lnet_sysfs_route_pid, "0");
+		return count;
+	}
+	lnet_sysfs_route_pid[i] = '\0';
+	i++;
+
+	/* Here is where processing of route commands will go. */
+
+	return count;
+}
+
+static ssize_t
+lnet_sysfs_route_read(struct device *dev, struct device_attribute *attr,
+		      char *buf)
+{
+	return scnprintf(buf, LNET_SYSFS_MAX_BUF, LNET_SYSFS_MESSAGE_FORMAT,
+			 lnet_sysfs_route_pid, lnet_sysfs_read_route);
+}
+
+static ssize_t
+lnet_sysfs_ni_write(struct device *dev, struct device_attribute *attr,
+		    const char *buf, size_t count)
+{
+	int i;
+
+	/* Parse out the pid from the beginning of message. */
+	for (i = 0; (i < sizeof(lnet_sysfs_ni_pid)) && (i < count) &&
+	     (buf[i] != ':'); i++)
+		lnet_sysfs_ni_pid[i] = buf[i];
+	if ((i >= sizeof(lnet_sysfs_ni_pid)) || (buf[i] != ':')) {
+		/* We did not hit a colon.  Message not formatted properly. */
+		strcpy(lnet_sysfs_read_ni, "Fail: Bad message format\n");
+		strcpy(lnet_sysfs_ni_pid, "0");
+		return count;
+	}
+	lnet_sysfs_ni_pid[i] = '\0';
+	i++;
+
+	/* Here is where processing of ni commands will go. */
+
+	return count;
+}
+
+static ssize_t
+lnet_sysfs_ni_read(struct device *dev, struct device_attribute *attr,
+		   char *buf)
+{
+	return scnprintf(buf, LNET_SYSFS_MAX_BUF, LNET_SYSFS_MESSAGE_FORMAT,
+			 lnet_sysfs_ni_pid, lnet_sysfs_read_ni);
+}
+
+DEVICE_ATTR(route, 644, lnet_sysfs_route_read, lnet_sysfs_route_write);
+DEVICE_ATTR(ni, 644, lnet_sysfs_ni_read, lnet_sysfs_ni_write);
+
+void
+lnet_sysfs_init(void)
+{
+	int rc = 0;
+	cfs_psdev_t *our_device;
+
+	/* get the device structure used by libcfs */
+	our_device = cfs_get_device();
+	if (our_device == NULL) {
+		CERROR("Unable to get device from libcfs\n");
+		return;
+	}
+
+	/* initialize all read buffers */
+	LIBCFS_ALLOC(lnet_sysfs_read_route, LNET_SYSFS_MAX_BUF);
+	if (lnet_sysfs_read_route == NULL) {
+		CERROR("Unable to allocate sysfs read buffer\n");
+		goto failed;
+	}
+	LIBCFS_ALLOC(lnet_sysfs_read_ni, LNET_SYSFS_MAX_BUF);
+	if (lnet_sysfs_read_ni == NULL) {
+		CERROR("Unable to allocate sysfs read buffer\n");
+		goto failed;
+	}
+
+	strncpy(lnet_sysfs_read_route, "No action taken\n", LNET_SYSFS_MAX_BUF);
+	lnet_sysfs_read_route[LNET_SYSFS_MAX_BUF-1] = '\0';
+	strncpy(lnet_sysfs_read_ni, "No action taken\n", LNET_SYSFS_MAX_BUF);
+	lnet_sysfs_read_ni[LNET_SYSFS_MAX_BUF-1] = '\0';
+
+	/* create the route sysfs file */
+	rc = device_create_file(our_device->this_device, &dev_attr_route);
+	if (rc)
+		CERROR("Unable to create sysfs file: route, rc = %d\n", rc);
+
+	/* create the ni sysfs file */
+	rc = device_create_file(our_device->this_device, &dev_attr_ni);
+	if (rc)
+		CERROR("Unable to create sysfs file: ni, rc = %d\n", rc);
+	return;
+
+failed:
+	if (lnet_sysfs_read_route) {
+		LIBCFS_FREE(lnet_sysfs_read_route, LNET_SYSFS_MAX_BUF);
+		lnet_sysfs_read_route = NULL;
+	}
+	if (lnet_sysfs_read_ni) {
+		LIBCFS_FREE(lnet_sysfs_read_ni, LNET_SYSFS_MAX_BUF);
+		lnet_sysfs_read_ni = NULL;
+	}
+}
+
+void
+lnet_sysfs_fini(void)
+{
+	cfs_psdev_t *our_device;
+
+	/* get the device structure used by libcfs */
+	our_device = cfs_get_device();
+	if (our_device == NULL) {
+		CERROR("Unable to get device from libcfs\n");
+		return;
+	}
+
+	device_remove_file(our_device->this_device, &dev_attr_route);
+	device_remove_file(our_device->this_device, &dev_attr_ni);
+
+	if (lnet_sysfs_read_route) {
+		LIBCFS_FREE(lnet_sysfs_read_route, LNET_SYSFS_MAX_BUF);
+		lnet_sysfs_read_route = NULL;
+	}
+	if (lnet_sysfs_read_ni) {
+		LIBCFS_FREE(lnet_sysfs_read_ni, LNET_SYSFS_MAX_BUF);
+		lnet_sysfs_read_ni = NULL;
+	}
+}
+
+#else
+void
+lnet_sysfs_init(void)
+{
+}
+
+void
+lnet_sysfs_fini(void)
+{
+}
+#endif
