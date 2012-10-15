c03504f819585cae25044cfeb5b3e9ab09f8a786
Li Wei <liwei@whamcloud.com>
2012-10-15 09:29:36 +0800

    LU-1717 ldlm: Fix rs_opc initialization
    By the time target_send_reply() initializes rs_opc, rs_msg has not been
    filled with a valid opc yet.  Following Oleg's suggestion on the Jira
    ticket, this patch changes target_send_reply() to initialize rs_opc with
    rq_reqmsg instead and silences a couple of related warnings that are of
    only informative nature.
    
    Change-Id: I4b96454e0bcf3dd0dc8f21b0de70a89ce37faacf
    Signed-off-by: Li Wei <liwei@whamcloud.com>

--- lustre/ptlrpc/service.c
+++ lustre/ptlrpc/service.c
@@ -1856,13 +1856,13 @@ ptlrpc_handle_rs (struct ptlrpc_reply_state *rs)
         rs->rs_nlocks = 0;                      /* locks still on rs_locks! */
 
         if (nlocks == 0 && !been_handled) {
-                /* If we see this, we should already have seen the warning
-                 * in mds_steal_ack_locks()  */
-                CWARN("All locks stolen from rs %p x"LPD64".t"LPD64
-                      " o%d NID %s\n",
-                      rs,
-                      rs->rs_xid, rs->rs_transno, rs->rs_opc,
-                      libcfs_nid2str(exp->exp_connection->c_peer.nid));
+		/* If we see this, we should already have seen the warning
+		 * in mdt_steal_ack_locks()  */
+		CDEBUG(D_HA, "All locks stolen from rs %p x"LPD64".t"LPD64
+		       " o%d NID %s\n",
+		       rs,
+		       rs->rs_xid, rs->rs_transno, rs->rs_opc,
+		       libcfs_nid2str(exp->exp_connection->c_peer.nid));
         }
 
         if ((!been_handled && rs->rs_on_net) || nlocks > 0) {
