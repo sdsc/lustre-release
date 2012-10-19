dde7bb1a623a060c676a3fb5a3eb8dbd6763c424
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2012-10-19 18:18:05 +0400

    LU-7251 osp: do not assign commit callback to every thandle
    with OSP there is a risk of getting a lot of commit callbacks.
    say, 10K unlinks/sec on 4-striped files could result in 4*10K*5
    = 200K commit callbacks. this patch implements another schema:
    every OSP registers own callback every second. this should result
    in 4*5 commit callbacks in the same situation. in case of forced
    sync the commit callback is registered unconditionally.
    
    the patch removes th_tags and th_ctx from struct thandle as they
    are not used anymore. this elimintates 3 allocations from every
    transaction:
    (lu_object.c:1714:keys_init()) kmalloced 'ctx->lc_value': 320
    (update_records.c:1217:update_key_init()) kmalloced 'value': 408
    (osp_dev.c:1807:osp_txn_key_init()) kmalloced 'value': 4
    
    Change-Id: I460d5eccb585b166423d84d5c142af2e27751d8b
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

--- lustre/target/update_trans.c
+++ lustre/target/update_trans.c
@@ -609,8 +609,6 @@ top_trans_create(const struct lu_env *env, struct dt_device *master_dev)
 		child_th->th_top = &top_th->tt_super;
 		child_th->th_wait_submit = 1;
 		top_th->tt_master_sub_thandle = child_th;
-
-		top_th->tt_super.th_tags |= child_th->th_tags;
 	}
 	return &top_th->tt_super;
 }
@@ -777,7 +775,6 @@ int top_trans_start(const struct lu_env *env, struct dt_device *master_dev,
 			top_th->tt_master_sub_thandle->th_sync = th->th_sync;
 		if (th->th_local)
 			top_th->tt_master_sub_thandle->th_local = th->th_local;
-		top_th->tt_master_sub_thandle->th_tags = th->th_tags;
 		rc = dt_trans_start(env, top_th->tt_master_sub_thandle->th_dev,
 				    top_th->tt_master_sub_thandle);
 		RETURN(rc);
@@ -795,7 +792,6 @@ int top_trans_start(const struct lu_env *env, struct dt_device *master_dev,
 			st->st_sub_th->th_sync = th->th_sync;
 		if (th->th_local)
 			st->st_sub_th->th_local = th->th_local;
-		st->st_sub_th->th_tags = th->th_tags;
 		rc = dt_trans_start(env, st->st_sub_th->th_dev,
 				    st->st_sub_th);
 		if (rc != 0)
@@ -933,7 +929,6 @@ int top_trans_stop(const struct lu_env *env, struct dt_device *master_dev,
 			top_th->tt_master_sub_thandle->th_sync = th->th_sync;
 		if (th->th_local)
 			top_th->tt_master_sub_thandle->th_local = th->th_local;
-		top_th->tt_master_sub_thandle->th_tags = th->th_tags;
 		rc = dt_trans_stop(env, master_dev,
 				   top_th->tt_master_sub_thandle);
 		OBD_FREE_PTR(top_th);
@@ -991,7 +986,6 @@ stop_master_trans:
 			master_st->st_sub_th->th_local = th->th_local;
 		if (th->th_sync)
 			master_st->st_sub_th->th_sync = th->th_sync;
-		master_st->st_sub_th->th_tags = th->th_tags;
 		master_st->st_sub_th->th_result = th->th_result;
 		rc = dt_trans_stop(env, master_st->st_dt, master_st->st_sub_th);
 		if (rc < 0) {
@@ -1048,7 +1042,6 @@ stop_other_trans:
 			st->st_sub_th->th_sync = th->th_sync;
 		if (th->th_local)
 			st->st_sub_th->th_local = th->th_local;
-		st->st_sub_th->th_tags = th->th_tags;
 		st->st_sub_th->th_result = th->th_result;
 		rc = dt_trans_stop(env, st->st_sub_th->th_dev,
 				   st->st_sub_th);
