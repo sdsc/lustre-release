b386eb9937740f4f6c41ddf80ab3ed5815f29f23
Hongchao Zhang <hongchao.zhang@whamcloud.com>
2012-10-20 09:59:38 +0800

    LU-921 llite: warning in case of discarding dirty pages
    when a client is evicted, dirty pages may get silently discarded,
    the caller of successful write(2) will not know that the data he
    wrote have been discarded due to eviction before it can be flushed
    to the OSS.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@whamcloud.com>
    Change-Id: Iecfbf096548ff08cdd6064d53ad8c688343fcddc

--- lustre/llite/rw26.c
+++ lustre/llite/rw26.c
@@ -79,41 +79,44 @@
  */
 static void ll_invalidatepage(struct page *vmpage, unsigned long offset)
 {
-        struct inode     *inode;
-        struct lu_env    *env;
-        struct cl_page   *page;
-        struct cl_object *obj;
+	struct inode     *inode;
+	struct lu_env    *env;
+	struct cl_page   *page;
+	struct cl_object *obj;
 
-        int refcheck;
+	int refcheck;
 
-        LASSERT(PageLocked(vmpage));
-        LASSERT(!PageWriteback(vmpage));
+	LASSERT(PageLocked(vmpage));
+	LASSERT(!PageWriteback(vmpage));
 
-        /*
-         * It is safe to not check anything in invalidatepage/releasepage
-         * below because they are run with page locked and all our io is
-         * happening with locked page too
-         */
-        if (offset == 0) {
-                env = cl_env_get(&refcheck);
-                if (!IS_ERR(env)) {
-                        inode = vmpage->mapping->host;
-                        obj = ll_i2info(inode)->lli_clob;
-                        if (obj != NULL) {
-                                page = cl_vmpage_page(vmpage, obj);
-                                if (page != NULL) {
-                                        lu_ref_add(&page->cp_reference,
-                                                   "delete", vmpage);
-                                        cl_page_delete(env, page);
-                                        lu_ref_del(&page->cp_reference,
-                                                   "delete", vmpage);
-                                        cl_page_put(env, page);
-                                }
-                        } else
-                                LASSERT(vmpage->private == 0);
-                        cl_env_put(env, &refcheck);
-                }
-        }
+	/*
+	 * It is safe to not check anything in invalidatepage/releasepage
+	 * below because they are run with page locked and all our io is
+	 * happening with locked page too
+	 */
+	if (offset == 0) {
+		if (PageDirty(vmpage))
+			ll_dirty_page_discard_warn(vmpage, 0);
+
+		env = cl_env_get(&refcheck);
+		if (!IS_ERR(env)) {
+			inode = vmpage->mapping->host;
+			obj = ll_i2info(inode)->lli_clob;
+			if (obj != NULL) {
+				page = cl_vmpage_page(vmpage, obj);
+				if (page != NULL) {
+					lu_ref_add(&page->cp_reference,
+						   "delete", vmpage);
+					cl_page_delete(env, page);
+					lu_ref_del(&page->cp_reference,
+						   "delete", vmpage);
+					cl_page_put(env, page);
+				}
+			} else
+				LASSERT(vmpage->private == 0);
+			cl_env_put(env, &refcheck);
+		}
+	}
 }
 
 #ifdef HAVE_RELEASEPAGE_WITH_INT
