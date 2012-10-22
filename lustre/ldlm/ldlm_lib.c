f3c802f216d3e1bfb6af736b4c6c88b7f6ae4df6
Prakash Surya <surya1@llnl.gov>
2012-10-22 13:38:11 -0700

    LU-2139 osc: Track number of "unstable" pages per osc
    This change adds simple accounting hooks for "unstable" pages on a per
    OSC basis. Now, in addition to the per filesystem tracking, each OSC
    will maintain a running total of its unstable pages. These counters are
    exported through the proc interface, and can be read using the lctl
    command.
    
    For example:
    
        # Read number of unstable pages contained by each OSC
        lctl get_param osc.*.unstable_stats
    
    The motivation for this change is in anticipation of implementing a
    "soft sync" functionality, urging servers to commit these unstable
    pages to stable storage. The per OSC accounting allows a client to
    limit the soft sync request to only the OSCs which have outstanding
    unstable pages.
    
    Signed-off-by: Prakash Surya <surya1@llnl.gov>
    Change-Id: Ife1f90515cb60ba9f85daba5d50483562f2697d1

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -371,6 +371,7 @@ int client_obd_setup(struct obd_device *obddev, struct lustre_cfg *lcfg)
 	client_obd_list_lock_init(&cli->cl_lru_list_lock);
 
 	cli->cl_unstable = NULL;
+	cfs_atomic_set(&cli->cl_unstable_count, 0);
 
         cfs_waitq_init(&cli->cl_destroy_waitq);
         cfs_atomic_set(&cli->cl_destroy_in_flight, 0);
