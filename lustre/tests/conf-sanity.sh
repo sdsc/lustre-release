6da86f8a3347bdd31f0af24fb90ff24897ab199a
wangdi <di.wang@whamcloud.com>
2012-10-27 16:18:32 -0700

    LU-718 mds: new MDS layer.
    Pull out ptlrpc service from MDT and create new MDS layer.
    
    Change-Id: Ib8a5d22c1f85d086f6ed37e4c46c7aa0e3c4b03a
    Signed-off-by: Wang Di <di.wang@whamcloud.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -2906,9 +2906,15 @@ test_53a() {
 run_test 53a "check OSS thread count params"
 
 test_53b() {
-	thread_sanity MDT $SINGLEMDS 'mdt.*.*.' 'mdt_num_threads' '16'
+	$LCTL get_param -N mds.*.*.threads_max
+	if [ $? -eq 0 ]; then
+		thread_sanity MDT $SINGLEMDS 'mds.*.*.' 'mds_num_threads' 16
+	else
+		#running this on an old MDT
+		thread_sanity MDT $SINGLEMDS 'mdt.*.*' 'mdt_num_threads' 16
+	fi
 }
-run_test 53b "check MDT thread count params"
+run_test 53b "check MDS thread count params"
 
 test_54a() {
 	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
