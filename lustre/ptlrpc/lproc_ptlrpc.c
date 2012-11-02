b161e5677367a56f67e089a30822255d438fe574
Hiroya Nozaki <nozaki.hiroya@jp.fujitsu.com>
2012-11-02 15:27:45 +0900

    LU-2258 mount: solve races between evict and umount
    There are two race conditions between evict and umount.
    
    1) The one is that obd_export_evict_by_xxx() can touch a lustre hash
    which has already been finalized but not unregistered yet.
    
    2) The other is that the class_decref() in lprocfs_wr_evict_client()
    can call lprocfs_remove() via osc_cleanup() while having _lprocfs_lock
    already. That's why this case ends up in a deadlock.
    
    This patch solve these problems with the below solutions.
    - When someone is touching evict_nid on procfs, mount thread waits for
      finishing touching it with a new function
    - After finishing the new function, obd_evict_client_frozen is set and
      this prohibits evictiong export objects.
    - Add a if-statement to the evict functions which checks obd_stopping
      and obd_evict_client_frozen so as not to touch the lustre hash which
      has already been finalized.
    
    Signed-off-by: Hiroya Nozaki <nozaki.hiroya@jp.fujitsu.com>
    Change-Id: I85a2ed0f59700c7551d16e53b35c45a564c0fe44

--- lustre/ptlrpc/lproc_ptlrpc.c
+++ lustre/ptlrpc/lproc_ptlrpc.c
@@ -742,6 +742,10 @@ int lprocfs_wr_evict_client(struct file *file, const char *buffer,
         struct obd_device *obd = data;
         char tmpbuf[sizeof(struct obd_uuid)];
 
+        /* umount has already run */
+        if (obd->obd_evict_client_frozen)
+                return count;
+
         /* Kludge code(deadlock situation): the lprocfs lock has been held
          * since the client is evicted by writting client's
          * uuid/nid to procfs "evict_client" entry. However,
@@ -749,7 +753,6 @@ int lprocfs_wr_evict_client(struct file *file, const char *buffer,
          * the proc entries under the being destroyed export{}, so I have
          * to drop the lock at first here.
          * - jay, jxiong@clusterfs.com */
-        class_incref(obd);
         LPROCFS_EXIT();
 
         sscanf(buffer, "%40s", tmpbuf);
@@ -761,7 +764,6 @@ int lprocfs_wr_evict_client(struct file *file, const char *buffer,
                 obd_export_evict_by_uuid(obd, tmpbuf);
 
         LPROCFS_ENTRY();
-        class_decref(obd);
 
         return count;
 }
