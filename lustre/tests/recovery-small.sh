543b131d938bfedb47963dc75908e6b7aa1697ba
Oleg Drokin <green@whamcloud.com>
2012-11-02 20:17:52 -0400

    LU-2266 tests: Wait for client reconnect in recovery-small 27
    In commit de311fb7 a bug was introduced where we no longer wait
    for client reconnect and instead the check is such that the client
    have not attempted reconnect yet. As such the test is a noop most
    of the time.
    Correct the waiting condiotion to include CFS_FAILED mask.
    
    Change-Id: I3502e17184016a5c526b77a37cc7b505d8bfd5ed
    Signed-off-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/recovery-small.sh
+++ lustre/tests/recovery-small.sh
@@ -831,7 +831,7 @@ test_27() {
 	do_facet $SINGLEMDS lctl set_param fail_loc=0x80000407
 	# need to wait for reconnect
 	echo waiting for fail_loc
-	wait_update_facet $SINGLEMDS "lctl get_param -n fail_loc" "-2147482617"
+	wait_update_facet $SINGLEMDS "lctl get_param -n fail_loc" "3221226503"
 	facet_failover $SINGLEMDS
 	#no crashes allowed!
         kill -USR1 $CLIENT_PID
