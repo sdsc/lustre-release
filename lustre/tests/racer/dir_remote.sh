762e2e0dc84b3b70869184b356423a731355fb6e
wangdi <di.wang@whamcloud.com>
2012-11-18 13:26:54 -0800

    LU-1187 tests: add create remote directory to racer
    Add create remote directory to racer
    
    Test-Parameters: envdefinitions=SLOW=yes testlist=racer
    Change-Id: Iac666d78ac8696dd4fd3e0318d105bb0040d10f8
    Signed-off-by: Wang Di <di.wang@whamcloud.com>

---
+++ lustre/tests/racer/dir_remote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LFS=${LFS:-lfs}
+DIR=$1
+MAX=$2
+MDSCOUNT=$3
+
+while /bin/true ; do
+	remote_dir=$((RANDOM % MAX))
+	file=$((RANDOM % MAX))
+	mdt_idx=$((RANDOM % MDSCOUNT))
+	mkdir -p $DIR
+	$LFS mkdir -i $mdt_idx $DIR/$remote_dir > /dev/null 2>&1
+	echo "abcd" > $DIR/$remote_dir/$file 2> /dev/null
+	$LFS getdirstripe $DIR/$remote_dir > /dev/null 2>&1
+done
