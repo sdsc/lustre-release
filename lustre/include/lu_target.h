5029eadd50b0f3a599e92d7e93456aadb37e1b4f
Mikhail Pershin <tappro@whamcloud.com>
2012-12-13 22:07:52 +0400

    LU-2145 server: use unified request handler for MGS
    - Unify request handler. It finds proper slice for particular
      request and calls appropriate handler for it. Generic handlers
      are moved to the unified target code as well. The tgt_session_info
      introduced to store all request-related data and passed to the
      handlers.
    
    - Pack reply in llog server functions early and use err_serious()
    - remove obsoleted llog_origin_handle_cancel(), it is not used
      anymore
    - remove push_ctxt/pop_ctxt from llog server function, it is based
      on OSD now.
    
    Signed-off-by: Mikhail Pershin <tappro@whamcloud.com>
    Change-Id: Idded90d8112bcab74de9a58e7885fa17dd3340b8

--- lustre/include/lu_target.h
+++ lustre/include/lu_target.h
@@ -59,11 +59,138 @@ struct lu_target {
 	unsigned long           *lut_client_bitmap;
 };
 
+extern struct lu_context_key tgt_session_key;
+
+struct tgt_session_info {
+	/*
+	 * The following members will be filled explicitly
+	 * with specific data in tgt_ses_init().
+	 */
+	struct req_capsule	*tsi_pill;
+
+	/*
+	 * Lock request for "habeo clavis" operations.
+	 */
+	struct ldlm_request	*tsi_dlm_req;
+
+	/* although we have export in req, there are cases when it is not
+	 * available, e.g. closing files upon export destroy */
+	struct obd_export	*tsi_exp;
+	const struct lu_env	*tsi_env;
+	struct lu_target	*tsi_tgt;
+	/*
+	 * Additional fail id that can be set by handler.
+	 */
+	int			 tsi_reply_fail_id;
+	int			 tsi_request_fail_id;
+};
+
+static inline struct tgt_session_info *tgt_ses_info(const struct lu_env *env)
+{
+	struct tgt_session_info *tsi;
+
+	LASSERT(env->le_ses != NULL);
+	tsi = lu_context_key_get(env->le_ses, &tgt_session_key);
+	LASSERT(tsi);
+	return tsi;
+}
+
+/*
+ * Generic unified target support.
+ */
+enum tgt_handler_flags {
+	/*
+	 * struct *_body is passed in the incoming message, and object
+	 * identified by this fid exists on disk.
+	 *                            *
+	 * "habeo corpus" == "I have a body"
+	 */
+	HABEO_CORPUS = (1 << 0),
+	/*
+	 * struct ldlm_request is passed in the incoming message.
+	 *
+	 * "habeo clavis" == "I have a key"
+	 *                                     */
+	HABEO_CLAVIS = (1 << 1),
+	/*
+	 * this request has fixed reply format, so that reply message can be
+	 * packed by generic code.
+	 *
+	 * "habeo refero" == "I have a reply"
+	 */
+	HABEO_REFERO = (1 << 2),
+	/*
+	 * this request will modify something, so check whether the file system
+	 * is readonly or not, then return -EROFS to client asap if necessary.
+	 *
+	 * "mutabor" == "I shall modify"
+	 */
+	MUTABOR      = (1 << 3)
+};
+
+struct tgt_handler {
+	/* The name of this handler. */
+	const char		*th_name;
+	/* Fail id, check at the beginning */
+	int			 th_fail_id;
+	/* Operation code */
+	__u32			 th_opc;
+	/* Flags in enum tgt_handler_flags */
+	__u32			 th_flags;
+	/* Handler function */
+	int			(*th_act)(struct tgt_session_info *tti);
+	/* Request format for this request */
+	const struct req_format	*th_fmt;
+};
+
+struct tgt_opc_slice {
+	__u32			 tos_opc_start; /* First op code */
+	int			 tos_opc_end; /* Last op code */
+	struct tgt_handler	*tos_hs; /* Registered handler */
+};
+
+/* target/tgt_main.c */
+int tgt_register_slice(struct tgt_opc_slice *slice, struct obd_type *obd_type,
+		       int request_fail_id, int reply_fail_id);
+void tgt_degister_slice(struct tgt_opc_slice *slice);
+
+static inline struct ptlrpc_request *tgt_ses_req(struct tgt_session_info *tsi)
+{
+	return tsi->tsi_pill ? tsi->tsi_pill->rc_req : NULL;
+}
+
+static inline __u64 tgt_conn_flags(struct tgt_session_info *tsi)
+{
+	LASSERT(tsi->tsi_exp);
+	return tsi->tsi_exp->exp_connect_flags;
+}
+
+/* target/tgt_handler.c */
+int tgt_request_handle(struct ptlrpc_request *req);
+char *tgt_name(struct tgt_session_info *tsi);
+void tgt_counter_incr(struct obd_export *exp, int opcode);
+int tgt_connect(struct tgt_session_info *tsi);
+int tgt_disconnect(struct tgt_session_info *uti);
+int tgt_obd_ping(struct tgt_session_info *tsi);
+int tgt_enqueue(struct tgt_session_info *tsi);
+int tgt_convert(struct tgt_session_info *tsi);
+int tgt_bl_callback(struct tgt_session_info *tsi);
+int tgt_cp_callback(struct tgt_session_info *tsi);
+int tgt_llog_open(struct tgt_session_info *tsi);
+int tgt_llog_close(struct tgt_session_info *tsi);
+int tgt_llog_destroy(struct tgt_session_info *tsi);
+int tgt_llog_read_header(struct tgt_session_info *tsi);
+int tgt_llog_next_block(struct tgt_session_info *tsi);
+int tgt_llog_prev_block(struct tgt_session_info *tsi);
+int tgt_sec_ctx_init(struct tgt_session_info *tsi);
+int tgt_sec_ctx_init_cont(struct tgt_session_info *tsi);
+int tgt_sec_ctx_fini(struct tgt_session_info *tsi);
+
 typedef void (*tgt_cb_t)(struct lu_target *lut, __u64 transno,
-                         void *data, int err);
+			 void *data, int err);
 struct tgt_commit_cb {
-        tgt_cb_t  tgt_cb_func;
-        void     *tgt_cb_data;
+	tgt_cb_t  tgt_cb_func;
+	void     *tgt_cb_data;
 };
 
 void tgt_boot_epoch_update(struct lu_target *lut);
