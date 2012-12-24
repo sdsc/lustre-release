a1687359b3d0eb14ade1949d0fccbfdb33e36815
Fan Yong <yong.fan@whamcloud.com>
2012-12-25 03:45:42 +0800

    LU-1866 osd: FID-in-LMA and OI files for non-IDIF object
    Generate FID-in-LMA not only for normal objects, but also for
    IGIF objects, server-side local files, LLOG objects and other
    non-OST objects.
    
    At the same time, OI mapping usage is also extended to above
    objects. Only the objects marked as "LDISKFS_STATE_LUSTRE_NOOI"
    or OST objects have no OI mappings the OI files.
    
    It is part of 1.8 IGIF objects upgrage handling. OI scrub will
    automatically generate IGIF-in-LMA and add related OI mappings
    in the OI files.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: If1e1434c238f2ab737e35433bfad1abbb00fcb00

--- lustre/obdclass/local_storage.h
+++ lustre/obdclass/local_storage.h
@@ -67,7 +67,16 @@ static inline struct dt_object *ls_locate(const struct lu_env *env,
 					  struct ls_device *ls,
 					  const struct lu_fid *fid)
 {
-	return dt_locate_at(env, ls->ls_osd, fid, &ls->ls_top_dev.dd_lu_dev);
+	return dt_locate_at(env, ls->ls_osd, fid, &ls->ls_top_dev.dd_lu_dev,
+			    NULL);
+}
+
+static inline struct dt_object *
+ls_locate_conf(const struct lu_env *env, struct ls_device *ls,
+	       const struct lu_fid *fid, const struct lu_object_conf *conf)
+{
+	return dt_locate_at(env, ls->ls_osd, fid, &ls->ls_top_dev.dd_lu_dev,
+			    conf);
 }
 
 struct ls_device *ls_device_get(struct dt_device *dev);
