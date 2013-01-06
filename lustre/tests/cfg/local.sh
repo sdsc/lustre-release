c15aaab751605380f7245dcef440c23e9f6084b0
Emoly Liu <emoly.liu@intel.com>
2013-01-06 14:21:51 +0800

    LU-958 tests: debug_mb set incorrectly for smp or vm
    For cpus with number of cores or for some VMs,
    number of possible CPUs in the system could
    be greater than number of cpu reported by getconf.
    Added check for maximum debug buffer size.
    Added check that "possible" is exist, if not - use old method.
    
    port of patch 2ccb34d882b01305794e7780b6dd691179ddae7e
    port of patch 28817cbd133c626042f9b142600c03187ba4a7ce
    
    Xyratex-bug-id: MRP-219 incorrect settings for debug_mb
    
    Signed-off-by: Denis Kondratenko <Denis_Kondratenko@xyratex.com>
    Change-Id: Ib1e39a26e4d4af8e599d6e5fcdb1fecff8a6f4fa
    Signed-off-by: Liu Ying <emoly.liu@intel.com>

--- lustre/tests/cfg/local.sh
+++ lustre/tests/cfg/local.sh
@@ -36,7 +36,23 @@ STRIPE_BYTES=${STRIPE_BYTES:-1048576}
 STRIPES_PER_OBJ=${STRIPES_PER_OBJ:-0}
 TIMEOUT=${TIMEOUT:-20}
 PTLDEBUG=${PTLDEBUG:-0x33f1504}
-DEBUG_SIZE=${DEBUG_SIZE:-10}
+
+# promise 2MB for every cpu
+if [ -f /sys/devices/system/cpu/possible ]; then
+    _debug_mb=$((($(cut -d "-" -f 2 /sys/devices/system/cpu/possible)+1)*2))
+else
+    _debug_mb=$(($(getconf _NPROCESSORS_CONF)*2))
+fi
+
+# but not less then 10Mb, and limited by 512Mb in libcfs
+if (( _debug_mb < 10 )); then
+        _debug_mb=10
+elif ((_debug_mb > 512 )); then
+        _debug_mb=512
+fi
+
+DEBUG_SIZE=${DEBUG_SIZE:-$_debug_mb}
+
 SUBSYSTEM=${SUBSYSTEM:- 0xffb7e3ff}
 
 L_GETGROUPS=${L_GETGROUPS:-`do_facet mds which l_getgroups || echo`}
