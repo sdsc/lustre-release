79cb759eb676ce78d0afdabda68d042e2cc7a35a
Aurelien Degremont <aurelien.degremont@cea.fr>
2013-01-14 23:43:54 +0100

    LU-1338 hsm: HSM flags feature
    This extends lustre_hsm_attrs struct in MDD to store HSM per-file
    information. It adds infrastructure (function, RPCs, ...) to read and
    modify it, from MDT code or from user-space on clients through llapi
    and lfs command.
    
    This patch implements:
    - lfs hsm_state
    - lfs hsm_set
    - lfs hsm_clear
    
    matching llapi functions:
    - llapi_hsm_state_get()
    - llapi_hsm_state_set()
    
    matching mdt functions:
    - mdt_hsm_state_get()
    - mdt_hsm_state_set()
    
    and MDS RPC:
    - MDS_HSM_STATE_GET
    - MDS_HSM_STATE_SET
    
    Signed-off-by: Aurelien Degremont <aurelien.degremont@cea.fr>
    Signed-off-by: JC Lafoucriere <jacques-charles.lafoucriere@cea.fr>
    Change-Id: Ic8f82ddc9a56206307c2e5be2523fb7ce42b8638

--- lustre/utils/req-layout.c
+++ lustre/utils/req-layout.c
@@ -84,6 +84,8 @@
 #define lustre_swab_mgs_config_res NULL
 #define lustre_swab_hsm_progress_kernel NULL
 #define lustre_swab_hsm_user_item NULL
+#define lustre_swab_hsm_user_state NULL
+#define lustre_swab_hsm_state_set NULL
 #define dump_rniobuf NULL
 #define dump_ioo NULL
 #define dump_obdo NULL
