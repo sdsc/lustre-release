ac04e1a952095f4fd38069a1c7ee0b422adfb6b1
Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
2013-01-15 00:16:41 +0200

    LU-2524 test: test_mkdir presents a regression
    Tests regressions: tests interrelation introduced.
    
    Made tdir be directory name instead of path portion,
    so test_mkdir doesn't require '-p' options.
    
    Added $tslink and $tlink variables declaration in
    the run_one function for test symlinks and hard links
    correspondingly.
    
    Removed not required -p in some tests.
    
    Fixed typo in test_26f.
    
    test_25a and test_25b are joined since they are part
    of one scenario.
    
    test_26d and test_26e are joined since they are part
    of one scenario.
    
    Signed-off-by: Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
    Change-Id: Icece27ec7dd04117d3ab586e8da1434dc6ed0e51

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -3543,7 +3543,7 @@ check_and_cleanup_lustre() {
     fi
 
 	if is_mounted $MOUNT; then
-		[ -n "$DIR" ] && rm -rf $DIR/[Rdfs][0-9]* ||
+		[ -n "$DIR" ] && rm -rf $DIR/[Rdfsl][0-9]* ||
 			error "remove sub-test dirs failed"
 		[ "$ENABLE_QUOTA" ] && restore_quota || true
 	fi
@@ -4190,28 +4190,33 @@ banner() {
 # exit() without stopping the whole script.
 #
 run_one() {
-    local testnum=$1
-    local message=$2
-    tfile=f.${TESTSUITE}.${testnum}
-    export tdir=d0.${TESTSUITE}/d${base}
-    export TESTNAME=test_$testnum
-    local SAVE_UMASK=`umask`
-    umask 0022
+	local testnum=$1
+	local message=$2
+	export tslink=s{$testnum}.${TESTSUITE}
+	export tlink=l{$testnum}.${TESTSUITE}
+	export tfile=f${testnum}.${TESTSUITE}
+	export tdir=d${testnum}.${TESTSUITE}
+	export TESTNAME=test_$testnum
+	local SAVE_UMASK=`umask`
+	umask 0022
 
-    banner "test $testnum: $message"
-    test_${testnum} || error "test_$testnum failed with $?"
-    cd $SAVE_PWD
-    reset_fail_loc
-    check_grant ${testnum} || error "check_grant $testnum failed with $?"
-    check_catastrophe || error "LBUG/LASSERT detected"
+	banner "test $testnum: $message"
+	test_${testnum} || error "test_$testnum failed with $?"
+	cd $SAVE_PWD
+	reset_fail_loc
+	check_grant ${testnum} || error "check_grant $testnum failed with $?"
+	check_catastrophe || error "LBUG/LASSERT detected"
 	if [ "$PARALLEL" != "yes" ]; then
 		ps auxww | grep -v grep | grep -q multiop &&
-					error "multiop still running"
+			error "multiop still running"
 	fi
-    unset TESTNAME
-    unset tdir
-    umask $SAVE_UMASK
-    return 0
+	unset TESTNAME
+	unset tdir
+	unset tfile
+	unset tlink
+	unset tslink
+	umask $SAVE_UMASK
+	return 0
 }
 
 #
