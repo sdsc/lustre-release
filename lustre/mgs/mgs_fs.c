9fb0949a577d1f40254c1505bca19a456846fe61
Jeff Mahoney <jeffm@suse.com>
2013-01-18 03:23:58 -0500

    LU-2642 mgs: fix missing error code return in mgs_fs_setu
    The GOTO macro calls in mgs_fs_setup doesn't assign rc to the value
    passed to it. As a result, if dt_locate_at fails, it will return early
    but also return instead of PTR_ERR(root) as intended.
    
    This was caught by PTR_ERR being __must_check as of v3.6.34.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Change-Id: Ie4897440d481980de6c4d345d2ab2120b44b32c7

--- lustre/mgs/mgs_fs.c
+++ lustre/mgs/mgs_fs.c
@@ -142,8 +142,10 @@ int mgs_fs_setup(const struct lu_env *env, struct mgs_device *mgs)
 
 	root = dt_locate_at(env, mgs->mgs_bottom, &rfid,
 			    &mgs->mgs_dt_dev.dd_lu_dev);
-	if (unlikely(IS_ERR(root)))
-		GOTO(out_los, PTR_ERR(root));
+	if (unlikely(IS_ERR(root))) {
+		rc = PTR_ERR(root);
+		GOTO(out_los, rc);
+	}
 
 	o = local_file_find_or_create(env, mgs->mgs_los, root,
 				      MOUNT_CONFIGS_DIR,
