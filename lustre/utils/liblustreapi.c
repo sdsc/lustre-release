5703fb6ad8578617298a9ba121317723b78d3ffd
Sebastien Buisson <sebastien.buisson@bull.net>
2013-01-28 14:40:35 +0100

    LU-2696 utils: fix error message in rmtacl_notify()
    Print mnt->mnt_dir instead of fd in error message, and print
    the ioctl type that failed.
    
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Change-Id: Ia10c992e4f6ed1dbab86ccfea9d85ea2e0f81cf5

--- lustre/utils/liblustreapi.c
+++ lustre/utils/liblustreapi.c
@@ -3463,7 +3463,9 @@ static int rmtacl_notify(int ops)
 		close(fd);
                 if (rc < 0) {
                         rc = -errno;
-                        llapi_error(LLAPI_MSG_ERROR, rc, "ioctl %d\n", fd);
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "ioctl RMTACL on '%s' err %d\n",
+				    mnt->mnt_dir, rc);
 			goto out;
                 }
 
