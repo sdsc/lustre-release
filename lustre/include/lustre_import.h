84aa7bcf3bb654090fe511a2402b9eaa83c4d3e3
Sebastien Buisson <sebastien.buisson@bull.net>
2013-02-04 13:23:14 +0100

    LU-2744 build: fix 'data race condition' issues
    Fix 'data race condition' defects found by Coverity version
    6.5.0:
    Data race condition (MISSING_LOCK)
    Accessing variable without holding lock. Elsewhere,
    this variable is accessed with lock held.
    
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Change-Id: I32245fac88674d9472a05baddb4dd61941832cc4

--- lustre/include/lustre_import.h
+++ lustre/include/lustre_import.h
@@ -318,7 +318,9 @@ static inline unsigned int at_timeout2est(unsigned int val)
 }
 
 static inline void at_reset(struct adaptive_timeout *at, int val) {
+	spin_lock(&at->at_lock);
         at->at_current = val;
+	spin_unlock(&at->at_lock);
         at->at_worst_ever = val;
         at->at_worst_time = cfs_time_current_sec();
 }
--- lustre/ptlrpc/service.c
+++ lustre/ptlrpc/service.c
@@ -3038,13 +3038,17 @@ ptlrpc_service_purge_all(struct ptlrpc_service *svc)
 
 			cfs_list_del(&req->rq_list);
 			svcpt->scp_nreqs_incoming--;
+			spin_lock(&svcpt->scp_req_lock);
 			svcpt->scp_nreqs_active++;
+			spin_unlock(&svcpt->scp_req_lock);
 			ptlrpc_server_finish_request(svcpt, req);
 		}
 
 		while (ptlrpc_server_request_pending(svcpt, true)) {
 			req = ptlrpc_server_request_get(svcpt, true);
+			spin_lock(&svcpt->scp_req_lock);
 			svcpt->scp_nreqs_active++;
+			spin_unlock(&svcpt->scp_req_lock);
 			ptlrpc_server_hpreq_fini(req);
 
 			if (req->rq_export != NULL)
