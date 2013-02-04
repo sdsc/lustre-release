a24f8402cc227f7786280f6f74810409aabd7b25
Sebastien Buisson <sebastien.buisson@bull.net>
2013-02-04 13:23:14 +0100

    LU-2744 build: fix 'data race condition' issues
    Fix 'data race condition' defects found by Coverity version
    6.5.0:
    Data race condition (MISSING_LOCK)
    Accessing variable without holding lock. Elsewhere,
    this variable is accessed with lock held.
    
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Change-Id: I32245fac88674d9472a05baddb4dd61941832cc4

--- lnet/selftest/conrpc.c
+++ lnet/selftest/conrpc.c
@@ -948,7 +948,9 @@ lstcon_sesnew_stat_reply(lstcon_rpc_trans_t *trans,
 
 	if (!trans->tas_feats_updated) {
 		trans->tas_feats_updated = 1;
+		spin_lock(&console_session.ses_rpc_lock);
 		trans->tas_features = reply->msg_ses_feats;
+		spin_unlock(&console_session.ses_rpc_lock);
 	}
 
 	if (reply->msg_ses_feats != trans->tas_features) {
