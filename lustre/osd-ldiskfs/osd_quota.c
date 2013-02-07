550f0c6a743199ada431aee2c840b0269d99eeb5
Niu Yawei <niu@whamcloud.com>
2013-02-07 01:58:18 -0500

    LU-2548 quota: proper iam index truncation
    In quota migration, the iam container needs be reinitialized
    after truncation.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I622cc8e4e095cc0c9dcd2d2ce0e245a6de376b3d

--- lustre/osd-ldiskfs/osd_quota.c
+++ lustre/osd-ldiskfs/osd_quota.c
@@ -833,6 +833,8 @@ out:
 	return rc;
 }
 
+/* Nobdy else can access the global index now, it's safe to truncate and
+ * reinitialize it */
 static int truncate_quota_index(const struct lu_env *env, struct dt_object *dt,
 				const struct dt_index_features *feat)
 {
@@ -842,7 +844,11 @@ static int truncate_quota_index(const struct lu_env *env, struct dt_object *dt,
 	struct osd_thandle	*oth;
 	struct inode		*inode;
 	int			 rc;
+	struct iam_container	*bag = &(osd_dt_obj(dt))->oo_dir->od_container;
 	ENTRY;
+
+	LASSERT(bag->ic_root_bh != NULL);
+	iam_container_fini(bag);
 
 	OBD_ALLOC_PTR(attr);
 	if (attr == NULL)
@@ -902,6 +908,12 @@ out_lock:
 out:
 	dt_trans_stop(env, &osd->od_dt_dev, th);
 	OBD_FREE_PTR(attr);
+
+	if (rc == 0) {
+		rc  = iam_container_setup(bag);
+		if (!rc)
+			iam_container_fini(bag);
+	}
 	RETURN(rc);
 }
 
@@ -1026,7 +1038,7 @@ int osd_quota_migration(const struct lu_env *env, struct dt_object *dt,
 	if (rc) {
 		CERROR("%s: Failed to truncate the quota index "DFID", rc:%d\n",
 		       osd->od_svname, PFID(lu_object_fid(&dt->do_lu)), rc);
-		RETURN(rc);
+		GOTO(out, rc);
 	}
 
 	/* set up indexing operations for the admin file */
