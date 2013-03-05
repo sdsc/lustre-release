sdsc/lustre-release

commit bb188afa0ccf4cb5c810a7a7276d8d27dd03dc27

LU-2658 llite: too many arguments in generic_file_llseek_size
A generic_file_llseek_size starting kernel 3.5 has 5 arguments.
We need to detect and use with the correct number of arguments.

Signed-off-by: Minh Diep <minh.diep@intel.com>
Change-Id: I966de37a880a94cde54be84062dec2b91e8334f5
Reviewed-on: http://review.whamcloud.com/5244
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Yang Sheng <yang.sheng@intel.com>
Reviewed-by: Peng Tao <bergwolf@gmail.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/autoconf/lustre-core.m4
@@ -2126,6 +2126,22 @@ LB_LINUX_TRY_COMPILE([
 EXTRA_KCFLAGS="$tmp_flags"
 ])
 
+# 3.5 has generic_file_llseek_size with 5 args
+AC_DEFUN([LC_FILE_LLSEEK_SIZE_5ARG],
+[AC_MSG_CHECKING([if kernel has generic_file_llseek_size with 5 args])
+LB_LINUX_TRY_COMPILE([
+	#include <linux/fs.h>
+],[
+	generic_file_llseek_size(NULL, 0, 0, 0, 0);
+], [
+	AC_MSG_RESULT([yes])
+	AC_DEFINE(HAVE_FILE_LLSEEK_SIZE_5ARGS, 1,
+		[kernel has generic_file_llseek_size with 5 args])
+],[
+	AC_MSG_RESULT([no])
+])
+])
+
 #
 # 3.6 switch i_dentry/d_alias from list to hlist
 #

lustre/include/linux/lustre_compat25.h
@@ -168,6 +168,14 @@ static inline struct file *ll_dentry_open(struct dentry *dentry,
 
 #endif
 
+#if !defined(HAVE_FILE_LLSEEK_SIZE) || defined(HAVE_FILE_LLSEEK_5ARGS)
+#define ll_generic_file_llseek_size(file, offset, origin, maxbytes, eof) \
+		generic_file_llseek_size(file, offset, origin, maxbytes, eof);
+#else
+#define ll_generic_file_llseek_size(file, offset, origin, maxbytes, eof) \
+		generic_file_llseek_size(file, offset, origin, maxbytes);
+#endif
+
 #ifdef HAVE_INODE_DIO_WAIT
 /* inode_dio_wait(i) use as-is for write lock */
 # define inode_dio_write_done(i)	do {} while (0) /* for write unlock */

lustre/llite/file.c
@@ -2231,7 +2231,7 @@ loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 		eof = i_size_read(inode);
 	}
 
-	retval = generic_file_llseek_size(file, offset, origin,
+	retval = ll_generic_file_llseek_size(file, offset, origin,
 					  ll_file_maxbytes(inode), eof);
 	RETURN(retval);
 }

commit 450f39f456f4616b1c595d5bed61d7a2de3ef7e0

LU-2908 autoconf: missing call to LC_FILE_LLSEEK_SIZE_5ARG
Commit bb188afa0ccf4cb5c810a7a7276d8d27dd03dc27 introduced
the new function LC_FILE_LLSEEK_SIZE_5ARG into autoconf,
but failed to call this new function anywhere.
A call needs to be added.

Signed-off-by: Minh Diep <minh.diep@intel.com>
Change-Id: I8435b56935da27b133f98723b13ded38a4edfce7

lustre/autoconf/lustre-core.m4
@@ -2384,6 +2384,7 @@ AC_DEFUN([LC_PROG_LINUX],
 	 # 3.5
 	 LC_HAVE_CLEAR_INODE
 	 LC_HAVE_ENCODE_FH_PARENT
+	 LC_FILE_LLSEEK_SIZE_5ARG
 
 	 # 3.6
 	 LC_HAVE_DENTRY_D_ALIAS_HLIST