c3a7025feb5543162aa864313f128b4543fcc2ee
Nathaniel Clark <nathaniel.l.clark@intel.com>
2013-03-10 17:27:02 -0400

    LU-2342 tests: account for log size in replay-single/20b
    Account for larger ondisk log size in ZFS (256) vs. ldiskfs (40).
    
    Test-Parameters: mdsfilesystemtype=zfs ostfilesystemtype=zfs   mdtfilesystemtype=zfs testlist=replay-single
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: I4150d58556240b128c02ba667c4c390c79b8a463

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -585,6 +585,18 @@ unload_modules() {
     return 0
 }
 
+fs_log_size() {
+    local facet=$1
+    local fstype=$(facet_fstype ost1)
+    local size=0
+    case $fstype in
+	ldiskfs) size=40;;
+	zfs)     size=256;;
+    esac
+
+    echo -n $size
+}
+
 check_gss_daemon_nodes() {
     local list=$1
     dname=$2
