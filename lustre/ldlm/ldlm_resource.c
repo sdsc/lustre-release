93094b7f8b6ba15a77ca1229d2327e086eb14e0f
Vitaly Fertman <vitaly_fertman@xyratex.com>
2013-03-25 18:47:57 +0400

    LU-3031 ldlm: disconnect speedup
    disconnect takes too long time if there are many locks to cancel.
    besides the amount of time spent on each lock cancel, there is a
    resched() in cfs_hash_for_each_relax(), i.e. disconnect or eviction
    may take unexpectedly long time.
    - do not cancel locks on disconnect_export;
    - export will be left in obd_unlinked_exports list pinned by live
      locks;
    - new re-connects will created other non-conflicting exports;
    - new locks will cancel obsolete locks on conflicts;
    - once all the locks on the disconnected export will be cancelled,
      the export will be destroyed on the last ref put;
    - pool shrink and recalc on server cancel obsolete locks in small
      portions;
    - free the namespace before obd_exports_barrier on umount;
    
    also make the proper order of events on umount:
    - disconnect export;
    - cleanup namespace, to cancel all the locks before export barrier;
    - exports barrier;
    - lprocfs_free_per_client_stats (requires nid_exp_ref_count == 0);
    - namespace_free_post is left in cleanup ensure will not get and
      segfault on absent namespace.
    
    Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Reviewed-by: Alexander Zarochentsev <Alexander_Zarochentsev@xyratex.com>
    Signed-off-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Change-Id: Ia39b09ce967237ed5078c8a71e760b1e103c6f55
    Xyratex-bug-id: MRP-395

--- lustre/ldlm/ldlm_resource.c
+++ lustre/ldlm/ldlm_resource.c
@@ -927,6 +927,7 @@ void ldlm_namespace_free_prior(struct ldlm_namespace *ns,
         }
         EXIT;
 }
+EXPORT_SYMBOL(ldlm_namespace_free_prior);
 
 /**
  * Performs freeing memory structures related to \a ns. This is only done
@@ -958,6 +959,7 @@ void ldlm_namespace_free_post(struct ldlm_namespace *ns)
 	ldlm_put_ref();
 	EXIT;
 }
+EXPORT_SYMBOL(ldlm_namespace_free_post);
 
 /**
  * Cleanup the resource, and free namespace.
