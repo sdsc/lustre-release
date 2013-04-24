afa74eecc0c8094b1d672069d5ceaa4ed3c82f39
Dmitry Eremin <dmitry.eremin@intel.com>
2013-04-24 17:25:14 +0400

    LU-3204 build: clean up unused variables and dead code
    Clean up the code from really unused variables and rearrange other
    to remove SET_BUT_UNUSED and UNUSED macro usage if possible.
    
    Clean up or comment on some dead code.
    
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Signed-off-by: Ned Bass <bass6@llnl.gov>
    Change-Id: If7b6b3576d3ecc79948a7a1eb46a08c27a3bbc03

--- lustre/include/lustre_lib.h
+++ lustre/include/lustre_lib.h
@@ -788,6 +788,7 @@ do {                                                                           \
 } while (0)
 
 #else /* !__KERNEL__ */
+
 #define __l_wait_event(wq, condition, info, ret, l_add_wait)            \
 do {                                                                    \
         long __timeout = info->lwi_timeout;                             \
@@ -830,7 +831,6 @@ do {                                                                    \
                         }                                               \
                 }                                                       \
         }                                                               \
-        SET_BUT_UNUSED(wq);                                             \
 } while (0)
 
 #endif /* __KERNEL__ */
