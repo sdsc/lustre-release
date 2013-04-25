e8760bc4c06ed2feab08693c957f3f19d7b0ef3b
Andreas Dilger <andreas.dilger@intel.com>
2013-04-24 22:23:57 -0600

    LU-1606 build: clean lustre_user.h for userspace
    Remove the use of Lustre-specific LPU64 and LPX64 macros from
    lustre_user.h so that it can be more easily used from userspace
    applications like e2fsprogs.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I4b4fcb3cc389b8d8ec4375fa92bfee9b353ebbe5

--- lustre/include/lustre/lustre_user.h
+++ lustre/include/lustre/lustre_user.h
@@ -190,8 +190,9 @@ struct ost_id {
 	};
 };
 
-#define DOSTID LPX64":"LPU64
-#define POSTID(oi) ostid_seq(oi), ostid_id(oi)
+#define DOSTID "%#llx:%llu"
+#define POSTID(oi) (unsigned long long)ostid_seq(oi), \
+		   (unsigned long long)ostid_id(oi)
 
 /*
  * The ioctl naming rules:
@@ -473,24 +474,16 @@ static inline void obd_uuid2fsname(char *buf, char *uuid, int buflen)
 
 /* printf display format
    e.g. printf("file FID is "DFID"\n", PFID(fid)); */
-#define DFID_NOBRACE LPX64":0x%x:0x%x"
+#define DFID_NOBRACE "%#llx:%#x:%#x"
 #define DFID "["DFID_NOBRACE"]"
-#define PFID(fid)     \
-        (fid)->f_seq, \
-        (fid)->f_oid, \
-        (fid)->f_ver
+#define PFID(fid) (unsigned long long)(fid)->f_seq, (fid)->f_oid, (fid)->f_ver
 
 /* scanf input parse format -- strip '[' first.
-   e.g. sscanf(fidstr, SFID, RFID(&fid)); */
-/* #define SFID "0x"LPX64i":0x"LPSZX":0x"LPSZX""
-liblustreapi.c:2893: warning: format '%lx' expects type 'long unsigned int *', but argument 4 has type 'unsigned int *'
-liblustreapi.c:2893: warning: format '%lx' expects type 'long unsigned int *', but argument 5 has type 'unsigned int *'
-*/
+ * e.g. sscanf(fidstr, SFID, RFID(&fid)); */
+#ifdef LPX64i
 #define SFID "0x"LPX64i":0x%x:0x%x"
-#define RFID(fid)     \
-        &((fid)->f_seq), \
-        &((fid)->f_oid), \
-        &((fid)->f_ver)
+#define RFID(fid) &((fid)->f_seq), &((fid)->f_oid), &((fid)->f_ver)
+#endif
 
 
 /********* Quotas **********/
