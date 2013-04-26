31b94fbc346649d38c5cd3eda870f29b46fe194d
Liang Zhen <liang.zhen@intel.com>
2013-04-26 14:47:06 +0800

    FF-115 daos: userspace library and EQ tests
    This patch includes these compoents:
    - daos_noop() which is an empty asynchronous API, it will submit
      an event for NOOP and return immediately.
    - userspace library for DAOS
    - test framework for DAOS
    - EQ and event tests
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Change-Id: Ic102875fe14e735ca95c0f1299d7abdae82fa520

---
+++ lustre/include/daos/daos.h
@@ -0,0 +1,46 @@
+/**
+ * SPECIAL LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+ * The Government's rights to use, modify, reproduce, release, perform, display,
+ * or disclose this software are subject to the terms of Contract No. B599860,
+ * and the terms of the LGPL License.
+ * Any reproduction of computer software, computer software documentation, or
+ * portions thereof marked with this legend must also reproduce the markings.
+ */
+/**
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * GPL HEADER END
+ */
+/**
+ * This file is part of lustre/DAOS
+ *
+ * lustre/include/daos/daos.h
+ *
+ * Author: Liang Zhen  <liang.zhen@intel.com>
+ */
+#ifndef __LUSTRE_DAOS_H__
+#define __LUSTRE_DAOS_H__
+
+#include <errno.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <daos/daos_types.h>
+#include <daos/daos_api.h>
+
+#endif /* __LUSTRE_DAOS_H__ */
