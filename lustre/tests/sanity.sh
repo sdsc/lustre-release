dfe7d7c667d87117a0f9d562a47abadacbe002b2
Girish Shilamkar <gshilamkar@ddn.com>
2013-05-19 13:57:23 +0530

    LU-946 lprocfs: List open files in filesystem
    Added lprocfs file on MDT to list open files in per-export
    directory for mdt.
    
    Signed-off-by: Girish Shilamkar <gshilamkar@ddn.com>
    Change-Id: If8f233d95dca4cd4c4044d85bd117a027dabd80e

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -8921,6 +8921,34 @@ test_154c() {
 }
 run_test 154c "lfs path2fid and fid2path multiple arguments"
 
+test_154d() {
+	[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.4.1) ]] &&
+		skip "Need MDS version at least 2.4.1" && return
+
+	if remote_mds; then
+		nid=$($LCTL list_nids | sed  "s/\./\\\./g")
+	else
+		nid="0@lo"
+	fi
+	local proc_ofile="mdt.*.exports.'$nid'.open_files"
+
+	rm -f $DIR/$tfile
+	touch $DIR/$tfile
+
+	fid=$($LFS path2fid $DIR/$tfile)
+	# Open the file
+	exec 7<$DIR/$tfile
+	fid_list=$(do_facet $SINGLEMDS $LCTL get_param $proc_ofile)
+	echo $fid_list | grep $fid
+	rc=$?
+
+	exec 7</dev/null
+	if [ $rc -ne 0 ]; then
+		error "FID $fid not found in open files list $fid_list"
+	fi
+}
+run_test 154d "Verify open file fid"
+
 test_155_small_load() {
     local temp=$TMP/$tfile
     local file=$DIR/$tfile
