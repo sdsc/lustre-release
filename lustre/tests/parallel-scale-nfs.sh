c149236ca699ed32734b5b97a2bcdb8268733492
Jian Yu <jian.yu@intel.com>
2013-05-27 14:18:56 +0800

    LU-2904 tests: mount Lustre client with "32bitapi" for NFSv3
    Currently, the "32bitapi" Lustre client mount option is only added
    while exporting the Lustre client as NFSv4 server. To interoperate
    with Lustre 2.4.0 server, the option is also needed while exporting
    the Lustre client as NFSv3 server.
    
    Test-Parameters: envdefinitions=SLOW=yes \
    serverjob=lustre-b2_4 serverbuildno=12 \
    testlist=parallel-scale-nfsv3,parallel-scale-nfsv4
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: Ia26f3e3d23d901725ab175a6d370f58a1a93519c

--- lustre/tests/parallel-scale-nfs.sh
+++ lustre/tests/parallel-scale-nfs.sh
@@ -20,7 +20,7 @@ check_and_setup_lustre
 cleanup_mount $MOUNT
 # mount lustre on mds
 lustre_client=$(facet_active_host $SINGLEMDS)
-[ "$NFSVERSION" = "4" ] && cl_mnt_opt="$MOUNTOPT,32bitapi" || cl_mnt_opt=""
+cl_mnt_opt="$MOUNTOPT,32bitapi"
 zconf_mount_clients $lustre_client $MOUNT "$cl_mnt_opt" || \
     error "mount lustre on $lustre_client failed"
 
