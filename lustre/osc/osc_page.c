be3e398d5cb257575fc3420b2c0f6122830c5cf9
Jinshan Xiong <jinshan.xiong@intel.com>
2013-06-04 13:26:12 -0700

    LU-3321 clio: add pages into writeback cache in batch
    in ll_write_end(), instead of adding the page into writeback
    cache directly, it will be held in a page list. After enough
    pages have been collected, issue them all with cio_commit_async().
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: Ia5396b38f2cad903a5f50d06dd2ab0eea48b77c5

--- lustre/osc/osc_page.c
+++ lustre/osc/osc_page.c
@@ -170,7 +170,7 @@ static void osc_page_fini(const struct lu_env *env,
         LASSERT(opg->ops_lock == NULL);
 }
 
-static void osc_page_transfer_get(struct osc_page *opg, const char *label)
+void osc_page_transfer_get(struct osc_page *opg, const char *label)
 {
         struct cl_page *page = cl_page_top(opg->ops_cl.cpl_page);
 
@@ -180,14 +180,13 @@ static void osc_page_transfer_get(struct osc_page *opg, const char *label)
         opg->ops_transfer_pinned = 1;
 }
 
-static void osc_page_transfer_put(const struct lu_env *env,
-                                  struct osc_page *opg)
+void osc_page_transfer_put(const struct lu_env *env, struct osc_page *opg)
 {
         struct cl_page *page = cl_page_top(opg->ops_cl.cpl_page);
 
         if (opg->ops_transfer_pinned) {
-                lu_ref_del(&page->cp_reference, "transfer", page);
                 opg->ops_transfer_pinned = 0;
+                lu_ref_del(&page->cp_reference, "transfer", page);
                 cl_page_put(env, page);
         }
 }
@@ -197,8 +196,8 @@ static void osc_page_transfer_put(const struct lu_env *env,
  * either opportunistic (osc_page_cache_add()), or immediate
  * (osc_page_submit()).
  */
-static void osc_page_transfer_add(const struct lu_env *env,
-                                  struct osc_page *opg, enum cl_req_type crt)
+void osc_page_transfer_add(const struct lu_env *env, struct osc_page *opg,
+			   enum cl_req_type crt)
 {
 	struct osc_object *obj = cl2osc(opg->ops_cl.cpl_obj);
 
@@ -212,11 +211,9 @@ static void osc_page_transfer_add(const struct lu_env *env,
 	spin_unlock(&obj->oo_seatbelt);
 }
 
-static int osc_page_cache_add(const struct lu_env *env,
-			      const struct cl_page_slice *slice,
-			      struct cl_io *io)
+int osc_page_cache_add(const struct lu_env *env,
+			const struct cl_page_slice *slice, struct cl_io *io)
 {
-	struct osc_io   *oio = osc_env_io(env);
 	struct osc_page *opg = cl2osc_page(slice);
 	int result;
 	ENTRY;
@@ -229,16 +226,6 @@ static int osc_page_cache_add(const struct lu_env *env,
 		osc_page_transfer_put(env, opg);
 	else
 		osc_page_transfer_add(env, opg, CRT_WRITE);
-
-	/* for sync write, kernel will wait for this page to be flushed before
-	 * osc_io_end() is called, so release it earlier.
-	 * for mkwrite(), it's known there is no further pages. */
-	if (cl_io_is_sync_write(io) || cl_io_is_mkwrite(io)) {
-		if (oio->oi_active != NULL) {
-			osc_extent_release(env, oio->oi_active);
-			oio->oi_active = NULL;
-		}
-	}
 
 	RETURN(result);
 }
@@ -330,18 +317,6 @@ static void osc_page_completion_write(const struct lu_env *env,
 				      int ioret)
 {
 }
-
-static int osc_page_fail(const struct lu_env *env,
-                         const struct cl_page_slice *slice,
-                         struct cl_io *unused)
-{
-        /*
-         * Cached read?
-         */
-        LBUG();
-        return 0;
-}
-
 
 static const char *osc_list(cfs_list_t *head)
 {
@@ -501,11 +476,9 @@ static const struct cl_page_operations osc_page_ops = {
         .cpo_disown        = osc_page_disown,
         .io = {
                 [CRT_READ] = {
-                        .cpo_cache_add  = osc_page_fail,
                         .cpo_completion = osc_page_completion_read
                 },
                 [CRT_WRITE] = {
-			.cpo_cache_add  = osc_page_cache_add,
 			.cpo_completion = osc_page_completion_write
 		}
 	},
