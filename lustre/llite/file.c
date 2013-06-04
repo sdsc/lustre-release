f81b8464e51a41a922d0b36910c31c0f1ef34ab1
Jinshan Xiong <jinshan.xiong@intel.com>
2013-06-04 13:26:12 -0700

    LU-3321 clio: add pages into writeback cache in batch
    in ll_write_end(), instead of adding the page into writeback
    cache directly, it will be held in a page list. After enough
    pages have been collected, issue them all with cio_commit_async().
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: Ia5396b38f2cad903a5f50d06dd2ab0eea48b77c5

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -859,6 +859,9 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
         ssize_t               result;
         ENTRY;
 
+	CDEBUG(D_VFSTRACE, "file: %s, type: %d ppos: "LPU64", count:%zd\n",
+		file->f_dentry->d_name.name, iot, *ppos, count);
+
 restart:
         io = ccc_env_thread_io(env);
         ll_io_init(io, file, iot == CIT_WRITE);
@@ -882,12 +885,11 @@ restart:
                         if ((iot == CIT_WRITE) &&
                             !(cio->cui_fd->fd_flags & LL_FILE_GROUP_LOCKED)) {
 				if (mutex_lock_interruptible(&lli->
-                                                               lli_write_mutex))
-                                        GOTO(out, result = -ERESTARTSYS);
-                                write_mutex_locked = 1;
-                        } else if (iot == CIT_READ) {
-				down_read(&lli->lli_trunc_sem);
-                        }
+							lli_write_mutex))
+					GOTO(out, result = -ERESTARTSYS);
+				write_mutex_locked = 1;
+			}
+			down_read(&lli->lli_trunc_sem);
                         break;
                 case IO_SENDFILE:
                         vio->u.sendfile.cui_actor = args->u.sendfile.via_actor;
@@ -902,10 +904,10 @@ restart:
                         LBUG();
                 }
                 result = cl_io_loop(env, io);
-                if (write_mutex_locked)
-			mutex_unlock(&lli->lli_write_mutex);
-                else if (args->via_io_subtype == IO_NORMAL && iot == CIT_READ)
+		if (args->via_io_subtype == IO_NORMAL)
 			up_read(&lli->lli_trunc_sem);
+		if (write_mutex_locked)
+			mutex_unlock(&lli->lli_write_mutex);
         } else {
                 /* cl_io_rw_init() handled IO */
                 result = io->ci_result;
@@ -941,6 +943,7 @@ out:
 			fd->fd_write_failed = true;
 		}
 	}
+	CDEBUG(D_VFSTRACE, "iot: %d, result: %zd\n", iot, result);
 
 	return result;
 }
