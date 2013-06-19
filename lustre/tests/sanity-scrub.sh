9048fee26329d36af1b67cb339abe3dcc2a88b01
Fan Yong <fan.yong@intel.com>
2013-06-20 00:15:06 +0800

    LU-3335 scrub: recover OST-objects from /lost+found
    Originally, Lustre used the tool ll_recover_lost_found_objs to recover
    orphans under /lost+found under offline mode. With OI scrub introduced
    it can be done by initial OI scrub during the OST mount automatically.
    
    Test-Parameters: testlist=sanity-scrub
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Ibf7e51cc0de10def47a117217b84c2afcd1209ca

--- lustre/tests/sanity-scrub.sh
+++ lustre/tests/sanity-scrub.sh
@@ -870,6 +870,52 @@ test_13() {
 }
 run_test 13 "OI scrub can rebuild missed /O entries"
 
+test_14() {
+	echo "stopall"
+	stopall > /dev/null
+	echo "formatall"
+	formatall > /dev/null
+	echo "setupall"
+	setupall > /dev/null
+
+	mkdir -p $DIR/$tdir
+	$SETSTRIPE -c 1 -i 0 $DIR/$tdir
+
+	#define OBD_FAIL_OSD_COMPAT_NO_ENTRY		0x196
+	do_facet ost1 $LCTL set_param fail_loc=0x196
+	createmany -o $DIR/$tdir/f 64
+	do_facet ost1 $LCTL set_param fail_loc=0
+
+	echo "stopall"
+	stopall > /dev/null
+	echo "setupall"
+	setupall > /dev/null
+
+	local STATUS=$($SHOW_SCRUB_ON_OST | awk '/^status/ { print $2 }')
+	[ "$STATUS" == "init" ] ||
+		error "(1) Expect 'init', but got '$STATUS'"
+
+	ls -ail $DIR/$tdir > /dev/null 2>&1 && error "(2) ls should fail"
+
+	echo "stopall"
+	stopall > /dev/null
+
+	echo "run e2fsck"
+	run_e2fsck $(facet_host ost1) $(ostdevname 1) "-y" ||
+		error "(3) Fail to run e2fsck error"
+
+	echo "setupall"
+	setupall > /dev/null
+
+	local LF_REPAIRED=$($SHOW_SCRUB_ON_OST |
+			    awk '/^lf_reparied/ { print $2 }')
+	[ $LF_REPAIRED -gt 0 ] ||
+		error "(4) Some entry under /lost+found should be repaired"
+
+	ls -ail $DIR/$tdir > /dev/null 2>&1 || error "(5) ls should succeed"
+}
+run_test 14 "OI scrub can repair objects under lost+found"
+
 # restore MDS/OST size
 MDSSIZE=${SAVED_MDSSIZE}
 OSTSIZE=${SAVED_OSTSIZE}
