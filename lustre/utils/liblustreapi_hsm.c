48fd1f6170e1be557e849541eee2c6bed7b1a7c5
John L. Hammond <john.hammond@intel.com>
2013-07-02 17:06:29 -0500

    LU-2549 llapi: add printf attribute to llapi_{printf,error}
    Add printf attributes to llapi_printf and llapi_error for compile time
    checking that the format string agrees with the argument list, and
    const qualify the format parameter of both functions. Fixup the
    mismatched calls to these functions.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: Ic8df148abb0bfe8ce72d05d76cb7ce0f1832c28f

--- lustre/utils/liblustreapi_hsm.c
+++ lustre/utils/liblustreapi_hsm.c
@@ -105,7 +105,7 @@ int llapi_hsm_copytool_start(struct hsm_copytool_private **priv, char *fsname,
 	for (rc = 0; rc < archive_count; rc++) {
 		if (archives[rc] > 8 * sizeof(ct->archives)) {
 			llapi_err_noerrno(LLAPI_MSG_ERROR,
-					  "Maximum of %d archives supported",
+					  "maximum of %zu archives supported",
 					  8 * sizeof(ct->archives));
 			goto out_err;
 		}
