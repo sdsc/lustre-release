61a2d41e50932fbdf41d453709f0bf54200cad3d
Fan Yong <fan.yong@intel.com>
2013-07-06 18:24:14 +0800

    LU-1267 lfsck: rebuild LAST_ID
    The /O/<seq>/LAST_ID records the last oid of the object allocated
    within the sequence. The LAST_ID file can be crashed or missed as
    the system running. The LFSCK for layout consistency verification
    can detect the LAST_ID lost/crashed cases, and can rebuild it via
    scanning the whole device.
    
    This functionality is also part of LU-14 live replacement of OST.
    
    Introduce lfsck_notify callback - the LFSCK events notification
    channel from the LFSCK to the registered users (MDD/OFD).
    
    Introduce new connection flag: OBD_CONNECT_LFSCK to indicate that
    whether the target server (MDT/OST) supports online LFSCK or not.
    
    Test-Parameters: testlist=sanity-scrub,sanity-lfsck
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Iee85056e2fda1ecba9424c9f0e822643e9f029a8

--- lustre/lfsck/lfsck_lib.c
+++ lustre/lfsck/lfsck_lib.c
@@ -78,6 +78,7 @@ const char *lfsck_flags_names[] = {
 	"inconsistent",
 	"upgrade",
 	"incomplete",
+	"crashed_lastid",
 	NULL
 };
 
@@ -107,6 +108,13 @@ static inline void lfsck_component_put(const struct lu_env *env,
 			OBD_FREE(com->lc_file_ram, com->lc_file_size);
 		if (com->lc_file_disk != NULL)
 			OBD_FREE(com->lc_file_disk, com->lc_file_size);
+
+		if (com->lc_data != NULL) {
+			LASSERT(com->lc_ops->lfsck_data_release != NULL);
+
+			com->lc_ops->lfsck_data_release(env, com);
+		}
+
 		OBD_FREE_PTR(com);
 	}
 }
@@ -1076,7 +1084,8 @@ int lfsck_stop(const struct lu_env *env, struct dt_device *key, bool pause)
 EXPORT_SYMBOL(lfsck_stop);
 
 int lfsck_register(const struct lu_env *env, struct dt_device *key,
-		   struct dt_device *next, bool master)
+		   struct dt_device *next, lfsck_out_notify notify,
+		   void *data, bool master)
 {
 	struct lfsck_instance	*lfsck;
 	struct dt_object	*root;
@@ -1102,6 +1111,8 @@ int lfsck_register(const struct lu_env *env, struct dt_device *key,
 	CFS_INIT_LIST_HEAD(&lfsck->li_list_idle);
 	atomic_set(&lfsck->li_ref, 1);
 	cfs_waitq_init(&lfsck->li_thread.t_ctl_waitq);
+	lfsck->li_out_notify = notify;
+	lfsck->li_out_notify_data = data;
 	lfsck->li_next = next;
 	lfsck->li_bottom = key;
 
