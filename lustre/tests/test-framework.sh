119e1224b6557986c0c3023b60cf964761e04313
Nathaniel Clark <nathaniel.l.clark@intel.com>
2013-07-10 13:16:57 -0400

    LU-2469 tests: use MDS/OSTDEV and MDS/OSTDEVBASE with zfs
    Add support for using MDSDEVn, OSTDEVn, MDSDEVBASE and OSTDEVBASE
    variables when testing with zfs.
    Add *ZFSDEVn variable for specifying ZFS format devices.
    Remove use of *DEVn use outside of test-framework.sh
    
    MDSDEV et al. specify physical device formatted.
    
    Formerly Change-Id I89115ce935294cdbbd6620fec3f9dc3eb76f8960
    
    Test-Parameters: mdsfilesystemtype=zfs ostfilesystemtype=zfs  mdtfilesystemtype=zfs test-list=sanity
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: If6506995d9d61f9c97827f45071f0d317c7a09f9

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -2649,9 +2649,10 @@ add() {
     do_facet ${facet} $MKFS $*
 }
 
+# Device formatted as ost
 ostdevname() {
-    num=$1
-    DEVNAME=OSTDEV$num
+	local num=$1
+	local DEVNAME=OSTDEV$num
 
 	local fstype=$(facet_fstype ost$num)
 
@@ -2660,8 +2661,9 @@ ostdevname() {
 			#if $OSTDEVn isn't defined, default is $OSTDEVBASE + num
 			eval DEVPTR=${!DEVNAME:=${OSTDEVBASE}${num}};;
 		zfs )
-			#dataset name is independent of vdev device names
-			eval DEVPTR=${FSNAME}-ost${num}/ost${num};;
+			#try $OSTZFSDEVn - independent of vdev
+			DEVNAME=OSTZFSDEV$num
+			eval DEVPTR=${!DEVNAME:=${FSNAME}-ost${num}/ost${num}};;
 		* )
 			error "unknown fstype!";;
 	esac
@@ -2669,9 +2671,11 @@ ostdevname() {
     echo -n $DEVPTR
 }
 
+# Physical device location of data
 ostvdevname() {
-	num=$1
-	DEVNAME=OSTDEV$num
+	local num=$1
+	local DEVNAME
+	local VDEVPTR
 
 	local fstype=$(facet_fstype ost$num)
 
@@ -2680,7 +2684,9 @@ ostvdevname() {
 			# vdevs are not supported by ldiskfs
 			eval VDEVPTR="";;
 		zfs )
-			#if $OSTDEVn isn't defined, default is $OSTDEVBASE + num
+			#if $OSTDEVn isn't defined, default is $OSTDEVBASE{n}
+			# Device formated by zfs
+			DEVNAME=OSTDEV$num
 			eval VDEVPTR=${!DEVNAME:=${OSTDEVBASE}${num}};;
 		* )
 			error "unknown fstype!";;
@@ -2689,19 +2695,21 @@ ostvdevname() {
 	echo -n $VDEVPTR
 }
 
+# Logical device formated for lustre
 mdsdevname() {
-    num=$1
-    DEVNAME=MDSDEV$num
+	local num=$1
+	local DEVNAME=MDSDEV$num
 
 	local fstype=$(facet_fstype mds$num)
 
 	case $fstype in
 		ldiskfs )
-			#if $MDSDEVn isn't defined, default is $MDSDEVBASE + num
+			#if $MDSDEVn isn't defined, default is $MDSDEVBASE{n}
 			eval DEVPTR=${!DEVNAME:=${MDSDEVBASE}${num}};;
 		zfs )
-			#dataset name is independent of vdev device names
-			eval DEVPTR=${FSNAME}-mdt${num}/mdt${num};;
+			# try $MDSZFSDEVn - independent of vdev
+			DEVNAME=MDSZFSDEV$num
+			eval DEVPTR=${!DEVNAME:=${FSNAME}-mdt${num}/mdt${num}};;
 		* )
 			error "unknown fstype!";;
 	esac
@@ -2709,10 +2717,10 @@ mdsdevname() {
 	echo -n $DEVPTR
 }
 
+# Physical location of data
 mdsvdevname() {
-	num=$1
-	DEVNAME=MDSDEV$num
-
+	local VDEVPTR=""
+	local num=$1
 	local fstype=$(facet_fstype mds$num)
 
 	case $fstype in
@@ -2720,7 +2728,9 @@ mdsvdevname() {
 			# vdevs are not supported by ldiskfs
 			eval VDEVPTR="";;
 		zfs )
-			#if $MDSDEVn isn't defined, default is $MDSDEVBASE + num
+			# if $MDSDEVn isn't defined, default is $MDSDEVBASE{n}
+			# Device formated by ZFS
+			local DEVNAME=MDSDEV$num
 			eval VDEVPTR=${!DEVNAME:=${MDSDEVBASE}${num}};;
 		* )
 			error "unknown fstype!";;
@@ -2743,10 +2753,11 @@ mgsdevname() {
 		fi;;
 	zfs )
 		if [ $(facet_host mgs) = $(facet_host mds1) ] &&
-		   ( [ -z "$MGSDEV" ] || [ $MGSDEV = $(mdsvdevname 1) ] ); then
+		    ( [ -z "$MGSZFSDEV" ] &&
+			[ -z "$MGSDEV" -o "$MGSDEV" = $(mdsvdevname 1) ] ); then
 			DEVPTR=$(mdsdevname 1)
 		else
-			DEVPTR=${FSNAME}-mgs/mgs
+			DEVPTR=${MGSZFSDEV:-${FSNAME}-mgs/mgs}
 		fi;;
 	* )
 		error "unknown fstype!";;
@@ -2756,8 +2767,7 @@ mgsdevname() {
 }
 
 mgsvdevname() {
-	local VDEVPTR
-	DEVNAME=MGSDEV
+	local VDEVPTR=""
 
 	local fstype=$(facet_fstype mgs)
 
@@ -2767,9 +2777,10 @@ mgsvdevname() {
 		;;
 	zfs )
 		if [ $(facet_host mgs) = $(facet_host mds1) ] &&
-		   ( [ -z "$MGSDEV" ] || [ $MGSDEV = $(mdsvdevname 1) ] ); then
+		   ( [ -z "$MGSDEV" ] &&
+		       [ -z "$MGSZFSDEV" -o "$MGSZFSDEV" = $(mdsdevname 1) ]); then
 			VDEVPTR=$(mdsvdevname 1)
-		else
+		elif [ -n "$MGSDEV" ]; then
 			VDEVPTR=$MGSDEV
 		fi;;
 	* )
