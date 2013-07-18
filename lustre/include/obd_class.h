40d037c05738237f5b873b3d92eae665dc5a7edc
wang di <di.wang@intel.com>
2013-07-18 00:00:09 -0700

    LU-3531 llite: move dir cache to MDC layer
    Move directory entries cache from llite to MDC, so client
    side dir stripe will use independent hash function(in LMV),
    which does not need to be tightly coupled with the backend
    storage dir-entry hash function. With striped directory, it
    will be 2-tier hash, LMV calculate hash value according to the
    name and hash-type in layout, then each MDT will store these
    entry in disk by its own hash.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: I14bb6bd81aad6fd59dcc22cf4bcea9d341dca2a1

--- lustre/include/obd_class.h
+++ lustre/include/obd_class.h
@@ -2004,17 +2004,31 @@ static inline int md_fsync(struct obd_export *exp, const struct lu_fid *fid,
 	rc = MDP(exp->exp_obd, fsync)(exp, fid, oc, request);
 	RETURN(rc);
 }
-
-static inline int md_readpage(struct obd_export *exp, struct md_op_data *opdata,
-                              struct page **pages,
-                              struct ptlrpc_request **request)
+static inline int md_readpage(struct obd_export *exp,
+			      struct md_op_data *op_data,
+			      struct md_callback *cb_op,
+			      struct ptlrpc_request **request,
+			      struct page **page)
 {
-        int rc;
-        ENTRY;
-        EXP_CHECK_MD_OP(exp, readpage);
-        EXP_MD_COUNTER_INCREMENT(exp, readpage);
-        rc = MDP(exp->exp_obd, readpage)(exp, opdata, pages, request);
-        RETURN(rc);
+	int rc;
+	ENTRY;
+	EXP_CHECK_MD_OP(exp, readpage);
+	EXP_MD_COUNTER_INCREMENT(exp, readpage);
+	rc = MDP(exp->exp_obd, readpage)(exp, op_data, cb_op, request, page);
+	RETURN(rc);
+}
+
+static inline int md_read_entry(struct obd_export *exp,
+				struct md_op_data *op_data,
+				struct md_callback *cb_op,
+				struct lu_dirent **ld)
+{
+	int rc;
+	ENTRY;
+	EXP_CHECK_MD_OP(exp, read_entry);
+	EXP_MD_COUNTER_INCREMENT(exp, read_entry);
+	rc = MDP(exp->exp_obd, read_entry)(exp, op_data, cb_op, ld);
+	RETURN(rc);
 }
 
 static inline int md_unlink(struct obd_export *exp, struct md_op_data *op_data,
