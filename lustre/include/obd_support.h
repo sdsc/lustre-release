98ccc78bf48a0e80e9e55280a25fcce44dda7758
Hiroya Nozaki <nozaki.hiroya@jp.fujitsu.com>
2013-07-22 09:24:25 +0900

    LU-2177 ptlrpc: flock/posix lock cause LBUGs because of a race
    The below races may happen when evicting clients
    
    - LBUG in ldlm_flock_decref_internal_nolock()
    1) A therad calls ldlm_flock_completion_ast()
    2) Another thread is dealing with client eviction and sets some flags
       to the same flock as (2), and calls ldlm_flock_completion_ast()
    
    Because of the flags which've been set at (2), the both completion
    ast call ldlm_lock_decref_internal() but it's more than necessary
    
    ----------------------------------------------------------------------
    - LBUG in ldlm_lock_destroy()
    1) a flock has been granted
    2) the client which (1)'s flock belongs to is evicted and the flock
       gets LDLM_FL_FAILED flag
    3) (1)'s thread goes ahead and calls ldlm_process_flock_lock()
    
    Because of (2), an error is detected and ldlm_flock_destroy() is
    called in ldlm_process_flock_lock(). But LDLM_FL_FAILED has already
    been set, So ldlm_flock_destroy() never call ldlm_lock_decref_internal
    _nolock() thought it's needed
    
    Signed-off-by: Hiroya Nozaki <nozaki.hiroya@jp.fujitsu.com>
    Change-Id: I0428f7807f410f3f4bd45474330741e216ab202f

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -339,6 +339,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_LDLM_AGL_DELAY          0x31a
 #define OBD_FAIL_LDLM_AGL_NOLOCK         0x31b
 #define OBD_FAIL_LDLM_OST_LVB		 0x31c
+#define OBD_FAIL_LDLM_FLOCK_CP           0x32d
 
 /* LOCKLESS IO */
 #define OBD_FAIL_LDLM_SET_CONTENTION     0x385
