404ca558e24e0d7f279a787bc1b4277fee58cb3f
James Nunez <james.a.nunez@intel.com>
2013-07-23 18:31:38 -0600

    LU-3485 tests: Add resize2fs testing for OST/MDT filesystems
    Add testing of the MST and OST filesystems with resize2fs.
    Currently, only offline resizing is possible until LU-137
    is finished. Thus, each file system is unmounted and
    e2fsck is run before and after the resize to check for
    errors.
    
    We are restricting the number of OSTs since large-lun
    test 2 run time is based on the number of OSTs and can
    time out if there are 16 OSTs as in the last large-lun
    test results. We've also increased the verbose flags
    to llverfs to track where all the time is being spent.
    
    Test-Parameters: testlist=large-lun ostcount=4 \
    mdsfilesystemtype=ldiskfs mdtfilesystemtype=ldiskfs \
    ostfilesystemtype=ldiskfs
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Change-Id: I7207ce4c4e6771fc128c8b1d02d15d453b8556ae

--- lustre/tests/large-lun.sh
+++ lustre/tests/large-lun.sh
@@ -44,6 +44,21 @@ run_dumpe2fs() {
 	do_facet $facet "$cmd"
 }
 
+# Run resize2fs on MDT or OST device.
+run_resize2fs() {
+	local node=$1
+	local target_dev=$2
+	local extra_opts=$3
+	local resize=$4
+
+	local cmd="resize2fs $extra_opts $target_dev $resize"
+	echo $cmd
+	local rc=0
+	do_node $node $cmd || rc=$?
+
+	return $rc
+}
+
 # Report Lustre filesystem disk space usage and inodes usage of each MDT/OST.
 client_df() {
 	local mnt_pnt=$1
@@ -101,6 +116,17 @@ run_mdsrate() {
 	fi
 }
 
+# Get the block count of the filesystem.
+get_block_count() {
+	local facet=$1
+	local device=$2
+	local bcount
+
+	bcount=$(do_facet $facet "$DUMPE2FS -h $device 2>&1" |
+		awk '/^Block count:/ {print $3}')
+	echo $bcount
+}
+
 # Run e2fsck on MDS and OST
 do_fsck() {
 	$RUN_FSCK || return
@@ -113,6 +139,52 @@ do_fsck() {
 			error "run e2fsck error"
 	done
 }
+
+# Run resize and e2fsck
+do_resize() {
+	local facet=$1
+	local dev=$2
+
+	stopall
+	run_e2fsck $facet $dev "-y"
+	# Get the original number of blocks for the file system, then resize to
+	# 1) smaller than minimum allowable fs size; expect failure
+	# 2) the maximum number of blocks allowed
+	# 3) the minimum number of blocks allowed
+	# 4) and, if possible, something inbetween the min and max
+	fs_blks=$(get_block_count $facet $dev)
+	run_resize2fs $facet $dev "-p -d 30" "10"
+	[[ $? -ne 1 ]] &&
+		error "resize2fs returned $?, expected 1 on too small resize"
+	run_resize2fs $facet $dev "-p -d 30"
+	[[ $? -ne 0 ]] &&
+		error "resize2fs returned $?, expected 0 for maximum resize"
+	max_blks=$(get_block_count $facet $dev)
+	run_resize2fs $facet $dev "-p -d 30 -M"
+	[[ $? -ne 0 ]] &&
+		error "resize2fs returned $?, expected 0 for minimum resize"
+	min_blks=$(get_block_count $facet $dev)
+
+	if [ $min_blks -ne $max_blks ]; then
+		fcount=$(( ( max_blks - min_blks )/2 + min_blks ))
+		resize_blks=$(run_resize2fs $facet $dev "-d 30 -p" "$fcount" |
+			     awk '/blocks long/ {print $7}')
+		[[ $? -ne 0 ]] &&
+			error "resize2fs returned $?, expected 0 for resize"
+		[[ $resize_blks -ne $fcount ]] &&
+			error "resize2fs returned $resize_blks, expected $fcount"
+	fi
+
+	# Return file system to original size and run e2fsck
+	resize_blks=$(run_resize2fs $facet $dev "-p -d 30" "$fs_blks" |
+		     awk '/blocks long/ {print $7}')
+	[[ $? -ne 0 ]] &&
+		error "resize2fs returned $?, expected 0 for original size resize"
+	[[ $fs_blks -ne $resize_blks ]] &&
+		error "Unable to resize back to original ${fs_blks}. New size $resize_blks"
+	run_e2fsck $facet $dev "-y" || error "run e2fsck error"
+}
+
 ################################## Main Flow ###################################
 trap cleanupall EXIT
 
@@ -148,8 +220,8 @@ test_2 () {
 		# to ensure that the kernel can perform filesystem operations
 		# on the complete device without any errors.
 		log "run llverfs in partial mode on the OST ldiskfs $ostmnt"
-		do_rpc_nodes $(facet_host ost${num}) run_llverfs $ostmnt -vpl \
-			"no" || error "run_llverfs error on ldiskfs"
+		do_rpc_nodes $(facet_host ost${num}) run_llverfs $ostmnt \
+			-vvvpl "no" || error "run_llverfs error on ldiskfs"
 
 		# Unmount the OST.
 		log "unmount the OST $dev"
@@ -174,7 +246,7 @@ test_2 () {
 			# operations on the complete device without any errors.
 			log "run llverfs in full mode on OST ldiskfs $ostmnt"
 			do_rpc_nodes $(facet_host ost${num}) run_llverfs \
-				$ostmnt -vl "no" ||
+				$ostmnt -vvl "no" ||
 				error "run_llverfs error on ldiskfs"
 
 			# Unmount the OST.
@@ -225,7 +297,7 @@ test_4 () {
 	# Run llverfs on the mounted Lustre filesystem both in partial and
 	# full mode to to fill the filesystem and verify the file contents.
 	log "run llverfs in partial mode on the Lustre filesystem $MOUNT"
-	run_llverfs $MOUNT -vp "no" || error "run_llverfs error on lustre"
+	run_llverfs $MOUNT -vvvp "no" || error "run_llverfs error on lustre"
 	client_df $MOUNT
 
 	sync; sleep 5; sync
@@ -247,7 +319,7 @@ test_4 () {
 		done
 
 		log "run llverfs in full mode on the Lustre filesystem $MOUNT"
-		run_llverfs $MOUNT -vl "no" ||
+		run_llverfs $MOUNT -vvvl "no" ||
 			error "run_llverfs error on lustre"
 		client_df $MOUNT
 
@@ -257,6 +329,40 @@ test_4 () {
 	fi
 }
 run_test 4 "run llverfs on lustre filesystem"
+
+test_5 () {
+	[ "$(facet_fstype $SINGLEMDS)" != ldiskfs ] &&
+		skip "Only applicable to ldiskfs-based MDTs" && return
+
+	# Setup the Lustre filesystem and create files
+	setupall
+	local mdshost=$(facet_host $SINGLEMDS)
+	local mdsdev=$(mdsdevname ${SINGLEMDS//mds/})
+
+	test_mkdir -p $DIR/$tdir || error "mkdir $tdir failed"
+	createmany -o $DIR/$tdir/t- 1000 ||
+		error "create files in $tdir/t- failed"
+
+	do_resize $mdshost $mdsdev
+}
+run_test 5 "run resize2fs on MDT"
+
+test_6 () {
+	[ "$(facet_fstype ost${OSTCOUNT})" != ldiskfs ] &&
+		skip "Only applicable to ldiskfs-based OSTs" && return
+
+	# Setup the Lustre filesystem and create files
+	setupall
+	local osthost=$(facet_host ost${OSTCOUNT})
+	local ostdev=$(ostdevname $OSTCOUNT)
+
+	test_mkdir -p $DIR/$tdir || error "mkdir $tdir failed"
+	createmany -o $DIR/$tdir/t- 1000 ||
+		error "create files in $tdir/t- failed"
+
+	do_resize $osthost $ostdev
+}
+run_test 6 "run resize2fs on OST"
 
 complete $SECONDS
 $LARGE_LUN_RESTORE_MOUNT && setupall
