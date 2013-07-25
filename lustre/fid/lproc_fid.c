b7889ce3425083d8324c043d939ffafe1a132ca3
Andreas Dilger <andreas.dilger@intel.com>
2013-07-25 16:27:41 -0600

    LU-3642 seq: make seq_proc_write_common() safer
    Don't allow seq_proc_write_common() to specify arbitrary ranges,
    since this can permanently corrupt the sequence controller and
    or server.  That would allow duplicate FID allocation, or possibly
    prevent any new files to be created or servers to be added to the
    filesystem.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I89da0223a52ce8bec787b1d5099402c84274291a

--- lustre/fid/lproc_fid.c
+++ lustre/fid/lproc_fid.c
@@ -56,35 +56,35 @@
  * Note: this function is only used for testing, it is no safe for production
  * use.
  */
-static int
-seq_proc_write_common(struct file *file, const char *buffer,
-                      unsigned long count, void *data,
-                      struct lu_seq_range *range)
+static int seq_proc_write_common(struct file *file, const char *buffer,
+				 unsigned long count, void *data,
+				 struct lu_seq_range *range)
 {
-	struct lu_seq_range tmp;
+	struct lu_seq_range tmp = { 0, };
 	int rc;
 	ENTRY;
 
 	LASSERT(range != NULL);
 
-        rc = sscanf(buffer, "[%llx - %llx]\n",
-                    (long long unsigned *)&tmp.lsr_start,
-                    (long long unsigned *)&tmp.lsr_end);
-	if (rc != 2 || !range_is_sane(&tmp) || range_is_zero(&tmp))
+	/* of the form "[0x0000000240000400 - 0x000000028000400):0:0" */
+	rc = sscanf(buffer, "[%llx - %llx]\n",
+		    (long long unsigned *)&tmp.lsr_start,
+		    (long long unsigned *)&tmp.lsr_end);
+	if (!range_is_sane(&tmp) || range_is_zero(&tmp) ||
+	    tmp.lsr_start < range->lsr_start || tmp.lsr_end > range->lsr_end)
 		RETURN(-EINVAL);
 	*range = tmp;
-        RETURN(0);
+	RETURN(0);
 }
 
-static int
-seq_proc_read_common(char *page, char **start, off_t off,
-                     int count, int *eof, void *data,
-                     struct lu_seq_range *range)
+static int seq_proc_read_common(char *page, char **start, off_t off,
+				int count, int *eof, void *data,
+				struct lu_seq_range *range)
 {
 	int rc;
 	ENTRY;
 
-        *eof = 1;
+	*eof = 1;
 	rc = snprintf(page, count, "["LPX64" - "LPX64"]:%x:%s\n",
 			PRANGE(range));
 	RETURN(rc);
