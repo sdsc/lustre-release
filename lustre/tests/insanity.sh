849eac29874d4b752c5e7ad0a5346809d4d46dca
Nathaniel Clark <nathaniel.l.clark@intel.com>
2013-07-25 09:32:11 -0400

    LU-4121 tests: Enable all tests waiting for LU-2059 to land
    With a fix for LU-2059 landed, all tests that were excepted for
    skipped can now be enabled.
    
    Test-Parameters: osscount=2 mdscount=2 mdsfilesystemtype=zfs   mdtfilesystemtype=zfs ostfilesystemtype=zfs
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: I3e27a7583c857d416ef3a0bd2d5ee74814975def

--- lustre/tests/insanity.sh
+++ lustre/tests/insanity.sh
@@ -173,9 +173,6 @@ run_test 0 "Fail all nodes, independently"
 test_1() {
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 
-	[ "$(facet_fstype mds2)" = "zfs" ] &&
-		skip "LU-2059: no local config for ZFS MDTs" && return
-
 	clients_up
 
 	shutdown_facet mds1
@@ -211,9 +208,6 @@ run_test 1 "MDS/MDS failure"
 test_2() {
 	echo "Verify Lustre filesystem is up and running"
 	[ -z "$(mounted_lustre_filesystems)" ] && error "Lustre is not running"
-
-	[ "$(facet_fstype ost1)" = "zfs" ] &&
-		skip "LU-2059: no local config for ZFS OSTs" && return
 
     clients_up
 
@@ -288,9 +282,6 @@ run_test 3  "Thirdb Failure Mode: MDS/CLIENT `date`"
 ############### Fourth Failure Mode ###############
 test_4() {
 	echo "Fourth Failure Mode: OST/MDS `date`"
-
-	[ "$(facet_fstype ost1)" = "zfs" ] &&
-		skip "LU-2059: no local config for ZFS OSTs" && return
 
     #OST Portion
     shutdown_facet ost1
