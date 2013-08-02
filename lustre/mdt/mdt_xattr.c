6f77edee9d43a14580dce615212a4fb47eae5e8c
Andrew Perepechko <andrew_perepechko@xyratex.com>
2013-08-02 21:05:55 +0400

    LU-3669 xattr: separate ACL and XATTR caches
    This patch separates ACL and XATTR caches, so that
    when updating an ACL only LOOKUP lock is needed and
    when updating another XATTR only XATTR lock is needed.
    
    This patch also reverts XATTR cache support for setxattr
    because client performing REINT under even PR lock
    can deadlock if an active server operation (like unlink)
    attempts to cancel all locks, and setxattr has to wait
    for it (MDC max-in-flight is 1).
    
    This patch also drops mot_xattr_sem because there is
    no use case for FLXATTRLS with locking.
    
    Signed-off-by: Andrew Perepechko <andrew_perepechko@xyratex.com>
    Change-Id: I53f63597d9ecc85bcca0d2b35cda48762b2187bb

--- lustre/mdt/mdt_xattr.c
+++ lustre/mdt/mdt_xattr.c
@@ -187,8 +187,6 @@ int mdt_getxattr(struct mdt_thread_info *info)
         if (rc)
                 RETURN(err_serious(rc));
 
-	down_read(&info->mti_object->mot_xattr_sem);
-
         next = mdt_object_child(info->mti_object);
 
         if (info->mti_body->valid & OBD_MD_FLRMTRGETFACL) {
@@ -258,6 +256,18 @@ int mdt_getxattr(struct mdt_thread_info *info)
 		for (b = buf->lb_buf;
 		     b < (char *)buf->lb_buf + eadatasize;
 		     b += strlen(b) + 1, v += rc) {
+			/* Filter out ACL ACCESS since it is cached separately */
+			if (!strcmp(b, XATTR_NAME_ACL_ACCESS)) {
+				/* Remove posix_acl_access from the list, but
+				 * exit early if it's the last attribute
+				 */
+				eadatasize -= sizeof(XATTR_NAME_ACL_ACCESS);
+				if (eadatasize - (b - (char *)buf->lb_buf) == 0)
+					break;
+				memmove(b, b + sizeof(XATTR_NAME_ACL_ACCESS),
+					eadatasize - (b - (char *)buf->lb_buf));
+			}
+
 			buf2.lb_buf = v;
 			rc = mdt_getxattr_one(info, b, next, &buf2, med, uc);
 			if (rc < 0)
@@ -272,19 +282,17 @@ int mdt_getxattr(struct mdt_thread_info *info)
 		repbody->max_mdsize = eavallens;
 
 		req_capsule_shrink(info->mti_pill, &RMF_EAVALS,
-					eavallen, RCL_SERVER);
+				   eavallen, RCL_SERVER);
 		req_capsule_shrink(info->mti_pill, &RMF_EAVALS_LENS,
-					eavallens * sizeof(__u32), RCL_SERVER);
+				   eavallens * sizeof(__u32), RCL_SERVER);
 		req_capsule_shrink(info->mti_pill, &RMF_EADATA,
-					eadatasize, RCL_SERVER);
+				   eadatasize, RCL_SERVER);
 		rc = eadatasize;
 	} else
 		LBUG();
 
 	EXIT;
 out:
-	up_read(&info->mti_object->mot_xattr_sem);
-
 	if (rc >= 0) {
 		mdt_counter_incr(req, LPROC_MDT_GETXATTR);
 		repbody->eadatasize = rc;
@@ -366,6 +374,9 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
 
         CDEBUG(D_INODE, "setxattr for "DFID"\n", PFID(rr->rr_fid1));
 
+	if (info->mti_dlm_req)
+		ldlm_request_cancel(req, info->mti_dlm_req, 0);
+
         if (OBD_FAIL_CHECK(OBD_FAIL_MDS_SETXATTR))
                 RETURN(err_serious(-ENOMEM));
 
@@ -416,10 +427,9 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
 	/* We need revoke both LOOKUP|PERM lock here, see mdt_attr_set. */
         if (!strcmp(xattr_name, XATTR_NAME_ACL_ACCESS))
 		lockpart |= MDS_INODELOCK_PERM | MDS_INODELOCK_LOOKUP;
-
 	/* We need to take the lock on behalf of old clients so that newer
 	 * clients flush their xattr caches */
-	if (!(valid & OBD_MD_FLXATTRLOCKED))
+	else
 		lockpart |= MDS_INODELOCK_XATTR;
 
         lh = &info->mti_lh[MDT_LH_PARENT];
@@ -429,8 +439,6 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
         obj = mdt_object_find_lock(info, rr->rr_fid1, lh, lockpart);
         if (IS_ERR(obj))
                 GOTO(out, rc =  PTR_ERR(obj));
-
-	down_write(&obj->mot_xattr_sem);
 
         info->mti_mos = obj;
         rc = mdt_version_get_check_save(info, obj, 0);
@@ -502,7 +510,6 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
 
         EXIT;
 out_unlock:
-	up_write(&obj->mot_xattr_sem);
         mdt_object_unlock_put(info, obj, lh, rc);
         if (unlikely(new_xattr != NULL))
                 lustre_posix_acl_xattr_free(new_xattr, xattr_len);
