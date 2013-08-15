f6888eb0a40b5156b71c96cd49941ef21958c5eb
Jian Yu <jian.yu@intel.com>
2013-08-15 18:23:00 +0800

    LU-0000 tests: perform interop tests between b2_4 and b2_1
    This is for test only.
    
    Test-Parameters: fortestonly \
    envdefinitions=SLOW=yes,ENABLE_QUOTA=yes \
    clientdistro=el6 serverdistro=el6 clientarch=x86_64 \
    serverarch=x86_64 \
    serverjob=lustre-b2_1 serverbuildno=215 mdtcount=1 \
    testgroup=full
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: I81cb77fe3b6c163de1912a85877cf119c96298ba

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -384,8 +384,14 @@ static int ll_intent_file_open(struct file *file, void *lmm,
                         opc = LUSTRE_OPC_CREATE;
         }
 
+	/*
+	 * LU-3765
+	 * name and len is packed in request for backward compatibility.
+	 */
 	op_data  = ll_prep_md_op_data(NULL, parent->d_inode,
-				      file->f_dentry->d_inode, NULL, 0,
+				      file->f_dentry->d_inode,
+				      file->f_dentry->d_name.name,
+				      file->f_dentry->d_name.len,
 				      O_RDWR, opc, NULL);
 
         if (IS_ERR(op_data))
