65f531ffcb69dfb6b2ddbc5f7b6c3c57828e1a9f
wang di <di.wang@intel.com>
2013-08-19 08:28:13 -0700

    LU-3535 osp: send all of updates for cross-MDT operation
    For recovery purpose, it needs to send the whole updates of one
    cross-MDT operation will be sent to all of MDTs.
    
    This patch will move update insert to obdclass/dt_update.c, so
    both OSD/OSP can use api to insert update to the buffer attached
    to thandle.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: I51dbf463bca99be5e52ec01ae51a15a9e2a1eaf3

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -902,7 +902,8 @@ out:
 /*
  * Concurrency: shouldn't matter.
  */
-static int osd_trans_stop(const struct lu_env *env, struct thandle *th)
+static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
+			  struct thandle *th)
 {
         int                     rc = 0;
         struct osd_thandle     *oh;
