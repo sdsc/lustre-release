eae01ec0dccd9349f911d349808e895098c252c8
wang di <di.wang@intel.com>
2013-08-21 00:04:43 -0700

    LU-3806 obdclass: add LCT_SERVER_SESSION for server session
    Add LCT_SERVER_SESSION for server session, and separate the
    server session flag from LCT_SESSION, so to avoid allocating
    session info for client stack for each server request, if
    client and server are on the same node.
    
    Signed-off-by: Wang Di <di.wang@intel.com>
    Change-Id: I808c3f58cd7a03ebc166e51fe1e32ea34ae0e3e8

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -5584,7 +5584,7 @@ static int mdt_ctxt_add_dirty_flag(struct lu_env *env,
 	int rc;
 	ENTRY;
 
-	rc = lu_context_init(&ses, LCT_SESSION);
+	rc = lu_context_init(&ses, LCT_SERVER_SESSION);
 	if (rc)
 		RETURN(rc);
 
@@ -6072,7 +6072,7 @@ static int mdt_ioc_child(struct lu_env *env, struct mdt_device *mdt,
         int rc;
         ENTRY;
 
-        rc = lu_context_init(&ioctl_session, LCT_SESSION);
+        rc = lu_context_init(&ioctl_session, LCT_SERVER_SESSION);
         if (rc)
                 RETURN(rc);
         ioctl_session.lc_thread = (struct ptlrpc_thread *)cfs_current();
