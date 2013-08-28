35d8ef1ce235b05bbfa8f4422f3dcc726c07ab0a
Oleg Drokin <oleg.drokin@intel.com>
2013-08-28 13:20:43 -0400

    LU-3027 clio: Do not shrink sublock at cancel
    Shrinking sublock at ldlm lock cancel time means whoever happened
    to attach to this lock just before will reenqueue the wrong lock.
    
    Change-Id: I8f2de683812621fb2f8d761cf2aceebc12868d75
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/lov/lovsub_lock.c
+++ lustre/lov/lovsub_lock.c
@@ -421,9 +421,7 @@ static void lovsub_lock_delete(const struct lu_env *env,
                 cfs_list_for_each_entry_safe(scan, temp,
                                              &sub->lss_parents, lll_list) {
                         lov     = scan->lll_super;
-                        subdata = &lov->lls_sub[scan->lll_idx];
                         lovsub_parent_lock(env, lov);
-                        subdata->sub_got = subdata->sub_descr;
                         lov_lock_unlink(env, scan, sub);
                         restart = lovsub_lock_delete_one(env, child, lov);
                         lovsub_parent_unlock(env, lov);
