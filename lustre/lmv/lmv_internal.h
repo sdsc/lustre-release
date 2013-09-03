c118f8bbd3bfcfa2e880cd8699991c9369a2ec01
James Simmons <uja.ornl@gmail.com>
2013-09-03 11:29:16 -0400

    LU-3319 procfs: move lmv proc handling over to seq_file
    In order to support 3.10+ kernels for clients we adapt
    the lmv proc handling to using seq_files.
    
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Change-Id: I34f58c3a484ee79b41b59d4a60f6a21726373152

--- lustre/lmv/lmv_internal.h
+++ lustre/lmv/lmv_internal.h
@@ -147,12 +147,7 @@ struct lmv_tgt_desc
 		struct lu_fid *fid);
 /* lproc_lmv.c */
 #ifdef LPROCFS
-void lprocfs_lmv_init_vars(struct lprocfs_static_vars *lvars);
-#else
-static inline void lprocfs_lmv_init_vars(struct lprocfs_static_vars *lvars)
-{
-        memset(lvars, 0, sizeof(*lvars));
-}
+void lprocfs_lmv_init_vars(struct obd_device *obd);
 #endif
 extern struct file_operations lmv_proc_target_fops;
 
