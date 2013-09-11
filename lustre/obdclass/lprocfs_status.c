1abccc4b65e277f49cf60387875bb944eda94d66
Bobi Jam <bobijam.xu@intel.com>
2013-09-11 11:23:46 +0800

    LU-3916 lprocfs: export lprocfs setup race
    Protect releasing export's old exp_nid_stats.
    
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Change-Id: I6a468845809c89beec50d71841238ad188e566ae

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -1725,10 +1725,12 @@ int lprocfs_exp_setup(struct obd_export *exp, lnet_nid_t *nid, int *newnid)
 
         /* we need to release old stats because lprocfs_exp_cleanup() hasn't
          * been and will never be called. */
+        spin_lock(&exp->exp_lock);
         if (exp->exp_nid_stats != NULL) {
                 nidstat_putref(exp->exp_nid_stats);
                 exp->exp_nid_stats = NULL;
         }
+        spin_unlock(&exp->exp_lock);
 
         /* Return -EALREADY here so that we know that the /proc
          * entry already has been created */
