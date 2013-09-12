8d10638f5afbef58f23a829e5ecca1354e33b727
Wei Liu <wei3.liu@intel.com>
2013-09-11 17:05:05 -0700

    LU-3915 obdclass: revert 5049
    revert 5049 to see whether LU-3915 still
    exists.
    
    Change-Id: I3846e819b89dccd4019211fdc87cf0bc4881213e
    Test-Parameters: fortestonly clientdistro=fc18 testlist=conf-sanity
    Signed-off-by: Wei Liu <wei3.liu@intel.com>

--- lustre/obdclass/local_storage.h
+++ lustre/obdclass/local_storage.h
@@ -29,8 +29,6 @@
  *
  * Author: Mikhail Pershin <mike.pershin@intel.com>
  */
-#ifndef __LOCAL_STORAGE_H
-#define __LOCAL_STORAGE_H
 
 #include <dt_object.h>
 #include <obd.h>
@@ -89,4 +87,3 @@ struct los_ondisk {
 
 #define LOS_MAGIC	0xdecafbee
 
-#endif
