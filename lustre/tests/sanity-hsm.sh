5718e8ff00e5bc88bdea25ed0e0fd0ea9661dbf0
Bruno Faccini <bruno.faccini@intel.com>
2013-09-23 16:34:25 +0200

    LU-3864 lov: return minimal FIEMAP for released files
    Since st_blocks = NULL is returned for released files, FIEMAP
    should at least return a minimal mapping to make users aware
    that file contains data but it is not immediately available.
    This will make coreutils and tools such tar happy and have
    them presume file is sparse.
    
    Also, add a new test_228 in sanity-hsm to verify it works
    for "[cp,tar] --sparse" commands.
    
    Also fix a LBUG ("lov_fiemap()) ASSERTION( fm_local ) failed")
    likely to occur when no-object/ENOMEM conditions and also now
    when released.
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Change-Id: Idc59f755d407fd34e7095898c68b4aceb5bf9c28

--- lustre/tests/sanity-hsm.sh
+++ lustre/tests/sanity-hsm.sh
@@ -2675,6 +2675,45 @@ test_227() {
 }
 run_test 227 "changelog when explicit setting of HSM flags"
 
+test_228() {
+	# test needs a running copytool
+	copytool_setup
+
+	dd if=/dev/urandom of=$DIR/$tfile bs=1M count=1 conv=sync ||
+		error "creating $DIR/$tfile"
+	$LFS hsm_archive --archive $HSM_ARCHIVE_NUMBER $DIR/$tfile
+	wait_request_state $(path2fid $DIR/$tfile) ARCHIVE SUCCEED
+
+	$LFS hsm_release $DIR/$tfile
+	check_hsm_flags $DIR/$tfile "0x0000000d"
+
+	# verify st_blocks == Null
+	# note this may not be the case if file has large ACL or file layout
+	[ $(stat -c "%b" $DIR/$tfile) -eq "0" ] || error "wrong block number"
+
+	mkdir $DIR/$tdir
+	# only newer versions of cp detect sparse files by stat/FIEMAP
+	# (LU-2580)
+	cp --sparse=auto $DIR/$tfile $DIR/$tfile.2 ||
+		error "copying $DIR/$tfile"
+	cmp $DIR/$tfile $DIR/$tfile.2 || error "comparing copied $DIR/$tfile"
+
+	$LFS hsm_release $DIR/$tfile
+	check_hsm_flags $DIR/$tfile "0x0000000d"
+
+	# verify st_blocks == Null
+	# note this may not be the case if file has large ACL or file layout
+	[ $(stat -c "%b" $DIR/$tfile) -eq "0" ] || error "wrong block number"
+
+	tar cf - --sparse $DIR/$tfile | tar xvf - -C $DIR/$tdir ||
+		error "tar failed"
+	cmp $DIR/$tfile $DIR/$tdir/$DIR/$tfile ||
+		error "comparing untarred $DIR/$tfile"
+
+	copytool_cleanup
+}
+run_test 228 "On released file, return extend to FIEMAP. For [cp,tar] --sparse"
+
 test_250() {
 	# test needs a running copytool
 	copytool_setup
