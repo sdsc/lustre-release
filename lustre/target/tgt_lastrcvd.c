sdsc/lustre-release

commit 4f5830f769f9cc9bacfcdfee9de5962324fb5c81

LU-3467 target: move OUT to the unified target code
- Move OUT handler to the unified target code, so it can be
  used by both MDS and OST.
- Make OFD able to handle OUT requests.
- Rename MDS_MDS_PORTAL to the OUT_PORTAL and MDS_OUT_... defines
  just OUT_... since they are independent from MDS now.

Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
Change-Id: I3d595f18245f1db2619b7d15f94e4b61e882f114
Reviewed-on: http://review.whamcloud.com/6763
Tested-by: Hudson
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

lustre/target/tgt_lastrcvd.c
@@ -684,3 +684,149 @@ int tgt_client_del(const struct lu_env *env, struct obd_export *exp)
 	RETURN(rc);
 }
 EXPORT_SYMBOL(tgt_client_del);
+
+/*
+ * last_rcvd & last_committed update callbacks
+ */
+int tgt_last_rcvd_update(const struct lu_env *env, struct lu_target *tgt,
+			 struct dt_object *obj, __u64 opdata,
+			 struct thandle *th, struct ptlrpc_request *req)
+{
+	struct tgt_thread_info	*tti = tgt_th_info(env);
+	struct tg_export_data	*ted;
+	__u64			*transno_p;
+	int			 rc = 0;
+	bool			 lw_client, update = false;
+
+	ENTRY;
+
+	/* that can be OUT target and we need tgt_session_info */
+	if (req == NULL) {
+		struct tgt_session_info	*tsi = tgt_ses_info(env);
+
+		req = tgt_ses_req(tsi);
+		if (req == NULL) /* echo client case */
+			RETURN(0);
+	}
+
+	ted = &req->rq_export->exp_target_data;
+
+	lw_client = exp_connect_flags(req->rq_export) & OBD_CONNECT_LIGHTWEIGHT;
+
+	tti->tti_transno = lustre_msg_get_transno(req->rq_reqmsg);
+	spin_lock(&tgt->lut_translock);
+	if (th->th_result != 0) {
+		if (tti->tti_transno != 0) {
+			CERROR("%s: replay transno "LPU64" failed: rc = %d\n",
+			       tgt_name(tgt), tti->tti_transno, th->th_result);
+		}
+	} else if (tti->tti_transno == 0) {
+		tti->tti_transno = ++tgt->lut_last_transno;
+	} else {
+		/* should be replay */
+		if (tti->tti_transno > tgt->lut_last_transno)
+			tgt->lut_last_transno = tti->tti_transno;
+	}
+	spin_unlock(&tgt->lut_translock);
+
+	/** VBR: set new versions */
+	if (th->th_result == 0 && obj != NULL)
+		dt_version_set(env, obj, tti->tti_transno, th);
+
+	/* filling reply data */
+	CDEBUG(D_INODE, "transno = "LPU64", last_committed = "LPU64"\n",
+	       tti->tti_transno, tgt->lut_obd->obd_last_committed);
+
+	req->rq_transno = tti->tti_transno;
+	lustre_msg_set_transno(req->rq_repmsg, tti->tti_transno);
+
+	/* if can't add callback, do sync write */
+	th->th_sync |= !!tgt_last_commit_cb_add(th, tgt, req->rq_export,
+						tti->tti_transno);
+
+	if (lw_client) {
+		/* All operations performed by LW clients are synchronous and
+		 * we store the committed transno in the last_rcvd header */
+		spin_lock(&tgt->lut_translock);
+		if (tti->tti_transno > tgt->lut_lsd.lsd_last_transno) {
+			tgt->lut_lsd.lsd_last_transno = tti->tti_transno;
+			update = true;
+		}
+		spin_unlock(&tgt->lut_translock);
+		/* Although lightweight (LW) connections have no slot in
+		 * last_rcvd, we still want to maintain the in-memory
+		 * lsd_client_data structure in order to properly handle reply
+		 * reconstruction. */
+	} else if (ted->ted_lr_off <= 0) {
+		CERROR("%s: client idx %d has offset %lld\n",
+		       tgt_name(tgt), ted->ted_lr_idx, ted->ted_lr_off);
+		RETURN(-EINVAL);
+	}
+
+	/* if the export has already been disconnected, we have no last_rcvd
+	 * slot, update server data with latest transno then */
+	if (ted->ted_lcd == NULL) {
+		CWARN("commit transaction for disconnected client %s: rc %d\n",
+		      req->rq_export->exp_client_uuid.uuid, rc);
+		GOTO(srv_update, rc = 0);
+	}
+
+	mutex_lock(&ted->ted_lcd_lock);
+	LASSERT(ergo(tti->tti_transno == 0, th->th_result != 0));
+	if (lustre_msg_get_opc(req->rq_reqmsg) == MDS_CLOSE ||
+	    lustre_msg_get_opc(req->rq_reqmsg) == MDS_DONE_WRITING) {
+		transno_p = &ted->ted_lcd->lcd_last_close_transno;
+		ted->ted_lcd->lcd_last_close_xid = req->rq_xid;
+		ted->ted_lcd->lcd_last_close_result = th->th_result;
+	} else {
+		/* VBR: save versions in last_rcvd for reconstruct. */
+		__u64 *pre_versions = lustre_msg_get_versions(req->rq_reqmsg);
+
+		if (pre_versions) {
+			ted->ted_lcd->lcd_pre_versions[0] = pre_versions[0];
+			ted->ted_lcd->lcd_pre_versions[1] = pre_versions[1];
+			ted->ted_lcd->lcd_pre_versions[2] = pre_versions[2];
+			ted->ted_lcd->lcd_pre_versions[3] = pre_versions[3];
+		}
+		transno_p = &ted->ted_lcd->lcd_last_transno;
+		ted->ted_lcd->lcd_last_xid = req->rq_xid;
+		ted->ted_lcd->lcd_last_result = th->th_result;
+		/* XXX: lcd_last_data is __u32 but intent_dispostion is __u64,
+		 * see struct ldlm_reply->lock_policy_res1; */
+		ted->ted_lcd->lcd_last_data = opdata;
+	}
+
+	/* Update transno in slot only if non-zero number, i.e. no errors */
+	if (likely(tti->tti_transno != 0)) {
+		if (*transno_p > tti->tti_transno) {
+			CERROR("%s: trying to overwrite bigger transno:"
+			       "on-disk: "LPU64", new: "LPU64" replay: %d. "
+			       "see LU-617.\n", tgt_name(tgt), *transno_p,
+			       tti->tti_transno, req_is_replay(req));
+			if (req_is_replay(req)) {
+				spin_lock(&req->rq_export->exp_lock);
+				req->rq_export->exp_vbr_failed = 1;
+				spin_unlock(&req->rq_export->exp_lock);
+			}
+			mutex_unlock(&ted->ted_lcd_lock);
+			RETURN(req_is_replay(req) ? -EOVERFLOW : 0);
+		}
+		*transno_p = tti->tti_transno;
+	}
+
+	if (!lw_client) {
+		tti->tti_off = ted->ted_lr_off;
+		rc = tgt_client_data_write(env, tgt, ted->ted_lcd, &tti->tti_off, th);
+		if (rc < 0) {
+			mutex_unlock(&ted->ted_lcd_lock);
+			RETURN(rc);
+		}
+	}
+	mutex_unlock(&ted->ted_lcd_lock);
+	EXIT;
+srv_update:
+	if (update)
+		rc = tgt_server_data_write(env, tgt, th);
+	return rc;
+}
+EXPORT_SYMBOL(tgt_last_rcvd_update);

lustre/include/lu_target.h
@@ -39,6 +39,7 @@
 
 #include <dt_object.h>
 #include <lustre_export.h>
+#include <lustre_update.h>
 #include <lustre_disk.h>
 
 struct lu_target {
@@ -177,6 +178,12 @@ static inline __u64 tgt_conn_flags(struct tgt_session_info *tsi)
 	return exp_connect_flags(tsi->tsi_exp);
 }
 
+static inline int req_is_replay(struct ptlrpc_request *req)
+{
+	LASSERT(req->rq_reqmsg);
+	return !!(lustre_msg_get_flags(req->rq_reqmsg) & MSG_REPLAY);
+}
+
 /* target/tgt_handler.c */
 int tgt_request_handle(struct ptlrpc_request *req);
 char *tgt_name(struct lu_target *tgt);
@@ -199,6 +206,12 @@ int tgt_llog_prev_block(struct tgt_session_info *tsi);
 int tgt_sec_ctx_init(struct tgt_session_info *tsi);
 int tgt_sec_ctx_init_cont(struct tgt_session_info *tsi);
 int tgt_sec_ctx_fini(struct tgt_session_info *tsi);
+
+extern struct tgt_handler tgt_sec_ctx_handlers[];
+extern struct tgt_handler tgt_obd_handlers[];
+extern struct tgt_handler tgt_dlm_handlers[];
+extern struct tgt_handler tgt_llog_handlers[];
+extern struct tgt_handler tgt_out_handlers[];
 
 typedef void (*tgt_cb_t)(struct lu_target *lut, __u64 transno,
 			 void *data, int err);
@@ -229,8 +242,13 @@ int tgt_client_data_write(const struct lu_env *env, struct lu_target *tg,
 int tgt_server_data_read(const struct lu_env *env, struct lu_target *tg);
 int tgt_server_data_write(const struct lu_env *env, struct lu_target *tg,
 			  struct thandle *th);
-int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg, int sync);
-int tgt_truncate_last_rcvd(const struct lu_env *env, struct lu_target *tg, loff_t off);
+int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg,
+			   int sync);
+int tgt_truncate_last_rcvd(const struct lu_env *env, struct lu_target *tg,
+			   loff_t off);
+int tgt_last_rcvd_update(const struct lu_env *env, struct lu_target *tgt,
+			 struct dt_object *obj, __u64 opdata,
+			 struct thandle *th, struct ptlrpc_request *req);
 
 enum {
 	ESERIOUS = 0x0001000
@@ -268,46 +286,77 @@ static inline int is_serious(int rc)
 	.th_version	= version					\
 }
 
+/* MDT Request with a format known in advance */
+#define TGT_MDT_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(MDS_FIRST_OPC, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MDS_VERSION)
+/* Request with a format we do not yet know */
+#define TGT_MDT_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(MDS_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_MDS_VERSION)
+
 /* MGS request with a format known in advance */
 #define TGT_MGS_HDL(flags, name, fn)					\
-	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
-			   LUSTRE_MGS_VERSION)
+	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MGS_VERSION)
 #define TGT_MGS_HDL_VAR(flags, name, fn)				\
-	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, NULL,	\
-			   LUSTRE_MGS_VERSION)
+	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_MGS_VERSION)
 
 /*
  * OBD handler macros and generic functions.
  */
 #define TGT_OBD_HDL(flags, name, fn)					\
-	TGT_RPC_HANDLER(OBD_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
-			   LUSTRE_OBD_VERSION)
+	TGT_RPC_HANDLER(OBD_FIRST_OPC, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_OBD_VERSION)
+#define TGT_OBD_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(OBD_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_OBD_VERSION)
 
 /*
  * DLM handler macros and generic functions.
  */
 #define TGT_DLM_HDL_VAR(flags, name, fn)				\
-	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, NULL,	\
-			   LUSTRE_DLM_VERSION)
+	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_DLM_VERSION)
 #define TGT_DLM_HDL(flags, name, fn)					\
-	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
-			   LUSTRE_DLM_VERSION)
+	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_DLM_VERSION)
 
 /*
  * LLOG handler macros and generic functions.
  */
 #define TGT_LLOG_HDL_VAR(flags, name, fn)				\
-	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, NULL,	\
-			   LUSTRE_LOG_VERSION)
+	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_LOG_VERSION)
 #define TGT_LLOG_HDL(flags, name, fn)					\
-	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
-			   LUSTRE_LOG_VERSION)
+	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_LOG_VERSION)
 
 /*
  * Sec context handler macros and generic functions.
  */
 #define TGT_SEC_HDL_VAR(flags, name, fn)				\
-	TGT_RPC_HANDLER(SEC_FIRST_OPC, flags, name, fn, NULL,	\
-			   LUSTRE_OBD_VERSION)
+	TGT_RPC_HANDLER(SEC_FIRST_OPC, flags, name, fn, NULL,		\
+			LUSTRE_OBD_VERSION)
+
+#define TGT_QUOTA_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(QUOTA_DQACQ, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MDS_VERSION)
+
+/* Sequence service handlers */
+#define TGT_SEQ_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(SEQ_QUERY, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MDS_VERSION)
+
+/* FID Location Database handlers */
+#define TGT_FLD_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(FLD_QUERY, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MDS_VERSION)
+
+/* Request with a format known in advance */
+#define TGT_UPDATE_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(UPDATE_OBJ, flags, name, fn, &RQF_ ## name,	\
+			LUSTRE_MDS_VERSION)
 
 #endif /* __LUSTRE_LU_TARGET_H */

lustre/include/lustre/lustre_idl.h
@@ -131,8 +131,7 @@
 //#define PTLBD_BULK_PORTAL              21
 #define MDS_SETATTR_PORTAL             22
 #define MDS_READPAGE_PORTAL            23
-#define MDS_MDS_PORTAL                 24
-
+#define OUT_PORTAL			24
 #define MGC_REPLY_PORTAL               25
 #define MGS_REQUEST_PORTAL             26
 #define MGS_REPLY_PORTAL               27

lustre/include/lustre_net.h
@@ -380,8 +380,8 @@
  * include linkea (4K maxim), together with other updates, we set it to 9K:
  * lustre_msg + ptlrpc_body + UPDATE_BUF_SIZE (8K)
  */
-#define MDS_OUT_MAXREQSIZE	(9 * 1024)
-#define MDS_OUT_MAXREPSIZE	MDS_MAXREPSIZE
+#define OUT_MAXREQSIZE	(9 * 1024)
+#define OUT_MAXREPSIZE	MDS_MAXREPSIZE
 
 /** MDS_BUFSIZE = max_reqsize (w/o LOV EA) + max sptlrpc payload size */
 #define MDS_BUFSIZE		max(MDS_MAXREQSIZE + SPTLRPC_MAX_PAYLOAD, \
@@ -407,11 +407,11 @@
 				    160 * 1024)
 
 /**
- * MDS_OUT_BUFSIZE = max_out_reqsize + max sptlrpc payload (~1K) which is
+ * OUT_BUFSIZE = max_out_reqsize + max sptlrpc payload (~1K) which is
  * about 10K, for the same reason as MDS_REG_BUFSIZE, we also give some
  * extra bytes to each request buffer to improve buffer utilization rate.
   */
-#define MDS_OUT_BUFSIZE		max(MDS_OUT_MAXREQSIZE + SPTLRPC_MAX_PAYLOAD, \
+#define OUT_BUFSIZE		max(OUT_MAXREQSIZE + SPTLRPC_MAX_PAYLOAD, \
 				    24 * 1024)
 
 /** FLD_MAXREQSIZE == lustre_msg + __u32 padding + ptlrpc_body + opc */

lustre/include/obd_target.h
@@ -73,11 +73,12 @@ struct echo_obd {
 };
 
 struct ost_obd {
-	struct ptlrpc_service	       *ost_service;
-	struct ptlrpc_service	       *ost_create_service;
-	struct ptlrpc_service	       *ost_io_service;
-	struct ptlrpc_service	       *ost_seq_service;
-	struct mutex			ost_health_mutex;
+	struct ptlrpc_service	*ost_service;
+	struct ptlrpc_service	*ost_create_service;
+	struct ptlrpc_service	*ost_io_service;
+	struct ptlrpc_service	*ost_seq_service;
+	struct ptlrpc_service	*ost_out_service;
+	struct mutex		 ost_health_mutex;
 };
 
 #endif /* __OBD_TARGET_H */

lustre/ldlm/ldlm_lib.c
@@ -1062,6 +1062,9 @@ dont_check_exports:
                         rc = obd_connect(req->rq_svc_thread->t_env,
                                          &export, target, &cluuid, data,
                                          client_nid);
+			if (mds_conn && OBD_FAIL_CHECK(OBD_FAIL_TGT_RCVG_FLAG))
+				lustre_msg_add_op_flags(req->rq_repmsg,
+						MSG_CONNECT_RECOVERING);
                         if (rc == 0)
                                 conn.cookie = export->exp_handle.h_cookie;
                 }

lustre/lod/lod_lov.c
@@ -247,7 +247,7 @@ int lod_add_device(const struct lu_env *env, struct lod_device *lod,
 		spin_lock(&imp->imp_lock);
 		imp->imp_server_timeout = 1;
 		spin_unlock(&imp->imp_lock);
-		imp->imp_client->cli_request_portal = MDS_MDS_PORTAL;
+		imp->imp_client->cli_request_portal = OUT_PORTAL;
 		CDEBUG(D_OTHER, "%s: Set 'mds' portal and timeout\n",
 		      obd->obd_name);
 		ltd = &lod->lod_mdt_descs;

lustre/mdc/mdc_request.c
@@ -2173,15 +2173,6 @@ int mdc_set_info_async(const struct lu_env *env,
                 sptlrpc_import_flush_my_ctx(imp);
                 RETURN(0);
         }
-        if (KEY_IS(KEY_MDS_CONN)) {
-                /* mds-mds import */
-		spin_lock(&imp->imp_lock);
-		imp->imp_server_timeout = 1;
-		spin_unlock(&imp->imp_lock);
-                imp->imp_client->cli_request_portal = MDS_MDS_PORTAL;
-                CDEBUG(D_OTHER, "%s: timeout / 2\n", exp->exp_obd->obd_name);
-                RETURN(0);
-        }
         if (KEY_IS(KEY_CHANGELOG_CLEAR)) {
                 rc = do_set_info_async(imp, MDS_SET_INFO, LUSTRE_MDS_VERSION,
                                        keylen, key, vallen, val, set);
@@ -2705,27 +2696,6 @@ static int mdc_renew_capa(struct obd_export *exp, struct obd_capa *oc,
         RETURN(0);
 }
 
-static int mdc_connect(const struct lu_env *env,
-                       struct obd_export **exp,
-                       struct obd_device *obd, struct obd_uuid *cluuid,
-                       struct obd_connect_data *data,
-                       void *localdata)
-{
-	struct obd_import *imp = obd->u.cli.cl_import;
-
-	/* mds-mds import features */
-	if (data && (data->ocd_connect_flags & OBD_CONNECT_MDS_MDS)) {
-		spin_lock(&imp->imp_lock);
-		imp->imp_server_timeout = 1;
-		spin_unlock(&imp->imp_lock);
-                imp->imp_client->cli_request_portal = MDS_MDS_PORTAL;
-                CDEBUG(D_OTHER, "%s: Set 'mds' portal and timeout\n",
-                       obd->obd_name);
-        }
-
-        return client_connect_import(env, exp, obd, cluuid, data, NULL);
-}
-
 struct obd_ops mdc_obd_ops = {
         .o_owner            = THIS_MODULE,
         .o_setup            = mdc_setup,
@@ -2733,7 +2703,7 @@ struct obd_ops mdc_obd_ops = {
         .o_cleanup          = mdc_cleanup,
         .o_add_conn         = client_import_add_conn,
         .o_del_conn         = client_import_del_conn,
-        .o_connect          = mdc_connect,
+        .o_connect          = client_connect_import,
         .o_disconnect       = client_disconnect_export,
         .o_iocontrol        = mdc_iocontrol,
         .o_set_info_async   = mdc_set_info_async,

commit 137ef448bc1da88028d150de6b23e55290863656

LU-3749 recovery: save versions from reply into lastrcvd
Fix bug with using the wrong buffer for reply versions.
Versions should be saved from rq_repmsg instead of rq_reqmsg.
This bug was introduced in commit
4f5830f769f9cc9bacfcdfee9de5962324fb5c81.

Test-Parameters: testgroup=review,failover
Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
Change-Id: I6881b5d23ec9349b8fe4e9a21e32a1c8be338182

lustre/target/tgt_lastrcvd.c
@@ -788,7 +788,7 @@ int tgt_last_rcvd_update(const struct lu_env *env, struct lu_target *tgt,
 		ted->ted_lcd->lcd_last_close_result = th->th_result;
 	} else {
 		/* VBR: save versions in last_rcvd for reconstruct. */
-		__u64 *pre_versions = lustre_msg_get_versions(req->rq_reqmsg);
+		__u64 *pre_versions = lustre_msg_get_versions(req->rq_repmsg);
 
 		if (pre_versions) {
 			ted->ted_lcd->lcd_pre_versions[0] = pre_versions[0];