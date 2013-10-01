8ffe3c116e5b1d5944872a9b3cfcd8447f5161d2
wangdi <di.wang@whamcloud.com>
2013-10-01 04:25:49 -0700

    LU-1187 mdd: a few missing stuff in MD stack for DNE.
    1. Assign the index operation for the directory create in declare
    phase, which is needed for creating the object in OUT.
    
    2. Declare dotdot insertion for remote directory creation, so
    the insert update can be packed into RPC and send to the remote
    MDT (out) to be executed.
    
    3. Add hint to the object declare phase, by which OSP can get some
    info during update RPC packing, like parent fid.
    
    4. dt_object_exists should always > 0 inside OSD, in case
    some remote object access OSD accidently.  But in MDD/MDT/LOD layer,
    there might be remote objects as well, so object_exists might
    either > 0 or < 0.
    
    5. Add mulitple catalog files for different MDT.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: Icb359443d9982ee8567af933c5def42dc51a3a7a

--- lustre/mdt/mdt_recovery.c
+++ lustre/mdt/mdt_recovery.c
@@ -497,7 +497,8 @@ static int mdt_txn_start_cb(const struct lu_env *env,
 	if (rc)
 		return rc;
 
-	if (mti->mti_mos != NULL)
+	if (mti->mti_mos != NULL && mdt_object_exists(mti->mti_mos) &&
+	    !mdt_object_remote(mti->mti_mos))
 		rc = dt_declare_version_set(env, mdt_obj2dt(mti->mti_mos), th);
 
 	return rc;
@@ -545,7 +546,9 @@ static int mdt_txn_stop_cb(const struct lu_env *env,
         LASSERT(req != NULL && req->rq_repmsg != NULL);
 
         /** VBR: set new versions */
-        if (txn->th_result == 0 && mti->mti_mos != NULL) {
+	if (txn->th_result == 0 && mti->mti_mos != NULL &&
+	    !mdt_object_remote(mti->mti_mos)) {
+
                 dt_version_set(env, mdt_obj2dt(mti->mti_mos),
                                mti->mti_transno, txn);
                 mti->mti_mos = NULL;
