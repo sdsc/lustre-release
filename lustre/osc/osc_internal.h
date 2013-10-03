2cf76e199740e52b68fe704e0ddf8d60b5c336cb
James Simmons <uja.ornl@gmail.com>
2013-10-03 18:15:38 -0400

    LU-3319 procfs: move configuration layer proc handling over to seq_file
    Various Lustre layers have a configuration abstraction for logs
    which implements a proc interface. For 3.10+ kernel support we
    adapt this proc handling to using seq_files.
    
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Change-Id: Ie507fd88cdf5193624991f7ab32bdc213a44aebc

--- lustre/osc/osc_internal.h
+++ lustre/osc/osc_internal.h
@@ -138,13 +138,10 @@ int osc_setup(struct obd_device *obd, struct lustre_cfg *lcfg);
 
 #ifdef LPROCFS
 int lproc_osc_attach_seqstat(struct obd_device *dev);
-void lprocfs_osc_init_vars(struct lprocfs_static_vars *lvars);
+void lprocfs_osc_init_vars(struct obd_device *obd);
 #else
 static inline int lproc_osc_attach_seqstat(struct obd_device *dev) {return 0;}
-static inline void lprocfs_osc_init_vars(struct lprocfs_static_vars *lvars)
-{
-        memset(lvars, 0, sizeof(*lvars));
-}
+static inline void lprocfs_osc_init_vars(struct obd_device *obd) { }
 #endif
 
 extern struct lu_device_type osc_device_type;
