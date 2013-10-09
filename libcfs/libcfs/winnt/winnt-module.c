5f64622fd5f48deb80f9e606519bfd6366c5ebff
Amir Shehata <amir.shehata@intel.com>
2013-10-09 11:30:04 -0700

    LU-2456 lnet: Dynamic LNet Configuration (DLC) IOCTL changes
    This is the second patch of a set of patches that enables DLC.
    
    This patch changes the IOCTL infrastructure in preparation of
    adding extra IOCTL communication between user and kernel space.
    The changes include:
    - adding a common header to be passed to ioctl infra functions
      instead of passing an exact structure.  This header is meant
      to be included in all structures to be passed through that
      interface.  The IOCTL handler casts this header to a particular
      type that it expects
    - All sanity testing on the past in structure is performed in the
      generic ioctl infrastructure code.
    - All ioctl handlers changed to take the header instead of a
      particular structure type
    
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Change-Id: I8b3e11046a65ae41e2e60b736c38f52798b66c19

--- libcfs/libcfs/winnt/winnt-module.c
+++ libcfs/libcfs/winnt/winnt-module.c
@@ -39,52 +39,52 @@
 
 #define LIBCFS_MINOR 240
 
-int libcfs_ioctl_getdata(char *buf, char *end, void *arg)
+int libcfs_ioctl_data_adjust(struct libcfs_ioctl_data *data)
 {
-        struct libcfs_ioctl_hdr *hdr;
-        struct libcfs_ioctl_data *data;
-        int err;
-        ENTRY;
+	if (libcfs_ioctl_is_invalid(data)) {
+		CERROR("PORTALS: ioctl not correctly formatted\n");
+		RETURN(-EINVAL);
+	}
 
-        hdr = (struct libcfs_ioctl_hdr *)buf;
-        data = (struct libcfs_ioctl_data *)buf;
+	if (data->ioc_inllen1)
+		data->ioc_inlbuf1 = &data->ioc_bulk[0];
 
-	err = copy_from_user(buf, (void *)arg, sizeof(*hdr));
-        if (err)
-                RETURN(err);
+	if (data->ioc_inllen2)
+		data->ioc_inlbuf2 = &data->ioc_bulk[0] +
+			cfs_size_round(data->ioc_inllen1);
 
-        if (hdr->ioc_version != LIBCFS_IOCTL_VERSION) {
-                CERROR("LIBCFS: version mismatch kernel vs application\n");
-                RETURN(-EINVAL);
-        }
+	RETURN(0);
+}
 
-        if (hdr->ioc_len + buf >= end) {
-                CERROR("LIBCFS: user buffer exceeds kernel buffer\n");
-                RETURN(-EINVAL);
-        }
+int libcfs_ioctl_getdata_len(void *arg, __u32 *buf_len)
+{
+	int err;
+	struct libcfs_ioctl_hdr hdr;
+	ENTRY;
 
-        if (hdr->ioc_len < sizeof(struct libcfs_ioctl_data)) {
-                CERROR("LIBCFS: user buffer too small for ioctl\n");
-                RETURN(-EINVAL);
-        }
+	err = cfs_copy_from_user(&hdr, arg, sizeof(hdr));
+	if (err)
+		RETURN(err);
 
-	err = copy_from_user(buf, (void *)arg, hdr->ioc_len);
-        if (err)
-                RETURN(err);
+	if (hdr.ioc_version != LIBCFS_IOCTL_VERSION) {
+		CERROR("PORTALS: version mismatch kernel vs application\n");
+		RETURN(-EINVAL);
+	}
 
-        if (libcfs_ioctl_is_invalid(data)) {
-                CERROR("LIBCFS: ioctl not correctly formatted\n");
-                RETURN(-EINVAL);
-        }
+	*buf_len = hdr.ioc_len;
+	RETURN(0);
+}
 
-        if (data->ioc_inllen1)
-                data->ioc_inlbuf1 = &data->ioc_bulk[0];
+int libcfs_ioctl_getdata(char *buf, __u32 buf_len, void *arg)
+{
+	int err;
+	ENTRY;
 
-        if (data->ioc_inllen2)
-                data->ioc_inlbuf2 = &data->ioc_bulk[0] +
-                        cfs_size_round(data->ioc_inllen1);
+	err = cfs_copy_from_user(buf, (void *)arg, buf_len);
+	if (err)
+		RETURN(err);
 
-        RETURN(0);
+	RETURN(0);
 }
 
 int libcfs_ioctl_popdata(void *arg, void *data, int size)
