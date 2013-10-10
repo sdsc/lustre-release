28817431df7f8667f819116f5a22d5fa7596120c
Thomas Stibor <thomas@stibor.net>
2013-10-10 15:33:33 +0200

    LU-4085 build: gss/krb5 is disabled despite functions found
    The function available check for krb5int_derive_key and
    krb5_derive_key by means of AC_CHECK_LIB($gssapi_lib,...)
    does not set shell variables HAVE_KRB5_DERIVE_KEY,
    HAVE_KRB5INT_DERIVE_KEY so the test
    AS_IF([test "x$HAVE_KRB5INT_DERIVE_KEY" = "x1"
    -o "x$HAVE_KRB5_DERIVE_KEY" = "x1"] gives false despite
    the functions are found. As a side effect this fix
    revealed that calls with real prototypes are required.
    Thus prototypes of krb5_derive_key(...) and
    krb5int_derive_key(...) are provided, and required
    structures from krb5-int.h are copied.
    
    Signed-off-by: Thomas Stibor <thomas@stibor.net>
    Change-Id: I8c4cce5f861c9e4d01071024c4b852a2274c1b40

--- lustre/utils/gss/context.h
+++ lustre/utils/gss/context.h
@@ -31,6 +31,8 @@
 #ifndef _CONTEXT_H_
 #define _CONTEXT_H_
 
+#include <krb5.h>
+
 /* Hopefully big enough to hold any serialized context */
 #define MAX_CTX_LEN 4096
 
@@ -40,10 +42,88 @@
 #define KRB5_CTX_FLAG_ACCEPTOR_SUBKEY   0x00000004
 
 #if HAVE_KRB5INT_DERIVE_KEY
-extern int krb5int_derive_key();
-extern int krb5_k_create_key();
+/* Taken from crypto_int.h */
+enum deriv_alg {
+	DERIVE_RFC3961,		/* RFC 3961 section 5.1 */
+#ifdef CAMELLIA
+	DERIVE_SP800_108_CMAC,	/* NIST SP 800-108 with CMAC as PRF */
+#endif
+};
+
+struct krb5_enc_provider {
+    /* keybytes is the input size to make_key;
+       keylength is the output size */
+    size_t block_size, keybytes, keylength;
+
+    krb5_error_code (*encrypt)(krb5_key key, const krb5_data *cipher_state,
+                               krb5_crypto_iov *data, size_t num_data);
+
+    krb5_error_code (*decrypt)(krb5_key key, const krb5_data *cipher_state,
+                               krb5_crypto_iov *data, size_t num_data);
+
+    /* May be NULL if the cipher is not used for a cbc-mac checksum. */
+    krb5_error_code (*cbc_mac)(krb5_key key, const krb5_crypto_iov *data,
+                               size_t num_data, const krb5_data *ivec,
+                               krb5_data *output);
+
+    krb5_error_code (*init_state)(const krb5_keyblock *key,
+                                  krb5_keyusage keyusage,
+                                  krb5_data *out_state);
+    void (*free_state)(krb5_data *state);
+
+    /* May be NULL if there is no key-derived data cached.  */
+    void (*key_cleanup)(krb5_key key);
+};
+
+extern krb5_error_code krb5int_derive_key(const struct krb5_enc_provider *enc,
+					  krb5_key inkey, krb5_key *outkey,
+					  const krb5_data *in_constant, 
+					  enum deriv_alg alg);
+extern krb5_error_code krb5_k_create_key(krb5_context context, 
+					 const krb5_keyblock *key_data,
+					 krb5_key *out);
 #else /* !HAVE_KRB5INT_DERIVE_KEY */
-extern int krb5_derive_key();
+struct krb5_enc_provider {
+	/* keybytes is the input size to make_key; 
+	   keylength is the output size */
+	size_t block_size, keybytes, keylength;
+
+	/* cipher-state == 0 fresh state thrown away at end */
+	krb5_error_code (*encrypt) (const krb5_keyblock *key,
+				    const krb5_data *cipher_state,
+				    const krb5_data *input,
+				    krb5_data *output);
+
+	krb5_error_code (*decrypt) (const krb5_keyblock *key,
+				    const krb5_data *ivec,
+				    const krb5_data *input,
+				    krb5_data *output);
+
+	krb5_error_code (*make_key) (const krb5_data *randombits,
+				     krb5_keyblock *key);
+
+	krb5_error_code (*init_state) (const krb5_keyblock *key,
+				       krb5_keyusage keyusage, 
+				       krb5_data *out_state);
+	krb5_error_code (*free_state) (krb5_data *state);
+
+	/* In-place encryption/decryption of multiple buffers */
+	krb5_error_code (*encrypt_iov) (const krb5_keyblock *key,
+					const krb5_data *cipher_state,
+					krb5_crypto_iov *data,
+					size_t num_data);
+
+
+	krb5_error_code (*decrypt_iov) (const krb5_keyblock *key,
+					const krb5_data *cipher_state,
+					krb5_crypto_iov *data,
+					size_t num_data);
+};
+
+extern krb5_error_code krb5_derive_key(const struct krb5_enc_provider *enc,
+				       const krb5_keyblock *inkey,
+				       krb5_keyblock *outkey,
+				       const krb5_data *in_constant);
 #endif
 
 int serialize_context_for_kernel(gss_ctx_id_t ctx, gss_buffer_desc *buf,
