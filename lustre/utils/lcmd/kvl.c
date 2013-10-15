22ec2f742ec72253afea796f94403f2f31466d13
Amir Shehata <amir.shehata@intel.com>
2013-10-15 14:47:43 -0700

    LU-2456 lnet: DLC user space Configuration utility
    This is the sixth patch of a set of patches that enables DLC.
    
    This patch adds a configuration utility, lcmd (lustre cmd),
    which provides a way for sys admins to configure LNET
    parameters.  This utility has been added to the /lustre/utils
    directory because it is anticipated that it will be used
    in the future to configure other non LNET parameters.
    
    This utility brings in an infrastructure for command
    parsing based on YAML command description.  The command
    description file will describe the syntax of each command
    and which function to call when each command is matched
    
    The utility uses readline library as its backbone. Therefore,
    the library must be installed on the system running lcmd
    
    The lcmd utility can be started with multiple command line
    parameters including, the location of the library to
    handle commands, the location of the file that describes
    the commands.  This makes the infrastructure flexible to
    be used by any command line utility.
    
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Change-Id: I81e8eef3452af0e7a246ca78213dd1a2d4b59775

---
+++ lustre/utils/lcmd/kvl.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "kvl.h"
+
+kvl_t *kvl_insert_key(kvl_t **kvl, char *key)
+{
+	kvl_t *new_node, *cur;
+
+	if (!kvl) {
+		return NULL;
+	}
+
+	cur = *kvl;
+
+	new_node = calloc(sizeof(char), sizeof(kvl_t));
+	if (!new_node) {
+		return NULL;
+	}
+
+	new_node->key = strdup(key);
+	if (!new_node->key) {
+		free(new_node);
+		return NULL;
+	}
+
+	/* insert as the head of the list */
+	if (!cur) {
+		*kvl = new_node;
+		return new_node;
+	}
+
+	/* insert at the tail of the list */
+	while (cur->next) {
+		cur = cur->next;
+	}
+	cur->next = new_node;
+
+	return new_node;
+}
+
+int kvl_insert_value(kvl_t *node, char *value)
+{
+	if ((!node) || (!value)) {
+		return -1;
+	}
+
+	node->value = strdup(value);
+	if (!node->value) {
+		return -1;
+	}
+
+	return 0;
+}
+
+kvl_t *kvl_find(kvl_t *kvl, char *key)
+{
+	kvl_t *cur = kvl;
+
+	/*
+	 * NOTE: the kvl->key might not necessarily be a complete name, so
+	 * always check the length of the kvl->key against the passed in
+	 * key
+	 */
+	while (cur) {
+		if (strncmp(cur->key, key, strlen(cur->key)) == 0) {
+			return cur;
+		}
+		cur = cur->next;
+	}
+
+	return NULL;
+}
+
+int kvl_convert_to_ui(kvl_t *kvl, unsigned int *v)
+{
+	char *end;
+
+	if (kvl && kvl->value) {
+		*v = strtoul(kvl->value, &end, 0);
+		if (*end != 0) {
+			fprintf(stderr, "Failed to parse [%s:%s]\n",
+				kvl->key, kvl->value);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+void kvl_free(kvl_t *kvl)
+{
+	kvl_t *free_node, *next;
+	next = kvl;
+	while(next) {
+		free_node = next;
+		next = next->next;
+		free(free_node->key);
+		free(free_node->value);
+		free(free_node);
+	}
+}
+
