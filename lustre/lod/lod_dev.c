a513cb3b7040fa646ea13cb9885baa2e77efc309
James Simmons <uja.ornl@gmail.com>
2013-10-18 16:29:35 -0400

    LU-3319 procfs: move lod proc handling to seq_files
    With 3.10 linux kernel and above proc handling now only
    uses struct seq_files. This patch migrates the lod
    layer proc entries over to using seq_files.
    
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Change-Id: Iaa0f617fcd430e91f12afbc0faf6906fd275a7a5

--- lustre/lod/lod_dev.c
+++ lustre/lod/lod_dev.c
@@ -392,12 +392,10 @@ static int lod_process_config(const struct lu_env *env,
 	}
 
 	case LCFG_PARAM: {
-		struct lprocfs_static_vars  v = { 0 };
-		struct obd_device	  *obd = lod2obd(lod);
+		struct obd_device *obd = lod2obd(lod);
 
-		lprocfs_lod_init_vars(&v);
-
-		rc = class_process_proc_param(PARAM_LOV, v.obd_vars, lcfg, obd);
+		rc = class_process_proc_seq_param(PARAM_LOV, obd->obd_vars,
+						  lcfg, obd);
 		if (rc > 0)
 			rc = 0;
 		GOTO(out, rc);
@@ -1002,41 +1000,26 @@ static struct obd_ops lod_obd_device_ops = {
 
 static int __init lod_mod_init(void)
 {
-	struct lprocfs_static_vars  lvars = { 0 };
-	cfs_proc_dir_entry_t       *lov_proc_dir;
 	int			    rc;
 
 	rc = lu_kmem_init(lod_caches);
 	if (rc)
 		return rc;
 
-	lprocfs_lod_init_vars(&lvars);
-
-	rc = class_register_type(&lod_obd_device_ops, NULL, lvars.module_vars,
+	rc = class_register_type(&lod_obd_device_ops, NULL,
+#ifndef HAVE_ONLY_PROCFS_SEQ
+				 NULL,
+#endif
 				 LUSTRE_LOD_NAME, &lod_device_type);
 	if (rc) {
 		lu_kmem_fini(lod_caches);
 		return rc;
 	}
-
-	/* create "lov" entry in procfs for compatibility purposes */
-	lov_proc_dir = lprocfs_srch(proc_lustre_root, "lov");
-	if (lov_proc_dir == NULL) {
-		lov_proc_dir = lprocfs_register("lov", proc_lustre_root,
-						NULL, NULL);
-		if (IS_ERR(lov_proc_dir))
-			CERROR("lod: can't create compat entry \"lov\": %d\n",
-			       (int)PTR_ERR(lov_proc_dir));
-	}
-
 	return rc;
 }
 
 static void __exit lod_mod_exit(void)
 {
-
-	lprocfs_try_remove_proc_entry("lov", proc_lustre_root);
-
 	class_unregister_type(LUSTRE_LOD_NAME);
 	lu_kmem_fini(lod_caches);
 }
