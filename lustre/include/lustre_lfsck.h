ff36f471272ca2967e0eedb724b42c4f10ca3c09
Fan Yong <fan.yong@intel.com>
2013-10-20 23:07:49 +0800

    LU-1267 lfsck: framework (1) for MDT-OST consistency
    1) New LFSCK component - lfsck_layout, its data structure, tracing
       file, lFSCK APIs, internal shared functions, and so on.
    
    2) Extend the existing LFSCK:
    2.1) new flag - LF_INCOMPLETE, for the case of some target (MDT/OST)
         not join the LFSCK or crashed during the LFSCK.
    2.2) New status - LS_PARTIAL, corresponding to above LF_INCOMPLETE,
         if some target (MDT/OST) does not join the LFSCK processing or
         crashed during the LFSCK, when it finished, its status will be
         set as LS_PARTIAL.
    
    3) Control the LFSCK speed by each component itself during the
       second-phase scanning.
    
    4) Avoid lfsck engines to access freed lfsck_instance.
    
    5) Some code cleanup.
    
    Test-Parameters: testlist=sanity-scrub,sanity-lfsck
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I6dc7bb881dc831f6c760be14aac2a066ad75ffec

--- lustre/include/lustre_lfsck.h
+++ lustre/include/lustre_lfsck.h
@@ -55,6 +55,6 @@ int lfsck_stop(const struct lu_env *env, struct dt_device *key,
 int lfsck_get_speed(struct dt_device *key, void *buf, int len);
 int lfsck_set_speed(struct dt_device *key, int val);
 
-int lfsck_dump(struct dt_device *key, void *buf, int len, __u16 type);
+int lfsck_dump(struct dt_device *key, void *buf, int len, enum lfsck_type type);
 
 #endif /* _LUSTRE_LFSCK_H */
