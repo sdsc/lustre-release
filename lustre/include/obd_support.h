d402ab482c762bad5dbbf0023828816689d39be0
Fan Yong <fan.yong@intel.com>
2013-10-22 07:43:31 +0800

    LU-3336 lfsck: recreate the lost MDT-object
    If the MDT-object does not exist but related OST-obejct still
    alive, then when handle the orphan OST-object, the MDT-object
    will be re-created under /.lustre/lost+found/MDTxxxx with the
    given OST-object stripe and owner information. The other part
    of the MDT-obejct layout EA will be empty which may be filled
    by other orphan OST-object(s) as the LFSCK processing.
    
    Test-Parameters: mdscount=2 testlist=sanity-lfsck
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Ia7700f7b2e4e09d6b576d025295fdb5c10ca90e0

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -506,6 +506,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_LFSCK_MULTIPLE_REF	0x1614
 #define OBD_FAIL_LFSCK_INVALID_PFID	0x1615
 #define OBD_FAIL_LFSCK_LOST_STRIPE	0x1616
+#define OBD_FAIL_LFSCK_LOST_MDTOBJ	0x1617
 
 /* UPDATE */
 #define OBD_FAIL_UPDATE_OBJ_NET			0x1700
