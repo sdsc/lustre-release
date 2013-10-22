fcdad90dde4699167cc1a49ff3492dc7171e8e14
yjp <n16490@cray.com>
2013-10-22 09:59:42 -0500

    LU-4129 libcfs: libcfs_debug_dumplog() errors
    This patch will make debug log file name more unique when successive
    calls with no delay to libcfs_debug_dumplog() between calls to limit
    number of log files and prevent error messages.
    
    Signed-off-by: Young Jong Pao <n16490@cray.com>
    Change-Id: Ibfc8d614817cf5130566806b45ff1a74d34ecc6f

--- libcfs/libcfs/debug.c
+++ libcfs/libcfs/debug.c
@@ -229,14 +229,16 @@ libcfs_debug_str2mask(int *mask, const char *str, int is_subsys)
  */
 void libcfs_debug_dumplog_internal(void *arg)
 {
+	static unsigned long log_num;
+
 	DECL_JOURNAL_DATA;
 
 	PUSH_JOURNAL;
 
 	if (strncmp(libcfs_debug_file_path_arr, "NONE", 4) != 0) {
 		snprintf(debug_file_name, sizeof(debug_file_name) - 1,
-			 "%s.%ld." LPLD, libcfs_debug_file_path_arr,
-			 cfs_time_current_sec(), (long_ptr_t)arg);
+			 "%s.%lu.%lu." LPLD, libcfs_debug_file_path_arr,
+			 cfs_time_current_sec(), log_num++, (long_ptr_t)arg);
 		printk(KERN_ALERT "LustreError: dumping log to %s\n",
 		       debug_file_name);
 		cfs_tracefile_dump_all_pages(debug_file_name);
