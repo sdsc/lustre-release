9419f3ccf8d214f892998c3923c17a65b384786e
Jian Yu <jian.yu@intel.com>
2013-11-05 23:29:25 +0800

    LU-3738 tests: improve posix.sh to support BASELINE_FS=zfs
    This patch improves posix.sh to support BASELINE_FS=zfs. It also
    adds create_zpool(), create_zfs() and destroy_zpool() common
    functions into test-framework.sh.
    
    Test-Parameters: envdefinitions=SLOW=yes \
    mdtfilesystemtype=zfs mdsfilesystemtype=zfs ostfilesystemtype=zfs \
    ostcount=2 testlist=posix
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: I7e59181ef5f3926c6a8799ac4f6c775d04350a99

--- lustre/tests/posix.sh
+++ lustre/tests/posix.sh
@@ -8,9 +8,6 @@ init_test_env $@
 . ${CONFIG:=$LUSTRE/tests/cfg/$NAME.sh}
 init_logging
 
-build_test_filter
-check_and_setup_lustre
-
 POSIX_DIR=${POSIX_DIR:-"$LUSTRE/tests/posix"}
 POSIX_SRC=${POSIX_SRC:-"/usr/src/posix"}
 BASELINE_FS=${BASELINE_FS:-"ext4"}
@@ -18,44 +15,76 @@ BASELINE_FS=${BASELINE_FS:-"ext4"}
 # SLES does not support read-write access to an ext4 file system by default
 [[ -e /etc/SuSE-release ]] && BASELINE_FS=ext3
 
-cleanup_loop_dev() {
-    local mnt=$1
-    local dev=$2
-    local file=$3
+if [[ $(facet_fstype $SINGLEMDS) = zfs ]]; then
+	BASELINE_FS=zfs
+	! do_facet $SINGLEMDS "which $ZFS $ZPOOL >/dev/null 2>&1" &&
+		skip_env "need $ZFS and $ZPOOL commands on $SINGLEMDS" && exit 0
 
-    # if we only have 1 arg, we will search for dev
-    if [[ $# = 1 ]]; then
-        dev=$(losetup -a | grep "$mnt" | cut -d: -f1)
-        [[ -n $dev ]] && losetup -d $dev
-    else # we need all args
-        [[ -z $mnt ]] || [[ -z $dev ]] || [[ -z $file ]] &&
-            error "Can't cleanup loop device"
-        umount -f $mnt
-        losetup -d $dev && rm -rf $mnt
-        rm -f $file
-    fi
+	POSIX_ZPOOL=$FSNAME-posix
+	POSIX_ZFS=$POSIX_ZPOOL/${POSIX_ZPOOL##$FSNAME-}
+fi
+
+if [[ -z $SHARED_DIRECTORY ]] || ! check_shared_dir $SHARED_DIRECTORY; then
+	skip_env "SHARED_DIRECTORY should be accessible on all nodes"
+	exit 0
+fi
+
+# cleanup the Lustre filesystem
+cleanupall
+
+build_test_filter
+
+cleanup_loop_dev() {
+	local facet=$1
+	local mnt=$2
+	local dev=$3
+	local file=$4
+
+	# if we only have 2 args, we will search for dev
+	if [[ $# = 2 ]]; then
+		do_facet $facet "
+			dev=\\\$(losetup -a 2>&1) | grep \($mnt\) | cut -d: -f1;
+			[[ -z \\\$dev ]] || losetup -d \\\$dev"
+	else # we need all args
+		[[ -z $mnt ]] || [[ -z $dev ]] || [[ -z $file ]] &&
+			error "Can't cleanup loop device"
+		do_facet $facet "
+			umount $mnt;
+			losetup -d $dev && rm -rf $mnt;
+			rm -f $file"
+	fi
+
+	[[ $BASELINE_FS != zfs ]] || destroy_zpool $facet $POSIX_ZPOOL
 }
 
 setup_loop_dev() {
-	local mnt=$1
-	local dev=$2
-	local file=$3
+	local facet=$1
+	local mnt=$2
+	local dev=$3
+	local file=$4
 	local rc=0
 
-	echo "Make a loop file system with $file on $dev"
-	dd if=/dev/zero of=$file bs=1024k count=500 > /dev/null
-	if ! losetup $dev $file; then
+	echo "Make a loop file system with $file on $dev on $facet"
+	do_facet $facet "dd if=/dev/zero of=$file bs=1024k count=500 >/dev/null"
+	if ! do_facet $facet "losetup $dev $file"; then
 		rc=$?
 		echo "can't set up $dev for $file"
 		return $rc
 	fi
-	if ! eval mkfs.$BASELINE_FS $dev; then
+
+	if [[ $BASELINE_FS = zfs ]]; then
+		create_zpool $facet $POSIX_ZPOOL $dev || return ${PIPESTATUS[0]}
+		create_zfs $facet $POSIX_ZFS || return ${PIPESTATUS[0]}
+		dev=$POSIX_ZFS
+
+	elif ! do_facet $facet "mkfs.$BASELINE_FS $dev"; then
 		rc=$?
 		echo "mkfs.$BASELINE_FS on $dev failed"
 		return $rc
 	fi
-	mkdir -p $mnt
-	if ! mount -t $BASELINE_FS $dev $mnt; then
+
+	do_facet $facet "mkdir -p $mnt" || return ${PIPESTATUS[0]}
+	if ! do_facet $facet "mount -t $BASELINE_FS $dev $mnt"; then
 		rc=$?
 		echo "mount $BASELINE_FS failed"
 		return $rc
@@ -67,53 +96,56 @@ setup_loop_dev() {
 test_1() {
 	local allnodes="$(comma_list $(nodes_list))"
 	local tfile="$TMP/$BASELINE_FS-file"
-	local mntpnt=$POSIX_SRC/$BASELINE_FS
-	local loopbase
+	local mntpnt="$SHARED_DIRECTORY/$TESTSUITE/$BASELINE_FS"
 	local loopdev
 	local rc=0
 
-    # We start at loop1 because posix build uses loop0
-    [ -b /dev/loop/1 ] && loopbase=/dev/loop/
-    [ -b /dev/loop1 ] && loopbase=/dev/loop
-    [ -z "$loopbase" ] && error "/dev/loop/1 and /dev/loop1 gone?"
+	# We start at loop1 because posix build uses loop0
+	loopdev=$(do_facet $SINGLEMDS \
+		"[[ -b /dev/loop/1 ]] && loopbase=/dev/loop/;
+		[[ -b /dev/loop1 ]] && loopbase=/dev/loop;
+		if [[ -n \\\$loopbase ]]; then
+			for i in \\\$(seq 7); do
+				losetup \\\$loopbase\\\$i >/dev/null 2>&1 &&
+					continue;
+				loopdev=\\\$loopbase\\\$i;
+				break;
+			done;
+		fi;
+		echo -n \\\$loopdev")
+	[[ -z $loopdev ]] && error "Can not find loop device"
 
-    for i in `seq 1 7`; do
-        losetup $loopbase$i > /dev/null 2>&1 && continue || true
-        loopdev=$loopbase$i
-        break
-    done
+	if ! setup_loop_dev $SINGLEMDS $mntpnt $loopdev $tfile; then
+		cleanup_loop_dev $SINGLEMDS $mntpnt $loopdev $tfile
+		error "Setup loop device failed"
+	fi
 
-    [ -z "$loopdev" ] && error "Can not find loop device"
+	# copy the source over to baseline filesystem mount point
+	if ! do_facet $SINGLEMDS "cp -af $POSIX_SRC/*.* $mntpnt"; then
+		cleanup_loop_dev $SINGLEMDS $mntpnt $loopdev $tfile
+		error "Copy POSIX test suite failed"
+	fi
 
-    if ! setup_loop_dev $mntpnt $loopdev $tfile; then
-        cleanup_loop_dev "$mntpnt" "$loopdev" "$tfile"
-        error "Setup loop device failed"
-    fi
+	export POSIX_SRC=$mntpnt
+	. $POSIX_DIR/posix.cfg
 
-    # copy the source over to ext mount point
-    if ! cp -af ${POSIX_SRC}/*.* $mntpnt; then
-        cleanup_loop_dev "$mntpnt" "$loopdev" "$tfile"
-        error "Copy POSIX test suite failed"
-    fi
-    export POSIX_SRC=$mntpnt
-    . $POSIX_DIR/posix.cfg
+	setup_posix_users $allnodes
+	if ! setup_posix $SINGLEMDS; then
+		delete_posix_users $allnodes
+		cleanup_loop_dev $SINGLEMDS $POSIX_SRC
+		cleanup_loop_dev $SINGLEMDS $mntpnt $loopdev $tfile
+		error "Setup POSIX test suite failed"
+	fi
 
-    setup_posix_users $allnodes
-    if ! setup_posix; then
-        delete_posix_users $allnodes
-        cleanup_loop_dev "$POSIX_SRC"
-        cleanup_loop_dev "$mntpnt" "$loopdev" "$tfile"
-        error "Setup POSIX test suite failed"
-    fi
+	log "Run POSIX test against lustre filesystem"
+	check_and_setup_lustre
+	run_posix $MOUNT compare ||
+		error_noexit "Run POSIX testsuite on $MOUNT failed"
 
-    log "Run POSIX test against lustre filesystem"
-    run_posix $MOUNT compare || \
-        error_noexit "Run POSIX testsuite on $MOUNT failed"
-
-    [[ -d "$MOUNT/TESTROOT" ]] && rm -fr $MOUNT/TESTROOT
-    delete_posix_users $allnodes
-    cleanup_loop_dev "$POSIX_SRC"
-    cleanup_loop_dev "$mntpnt" "$loopdev" "$tfile"
+	[[ ! -d "$MOUNT/TESTROOT" ]] || rm -fr $MOUNT/TESTROOT
+	delete_posix_users $allnodes
+	cleanup_loop_dev $SINGLEMDS $POSIX_SRC
+	cleanup_loop_dev $SINGLEMDS $mntpnt $loopdev $tfile
 }
 run_test 1 "install, build, run posix on $BASELINE_FS and lustre, then compare"
 
