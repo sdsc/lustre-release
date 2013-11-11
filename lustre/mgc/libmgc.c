b9e744d92858364a093d78a52b39e0fef274e8c5
James Simmons <uja.ornl@gmail.com>
2013-11-11 08:36:42 -0500

    LU-3319 procfs: move configuration layer proc handling over to seq_file
    Various Lustre layers have a configuration abstraction for logs
    which implements a proc interface. For 3.10+ kernel support we
    adapt this proc handling to using seq_files.
    
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Change-Id: Ie507fd88cdf5193624991f7ab32bdc213a44aebc

--- lustre/mgc/libmgc.c
+++ lustre/mgc/libmgc.c
@@ -158,6 +158,6 @@ struct obd_ops mgc_obd_ops = {
 
 int __init mgc_init(void)
 {
-        return class_register_type(&mgc_obd_ops, NULL,
-                                   NULL, LUSTRE_MGC_NAME, NULL);
+	return class_register_type(&mgc_obd_ops, NULL, NULL, LUSTRE_MGC_NAME,
+					NULL);
 }
