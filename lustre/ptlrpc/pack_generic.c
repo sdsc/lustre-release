a917622c1ce973dd1e049b761bd9fd0c5fda5e61
Fan Yong <fan.yong@intel.com>
2013-12-26 00:17:08 +0800

    LU-3336 lfsck: orphan OST-objects iteration (1)
    During the second stage scanning, the LFSCK on the MDT(s) will scan
    the orphan OST-objects via OSP level iteration which fetches remote
    orphan OST-objects information via OBD_IDX_READ RPC, and shares the
    existing framework/functions with others, such as quota.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I67d5d870dbf9b80530f4d61ed1a3e5b5df70b1a0

--- lustre/ptlrpc/pack_generic.c
+++ lustre/ptlrpc/pack_generic.c
@@ -2048,6 +2048,7 @@ void lustre_swab_idx_info(struct idx_info *ii)
 	__swab32s(&ii->ii_magic);
 	__swab32s(&ii->ii_flags);
 	__swab16s(&ii->ii_count);
+	CLASSERT(offsetof(typeof(*ii), ii_pad0) != 0);
 	__swab32s(&ii->ii_attrs);
 	lustre_swab_lu_fid(&ii->ii_fid);
 	__swab64s(&ii->ii_version);
@@ -2055,6 +2056,11 @@ void lustre_swab_idx_info(struct idx_info *ii)
 	__swab64s(&ii->ii_hash_end);
 	__swab16s(&ii->ii_keysize);
 	__swab16s(&ii->ii_recsize);
+	__swab32s(&ii->ii_oid_start);
+	__swab32s(&ii->ii_oid_end);
+	__swab32s(&ii->ii_ver_start);
+	__swab32s(&ii->ii_ver_end);
+	__swab32s(&ii->ii_index);
 }
 
 void lustre_swab_lip_header(struct lu_idxpage *lip)
@@ -2613,3 +2619,12 @@ void lustre_swab_lfsck_reply(struct lfsck_reply *lr)
 	CLASSERT(offsetof(typeof(*lr), lr_padding_2) != 0);
 }
 EXPORT_SYMBOL(lustre_swab_lfsck_reply);
+
+void lustre_swab_orphan_ent(struct lu_orphan_ent *ent)
+{
+	lustre_swab_lu_fid(&ent->loe_key);
+	lustre_swab_lu_fid(&ent->loe_rec.lor_fid);
+	__swab32s(&ent->loe_rec.lor_uid);
+	__swab32s(&ent->loe_rec.lor_gid);
+}
+EXPORT_SYMBOL(lustre_swab_orphan_ent);
