db84ce49ebd52515663fbe73b5fced329dabbc1e
Fan Yong <fan.yong@intel.com>
2013-12-31 17:15:37 +0800

    LU-1267 lfsck: enhance RPCs (3) for MDT-OST consistency
    The LFSCK on the OST uses LFSCK_NOTIFY RPC to notify the LFSCK
    on the MDT about the LFSCK progress for the layout consistency
    verification. And uses the LFSCK_QUERY RPC to query the LFSCK
    status on the MDT.
    
    The LFSCK RPC from OST to MDT is sent via the reserse connection
    from OST-x to MDT-y.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I138fa9b9ad8ab539379f25bb59ec04a1a482fddb

--- lustre/lfsck/lfsck_internal.h
+++ lustre/lfsck/lfsck_internal.h
@@ -269,7 +269,8 @@ struct lfsck_operations {
 				bool init);
 
 	int (*lfsck_prep)(const struct lu_env *env,
-			  struct lfsck_component *com);
+			  struct lfsck_component *com,
+			  struct lfsck_start_param *lsp);
 
 	int (*lfsck_exec_oit)(const struct lu_env *env,
 			      struct lfsck_component *com,
@@ -325,7 +326,8 @@ struct lfsck_tgt_desc {
 	atomic_t	   ltd_ref;
 	__u32              ltd_index;
 	__u32		   ltd_layout_gen;
-	unsigned int	   ltd_dead:1;
+	unsigned int	   ltd_dead:1,
+			   ltd_layout_done:1;
 };
 
 struct lfsck_tgt_desc_idx {
@@ -430,6 +432,7 @@ struct lfsck_instance {
 	void			 *li_out_notify_data;
 	struct dt_device	 *li_next;
 	struct dt_device	 *li_bottom;
+	struct obd_device	 *li_obd;
 	struct ldlm_namespace	 *li_namespace;
 	struct local_oid_storage *li_los;
 	struct lu_fid		  li_local_root_fid;  /* backend root "/" */
@@ -500,9 +503,10 @@ struct lfsck_async_interpret_args {
 };
 
 struct lfsck_thread_args {
-	struct lu_env		 lta_env;
-	struct lfsck_instance	*lta_lfsck;
-	struct lfsck_component	*lta_com;
+	struct lu_env			 lta_env;
+	struct lfsck_instance		*lta_lfsck;
+	struct lfsck_component		*lta_com;
+	struct lfsck_start_param	*lta_lsp;
 };
 
 struct lfsck_thread_info {
@@ -525,6 +529,7 @@ struct lfsck_thread_info {
 	char			lti_key[NAME_MAX + 16];
 	struct lfsck_request	lti_lr;
 	struct lfsck_async_interpret_args lti_laia;
+	struct lfsck_stop	lti_stop;
 };
 
 /* lfsck_lib.c */
@@ -547,12 +552,14 @@ void lfsck_control_speed_by_self(struct lfsck_component *com);
 int lfsck_reset(const struct lu_env *env, struct lfsck_instance *lfsck,
 		bool init);
 struct lfsck_thread_args *lfsck_thread_args_init(struct lfsck_instance *lfsck,
-						 struct lfsck_component *com);
+						 struct lfsck_component *com,
+						 struct lfsck_start_param *lsp);
 void lfsck_thread_args_fini(struct lfsck_thread_args *lta);
 void lfsck_fail(const struct lu_env *env, struct lfsck_instance *lfsck,
 		bool new_checked);
 int lfsck_checkpoint(const struct lu_env *env, struct lfsck_instance *lfsck);
-int lfsck_prep(const struct lu_env *env, struct lfsck_instance *lfsck);
+int lfsck_prep(const struct lu_env *env, struct lfsck_instance *lfsck,
+	       struct lfsck_start_param *lsp);
 int lfsck_exec_oit(const struct lu_env *env, struct lfsck_instance *lfsck,
 		   struct dt_object *obj);
 int lfsck_exec_dir(const struct lu_env *env, struct lfsck_instance *lfsck,
