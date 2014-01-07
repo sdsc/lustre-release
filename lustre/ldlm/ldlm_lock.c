fa2c27bfe7d4d11a593e282a11770680fb32c44f
Bobi Jam <bobijam.xu@intel.com>
2014-01-07 16:58:25 +0800

    LU-4269 ldlm: add ldlm lock debug info
    Print ldlm lock state in log after it's destroyed.
    
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Change-Id: I5b7992e6556b16c9bd91887d2a86fd9f6c3fff8c

--- lustre/ldlm/ldlm_lock.c
+++ lustre/ldlm/ldlm_lock.c
@@ -392,6 +392,7 @@ int ldlm_lock_destroy_internal(struct ldlm_lock *lock)
         ldlm_lock_remove_from_lru(lock);
         class_handle_unhash(&lock->l_handle);
 
+	LDLM_DEBUG(lock, "ldlm lock destroyed");
 #if 0
         /* Wake anyone waiting for this lock */
         /* FIXME: I should probably add yet another flag, instead of using
