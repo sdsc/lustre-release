894b360ba0bd748e824ae7764021f9fd0e793136
Fan Yong <fan.yong@intel.com>
2014-01-24 00:02:37 +0800

    LU-4413 osp: move seq allocation out of osp_import_event
    Because seq allocation(osp_init_pre_fid) might be stuck
    during RPC, move it out of osp_import_event, which is
    inside ptlrpcd_rcv. Otherwise, some other import RPCs(like
    connect req)might be blocked in ptlrpcd_rcv.
    
    To fix random failures in conf-sanity caused by LU-4413,
    copy the patch: Ib4014f8b0088ea3613fa4d53d3e274f5bdfe70c7
    to avoid update/retest the original patch. This patch will
    not be landed, but only for making subsequent LFSCK patches
    to pass Maloo tests.
    
    This patch also contains the fixing for LU-4456 from wangdi:
    
    1. Add extra check for opd_pre in statfs_interpret, in case
    opd_pre has been freed before the callback.
    
    2. switch the sync_fini and pre_fini, so opd_pre will be freed
    after all of the possible access has been stopped.
    
    3. opd_pre_waitq will be accessed in several update threads,
    osp_precreate, osp_statfs_timer_cb, statfs_interrupt, move
    it to osp_device to make sure it is accessiable even after
    osp_pre is freed.
    
    Test-Parameters: fortestonly
    Signed-off-by: wang di <di.wang@intel.com>
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Ic52afd1286dd2a09d6a0cf830eb8f541e82426c8

--- lustre/osp/osp_precreate.c
+++ lustre/osp/osp_precreate.c
@@ -65,12 +65,29 @@ static inline int osp_statfs_need_update(struct osp_device *d)
 				d->opd_statfs_fresh_till);
 }
 
+/*
+ * OSP tries to maintain pool of available objects so that calls to create
+ * objects don't block most of time
+ *
+ * each time OSP gets connected to OST, we should start from precreation cleanup
+ */
+static inline bool osp_precreate_running(struct osp_device *d)
+{
+	return !!(d->opd_pre_thread.t_flags & SVC_RUNNING);
+}
+
+static inline bool osp_precreate_stopped(struct osp_device *d)
+{
+	return !!(d->opd_pre_thread.t_flags & SVC_STOPPED);
+}
+
 static void osp_statfs_timer_cb(unsigned long _d)
 {
 	struct osp_device *d = (struct osp_device *) _d;
 
 	LASSERT(d);
-	wake_up(&d->opd_pre_waitq);
+	if (d->opd_pre != NULL && osp_precreate_running(d))
+		wake_up(&d->opd_pre_waitq);
 }
 
 static int osp_statfs_interpret(const struct lu_env *env,
@@ -108,7 +125,9 @@ static int osp_statfs_interpret(const struct lu_env *env,
 	RETURN(0);
 out:
 	/* couldn't update statfs, try again as soon as possible */
-	wake_up(&d->opd_pre_waitq);
+	if (d->opd_pre != NULL && osp_precreate_running(d))
+		wake_up(&d->opd_pre_waitq);
+
 	if (req->rq_import_generation == imp->imp_generation)
 		CDEBUG(D_CACHE, "%s: couldn't update statfs: rc = %d\n",
 		       d->opd_obd->obd_name, rc);
@@ -176,23 +195,6 @@ void osp_statfs_need_now(struct osp_device *d)
 		cfs_timer_disarm(&d->opd_statfs_timer);
 		wake_up(&d->opd_pre_waitq);
 	}
-}
-
-
-/*
- * OSP tries to maintain pool of available objects so that calls to create
- * objects don't block most of time
- *
- * each time OSP gets connected to OST, we should start from precreation cleanup
- */
-static inline int osp_precreate_running(struct osp_device *d)
-{
-	return !!(d->opd_pre_thread.t_flags & SVC_RUNNING);
-}
-
-static inline int osp_precreate_stopped(struct osp_device *d)
-{
-	return !!(d->opd_pre_thread.t_flags & SVC_STOPPED);
 }
 
 static inline int osp_objs_precreated(const struct lu_env *env,
@@ -812,7 +814,7 @@ out:
 	wake_up(&d->opd_pre_user_waitq);
 }
 
-int osp_init_pre_fid(struct osp_device *osp)
+static int osp_init_pre_fid(struct osp_device *osp)
 {
 	struct lu_env		env;
 	struct osp_thread_info	*osi;
@@ -915,6 +917,16 @@ static int osp_precreate_thread(void *_arg)
 		/* Sigh, fid client is not ready yet */
 		if (d->opd_obd->u.cli.cl_seq->lcs_exp == NULL)
 			continue;
+
+		/* Init fid for osp_precreate if necessary */
+		rc = osp_init_pre_fid(d);
+		if (rc != 0) {
+			class_export_put(d->opd_exp);
+			d->opd_obd->u.cli.cl_seq->lcs_exp = NULL;
+			CERROR("%s: init pre fid error: rc = %d\n",
+			       d->opd_obd->obd_name, rc);
+			continue;
+		}
 
 		osp_statfs_update(d);
 
