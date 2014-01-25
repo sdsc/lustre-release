0413b22d118ae23453c106453f7f725f083cd083
Andreas Dilger <andreas.dilger@intel.com>
2014-01-24 18:16:43 -0700

    LU-4413 ptlrpc: don't try to recover no_recov connection
    If a connection has been stopped with ptlrpc_pinger_del_import() and
    marked obd_no_recov, don't reconnect in ptlrpc_disconnect_import() if
    the import is already disconnected.  Otherwise, without the pinger it
    will just wait there indefinitely for the reconnection that will never
    happen.
    
    Put the obd_no_recov check inside ptlrpc_import_in_recovery() so that
    any threads waiting on the connection to recover would also be broken
    out of their sleep if obd_no_recov is set.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: Icd8041be0ce344add8d67b026353df1b1e0cab07

--- lustre/osp/lwp_dev.c
+++ lustre/osp/lwp_dev.c
@@ -172,7 +172,7 @@ int lwp_disconnect(struct lwp_device *d)
 	(void)ptlrpc_pinger_del_import(imp);
 
 	rc = ptlrpc_disconnect_import(imp, 0);
-	if (rc && rc != -ETIMEDOUT)
+	if (rc != 0)
 		CERROR("%s: can't disconnect: rc = %d\n",
 		       d->lpd_obd->obd_name, rc);
 
