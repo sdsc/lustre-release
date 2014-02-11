d6bd04aaa97368411fb7d7ceb035c00502d7e03d
Joshua Walgenbach <jjw@iu.edu>
2014-02-11 01:36:53 +0100

    LU-3527 nodemap: idmap management functions
    This adds support for uid/gid mapping management to the nodemap
    module. This mapping is added as four rbtrees in the nodemap
    structure for forward and reverse mapping of UIDs and GIDs.
    Postorder traversal for rbtrees have been added for cleanup
    upon nodemap removal.
    
    lctl commands have been added for adding, removing, and testing
    maps.
    
    Unit tests have been added to test the maps in all the relevant
    modes (allow root access, trust client ids).
    
    Signed-off-by: Joshua Walgenbach <jjw@iu.edu>
    Change-Id: I9a8672f2883185a35d3553e5c2e91ae1d2cb094d

--- libcfs/libcfs/nidstrings.c
+++ libcfs/libcfs/nidstrings.c
@@ -1046,6 +1046,7 @@ bool cfs_nidrange_is_contiguous(struct list_head *nidlist)
 
 	return true;
 }
+EXPORT_SYMBOL(cfs_nidrange_is_contiguous);
 
 /**
  * Determines whether an expression list in an num nidrange contains exactly
@@ -1089,6 +1090,7 @@ static bool cfs_num_is_contiguous(struct list_head *nidlist)
 			}
 		}
 	}
+
 	return true;
 }
 
@@ -1145,7 +1147,6 @@ static bool cfs_ip_is_contiguous(struct list_head *nidlist)
 
 	return true;
 }
-EXPORT_SYMBOL(cfs_ip_is_contiguous);
 
 /**
  * Takes a linked list of nidrange expressions, determines the minimum
@@ -1183,6 +1184,7 @@ void cfs_nidrange_find_min_max(struct list_head *nidlist, char *min_nid,
 	snprintf(max_nid, nidstr_length, "%s@%s%d", max_addr_str, lndname,
 		 netnum);
 }
+EXPORT_SYMBOL(cfs_nidrange_find_min_max);
 
 /**
  * Determines the min and max NID values for num LNDs
@@ -1249,7 +1251,6 @@ static void cfs_ip_min_max(struct list_head *nidlist, __u32 *min_nid,
 	if (max_nid != NULL)
 		*max_nid = max_ip_addr;
 }
-EXPORT_SYMBOL(cfs_ip_min_max);
 
 #ifdef __KERNEL__
 
