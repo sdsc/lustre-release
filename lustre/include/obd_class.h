a0fd0936abf2d27f554e59259779b64087880cf3
Gregoire Pichon <gregoire.pichon@bull.net>
2014-02-13 16:33:34 +0100

    LU-1778 llite: fix inconsistencies of root squash feature
    Root squash exhibits inconsistent behaviour on a client when
    enabled. If a file is not cached on the client, then root will get
    a permission denied error when accessing the file. When
    the file has recently been accessed by a regular user and is
    still in cache, root will be able to access the file without error
    because the permission check is only done by the client that
    isn't aware of root squash.
    
    While the only real security benefit from root squash is to deny
    clients access to files owned by root itself, it also makes sense
    to treat file access on the client in a consistent manner
    regardless of whether the file is in cache or not.
    
    This patch adds root squash settings to llite so that client is able
    to apply root squashing when it is relevant.
    
    Configuration of MDT root squash settings will automatically be
    applied to llite config log as well.
    
    Update cfs_str2num_check() routine by removing any modification
    of the specified string parameter. Since string can come from ls_str
    field of a lstr structure, this avoids inconsistent ls_len field.
    
    Signed-off-by: Gregoire Pichon <gregoire.pichon@bull.net>
    Change-Id: Ie52d04424d0937af9fed777a4e4912c90405c19b

--- lustre/include/obd_class.h
+++ lustre/include/obd_class.h
@@ -2300,6 +2300,15 @@ int raw_name2idx(int hashtype, int count, const char *name, int namelen);
 /* prng.c */
 #define ll_generate_random_uuid(uuid_out) cfs_get_random_bytes(uuid_out, sizeof(class_uuid_t))
 
+/* root squash info */
+struct rw_semaphore;
+struct root_squash_info {
+	uid_t			rsi_uid;
+	gid_t			rsi_gid;
+	struct list_head	rsi_nosquash_nids;
+	struct rw_semaphore	rsi_sem;
+};
+
 #ifdef __KERNEL__
 int server_name2index(const char *svname, __u32 *idx, const char **endptr);
 #else
