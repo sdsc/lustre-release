7e2d89982edaad1a7657856832ac7562e678fcbd
Lai Siyao <lai.siyao@intel.com>
2014-03-13 20:10:25 +0800

    LU-3270 statahead: race in start/stop statahead
    When starting statahead thread, it should check whether current
    lli_opendir_key was deauthorized in the mean time by another
    process.
    
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Change-Id: I5bc00ddd1a501b94cdfe7509ddb0e7d922bef624

--- lustre/llite/statahead.c
+++ lustre/llite/statahead.c
@@ -1519,7 +1519,22 @@ static int start_statahead_thread(struct inode *dir, struct dentry *dentry)
 	CDEBUG(D_READA, "start statahead thread: [pid %d] [parent %.*s]\n",
 	       current_pid(), parent->d_name.len, parent->d_name.name);
 
+	/* if another process deauthorized current lli_opendir_key, don't
+	 * start statahead, otherwise the newly spawned statahead thread
+	 * won't be notified to quit. */
+	spin_lock(&lli->lli_sa_lock);
+	if (lli->lli_opendir_key == NULL ||
+	    lli->lli_opendir_pid != current->pid) {
+		spin_unlock(&lli->lli_sa_lock);
+
+		dput(parent);
+		iput(sai->sai_inode);
+		GOTO(out, rc = -EAGAIN);
+	}
 	lli->lli_sai = sai;
+	spin_unlock(&lli->lli_sa_lock);
+
+	atomic_inc(&ll_i2sbi(parent->d_inode)->ll_sa_running);
 
 	rc = PTR_ERR(kthread_run(ll_statahead_thread, parent,
 				 "ll_sa_%u", lli->lli_opendir_pid));
@@ -1527,9 +1542,12 @@ static int start_statahead_thread(struct inode *dir, struct dentry *dentry)
 	if (IS_ERR_VALUE(rc)) {
 		CERROR("can't start ll_sa thread, rc: %d\n", rc);
 		dput(parent);
-		lli->lli_opendir_key = NULL;
+
+		spin_lock(&lli->lli_sa_lock);
 		thread_set_flags(thread, SVC_STOPPED);
 		thread_set_flags(&sai->sai_agl_thread, SVC_STOPPED);
+		spin_unlock(&lli->lli_sa_lock);
+
 		ll_sai_put(sai);
 		LASSERT(lli->lli_sai == NULL);
 		RETURN(-EAGAIN);
@@ -1540,8 +1558,6 @@ static int start_statahead_thread(struct inode *dir, struct dentry *dentry)
 		     &lwi);
 	ll_sai_put(sai);
 
-	atomic_inc(&ll_i2sbi(parent->d_inode)->ll_sa_running);
-
 	/*
 	 * We don't stat-ahead for the first dirent since we are already in
 	 * lookup.
@@ -1551,9 +1567,10 @@ static int start_statahead_thread(struct inode *dir, struct dentry *dentry)
 out:
 	if (sai != NULL)
 		OBD_FREE_PTR(sai);
+
+	/* once we start statahead thread failed, disable statahead so
+	 * subsequent won't waste time to try it. */
 	spin_lock(&lli->lli_sa_lock);
-	lli->lli_opendir_key = NULL;
-	lli->lli_opendir_pid = 0;
 	lli->lli_sa_enabled = 0;
 	spin_unlock(&lli->lli_sa_lock);
 
