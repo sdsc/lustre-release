1d3ad1d77bdb58873d38f10e04f99807f9b30eb8
Fan Yong <fan.yong@intel.com>
2014-03-17 12:06:32 +0800

    LU-4556 tests: speed up sanity-lfsck and sanity-scrub tests
    1) drop unnecessary devices reformat.
    2) drop unnecessary system stop/re-start.
    3) replace 'sleep' with wait_update_facet to avoid idle wait.
    4) drop unnecessary "-p" option for some "mkdir" cases.
    5) replace "touch" with "createmany -m".
    6) other code style changes and cleanup.
    
    Test-Parameters: envdefinitions=SLOW=yes testlist=lfsck-performance
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I047c63b8793fa843fabe1a69b332c42f2f523f68

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -6784,3 +6784,26 @@ free_fd()
         [ $fd -lt $max_fd ] || error "finding free file descriptor failed"
         echo $fd
 }
+
+check_mount_and_prep()
+{
+	is_mounted $MOUNT || setupall
+
+	rm -rf $DIR/[df][0-9]* || error "Fail to cleanup the env!"
+	mkdir $DIR/$tdir || error "Fail to mkdir $DIR/$tdir."
+}
+
+get_precreated()
+{
+	local mdt_idx=$1
+	local ost_idx=$2
+	local mdt_name="MDT$(printf '%04x' $((mdt_idx - 1)))"
+	local ost_name="OST$(printf '%04x' $((ost_idx - 1)))"
+	local proc_path="${FSNAME}-${ost_name}-osc-${mdt_name}"
+	local last_id=$(do_facet mds${mdt_idx} lctl get_param -n \
+			osp.$proc_path.prealloc_last_id)
+	local next_id=$(do_facet mds${mdt_idx} lctl get_param -n \
+			osp.$proc_path.prealloc_next_id)
+
+	echo $((last_id - next_id + 1))
+}
