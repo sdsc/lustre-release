1256cd7f8ff6b1798f2aa95571e644b391d834ea
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2014-03-19 10:28:59 +0400

    LU-0000 osd: remove unused ->do_data_get() method
    in the past the method was used to implement quota, but
    now we have another quota implementation which doesn't
    use this method.
    
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Change-Id: If28fc1fbb17c7603d2bdbac89fd4faf3740654f6

--- lustre/mdd/mdd_internal.h
+++ lustre/mdd/mdd_internal.h
@@ -214,7 +214,6 @@ int mdd_get_md(const struct lu_env *env, struct mdd_object *obj,
                void *md, int *md_size, const char *name);
 int mdd_get_md_locked(const struct lu_env *env, struct mdd_object *obj,
                       void *md, int *md_size, const char *name);
-int mdd_data_get(const struct lu_env *env, struct mdd_object *obj, void **data);
 int mdd_la_get(const struct lu_env *env, struct mdd_object *obj,
                struct lu_attr *la, struct lustre_capa *capa);
 void mdd_flags_xlate(struct mdd_object *obj, __u32 flags);
@@ -621,15 +620,6 @@ static inline int mdd_permission_internal_locked(const struct lu_env *env,
 						enum mdd_object_role role)
 {
 	return __mdd_permission_internal(env, obj, la, mask, role);
-}
-
-static inline int mdo_data_get(const struct lu_env *env,
-                               struct mdd_object *obj,
-                               void **data)
-{
-        struct dt_object *next = mdd_object_child(obj);
-        next->do_ops->do_data_get(env, next, data);
-        return 0;
 }
 
 /* mdd inline func for calling osd_dt_object ops */
