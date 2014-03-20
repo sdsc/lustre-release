fc7d8cd2b05c4c9d90d86689bf5f30ad33696fe4
Wei Liu <wei3.liu@intel.com>
2014-03-20 13:56:53 -0700

    LU-4745 tests: Skip dry-run for OST version older than 2.5.56
    Skip the dry-run testing in test_52 for OST version older than
    2.5.56
    
    Test-Parameters: mdsjob=lustre-b2_5 ossjob=lustre-b2_5 \
    mdsbuildno=42 mdsdistro=el6 mdsarch=x86_64 \
    ossbuildno=42 ossdistro=el6 ossarch=x86_64 \
    clientdistro=el6 clientarch=x86_64 alwaysuploadlogs \
    testlist=conf-sanity
    
    Change-Id: If3b45f6d29b83bce21ed40c67c5e985a8a9fd6db
    Signed-off-by: Wei Liu <wei3.liu@intel.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -3442,9 +3442,11 @@ test_52() {
 	[ $? -eq 0 ] || { error "Unable to move objects"; return 14; }
 
 	# recover objects dry-run
-	echo "ll_recover_lost_found_objs dry_run"
-	do_node $ost1node "ll_recover_lost_found_objs -n -d $ost1mnt/O" ||
-		error "ll_recover_lost_found_objs failed"
+	if [ $(lustre_version_code ost1) -lt $(version_code 2.5.56) ]; then
+		echo "ll_recover_lost_found_objs dry_run"
+		do_node $ost1node "ll_recover_lost_found_objs -n -d $ost1mnt/O" ||
+			error "ll_recover_lost_found_objs failed"
+	fi
 
 	# recover objects
 	echo "ll_recover_lost_found_objs fix run"
