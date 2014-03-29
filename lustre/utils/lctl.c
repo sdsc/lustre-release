c10d33bf6c88a1766bd50e40c52c7ab6017e213f
Fan Yong <fan.yong@intel.com>
2014-03-30 05:13:42 +0800

    LU-4955 lfsck: reset param when the LFSCK is reset
    If "--reset" or "-r" is specified when start the LFSCK manually,
    then all non-specified parameters will be set as default values.
    
    If the former LFSCK instance completed, then when re-trigger the
    LFSCK, it will restart from the beginning, and all non-specified
    parameters will be set as default values.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I5b1d7dd2507ab2f50e152f5a839bf9544a9dc255

--- lustre/utils/lctl.c
+++ lustre/utils/lctl.c
@@ -382,9 +382,9 @@ command_t cmdlist[] = {
 	{"==== LFSCK ====", jt_noop, 0, "LFSCK"},
 	{"lfsck_start", jt_lfsck_start, 0, "start LFSCK\n"
 	 "usage: lfsck_start <-M | --device [MDT,OST]_device>\n"
-	 "		     [-A | --all] [-c | --create_ostobj [swtich]]\n"
-	 "		     [-e | --error error_handle] [-h | --help]\n"
-	 "		     [-n | --dryrun [switch]] [-o | --orphan]\n"
+	 "		     [-A | --all] [-c | --create_ostobj [on | off]]\n"
+	 "		     [-e | --error <continue | abort>] [-h | --help]\n"
+	 "		     [-n | --dryrun [on | off]] [-o | --orphan]\n"
 	 "		     [-r | --reset] [-s | --speed speed_limit]\n"
 	 "		     [-t | --type lfsck_type[,lfsck_type...]]\n"
 	 "		     [-w | --windows win_size]"},
