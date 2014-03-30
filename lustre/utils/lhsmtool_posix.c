eba1d61dd66c75c8bf5a839fb1b1b42c88bba1a7
Bruno Faccini <bruno.faccini@intel.com>
2014-03-30 17:44:43 +0200

    LU-4438 utils: handle ct_setup() errors
    Problem has been encountered causing sanity-hsm/test_402 errors
    because copytool threads were found still alive after MDCs
    deactivation. This appears to be due to no ct_setup() error
    handling with continuing+unnecessary startup actions to occur.
    Also, now that copytool will exit earlier, HSMTOOL_NOERROR variable
    has been added to allow errors in copytool_setup().
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Change-Id: I108ea679faf2731f2a64fb08d2f8fba9db01c71f

--- lustre/utils/lhsmtool_posix.c
+++ lustre/utils/lhsmtool_posix.c
@@ -1846,7 +1846,7 @@ static int ct_setup(void)
 	if (rc < 0) {
 		CT_ERROR(rc, "cannot find a Lustre filesystem mounted at '%s'",
 			 opt.o_mnt);
-		return -rc;
+		return rc;
 	}
 
 	return rc;
@@ -1879,7 +1879,9 @@ int main(int argc, char **argv)
 		return -rc;
 	}
 
-	ct_setup();
+	rc = ct_setup();
+	if (rc < 0)
+		goto error_cleanup;
 
 	switch (opt.o_action) {
 	case CA_IMPORT:
@@ -1901,6 +1903,7 @@ int main(int argc, char **argv)
 			 " rc=%d (%s)", err_major, err_minor, rc,
 			 strerror(-rc));
 
+error_cleanup:
 	ct_cleanup();
 
 	return -rc;
