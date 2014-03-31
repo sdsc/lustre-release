0000837c43c893b9b48b8789c5eb6fcdff375398
wang di <di.wang@intel.com>
2014-03-31 07:46:39 -0700

    LU-4715 lmv: skip . and .. for slave stripe
    1. skip . and .. for slave stripe.
    2. If user buffer is full in ll_dir_read, remember
    the last unsuccessful entry in op_hash_offset, so in next
    read, it will locate entry by >= hash_offset,  instead
    of >.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: I36f17ae8955053a95b0f45714c5ff756931ce4fa

--- lustre/llite/llite_internal.h
+++ lustre/llite/llite_internal.h
@@ -959,6 +959,7 @@ extern struct export_operations lustre_export_operations;
 __u32 get_uuid2int(const char *name, int len);
 struct inode *search_inode_for_lustre(struct super_block *sb,
 				      const struct lu_fid *fid);
+int ll_dir_get_parent_fid(struct inode *dir, struct lu_fid *parent_fid);
 
 /* llite/special.c */
 extern struct inode_operations ll_special_inode_operations;
