5763d1751236e3c550b18b798082bd686386dfe3
Henri Doreau <henri.doreau@cea.fr>
2014-04-18 16:17:01 +0200

    LU-4840 lfs: Use file lease to implement migration
    Implement non-blocking migration based on exclusive open instead of
    group lock. Implemented exclusive close operation to atomically put
    a lease, swap two layouts and close a file. This allows race-free
    migrations.
    
    Make the caller responsible for retrying on failure (EBUSY, EAGAIN)
    in non-blocking mode.
    
    In blocking mode, allow applications to trigger layout swaps using a
    grouplock they already own, to prevent race conditions between the
    actual data copy and the layout swap. Updated lfs accordingly. File
    leases are also taken in blocking mode, so that lfs migrate can issue
    a warning if an application attempts to open a file that is being
    migrated and gets blocked.
    
    Timestamps (atime/mtime) are set from userland, after the layout swap
    is performed, to prevent conflicts with the grouplock.
    
    lli_trunc_sem is taken/released in the vvp_io layer, under the DLM
    lock. This re-ordering fixes the original issue between truncate and
    migrate.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: Ie420e1998cae03928ae24834070c16642e8cd3b9

--- lustre/mdt/mdt_lib.c
+++ lustre/mdt/mdt_lib.c
@@ -1016,6 +1016,11 @@ static int mdt_setattr_unpack_rec(struct mdt_thread_info *info)
 	else
 		ma->ma_attr_flags &= ~MDS_HSM_RELEASE;
 
+	if (rec->sa_bias & MDS_CLOSE_LAYOUT_SWAP)
+		ma->ma_attr_flags |= MDS_CLOSE_LAYOUT_SWAP;
+	else
+		ma->ma_attr_flags &= ~MDS_CLOSE_LAYOUT_SWAP;
+
 	if (req_capsule_get_size(pill, &RMF_CAPA1, RCL_CLIENT))
 		mdt_set_capainfo(info, 0, rr->rr_fid1,
 				 req_capsule_client_get(pill, &RMF_CAPA1));
@@ -1092,16 +1097,16 @@ static int mdt_setattr_unpack(struct mdt_thread_info *info)
 	RETURN(rc);
 }
 
-static int mdt_hsm_release_unpack(struct mdt_thread_info *info)
+static int mdt_intent_close_unpack(struct mdt_thread_info *info)
 {
 	struct md_attr          *ma = &info->mti_attr;
-	struct req_capsule      *pill = info->mti_pill;
+	struct req_capsule	*pill = info->mti_pill;
 	ENTRY;
 
-	if (!(ma->ma_attr_flags & MDS_HSM_RELEASE))
+	if (!(ma->ma_attr_flags & (MDS_HSM_RELEASE | MDS_CLOSE_LAYOUT_SWAP)))
 		RETURN(0);
 
-	req_capsule_extend(pill, &RQF_MDS_RELEASE_CLOSE);
+	req_capsule_extend(pill, &RQF_MDS_INTENT_CLOSE);
 
 	if (!(req_capsule_has_field(pill, &RMF_CLOSE_DATA, RCL_CLIENT) &&
 	    req_capsule_field_present(pill, &RMF_CLOSE_DATA, RCL_CLIENT)))
@@ -1123,7 +1128,7 @@ int mdt_close_unpack(struct mdt_thread_info *info)
 	if (rc)
 		RETURN(rc);
 
-	rc = mdt_hsm_release_unpack(info);
+	rc = mdt_intent_close_unpack(info);
 	if (rc)
 		RETURN(rc);
 
