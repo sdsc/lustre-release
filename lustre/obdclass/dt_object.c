c70d6ebeaa2150db0c495e9a9cf631c161f70771
Hongchao Zhang <hongchao.zhang@intel.com>
2014-04-20 07:54:22 +0800

    LU-4758 osd: return 0 if no more dirents
    In osd_ldiskfs_it_load, if there is no more subdirectory,
    it should return 0 instead of +1, otherwise the caller could
    use the "osd_it_ea" and cause some unpredictable problem.
    
    Change-Id: If6e284cd32a2d220b5d21266396dd2a7e7c8970e
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>

--- lustre/obdclass/dt_object.c
+++ lustre/obdclass/dt_object.c
@@ -806,6 +806,8 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 		 *     requested---skip it.
 		 *     - or not positioned at all (is in IAM_IT_SKEWED
 		 *     state)---position it on the next item.
+		 *     - or reach the end, "next" should be called to set rc to
+		 *     proper value, otherwise the invalid "it" will be used.
 		 */
 		rc = iops->next(env, it);
 	} else if (rc > 0) {
