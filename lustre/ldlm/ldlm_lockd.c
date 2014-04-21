db2505b5d2edd3003aff3b63e6d1e674b765e65e
Oleg Drokin <oleg.drokin@intel.com>
2014-04-21 14:02:32 -0400

    LU-4801 ldlm: Do not call ldlm_reprocess_all from enqueue
    ldlm_handle_enqueue0 seems to be always calling ldlm_reprocess_all
    for the lock resource hich iseems to be excessive and a waste of
    resources.
    Move the call to the cancel case for cancel on block locks.
    
    Change-Id: Ie380497866b83a81cb099777bb4c94936c6bd91a
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/ldlm/ldlm_lockd.c
+++ lustre/ldlm/ldlm_lockd.c
@@ -1353,6 +1353,7 @@ existing_lock:
                         if (dlm_rep->lock_flags & LDLM_FL_CANCEL_ON_BLOCK) {
                                 unlock_res_and_lock(lock);
                                 ldlm_lock_cancel(lock);
+				ldlm_reprocess_all(lock->l_resource);
                                 lock_res_and_lock(lock);
                         } else
                                 ldlm_add_waiting_lock(lock);
@@ -1394,8 +1395,6 @@ existing_lock:
                         rc = err;
         }
 
-        /* The LOCK_CHANGED code in ldlm_lock_enqueue depends on this
-         * ldlm_reprocess_all.  If this moves, revisit that code. -phil */
 	if (!IS_ERR(lock)) {
 		LDLM_DEBUG(lock, "server-side enqueue handler, sending reply"
 			   "(err=%d, rc=%d)", err, rc);
@@ -1433,9 +1432,6 @@ existing_lock:
                         ldlm_lock_destroy_nolock(lock);
                         unlock_res_and_lock(lock);
                 }
-
-                if (!err && dlm_req->lock_desc.l_resource.lr_type != LDLM_FLOCK)
-                        ldlm_reprocess_all(lock->l_resource);
 
                 LDLM_LOCK_RELEASE(lock);
         }
