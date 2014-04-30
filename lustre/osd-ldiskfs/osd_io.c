b2b2208c7b3ff3657dc8dea98896a515568bc2a9
Jeff Mahoney <jeffm@suse.com>
2014-04-29 21:05:29 -0400

    LU-4416 osd-ldiskfs: add support for ext4_map_blocks
    Linux 2.6.35 added ext4_map_blocks but kept the extent walking
    interfaces around for a while. Since Linux 3.8, those interfaces
    have significantly changed and the right fix is to use
    ext4_map_blocks.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Change-Id: I6f0d0280f8cfd7fe3717f3afea45af6eb740e4fa

--- lustre/osd-ldiskfs/osd_io.c
+++ lustre/osd-ldiskfs/osd_io.c
@@ -508,6 +508,7 @@ static int osd_bufs_put(const struct lu_env *env, struct dt_object *dt,
 #define ldiskfs_ext_pblock(ex) ext_pblock((ex))
 #endif
 
+#ifndef HAVE_EXT4_MAP_BLOCKS
 struct bpointers {
 	unsigned long *blocks;
 	unsigned long start;
@@ -751,6 +752,69 @@ int osd_ldiskfs_map_nblocks(struct inode *inode, unsigned long block,
 
 	return err;
 }
+#else
+int osd_ldiskfs_map_nblocks(struct inode *inode, unsigned long block,
+			    unsigned long num, unsigned long *blocks,
+			    int create)
+{
+	handle_t *handle = NULL;
+	int flags = create ? LDISKFS_GET_BLOCKS_CREATE : 0;
+	int ret;
+
+	CDEBUG(D_OTHER, "blocks %lu-%lu requested for inode %u\n",
+	       block, block + num - 1, (unsigned) inode->i_ino);
+
+	if (create) {
+		int credits = ldiskfs_chunk_trans_blocks(inode, num);
+		handle = osd_journal_start(inode, LDISKFS_HT_MAP_BLOCKS,
+					   credits);
+		if (IS_ERR(handle))
+			return PTR_ERR(handle);
+	}
+
+	while (num) {
+		int i;
+		struct ldiskfs_map_blocks map;
+
+		map.m_lblk = block;
+		map.m_len = num;
+		map.m_flags = 0;
+		map.m_pblk = 0;
+
+		ret = ldiskfs_map_blocks(handle, inode, &map, flags);
+
+		if (ret < 0)
+			break;
+
+		if (map.m_flags & LDISKFS_MAP_MAPPED) {
+			for (i = 0; i < ret; i++)
+				blocks[i] = map.m_pblk + i;
+		} else if (ret == 0) {
+			*blocks = 0;
+			ret = 1;
+		} else {
+			/* ret > 0 w/o MAP_MAPPED should not happen */
+			WARN_ON(1);
+		}
+
+		num -= ret;
+		blocks += ret;
+		block += ret;
+
+		ret = 0;
+	}
+
+	if (handle) {
+		int err = ldiskfs_journal_stop(handle);
+		if (!ret && err)
+			ret = err;
+	}
+
+	return ret;
+}
+#define osd_ldiskfs_map_inode_pages(inode, page, pages, blocks, create) \
+	osd_ldiskfs_map_ext_inode_pages(inode, page, pages, blocks, create)
+#endif
 
 int osd_ldiskfs_map_ext_inode_pages(struct inode *inode, struct page **page,
 				    int pages, unsigned long *blocks,
@@ -801,6 +865,7 @@ cleanup:
 	return rc;
 }
 
+#ifndef HAVE_EXT4_MAP_BLOCKS
 int osd_ldiskfs_map_bm_inode_pages(struct inode *inode, struct page **page,
 				   int pages, unsigned long *blocks,
 				   int create)
@@ -837,6 +902,7 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode, struct page **page,
 
 	return rc;
 }
+#endif
 
 static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
                           struct niobuf_local *lnb, int npages)
