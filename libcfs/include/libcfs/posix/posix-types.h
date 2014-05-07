4595e3d8fe02cb3ab671c6d4db8f52eae116c205
Andreas Dilger <andreas.dilger@intel.com>
2014-05-07 15:57:00 -0600

    LU-5018 libcfs: don't depend on configure for lustre_user.h
    Don't depend on configure to detect the presence of <linux/types.h>
    in order to define the __u8, __u16, __u32, and __u64 types.  Instead,
    just define these types directly if <linux/types.h> is not included
    already.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: Ida0915fe1b5829f3378e459c782844cf9df060e6

--- libcfs/include/libcfs/posix/posix-types.h
+++ libcfs/include/libcfs/posix/posix-types.h
@@ -42,15 +42,13 @@
 #ifndef _LUSTRE_POSIX_TYPES_H
 #define _LUSTRE_POSIX_TYPES_H
 
-#ifdef HAVE_LINUX_TYPES_H
-# include <linux/types.h>
-#endif
 #include <stdbool.h> /* for bool */
 #ifndef HAVE_UMODE_T
 typedef unsigned short umode_t;
 #else
 #endif
 
+#ifndef _LINUX_TYPES_H
 /*
  * __xx is ok: it doesn't pollute the POSIX namespace. Use these in the
  * header files exported to user space
@@ -95,7 +93,8 @@ typedef __signed__ long long __s64;
 # ifndef HAVE___U64
 typedef unsigned long long __u64;
 # endif
-#endif
+#endif /* defined(__powerpc64__) && !defined(__KERNEL__) */
+#endif /* _LINUX_TYPES_H */
 
 /* long integer with size equal to pointer */
 typedef unsigned long ulong_ptr_t;
