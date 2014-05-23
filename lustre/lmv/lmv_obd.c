18eed8294d07e7c83b224c1575f340a8d1564956
wang di <di.wang@intel.com>
2014-05-23 01:44:33 -0700

    LU-3531 mdt: delete striped directory
    Add delete striped directory, it includes
    
    1. enable sync log between MDTs, so slave objects will
    be delete by unlink log, which is similar as deleting ost
    object.
    
    2. retireve layout information of striped directory on MDT,
    then lock all of the slave objects before unlink.
    
    3. remove a few unnecessary cfs_size_round, because update_size
    and update_buf_size already do size_around inside.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: Ib461156bbff9e416ac9d2500a0b9491427542340

--- lustre/lmv/lmv_obd.c
+++ lustre/lmv/lmv_obd.c
@@ -1765,12 +1765,31 @@ static int lmv_close(struct obd_export *exp, struct md_op_data *op_data,
 }
 
 struct lmv_tgt_desc
+*lmv_locate_target_by_name(struct lmv_obd *lmv, struct lmv_stripe_md *lsm,
+			   const char *name, int namelen, struct lu_fid *fid,
+			   mdsno_t *mds)
+{
+        struct lmv_tgt_desc	*tgt;
+	int			sidx;
+
+	sidx = raw_name2idx(lsm->lsm_hash_type, lsm->lsm_count,
+			    name, namelen);
+
+	LASSERT(sidx < lsm->lsm_count);
+	*fid = lsm->lsm_oinfo[sidx].lmo_fid;
+	*mds = lsm->lsm_oinfo[sidx].lmo_mds;
+	tgt = lmv_get_target(lmv, *mds);
+
+	CDEBUG(D_INFO, "locate on idx %d mds %u\n", sidx, *mds);
+	return tgt;
+}
+
+struct lmv_tgt_desc
 *lmv_locate_mds(struct lmv_obd *lmv, struct md_op_data *op_data,
                 struct lu_fid *fid)
 {
 	struct lmv_stripe_md	*lsm = op_data->op_mea1;
         struct lmv_tgt_desc	*tgt;
-        int			sidx;
 
         if (!lsm || lsm->lsm_count <= 1 || op_data->op_namelen == 0) {
                 tgt = lmv_find_target(lmv, fid);
@@ -1781,17 +1800,9 @@ struct lmv_tgt_desc
                 return tgt;
         }
 
-	sidx = raw_name2idx(lsm->lsm_hash_type, lsm->lsm_count,
-			    op_data->op_name, op_data->op_namelen);
-
-	LASSERT(sidx < lsm->lsm_count);
-	*fid = lsm->lsm_oinfo[sidx].lmo_fid;
-	op_data->op_mds = lsm->lsm_oinfo[sidx].lmo_mds;
-	tgt = lmv_get_target(lmv, lsm->lsm_oinfo[sidx].lmo_mds);
-
-	CDEBUG(D_INFO, "locate on idx %d mds %u \n", sidx,
-	       op_data->op_mds);
-	return tgt;
+	return lmv_locate_target_by_name(lmv, lsm, op_data->op_name,
+					 op_data->op_namelen, fid,
+					 &op_data->op_mds);
 }
 
 int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
@@ -2438,10 +2449,25 @@ static int lmv_unlink(struct obd_export *exp, struct md_op_data *op_data,
 		RETURN(rc);
 retry:
 	/* Send unlink requests to the MDT where the child is located */
-	if (likely(!fid_is_zero(&op_data->op_fid2)))
-		tgt = lmv_locate_mds(lmv, op_data, &op_data->op_fid2);
-	else
+	if (likely(!fid_is_zero(&op_data->op_fid2))) {
+                tgt = lmv_find_target(lmv, &op_data->op_fid2);
+		if (IS_ERR(tgt))
+			RETURN(PTR_ERR(tgt));
+		/* For striped directory, it needs to located the parent
+		 * as well */
+		if (op_data->op_mea1 != NULL &&
+		    op_data->op_mea1->lsm_count > 1) {
+			LASSERT(op_data->op_name != NULL &&
+				op_data->op_namelen != 0);
+			lmv_locate_target_by_name(lmv, op_data->op_mea1,
+							 op_data->op_name,
+							 op_data->op_namelen,
+							 &op_data->op_fid1,
+							 &op_data->op_mds);
+		}
+	} else {
 		tgt = lmv_locate_mds(lmv, op_data, &op_data->op_fid1);
+	}
 	if (IS_ERR(tgt))
 		RETURN(PTR_ERR(tgt));
 
