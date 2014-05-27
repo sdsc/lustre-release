92a69a3a69c07094995f9fe767aa007e8ad6546a
Frank Zago <fzago@cray.com>
2014-05-27 15:36:27 -0500

    LU-XXXX: Remove prototype for function llapi_lmv_get_uuids
    That function doesn't exist.
    
    Change-Id: Ieb7b99404e27c728503f69d980fcb2d93df0b0d4
    Signed-off-by: frank zago <fzago@cray.com>

--- lustre/include/lustre/lustreapi.h
+++ lustre/include/lustre/lustreapi.h
@@ -237,7 +237,6 @@ extern int llapi_file_get_lov_uuid(const char *path, struct obd_uuid *lov_uuid);
 extern int llapi_file_get_lmv_uuid(const char *path, struct obd_uuid *lmv_uuid);
 extern int llapi_file_fget_lov_uuid(int fd, struct obd_uuid *lov_uuid);
 extern int llapi_lov_get_uuids(int fd, struct obd_uuid *uuidp, int *ost_count);
-extern int llapi_lmv_get_uuids(int fd, struct obd_uuid *uuidp, int *mdt_count);
 extern int llapi_is_lustre_mnttype(const char *type);
 extern int llapi_search_ost(char *fsname, char *poolname, char *ostname);
 extern int llapi_get_obd_count(char *mnt, int *count, int is_mdt);
