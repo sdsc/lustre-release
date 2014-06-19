0f8989b9cd5d6bc29f247587d6b2629d18154d85
Wang Di <di.wang@intel.com>
2014-06-19 03:18:29 -0700

    LU-3536 lod: Separate thandle to different layers.
    Separate thandle into different layers on MDT stack.
    
    The current implementation use single thandle in all
    layers, which might cause some issues for cross-MDT
    transaction, for example during transaction stop,
    it needs to stop local OSD transaction first,
    then send remote RPC, because we do not want hold the
    transaction, during RPC sending, but once we stop osd
    transaction, which might cause this single thandle be
    destroyed (see osd_trans_stop()), but all of remote
    updates are still attached in this thandle.
    
    This patch will separate the thandle to different layers:
    1. MDD thandle will present itself in MDD transaction.
    2. LOD thandle will distribute the thandle to all sub-thandles,
       and also attach the update blob buffer to store updates for
       cross-MDT operations.
    3. OSP thandle will store the updates for the remote
       correspondent target, and also manage to send them to the
       remote target.
    4. OSD thandle will still be the same as the original one,
       relying on the bottom FS to achieve the "atomic".
    
    Though there are a few expection here,
    1. Because of llog, MDD needs to access the OSD thandle directly
       i.e. LLOG in MDD does not go-through LOD, instead it will go
       to OSD layer directly.
    2. OSP will need to access OSD thandle sometimes to update
       last used OID and sequence.
    
    So it add th_storage_th in thandle, for MDD and OSP to access
    the OSD thandle.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: I0c73cc80fa692c2e5d5a09e441c28e228d822ce0

--- lustre/mdd/mdd_dir.c
+++ lustre/mdd/mdd_dir.c
@@ -682,7 +682,8 @@ int mdd_declare_changelog_store(const struct lu_env *env,
 	if (ctxt == NULL)
 		return -ENXIO;
 
-	rc = llog_declare_add(env, ctxt->loc_handle, &rec->cr_hdr, handle);
+	rc = llog_declare_add(env, ctxt->loc_handle, &rec->cr_hdr,
+			      handle->th_storage_th);
 	llog_ctxt_put(ctxt);
 
 	return rc;
@@ -720,7 +721,8 @@ static int mdd_declare_changelog_ext_store(const struct lu_env *env,
 	if (ctxt == NULL)
 		return -ENXIO;
 
-	rc = llog_declare_add(env, ctxt->loc_handle, &rec->cr_hdr, handle);
+	rc = llog_declare_add(env, ctxt->loc_handle, &rec->cr_hdr,
+			      handle->th_storage_th);
 	llog_ctxt_put(ctxt);
 
 	return rc;
@@ -755,7 +757,8 @@ int mdd_changelog_store(const struct lu_env *env, struct mdd_device *mdd,
 	if (ctxt == NULL)
 		return -ENXIO;
 
-	rc = llog_add(env, ctxt->loc_handle, &rec->cr_hdr, NULL, th);
+	rc = llog_add(env, ctxt->loc_handle, &rec->cr_hdr, NULL,
+		      th->th_storage_th);
 	llog_ctxt_put(ctxt);
 	if (rc > 0)
 		rc = 0;
@@ -794,7 +797,8 @@ mdd_changelog_ext_store(const struct lu_env *env, struct mdd_device *mdd,
 		return -ENXIO;
 
 	/* nested journal transaction */
-	rc = llog_add(env, ctxt->loc_handle, &rec->cr_hdr, NULL, th);
+	rc = llog_add(env, ctxt->loc_handle, &rec->cr_hdr, NULL,
+		      th->th_storage_th);
 	llog_ctxt_put(ctxt);
 	if (rc > 0)
 		rc = 0;
@@ -2093,13 +2097,6 @@ static int mdd_declare_create(const struct lu_env *env, struct mdd_device *mdd,
 		if (rc)
 			return rc;
 	}
-
-	/* XXX: For remote create, it should indicate the remote RPC
-	 * will be sent after local transaction is finished, which
-	 * is not very nice, but it will be removed once we fully support
-	 * async update */
-	if (mdd_object_remote(p) && handle->th_update != NULL)
-		handle->th_update->tu_sent_after_local_trans = 1;
 out:
 	return rc;
 }
