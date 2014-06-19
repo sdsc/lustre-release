5d6426e4f2e9bf60c7343878a46998e048d2b718
Mikhail Pershin <mike.pershin@intel.com>
2014-06-19 23:49:10 +0400

    LU-5218 llog: don't change indexes of llog context
    The llog context id table cannot be shrinked easily because that
    will cause index shifting and incompatibility between old client
    and new server and vice versa.
    
    Patch adds also checks in llog server for wire data.
    
    Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
    Change-Id: I003120223504d95164e4392b01c096e3e8b0fef0

--- lustre/include/obd.h
+++ lustre/include/obd.h
@@ -199,18 +199,30 @@ struct brw_page {
 	obd_flag flag;
 };
 
-/* llog contexts */
+/**
+ * llog contexts indexes.
+ *
+ * There is compatibility problem mentioned in comments below. Obsoleted
+ * values can't be just removed because all indexes will be shifted so old
+ * client can't work with new server and vice versa.
+ */
 enum llog_ctxt_id {
 	LLOG_CONFIG_ORIG_CTXT  =  0,
 	LLOG_CONFIG_REPL_CTXT,
 	LLOG_MDS_OST_ORIG_CTXT,
+	LLOG_MDS_OST_REPL_CTXT, /* obsoleted, kept for compatibility */
 	LLOG_SIZE_ORIG_CTXT,
 	LLOG_SIZE_REPL_CTXT,
+	LLOG_RD1_ORIG_CTXT, /* obsoleted, kept for compatibility */
+	LLOG_RD1_REPL_CTXT, /* obsoleted, kept for compatibility */
 	LLOG_TEST_ORIG_CTXT,
-	LLOG_CHANGELOG_ORIG_CTXT,	/**< changelog generation on mdd */
-	LLOG_CHANGELOG_REPL_CTXT,	/**< changelog access on clients */
-	LLOG_CHANGELOG_USER_ORIG_CTXT,	/**< for multiple changelog consumers */
-	LLOG_AGENT_ORIG_CTXT,		/**< agent requests generation on cdt */
+	LLOG_TEST_REPL_CTXT, /* obsoleted, kept for compatibility */
+	LLOG_LOVEA_ORIG_CTXT, /* obsoleted, kept for compatibility */
+	LLOG_LOVEA_REPL_CTXT, /* obsoleted, kept for compatibility */
+	LLOG_CHANGELOG_ORIG_CTXT, /**< changelog generation on mdd */
+	LLOG_CHANGELOG_REPL_CTXT, /**< changelog access on clients */
+	LLOG_CHANGELOG_USER_ORIG_CTXT, /**< for multiple changelog consumers */
+	LLOG_AGENT_ORIG_CTXT, /**< agent requests generation on cdt */
 	LLOG_MAX_CTXTS
 };
 
