04b9880fad5757040dc6e80ef654be63315cceb9
Wang Di <di.wang@intel.com>
2014-06-21 00:59:25 -0700

    LU-4843 mdt: Use OBD_CONNECT_LFSCK to check striped dir client
    Replace OBD_CONNECT_DISP_STRIPE with OBD_CONNECT_LFSCK to check
    if the client understand striped directory in mdt_is_striped_client(),
    because OBD_CONNECT_DISP_STRIPE is also included in 2.5 client.
    
    Change-Id: I5d5f1a6a9f5efdce0c6fe22106a8853cd78bb486
    Signed-off-by: Wang Di <di.wang@intel.com>

--- lustre/mdt/mdt_internal.h
+++ lustre/mdt/mdt_internal.h
@@ -594,11 +594,11 @@ static inline bool mdt_is_dne_client(struct obd_export *exp)
 	return !!(exp_connect_flags(exp) & OBD_CONNECT_LVB_TYPE);
 }
 
-/* Here we use OBD_CONNECT_DISP_STRIPE to check the client who support
+/* Here we use OBD_CONNECT_LFSCK to check the client who support
  * striped directory, because it is also landed on 2.6 */
 static inline bool mdt_is_striped_client(struct obd_export *exp)
 {
-	return exp_connect_flags(exp) & OBD_CONNECT_DISP_STRIPE;
+	return exp_connect_flags(exp) & OBD_CONNECT_LFSCK;
 }
 
 int mdt_get_disposition(struct ldlm_reply *rep, int flag);
