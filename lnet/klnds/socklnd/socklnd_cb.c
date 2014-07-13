6b15aeccda2c7ce0e015fdcdccaf92c68f247c64
frank zago <fzago@cray.com>
2014-07-13 12:06:02 -0500

    LU-5396: add sparse locking annotations
    Adds __acquires / __releases / __must_hold to several function.
    
    Fixes sparse warnings like:
    
    libcfs/libcfs/hash.c:127:1: warning: context imbalance in 'cfs_hash_spin_lock'
        - wrong count at exit
    libcfs/libcfs/hash.c:133:1: warning: context imbalance in 'cfs_hash_spin_unlock'
        - unexpected unlock
    libcfs/libcfs/hash.c:141:9: warning: context imbalance in 'cfs_hash_rw_lock'
        - wrong count at exit
    include/linux/rwlock_api_smp.h:221:9: warning: context imbalance in
        'cfs_hash_rw_unlock' - unexpected unlock
    
    Change-Id: I91834ea62a2bc21ee853a80b0b266e3d0e960bc3
    Signed-off-by: frank zago <fzago@cray.com>

--- lnet/klnds/socklnd/socklnd_cb.c
+++ lnet/klnds/socklnd/socklnd_cb.c
@@ -2363,6 +2363,7 @@ ksocknal_flush_stale_txs(ksock_peer_t *peer)
 
 int
 ksocknal_send_keepalive_locked(ksock_peer_t *peer)
+__must_hold(&ksocknal_data.ksnd_global_lock)
 {
         ksock_sched_t  *sched;
         ksock_conn_t   *conn;
