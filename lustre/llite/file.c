89a801459731a8f6d77942a783e86882d665a9b8
Frank Zago <fzago@cray.com>
2014-07-19 13:55:09 -0500

    LU-5389: optimize ll_fid2path()
    The only parameter from userspace that matters is the length of the
    buffer. We don't need to allocate then import the whole structure. By
    importing only that length, we can save a memory allocation.
    
    Add sparse annotations to that function.
    
    Change-Id: I4baa941f0128f9bcc19fade112915f3c52cd5b8b
    Signed-off-by: frank zago <fzago@cray.com>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -1893,44 +1893,41 @@ out:
 	RETURN(rc);
 }
 
-int ll_fid2path(struct inode *inode, void *arg)
+int ll_fid2path(struct inode *inode, void __user *arg)
 {
 	struct obd_export	*exp = ll_i2mdexp(inode);
-	struct getinfo_fid2path	*gfout, *gfin;
-	int			 outsize, rc;
+	const struct getinfo_fid2path __user *gfin = arg;
+        __u32			 gf_pathlen;
+	struct getinfo_fid2path	*gfout;
+	size_t			 outsize;
+	int			 rc;
+
 	ENTRY;
 
 	if (!cfs_capable(CFS_CAP_DAC_READ_SEARCH) &&
 	    !(ll_i2sbi(inode)->ll_flags & LL_SBI_USER_FID2PATH))
 		RETURN(-EPERM);
 
-	/* Need to get the buflen */
-	OBD_ALLOC_PTR(gfin);
-	if (gfin == NULL)
-		RETURN(-ENOMEM);
-	if (copy_from_user(gfin, arg, sizeof(*gfin))) {
-		OBD_FREE_PTR(gfin);
+	/* Only need to get the buflen */
+	if (copy_from_user(&gf_pathlen, &gfin->gf_pathlen, sizeof(gf_pathlen)))
 		RETURN(-EFAULT);
-	}
 
-	outsize = sizeof(*gfout) + gfin->gf_pathlen;
+	outsize = sizeof(*gfout) + gf_pathlen;
 	OBD_ALLOC(gfout, outsize);
-	if (gfout == NULL) {
-		OBD_FREE_PTR(gfin);
+	if (gfout == NULL)
 		RETURN(-ENOMEM);
-	}
-	memcpy(gfout, gfin, sizeof(*gfout));
-	OBD_FREE_PTR(gfin);
 
-	/* Call mdc_iocontrol */
-	rc = obd_iocontrol(OBD_IOC_FID2PATH, exp, outsize, gfout, NULL);
-	if (rc)
-		GOTO(gf_free, rc);
-
-	if (copy_to_user(arg, gfout, outsize))
+	if (copy_from_user(gfout, arg, sizeof(*gfout))) {
 		rc = -EFAULT;
+	} else {
+		/* Call mdc_iocontrol */
+		rc = obd_iocontrol(OBD_IOC_FID2PATH, exp, outsize, gfout, NULL);
+		if (rc == 0) {
+			if (copy_to_user(arg, gfout, outsize))
+				rc = -EFAULT;
+		}
+	}
 
-gf_free:
 	OBD_FREE(gfout, outsize);
 	RETURN(rc);
 }
