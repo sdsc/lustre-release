34c401cccf2221dea5a19cce7435666739b753e7
Fan Yong <fan.yong@intel.com>
2014-07-26 13:46:51 +0800

    LU-4788 lfsck: enable verification for remote object
    Based on the LFSCK 1.5 framework, enable the namespace LFSCK
    scanning for remote object.
    
    During the first-stage scanning, if the object contains remote
    linkEA entry or multiple linkEA entries or claims as multiple
    linked, then it will be recorded in the namespace LFSCK tracing
    file for double scanning.
    
    Some cleanup for the namespace LFSCK tracing file (lfsck_namespace)
    and other code cleanup.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Ibc87ae9a5c6b7f67a9215140cf2cb89640bce0a9

--- lustre/lfsck/lfsck_layout.c
+++ lustre/lfsck/lfsck_layout.c
@@ -996,14 +996,14 @@ static int fid_is_for_ostobj(const struct lu_env *env, struct dt_device *dt,
 			     struct dt_object *obj, const struct lu_fid *fid)
 {
 	struct seq_server_site	*ss	= lu_site2seq(dt->dd_lu_dev.ld_site);
-	struct lu_seq_range	 range	= { 0 };
+	struct lu_seq_range	*range	= &lfsck_env_info(env)->lti_range;
 	struct lustre_mdt_attrs *lma;
 	int			 rc;
 
-	fld_range_set_any(&range);
-	rc = fld_server_lookup(env, ss->ss_server_fld, fid_seq(fid), &range);
+	fld_range_set_any(range);
+	rc = fld_server_lookup(env, ss->ss_server_fld, fid_seq(fid), range);
 	if (rc == 0) {
-		if (fld_range_is_ost(&range))
+		if (fld_range_is_ost(range))
 			return 1;
 
 		return 0;
@@ -1355,7 +1355,6 @@ static int lfsck_layout_double_scan_result(const struct lu_env *env,
 	lo->ll_objs_checked_phase2 += com->lc_new_checked;
 
 	if (rc > 0) {
-		com->lc_journal = 0;
 		if (lo->ll_flags & LF_INCOMPLETE) {
 			lo->ll_status = LS_PARTIAL;
 		} else {
@@ -3304,6 +3303,9 @@ static int lfsck_layout_assistant_handler_p2(const struct lu_env *env,
 	int				 rc	= 0;
 	ENTRY;
 
+	CDEBUG(D_LFSCK, "%s: layout LFSCK phase2 scan start\n",
+	       lfsck_lfsck2name(lfsck));
+
 	spin_lock(&ltds->ltd_lock);
 	while (!list_empty(&lad->lad_ost_phase2_list)) {
 		ltd = list_entry(lad->lad_ost_phase2_list.next,
@@ -3328,6 +3330,9 @@ static int lfsck_layout_assistant_handler_p2(const struct lu_env *env,
 	else
 		rc = 0;
 	spin_unlock(&ltds->ltd_lock);
+
+	CDEBUG(D_LFSCK, "%s: layout LFSCK phase2 scan stop: rc = %d\n",
+	       lfsck_lfsck2name(lfsck), rc);
 
 	RETURN(rc);
 }
@@ -3685,22 +3690,22 @@ static int lfsck_layout_slave_check_pairs(const struct lu_env *env,
 	struct obd_export	 *exp	 = NULL;
 	struct ptlrpc_request	 *req	 = NULL;
 	struct lfsck_request	 *lr;
-	struct lu_seq_range	  range	 = { 0 };
+	struct lu_seq_range	 *range  = &lfsck_env_info(env)->lti_range;
 	int			  rc	 = 0;
 	ENTRY;
 
 	if (unlikely(fid_is_idif(pfid)))
 		RETURN(1);
 
-	fld_range_set_any(&range);
-	rc = fld_server_lookup(env, ss->ss_server_fld, fid_seq(pfid), &range);
+	fld_range_set_any(range);
+	rc = fld_server_lookup(env, ss->ss_server_fld, fid_seq(pfid), range);
 	if (rc != 0)
 		RETURN(rc == -ENOENT ? 1 : rc);
 
-	if (unlikely(!fld_range_is_mdt(&range)))
+	if (unlikely(!fld_range_is_mdt(range)))
 		RETURN(1);
 
-	exp = lustre_find_lwp_by_index(obd->obd_name, range.lsr_index);
+	exp = lustre_find_lwp_by_index(obd->obd_name, range->lsr_index);
 	if (unlikely(exp == NULL))
 		RETURN(1);
 
@@ -5459,7 +5464,7 @@ static int lfsck_fid_match_idx(const struct lu_env *env,
 {
 	struct seq_server_site	*ss;
 	struct lu_server_fld	*sf;
-	struct lu_seq_range	 range	= { 0 };
+	struct lu_seq_range	*range = &lfsck_env_info(env)->lti_range;
 	int			 rc;
 
 	/* All abnormal cases will be returned to MDT0. */
@@ -5477,15 +5482,15 @@ static int lfsck_fid_match_idx(const struct lu_env *env,
 	sf = ss->ss_server_fld;
 	LASSERT(sf != NULL);
 
-	fld_range_set_any(&range);
-	rc = fld_server_lookup(env, sf, fid_seq(fid), &range);
+	fld_range_set_any(range);
+	rc = fld_server_lookup(env, sf, fid_seq(fid), range);
 	if (rc != 0)
 		return rc;
 
-	if (!fld_range_is_mdt(&range))
+	if (!fld_range_is_mdt(range))
 		return -EINVAL;
 
-	if (range.lsr_index == idx)
+	if (range->lsr_index == idx)
 		return 1;
 
 	return 0;
