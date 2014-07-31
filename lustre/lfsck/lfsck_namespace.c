c4b988cc8e3f018d6700f15d31dd8f72f326ec2b
Fan Yong <fan.yong@intel.com>
2014-07-31 16:12:03 +0800

    LU-5519 lfsck: repair bad name hash for striped directory
    If the name hash of some name entry under the striped directory
    does not match the shard of the striped directory, then the LFSCK
    will repair the inconsistency. Ideally, the LFSCK should migrate
    the name entry from the current MDT to the right MDT (another one),
    but before the async commit finished, the LFSCK will change the
    striped directory's hash type as LMV_HASH_TYPE_UNKNOWN and mark
    the lmv flags as LMV_HASH_FLAG_BAD_TYPE.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I665a77473be1fead09c12cb7fb5d0ffa739ffe87

--- lustre/lfsck/lfsck_namespace.c
+++ lustre/lfsck/lfsck_namespace.c
@@ -4392,6 +4392,28 @@ log:
 
 		return 0;
 	}
+	case LE_SET_LMV_MASTER: {
+		struct lmv_mds_md_v1	*lmv = &lfsck_env_info(env)->lti_lmv;
+		struct dt_object	*obj;
+
+		obj = lfsck_object_find_by_dev(env, lfsck->li_bottom,
+					       &lr->lr_fid);
+		if (IS_ERR(obj))
+			RETURN(PTR_ERR(obj));
+
+		memset(lmv, 0, sizeof(*lmv));
+		lmv->lmv_stripe_count = lr->lr_stripe_count;
+		lmv->lmv_hash_type = lr->lr_hash_type;
+		lmv->lmv_layout_version = lr->lr_layout_version;
+		memcpy(lmv->lmv_pool_name, lr->lr_pool_name,
+		       sizeof(lmv->lmv_pool_name));
+
+		rc = lfsck_namespace_set_lmv_master_local(env, com, obj, lmv,
+				&lr->lr_fid2, lr->lr_index2, lr->lr_flags);
+		lfsck_object_put(env, obj);
+
+		RETURN(rc > 0 ? 0 : rc);
+	}
 	case LE_PHASE1_DONE:
 	case LE_PHASE2_DONE:
 	case LE_PEER_EXIT:
@@ -4756,6 +4778,7 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 	bool			    remove;
 	bool			    newdata;
 	bool			    log      = false;
+	bool			    bad_hash = false;
 	int			    idx      = 0;
 	int			    count    = 0;
 	int			    rc;
@@ -4780,6 +4803,12 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 						LNTF_CHECK_PARENT, true);
 
 		GOTO(out, rc);
+	}
+
+	if (unlikely(lnr->lnr_dir_cookie == MDS_DIR_END_OFF)) {
+		rc = lfsck_namespace_striped_dir_scanned(env, com, lnr);
+
+		RETURN(rc);
 	}
 
 	if (lnr->lnr_name[0] == '.' &&
@@ -5083,6 +5112,19 @@ stop:
 out:
 	lfsck_ibits_unlock(&lh, LCK_EX);
 
+	if (!name_is_dot_or_dotdot(lnr->lnr_name, lnr->lnr_namelen) &&
+	    !lfsck_is_valid_slave_name_entry(env, lnr->lnr_lmv,
+					     lnr->lnr_name, lnr->lnr_namelen) &&
+	    type != LNIT_BAD_DIRENT) {
+		ns->ln_flags |= LF_INCONSISTENT;
+
+		log = false;
+		rc = lfsck_namespace_repair_bad_namehash(env, com, dir,
+						lnr->lnr_lmv, lnr->lnr_name);
+		if (rc >= 0)
+			bad_hash = true;
+	}
+
 	if (rc >= 0) {
 		switch (type) {
 		case LNIT_BAD_TYPE:
@@ -5159,6 +5201,21 @@ out:
 					       &ns->ln_pos_first_inconsistent,
 					       false);
 		}
+
+		if (bad_hash) {
+			ns->ln_namehash_repaired++;
+
+			/* Not count repeatedly. */
+			if (!repaired)
+				ns->ln_items_repaired++;
+
+			if (bk->lb_param & LPF_DRYRUN &&
+			    lfsck_pos_is_zero(&ns->ln_pos_first_inconsistent))
+				lfsck_pos_fill(env, lfsck,
+					       &ns->ln_pos_first_inconsistent,
+					       false);
+		}
+
 
 		rc = 0;
 	}
