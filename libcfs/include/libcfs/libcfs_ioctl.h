7c11bb1ffcdb9b730a7a1287eff701ad02cc8e20
Liang Zhen <liang.zhen@intel.com>
2014-08-04 16:53:15 +0800

    LU-5435 libcfs: copy out ioctl inline buffer
    - libcfs_ioctl_popdata should copy out inline buffers.
    - code cleanup for libcfs ioctl handler
    - error number fix for obd_ioctl_getdata
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Change-Id: I3969e4e96b1dc0c2f6c3e86039b97af5482edc74

--- libcfs/include/libcfs/libcfs_ioctl.h
+++ libcfs/include/libcfs/libcfs_ioctl.h
@@ -49,6 +49,9 @@ struct libcfs_ioctl_hdr {
 	__u32 ioc_version;
 };
 
+/** max size to copy from userspace */
+#define LIBCFS_IOC_DATA_MAX	(128 * 1024)
+
 struct libcfs_ioctl_data {
 	struct libcfs_ioctl_hdr ioc_hdr;
 
@@ -74,7 +77,6 @@ struct libcfs_ioctl_data {
 };
 
 #define ioc_priority ioc_u32[0]
-
 
 struct libcfs_debug_ioctl_data
 {
@@ -251,11 +253,22 @@ static inline bool libcfs_ioctl_is_invalid(struct libcfs_ioctl_data *data)
 
 extern int libcfs_register_ioctl(struct libcfs_ioctl_handler *hand);
 extern int libcfs_deregister_ioctl(struct libcfs_ioctl_handler *hand);
-extern int libcfs_ioctl_getdata(struct libcfs_ioctl_hdr *buf, __u32 buf_len,
-				const void __user *arg);
-extern int libcfs_ioctl_getdata_len(const struct libcfs_ioctl_hdr __user *arg,
-				    __u32 *buf_len);
-extern int libcfs_ioctl_popdata(void __user *arg, void *buf, int size);
+extern int libcfs_ioctl_getdata(struct libcfs_ioctl_hdr **hdr_pp,
+				struct libcfs_ioctl_hdr __user *uparam);
+
+static inline int libcfs_ioctl_popdata(struct libcfs_ioctl_hdr *hdr,
+				       struct libcfs_ioctl_hdr __user *uparam)
+{
+	 if (copy_to_user(uparam, hdr, hdr->ioc_len))
+		return -EFAULT;
+	 return 0;
+}
+
+static inline void libcfs_ioctl_freedata(struct libcfs_ioctl_hdr *hdr)
+{
+	LIBCFS_FREE(hdr, hdr->ioc_len);
+}
+
 #endif
 
 extern int libcfs_ioctl_data_adjust(struct libcfs_ioctl_data *data);
