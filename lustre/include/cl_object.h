59f5ab04f4d9056b282b2f52442e57909d17bf45
Frank Zago <fzago@cray.com>
2014-08-07 14:48:03 -0500

    LU-XXXX: obd: make cl_page_alloc() static
    cl_page_alloc is only called once, in the same file. Make it static and
    don't export it.
    
    Change-Id: Ib6cd05f3654138fd838058d524730ce7f65dbde2
    Signed-off-by: frank zago <fzago@cray.com>

--- lustre/include/cl_object.h
+++ lustre/include/cl_object.h
@@ -2739,10 +2739,6 @@ struct cl_page *cl_page_find        (const struct lu_env *env,
                                      struct cl_object *obj,
                                      pgoff_t idx, struct page *vmpage,
                                      enum cl_page_type type);
-struct cl_page *cl_page_alloc       (const struct lu_env *env,
-				     struct cl_object *o, pgoff_t ind,
-				     struct page *vmpage,
-				     enum cl_page_type type);
 void            cl_page_get         (struct cl_page *page);
 void            cl_page_put         (const struct lu_env *env,
                                      struct cl_page *page);
