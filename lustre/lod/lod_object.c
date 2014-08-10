fa9a74adaf62fbc4c785f82e7b873f1161e73f90
wang di <di.wang@intel.com>
2014-08-10 02:58:24 -0700

    LU-2430 mdd: add lfs mv to migrate inode.
    Add lfs mv to migrate the individual inode from one MDT to
    another MDT, and this function will only migrate inode layout
    on MDT but not touch data object on OST.
    
    lfs mv -M 1 /mnt/lustre/test1 #move test1 to MDT1.
    
    The directory will be migrated from top to the bottom, i.e.
    migrating parent first, then migrating the child.
    
    Add migrate into sanity 17n to check on-disk format.
    
    Add sanity 230(c,d,e,f) sanityn 80 to for migration.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: Ib4456a1db8909bd96260c67fff48922081948dcd

--- lustre/lod/lod_object.c
+++ lustre/lod/lod_object.c
@@ -566,6 +566,9 @@ int lod_parse_dir_striping(const struct lu_env *env, struct lod_object *lo,
 	int			rc = 0;
 	ENTRY;
 
+	if (le32_to_cpu(lmv1->lmv_magic) == LMV_MAGIC_MIGRATE)
+		RETURN(0);
+
 	if (le32_to_cpu(lmv1->lmv_magic) != LMV_MAGIC_V1)
 		RETURN(-EINVAL);
 
@@ -1289,6 +1292,11 @@ static int lod_cache_parent_lov_striping(const struct lu_env *env,
 	if (v1->lmm_pattern != LOV_PATTERN_RAID0 && v1->lmm_pattern != 0)
 		GOTO(unlock, rc = 0);
 
+	CDEBUG(D_INFO, DFID" stripe_count=%d stripe_size=%d stripe_offset=%d\n",
+	       PFID(lu_object_fid(&lp->ldo_obj.do_lu)),
+	       (int)v1->lmm_stripe_count,
+	       (int)v1->lmm_stripe_size, (int)v1->lmm_stripe_offset);
+
 	lp->ldo_def_stripenr = v1->lmm_stripe_count;
 	lp->ldo_def_stripe_size = v1->lmm_stripe_size;
 	lp->ldo_def_stripe_offset = v1->lmm_stripe_offset;
@@ -1618,22 +1626,30 @@ int lod_declare_striped_object(const struct lu_env *env, struct dt_object *dt,
 		GOTO(out, rc = -ENOMEM);
 	}
 
-	/* choose OST and generate appropriate objects */
-	rc = lod_qos_prep_create(env, lo, attr, lovea, th);
-	if (rc) {
-		/* failed to create striping, let's reset
-		 * config so that others don't get confused */
-		lod_object_free_striping(env, lo);
-		GOTO(out, rc);
+	if (!dt_object_remote(next)) {
+		/* choose OST and generate appropriate objects */
+		rc = lod_qos_prep_create(env, lo, attr, lovea, th);
+		if (rc) {
+			/* failed to create striping, let's reset
+			 * config so that others don't get confused */
+			lod_object_free_striping(env, lo);
+			GOTO(out, rc);
+		}
+
+		/*
+		 * declare storage for striping data
+		 */
+		info->lti_buf.lb_len = lov_mds_md_size(lo->ldo_stripenr,
+				lo->ldo_pool ?  LOV_MAGIC_V3 : LOV_MAGIC_V1);
+	} else {
+		/* LOD can not choose OST objects for remote objects, i.e.
+		 * stripes must be ready before that */
+		LASSERT(lovea != NULL);
+		info->lti_buf = *lovea;
 	}
 
-	/*
-	 * declare storage for striping data
-	 */
-	info->lti_buf.lb_len = lov_mds_md_size(lo->ldo_stripenr,
-				lo->ldo_pool ?  LOV_MAGIC_V3 : LOV_MAGIC_V1);
-	rc = dt_declare_xattr_set(env, next, &info->lti_buf, XATTR_NAME_LOV,
-				  0, th);
+	rc = dt_declare_xattr_set(env, next, &info->lti_buf,
+				  XATTR_NAME_LOV, 0, th);
 	if (rc)
 		GOTO(out, rc);
 
@@ -1829,7 +1845,11 @@ static int lod_declare_object_create(const struct lu_env *env,
 			rc = lod_declare_striped_object(env, dt, attr,
 							NULL, th);
 	} else if (dof->dof_type == DFT_DIR) {
-		rc = lod_declare_dir_striping_create(env, dt, attr, dof, th);
+		/* Orphan object (like migrating object) does not have
+		 * lod_dir_stripe, see lod_ah_init */
+		if (lo->ldo_dir_stripe != NULL)
+			rc = lod_declare_dir_striping_create(env, dt, attr,
+							     dof, th);
 	}
 out:
 	RETURN(rc);
@@ -1873,7 +1893,8 @@ static int lod_object_create(const struct lu_env *env, struct dt_object *dt,
 	rc = dt_create(env, next, attr, hint, dof, th);
 
 	if (rc == 0) {
-		if (S_ISDIR(dt->do_lu.lo_header->loh_attr))
+		if (S_ISDIR(dt->do_lu.lo_header->loh_attr) &&
+		    lo->ldo_dir_stripe != NULL)
 			rc = lod_dir_striping_create(env, dt, attr, dof, th);
 		else if (lo->ldo_stripe && dof->u.dof_reg.striped != 0)
 			rc = lod_striping_create(env, dt, attr, dof, th);
@@ -2175,11 +2196,11 @@ static ssize_t lod_read(const struct lu_env *env, struct dt_object *dt,
 
 static ssize_t lod_declare_write(const struct lu_env *env,
 				 struct dt_object *dt,
-				 const loff_t size, loff_t pos,
+				 const struct lu_buf *buf, loff_t pos,
 				 struct thandle *th)
 {
 	return dt_declare_record_write(env, dt_object_child(dt),
-				       size, pos, th);
+				       buf, pos, th);
 }
 
 static ssize_t lod_write(const struct lu_env *env, struct dt_object *dt,
