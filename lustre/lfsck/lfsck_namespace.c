0af7e075b257e163b1daf4b2365e3434df0c5598
Fan Yong <fan.yong@intel.com>
2014-08-14 16:43:17 +0800

    LU-5707 lfsck: recreate LFSCK tracing file for load failure
    The extended attribute XATTR_NAME_LFSCK_NAMESPACE for namespace
    LFSCK tracing file is enlarged since Lustre-2.7. When downgrade
    from Lustre-2.7, the old MDS will get -ERANGE when loads the
    XATTR_NAME_LFSCK_NAMESPACE extended attribute from the new
    formatted on-disk namespace LFSCK tracing file. Under such case,
    the LFSCK should not return failure, instead, it can recreate the
    XATTR_NAME_LFSCK_NAMESPACE EA as initialization case.
    
    More further, if the LFSCK failed to load tracing file, it is
    unncessary to fail the whole LFSCK, instead, just recreate the
    tracing file.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Icc2c7087f9f4a20d57664eaaf5a92f6c6260c3aa

--- lustre/lfsck/lfsck_namespace.c
+++ lustre/lfsck/lfsck_namespace.c
@@ -142,17 +142,20 @@ static int lfsck_namespace_load(const struct lu_env *env,
 		} else {
 			rc = 0;
 		}
-	} else if (rc != -ENODATA) {
-		CERROR("%.16s: fail to load lfsck_namespace, expected = %d, "
-		       "rc = %d\n", lfsck_lfsck2name(com->lc_lfsck), len, rc);
-		if (rc >= 0)
-			rc = 1;
+	} else {
+		if (rc != -ENODATA)
+			CERROR("%.16s: fail to load lfsck_namespace, "
+			       "expected = %d, rc = %d\n",
+			       lfsck_lfsck2name(com->lc_lfsck), len, rc);
+		else if (rc > 0)
+			rc = -EINVAL;
 	}
+
 	return rc;
 }
 
 static int lfsck_namespace_store(const struct lu_env *env,
-				 struct lfsck_component *com, bool init)
+				 struct lfsck_component *com)
 {
 	struct dt_object	*obj    = com->lc_obj;
 	struct lfsck_instance	*lfsck  = com->lc_lfsck;
@@ -189,8 +192,7 @@ static int lfsck_namespace_store(const struct lu_env *env,
 
 	rc = dt_xattr_set(env, obj,
 			  lfsck_buf_get(env, com->lc_file_disk, len),
-			  XATTR_NAME_LFSCK_NAMESPACE,
-			  init ? LU_XATTR_CREATE : LU_XATTR_REPLACE,
+			  XATTR_NAME_LFSCK_NAMESPACE, 0,
 			  handle, BYPASS_CAPA);
 	if (rc != 0)
 		CERROR("%.16s: fail to store lfsck_namespace, len = %d, "
@@ -213,7 +215,7 @@ static int lfsck_namespace_init(const struct lu_env *env,
 	ns->ln_magic = LFSCK_NAMESPACE_MAGIC;
 	ns->ln_status = LS_INIT;
 	down_write(&com->lc_sem);
-	rc = lfsck_namespace_store(env, com, true);
+	rc = lfsck_namespace_store(env, com);
 	up_write(&com->lc_sem);
 	return rc;
 }
@@ -696,7 +698,7 @@ static int lfsck_namespace_reset(const struct lu_env *env,
 	if (rc != 0)
 		GOTO(out, rc);
 
-	rc = lfsck_namespace_store(env, com, true);
+	rc = lfsck_namespace_store(env, com);
 
 	GOTO(out, rc);
 
@@ -746,7 +748,7 @@ static int lfsck_namespace_checkpoint(const struct lu_env *env,
 		com->lc_new_checked = 0;
 	}
 
-	rc = lfsck_namespace_store(env, com, false);
+	rc = lfsck_namespace_store(env, com);
 
 	up_write(&com->lc_sem);
 	return rc;
@@ -1084,7 +1086,7 @@ static int lfsck_namespace_post(const struct lu_env *env,
 		com->lc_new_checked = 0;
 	}
 
-	rc = lfsck_namespace_store(env, com, false);
+	rc = lfsck_namespace_store(env, com);
 
 	up_write(&com->lc_sem);
 	return rc;
@@ -1458,7 +1460,7 @@ checkpoint:
 			ns->ln_time_last_checkpoint = cfs_time_current_sec();
 			ns->ln_objs_checked_phase2 += com->lc_new_checked;
 			com->lc_new_checked = 0;
-			rc = lfsck_namespace_store(env, com, false);
+			rc = lfsck_namespace_store(env, com);
 			up_write(&com->lc_sem);
 			if (rc != 0)
 				GOTO(put, rc);
@@ -1515,7 +1517,7 @@ fini:
 		spin_unlock(&lfsck->li_lock);
 	}
 
-	rc = lfsck_namespace_store(env, com, false);
+	rc = lfsck_namespace_store(env, com);
 
 	up_write(&com->lc_sem);
 	return rc;
@@ -1585,8 +1587,9 @@ int lfsck_namespace_setup(const struct lu_env *env,
 	rc = lfsck_namespace_load(env, com);
 	if (rc > 0)
 		rc = lfsck_namespace_reset(env, com, true);
-	else if (rc == -ENODATA)
+	else if (rc < 0)
 		rc = lfsck_namespace_init(env, com);
+
 	if (rc != 0)
 		GOTO(out, rc);
 
