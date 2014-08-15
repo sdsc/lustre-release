8a33453264d9430a706681f707bc51bcfd475319
Frank Zago <fzago@cray.com>
2014-08-15 14:42:41 -0500

    LU-5396: define __acquires, __releases and __must_hold for userspace
    Some functions that are shared between userspace and the kernel have sparse
    annotations. So we need to also define these annotations in userspace.
    
    Change-Id: Ie1e55416554d3295ed61fe26615d5241faa53818
    Signed-off-by: frank zago <fzago@cray.com>

--- libcfs/include/libcfs/libcfs.h
+++ libcfs/include/libcfs/libcfs.h
@@ -90,7 +90,11 @@ static inline int __is_po2(unsigned long long val)
 # define __must_hold(x)
 #endif
 #endif	/* !__must_hold */
-#endif
+#else  /* !__KERNEL__ */
+# define __acquires(x)
+# define __releases(x)
+# define __must_hold(x)
+#endif /* !__KERNEL__ */
 
 /*
  * Lustre Error Checksum: calculates checksum
