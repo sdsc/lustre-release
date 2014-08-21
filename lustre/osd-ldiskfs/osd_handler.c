849e9f8f1cb3a6518c48ff87e4671db8028d490d
Wang Di <di.wang@intel.com>
2014-08-21 15:39:19 -0700

    LU-3536 lod: cancel update log after all committed
    Cancel the log cookies after all of updates have been
    committed to disk.
    
    In DNE2, the distributed operation will write update log in all
    of involve MDTs, when these the updates are committed on all of
    MDT, these update records will be cancelled. If one of these MDTs
    restart during the operation, it will replay the updates based
    on these update records.
    
    The distribute operation will be represent as distribute transaction
    (top_multiple_thandle), and all of distribute transaction will be
    linked into a list, which is attached to lu_target (see
    struct target_distribute_txn_data), after distribute thandle is
    committed, the commit thread (distribute_txn_commit_thread) will
    update the commit batchid, after it is committed, it will then
    cancel the update records.
    
    Change-Id: I3023f95515b2c15307479f7f85c3f13926d4bcc9
    Signed-off-by: Wang Di <di.wang@intel.com>
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -1140,7 +1140,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 	qtrans = oh->ot_quota_trans;
 	oh->ot_quota_trans = NULL;
 
-        if (oh->ot_handle != NULL) {
+	if (oh->ot_handle != NULL) {
                 handle_t *hdl = oh->ot_handle;
 
                 /*
@@ -1153,6 +1153,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 
                 LASSERT(oti->oti_txns == 1);
                 oti->oti_txns--;
+
                 rc = dt_txn_hook_stop(env, th);
                 if (rc != 0)
 			CERROR("%s: failed in transaction hook: rc = %d\n",
@@ -5652,11 +5653,22 @@ static int osd_fid_init(const struct lu_env *env, struct osd_device *osd)
 
 	rc = seq_client_init(osd->od_cl_seq, NULL, LUSTRE_SEQ_METADATA,
 			     osd->od_svname, ss->ss_server_seq);
-
 	if (rc != 0) {
 		OBD_FREE_PTR(osd->od_cl_seq);
 		osd->od_cl_seq = NULL;
+		RETURN(rc);
 	}
+
+	do {
+		/* Allocate new sequence now to avoid creating local transaction
+		 * in the normal transaction process */
+
+		/* If it return -EINPROGRESS or EAGAIN, then it means meta
+		 * server might not be ready to allocate super sequence from
+		 * sequence controller (MDT0)yet */
+		rc = seq_server_alloc_meta(osd->od_cl_seq->lcs_srv,
+					   &osd->od_cl_seq->lcs_space, env);
+	} while (rc == -EINPROGRESS || rc == -EAGAIN);
 
 	RETURN(rc);
 }
