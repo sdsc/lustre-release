bd34df80b0eca7c6c2155a6a2688a123de4e1a1f
James Simmons <uja.ornl@gmail.com>
2014-08-28 13:32:50 -0400

    LU-5275 obdclass: Remove lprocfs_vars argument from class_register_type function
    Lustre no longer uses struct lprocfs_vars with any instance
    of class_register_type function. We can safely remove it.
    
    Change-Id: Ia4ecabab4f286bfe9991d1a860e2485694fca5d0
    Signed-off-by: James Simmons <uja.ornl@gmail.com>

--- lustre/osc/osc_request.c
+++ lustre/osc/osc_request.c
@@ -3310,9 +3310,6 @@ int __init osc_init(void)
 		enable_proc = false;
 
 	rc = class_register_type(&osc_obd_ops, NULL, enable_proc, NULL,
-#ifndef HAVE_ONLY_PROCFS_SEQ
-				 NULL,
-#endif
 				 LUSTRE_OSC_NAME, &osc_device_type);
         if (rc) {
                 lu_kmem_fini(osc_caches);
