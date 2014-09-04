9f8a91c95434250b0a3c741515261b68451fc14d
Wang Di <di.wang@intel.com>
2014-09-04 16:00:06 -0700

    LU-3564 LOD: add distribution id to identify updates
    Add distribution ID to identify updates from one operation.
    Master MDT will use the last committed transno as the starter
    of the ID, and also recovery process will update the distribution
    ID if needed, to make sure the ID + mdt_index is unique to identify
    the operation over the whole cluster.
    
    Change-Id: Ibfd06dec47d7a351f882d719716bcb27671be7a3
    Signed-off-by: Wang Di <di.wang@intel.com>

--- lustre/include/lu_object.h
+++ lustre/include/lu_object.h
@@ -642,6 +642,9 @@ struct lu_site {
 	 * XXX: a hack! fld has to find md_site via site, remove when possible
 	 */
 	struct seq_server_site	*ld_seq_site;
+
+	/* Put lu_target here to get it from every layer */
+	struct lu_target	*ls_target;
 };
 
 static inline struct lu_site_bkt_data *
