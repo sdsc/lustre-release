6735062312dd509c5401672d5fa1f81df3ccea8f
Fan Yong <fan.yong@intel.com>
2014-09-06 12:39:46 +0800

    LU-5848 debug: more debug log for dt_sync
    Add some D_CACHE logs at the entry/exit for osp_sync()/osd_sync().
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Iaa7fbfbbadb9312528b5092d64615b277de6b679

--- lustre/tests/sanity-lfsck.sh
+++ lustre/tests/sanity-lfsck.sh
@@ -50,8 +50,6 @@ setupall
 
 build_test_filter
 
-$LCTL set_param debug=+lfsck > /dev/null || true
-
 MDT_DEV="${FSNAME}-MDT0000"
 OST_DEV="${FSNAME}-OST0000"
 MDT_DEVNAME=$(mdsdevname ${SINGLEMDS//mds/})
@@ -1654,6 +1652,8 @@ test_17() {
 }
 run_test 17 "LFSCK can repair multiple references"
 
+$LCTL set_param debug=+cache > /dev/null
+
 test_18a() {
 	echo "#####"
 	echo "The target MDT-object is there, but related stripe information"
@@ -2340,6 +2340,8 @@ test_18f() {
 	fi
 }
 run_test 18f "Skip the failed OST(s) when handle orphan OST-objects"
+
+$LCTL set_param debug=-cache > /dev/null
 
 test_19a() {
 	check_mount_and_prep
@@ -4130,8 +4132,6 @@ test_31h() {
 		error "(9) Fail to remove the striped directory after LFSCK"
 }
 run_test 31h "Repair the corrupted shard's name entry"
-
-$LCTL set_param debug=-lfsck > /dev/null || true
 
 # restore MDS/OST size
 MDSSIZE=${SAVED_MDSSIZE}
