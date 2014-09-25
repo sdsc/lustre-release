6bf7a36c661184b8e6a491adb20f5e47bafea112
Niu Yawei <yawei.niu@intel.com>
2014-09-24 20:59:10 -0400

    LU-1323 llite: find_vma() should be protected by mmap_sem
    It had been wrongly protected by page_table_lock since 2004.
    
    Lustre-commit: 5eb306e7634ab825306b850226ef9b199a888759
    Lustre-change:  http://review.whamcloud.com/2543
    
    LU-5221 vvp: release mmap_sem in error case
    
    The mmap_sem is downed in vvp_mmap_locks, but in case of
    error from cl_io_lock_alloc_add, it is not upped.
    
    Credit to Paul Casella at Cray for finding this.
    
    Lustre-commit: dece70280b7075c4ca39472ad48e72d696b6fcf8
    Lustre-change: http://review.whamcloud.com/10741
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I188bb5c4008065fe8dc3bf6d108fb20e8a3016a4

--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -133,6 +133,7 @@ static int vvp_mmap_locks(const struct lu_env *env,
                           struct ccc_io *vio, struct cl_io *io)
 {
         struct ccc_thread_info *cti = ccc_env_info(env);
+        struct mm_struct       *mm = current->mm;
         struct vm_area_struct  *vma;
         struct cl_lock_descr   *descr = &cti->cti_descr;
         ldlm_policy_data_t      policy;
@@ -147,6 +148,10 @@ static int vvp_mmap_locks(const struct lu_env *env,
         if (!cl_is_normalio(env, io))
                 RETURN(0);
 
+        /* No MM (e.g. NFS)? No vmas too. */
+        if (mm == NULL)
+                RETURN(0);
+
         for (seg = 0; seg < vio->cui_nrsegs; seg++) {
                 const struct iovec *iv = &vio->cui_iov[seg];
 
@@ -157,12 +162,14 @@ static int vvp_mmap_locks(const struct lu_env *env,
 
                 count += addr & (~CFS_PAGE_MASK);
                 addr &= CFS_PAGE_MASK;
-                while((vma = our_vma(addr, count)) != NULL) {
+
+                down_read(&mm->mmap_sem);
+                while((vma = our_vma(mm, addr, count)) != NULL) {
                         struct inode *inode = vma->vm_file->f_dentry->d_inode;
                         int flags = CEF_MUST;
 
                         if (ll_file_nolock(vma->vm_file)) {
-                                /* 
+                                /*
                                  * For no lock case, a lockless lock will be
                                  * generated.
                                  */
@@ -188,8 +195,10 @@ static int vvp_mmap_locks(const struct lu_env *env,
                                descr->cld_mode, descr->cld_start,
                                descr->cld_end);
 
-                        if (result < 0)
-                                RETURN(result);
+			if (result < 0) {
+				up_read(&mm->mmap_sem);
+				RETURN(result);
+			}
 
                         if (vma->vm_end - addr >= count)
                                 break;
@@ -197,6 +206,7 @@ static int vvp_mmap_locks(const struct lu_env *env,
                         count -= vma->vm_end - addr;
                         addr = vma->vm_end;
                 }
+                up_read(&mm->mmap_sem);
         }
         RETURN(0);
 }
