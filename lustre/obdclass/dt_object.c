sdsc/lustre-release

commit 60e07b972114df24105a3a1bfa7365892f72a4a7

LU-4690 lod: separate master object with master stripe
Separate master stripe with master object, so
1. stripeEA only exists on master object.
2. sub-stripe object will be inserted into master object
as sub-directory, and it can get the master object by "..".

By this, it will remove those specilities for stripe0 in
LMV and LOD. And also simplify LFSCK, i.e. consistency check
would be easier.

And also after this separation, LOD will know whether iterating
the whole stripe or single stripe eaisly, i.e. for master_object,
it will iterate the whole stripes, for sub_stripe, it will only
iterate the single stripe.

This patch also fixes a few things in osp orphan iteration, to
make it work with remote dir entry iteration.

When then master object becomes an orphan, we should
mark all of its sub-stripes as dead object as well,
otherwise client might still be able to create files
under these stripes.

A few fixes for striped directory layout lock:

1. stripe 0 should be locked as EX, same as other stripes.
2. Acquire the layout for directory, when it is being unliked.

Signed-off-by: wang di <di.wang@intel.com>
Change-Id: I6212fb97a2360664b48e0a75424a89c857da2043
Reviewed-on: http://review.whamcloud.com/9511
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: John L. Hammond <john.hammond@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/obdclass/dt_object.c
@@ -677,10 +677,6 @@ static int dt_index_page_build(const struct lu_env *env, union lu_page *lp,
 	int			 rc, size;
 	ENTRY;
 
-	/* no support for variable key & record size for now */
-	LASSERT((ii->ii_flags & II_FL_VARKEY) == 0);
-	LASSERT((ii->ii_flags & II_FL_VARREC) == 0);
-
 	/* initialize the header of the new container */
 	memset(lip, 0, LIP_HDR_SIZE);
 	lip->lip_magic = LIP_MAGIC;
@@ -696,7 +692,9 @@ static int dt_index_page_build(const struct lu_env *env, union lu_page *lp,
 	do {
 		char		*tmp_entry = entry;
 		struct dt_key	*key;
-		__u64		 hash;
+		__u64		hash;
+		__u16		keysize;
+		__u16		recsize;
 
 		/* fetch 64-bit hash value */
 		hash = iops->store(env, it);
@@ -713,18 +711,24 @@ static int dt_index_page_build(const struct lu_env *env, union lu_page *lp,
 			GOTO(out, rc = 0);
 		}
 
-		if ((ii->ii_flags & II_FL_NOHASH) == 0) {
+		if (!(ii->ii_flags & II_FL_NOHASH)) {
 			/* client wants to the 64-bit hash value associated with
 			 * each record */
 			memcpy(tmp_entry, &hash, sizeof(hash));
 			tmp_entry += sizeof(hash);
 		}
 
-		/* then the key value */
-		LASSERT(iops->key_size(env, it) == ii->ii_keysize);
-		key = iops->key(env, it);
-		memcpy(tmp_entry, key, ii->ii_keysize);
-		tmp_entry += ii->ii_keysize;
+		if (ii->ii_flags & II_FL_VARKEY)
+			keysize = iops->key_size(env, it);
+		else
+			keysize = ii->ii_keysize;
+
+		if (!(ii->ii_flags & II_FL_NOKEY)) {
+			/* then the key value */
+			key = iops->key(env, it);
+			memcpy(tmp_entry, key, keysize);
+			tmp_entry += keysize;
+		}
 
 		/* and finally the record */
 		rc = iops->rec(env, it, (struct dt_rec *)tmp_entry, attr);
@@ -736,7 +740,13 @@ static int dt_index_page_build(const struct lu_env *env, union lu_page *lp,
 			lip->lip_nr++;
 			if (unlikely(lip->lip_nr == 1 && ii->ii_count == 0))
 				ii->ii_hash_start = hash;
-			entry = tmp_entry + ii->ii_recsize;
+
+			if (ii->ii_flags & II_FL_VARREC)
+				recsize = iops->rec_size(env, it, attr);
+			else
+				recsize = ii->ii_recsize;
+
+			entry = tmp_entry + recsize;
 			nob -= size;
 		}
 
@@ -757,6 +767,7 @@ out:
 		ii->ii_hash_end = II_END_OFF;
 	return rc;
 }
+
 
 /*
  * Walk index and fill lu_page containers with key/record pairs
@@ -810,6 +821,10 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 		rc = iops->next(env, it);
 	} else if (rc > 0) {
 		rc = 0;
+	} else {
+		if (rc == -ENODATA)
+			rc = 0;
+		GOTO(out, rc);
 	}
 
 	/* Fill containers one after the other. There might be multiple
@@ -841,6 +856,7 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 		kunmap(rdpg->rp_pages[i]);
 	}
 
+out:
 	iops->put(env, it);
 	iops->fini(env, it);
 
@@ -878,15 +894,9 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 	if (rdpg->rp_count <= 0 && (rdpg->rp_count & (LU_PAGE_SIZE - 1)) != 0)
 		RETURN(-EFAULT);
 
-	if (fid_seq(&ii->ii_fid) >= FID_SEQ_NORMAL)
-		/* we don't support directory transfer via OBD_IDX_READ for the
-		 * time being */
+	if (!fid_is_quota(&ii->ii_fid) && !fid_is_layout_rbtree(&ii->ii_fid) &&
+	    !fid_is_norm(&ii->ii_fid))
 		RETURN(-EOPNOTSUPP);
-
-	if (!fid_is_quota(&ii->ii_fid) && !fid_is_layout_rbtree(&ii->ii_fid))
-		/* Block access to all local files except quota files and
-		 * layout rbtree. */
-		RETURN(-EPERM);
 
 	/* lookup index object subject to the transfer */
 	obj = dt_locate(env, dev, &ii->ii_fid);
@@ -909,25 +919,16 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 	}
 
 	/* fill ii_flags with supported index features */
-	ii->ii_flags &= II_FL_NOHASH;
+	ii->ii_flags &= (II_FL_NOHASH | II_FL_NOKEY | II_FL_VARKEY |
+			 II_FL_VARREC);
 
-	ii->ii_keysize = feat->dif_keysize_max;
-	if ((feat->dif_flags & DT_IND_VARKEY) != 0) {
-		/* key size is variable */
-		ii->ii_flags |= II_FL_VARKEY;
-		/* we don't support variable key size for the time being */
-		GOTO(out, rc = -EOPNOTSUPP);
-	}
+	if (!(feat->dif_flags & DT_IND_VARKEY))
+		ii->ii_keysize = feat->dif_keysize_max;
 
-	ii->ii_recsize = feat->dif_recsize_max;
-	if ((feat->dif_flags & DT_IND_VARREC) != 0) {
-		/* record size is variable */
-		ii->ii_flags |= II_FL_VARREC;
-		/* we don't support variable record size for the time being */
-		GOTO(out, rc = -EOPNOTSUPP);
-	}
+	if (!(feat->dif_flags & DT_IND_VARREC))
+		ii->ii_recsize = feat->dif_recsize_max;
 
-	if ((feat->dif_flags & DT_IND_NONUNQ) != 0)
+	if (!(feat->dif_flags & DT_IND_NONUNQ))
 		/* key isn't necessarily unique */
 		ii->ii_flags |= II_FL_NONUNQ;
 
@@ -938,7 +939,7 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 	}
 
 	/* walk the index and fill lu_idxpages with key/record pairs */
-	rc = dt_index_walk(env, obj, rdpg, dt_index_page_build ,ii);
+	rc = dt_index_walk(env, obj, rdpg, dt_index_page_build, ii);
 	if (!fid_is_layout_rbtree(&ii->ii_fid))
 		dt_read_unlock(env, obj);
 

lustre/include/dt_object.h
@@ -630,6 +630,9 @@ struct dt_index_operations {
                                       const struct dt_it *di,
                                       struct dt_rec *rec,
                                       __u32 attr);
+		int	   (*rec_size)(const struct lu_env *env,
+				       const struct dt_it *di,
+				      __u32 attr);
                 __u64        (*store)(const struct lu_env *env,
                                       const struct dt_it *di);
                 int           (*load)(const struct lu_env *env,

lustre/include/lustre/lustre_idl.h
@@ -2741,17 +2741,46 @@ struct lmv_desc {
 
 extern void lustre_swab_lmv_desc (struct lmv_desc *ld);
 
-/* lmv structures */
-#define LMV_MAGIC_V1	0x0CD10CD0    /* normal stripe lmv magic */
-#define LMV_USER_MAGIC	0x0CD20CD0    /* default lmv magic*/
-#define LMV_MAGIC_MIGRATE	0x0CD30CD0    /* migrate stripe lmv magic */
+/* LMV layout EA, and it will be stored both in master and slave object */
+struct lmv_mds_md_v1 {
+	__u32 lmv_magic;
+	__u32 lmv_stripe_count;
+	__u32 lmv_master_mdt_index;	/* On master object, it is master
+					 * MDT index, on slave object, it
+					 * is stripe index of the slave obj */
+	__u32 lmv_hash_type;		/* dir stripe policy, i.e. indicate
+					 * which hash function to be used,
+					 * Note: only lower 16 bits is being
+					 * used for now. Higher 16 bits will
+					 * be used to mark the object status,
+					 * for example migrating or dead. */
+	__u32 lmv_layout_version;	/* Used for directory restriping */
+	__u32 lmv_padding;
+	struct lu_fid	lmv_master_fid; /* The FID of the master object, which
+					 * is the namespace-visible dir FID */
+	char lmv_pool_name[LOV_MAXPOOLNAME];	/* pool name */
+	struct lu_fid lmv_stripe_fids[0];	/* FIDs for each stripe */
+};
+
+#define LMV_MAGIC_V1	0x0CD20CD0    /* normal stripe lmv magic */
 #define LMV_MAGIC	LMV_MAGIC_V1
 
+/* #define LMV_USER_MAGIC 0x0CD30CD0 */
+#define LMV_MAGIC_STRIPE 0x0CD40CD0 /* magic for dir sub_stripe */
+
+/* Right now only the lower part(0-16bits) of lmv_hash_type is being used,
+ * and the higher part will be the flag to indicate the status of object,
+ * for example the object is being migrated. And the hash function
+ * might be interpreted differently with different flags. */
 enum lmv_hash_type {
 	LMV_HASH_TYPE_ALL_CHARS = 1,
 	LMV_HASH_TYPE_FNV_1A_64 = 2,
-	LMV_HASH_TYPE_MIGRATION = 3,
 };
+
+#define LMV_HASH_TYPE_MASK 0x0000ffff
+
+#define LMV_HASH_FLAG_MIGRATION	0x80000000
+#define LMV_HASH_FLAG_DEAD	0x40000000
 
 #define LMV_HASH_NAME_ALL_CHARS	"all_char"
 #define LMV_HASH_NAME_FNV_1A_64	"fnv_1a_64"
@@ -2784,18 +2813,6 @@ static inline __u64 lustre_hash_fnv_1a_64(const void *buf, size_t size)
 	return hash;
 }
 
-struct lmv_mds_md_v1 {
-	__u32 lmv_magic;
-	__u32 lmv_stripe_count;		/* stripe count */
-	__u32 lmv_master_mdt_index;	/* master MDT index */
-	__u32 lmv_hash_type;		/* dir stripe policy, i.e. indicate
-					 * which hash function to be used */
-	__u32 lmv_layout_version;	/* Used for directory restriping */
-	__u32 lmv_padding;
-	char lmv_pool_name[LOV_MAXPOOLNAME];	/* pool name */
-	struct lu_fid lmv_stripe_fids[0];	/* FIDs for each stripe */
-};
-
 union lmv_mds_md {
 	__u32			 lmv_magic;
 	struct lmv_mds_md_v1	 lmv_md_v1;
@@ -2807,8 +2824,7 @@ extern void lustre_swab_lmv_mds_md(union lmv_mds_md *lmm);
 static inline int lmv_mds_md_size(int stripe_count, unsigned int lmm_magic)
 {
 	switch (lmm_magic) {
-	case LMV_MAGIC_V1:
-	case LMV_MAGIC_MIGRATE: {
+	case LMV_MAGIC_V1:{
 		struct lmv_mds_md_v1 *lmm1;
 
 		return sizeof(*lmm1) + stripe_count *
@@ -2823,7 +2839,6 @@ static inline int lmv_mds_md_stripe_count_get(const union lmv_mds_md *lmm)
 {
 	switch (le32_to_cpu(lmm->lmv_magic)) {
 	case LMV_MAGIC_V1:
-	case LMV_MAGIC_MIGRATE:
 		return le32_to_cpu(lmm->lmv_md_v1.lmv_stripe_count);
 	case LMV_USER_MAGIC:
 		return le32_to_cpu(lmm->lmv_user_md.lum_stripe_count);
@@ -2837,7 +2852,6 @@ static inline int lmv_mds_md_stripe_count_set(union lmv_mds_md *lmm,
 {
 	switch (le32_to_cpu(lmm->lmv_magic)) {
 	case LMV_MAGIC_V1:
-	case LMV_MAGIC_MIGRATE:
 		lmm->lmv_md_v1.lmv_stripe_count = cpu_to_le32(stripe_count);
 		break;
 	case LMV_USER_MAGIC:
@@ -3719,6 +3733,7 @@ enum idx_info_flags {
 	II_FL_VARKEY	= 1 << 1, /* keys can be of variable size */
 	II_FL_VARREC	= 1 << 2, /* records can be of variable size */
 	II_FL_NONUNQ	= 1 << 3, /* index supports non-unique keys */
+	II_FL_NOKEY	= 1 << 4, /* client doesn't care about key */
 };
 
 #define LIP_MAGIC 0x8A6D6B6C
@@ -3966,6 +3981,7 @@ enum update_type {
 	OUT_INDEX_INSERT	= 10,
 	OUT_INDEX_DELETE	= 11,
 	OUT_WRITE		= 12,
+	OUT_XATTR_DEL		= 13,
 	OUT_LAST
 };
 

lustre/include/lustre/lustre_user.h
@@ -300,8 +300,7 @@ struct ost_id {
 #define LOV_USER_MAGIC_JOIN_V1 0x0BD20BD0
 #define LOV_USER_MAGIC_V3 0x0BD30BD0
 
-#define LMV_MAGIC_V1      0x0CD10CD0    /*normal stripe lmv magic */
-#define LMV_USER_MAGIC    0x0CD20CD0    /*default lmv magic*/
+#define LMV_USER_MAGIC    0x0CD30CD0    /*default lmv magic*/
 
 #define LOV_PATTERN_RAID0 0x001
 #define LOV_PATTERN_RAID1 0x002

lustre/include/lustre_lmv.h
@@ -48,10 +48,35 @@ struct lmv_stripe_md {
 	__u32	lsm_md_layout_version;
 	__u32	lsm_md_default_count;
 	__u32	lsm_md_default_index;
+	struct lu_fid	lsm_md_master_fid;
 	char	lsm_md_pool_name[LOV_MAXPOOLNAME];
 	struct lmv_oinfo lsm_md_oinfo[0];
 };
 
+static inline bool
+lsm_md_eq(const struct lmv_stripe_md *lsm1, const struct lmv_stripe_md *lsm2)
+{
+	int idx;
+
+	if (lsm1->lsm_md_magic != lsm2->lsm_md_magic ||
+	    lsm1->lsm_md_stripe_count != lsm2->lsm_md_stripe_count ||
+	    lsm1->lsm_md_master_mdt_index !=
+				lsm2->lsm_md_master_mdt_index ||
+	    lsm1->lsm_md_hash_type != lsm2->lsm_md_hash_type ||
+	    lsm1->lsm_md_layout_version !=
+				lsm2->lsm_md_layout_version ||
+	    strcmp(lsm1->lsm_md_pool_name,
+		      lsm2->lsm_md_pool_name) != 0)
+		return false;
+
+	for (idx = 0; idx < lsm1->lsm_md_stripe_count; idx++) {
+		if (!lu_fid_eq(&lsm1->lsm_md_oinfo[idx].lmo_fid,
+			       &lsm2->lsm_md_oinfo[idx].lmo_fid))
+			return false;
+	}
+
+	return true;
+}
 union lmv_mds_md;
 
 int lmv_pack_md(union lmv_mds_md **lmmp, const struct lmv_stripe_md *lsm,
@@ -98,10 +123,8 @@ static inline void lmv_cpu_to_le(union lmv_mds_md *lmv_dst,
 {
 	switch (lmv_src->lmv_magic) {
 	case LMV_MAGIC_V1:
-	case LMV_MAGIC_MIGRATE: {
 		lmv1_cpu_to_le(&lmv_dst->lmv_md_v1, &lmv_src->lmv_md_v1);
 		break;
-	}
 	default:
 		break;
 	}
@@ -112,10 +135,8 @@ static inline void lmv_le_to_cpu(union lmv_mds_md *lmv_dst,
 {
 	switch (le32_to_cpu(lmv_src->lmv_magic)) {
 	case LMV_MAGIC_V1:
-	case LMV_MAGIC_MIGRATE: {
 		lmv1_le_to_cpu(&lmv_dst->lmv_md_v1, &lmv_src->lmv_md_v1);
 		break;
-	}
 	default:
 		break;
 	}

lustre/include/md_object.h
@@ -423,6 +423,14 @@ struct md_object {
         const struct md_dir_operations    *mo_dir_ops;
 };
 
+/* Mark the object to be dead, and can not be accessed anymore.
+ * XXX, right now, it will only be used for striped directory to
+ * mark the slave stripes dead, when deleting master object. It will be
+ * stored in slave LMV EA (see lod_mark_dead_object), which is only
+ * temporary, and will be removed later when we have proper way to mark
+ * the dead object. */
+#define LUSTRE_SLAVE_DEAD_FL		0x80000000
+
 /**
  * seq-server site.
  */

lustre/include/obd.h
@@ -1058,9 +1058,9 @@ struct obd_ops {
 			  struct obd_export *exp, enum lu_cli_type type);
 	int (*o_fid_fini)(struct obd_device *obd);
 
-        /* Allocate new fid according to passed @hint. */
-        int (*o_fid_alloc)(struct obd_export *exp, struct lu_fid *fid,
-                           struct md_op_data *op_data);
+	/* Allocate new fid according to passed @hint. */
+	int (*o_fid_alloc)(const struct lu_env *env, struct obd_export *exp,
+			   struct lu_fid *fid, struct md_op_data *op_data);
 
         /*
          * Object with @fid is getting deleted, we may want to do something

lustre/include/obd_class.h
@@ -1043,18 +1043,19 @@ static inline int obd_fid_fini(struct obd_device *obd)
 	RETURN(rc);
 }
 
-static inline int obd_fid_alloc(struct obd_export *exp,
+static inline int obd_fid_alloc(const struct lu_env *env,
+				struct obd_export *exp,
                                 struct lu_fid *fid,
                                 struct md_op_data *op_data)
 {
-        int rc;
-        ENTRY;
+	int rc;
+	ENTRY;
 
-        EXP_CHECK_DT_OP(exp, fid_alloc);
-        EXP_COUNTER_INCREMENT(exp, fid_alloc);
+	EXP_CHECK_DT_OP(exp, fid_alloc);
+	EXP_COUNTER_INCREMENT(exp, fid_alloc);
 
-        rc = OBP(exp->exp_obd, fid_alloc)(exp, fid, op_data);
-        RETURN(rc);
+	rc = OBP(exp->exp_obd, fid_alloc)(env, exp, fid, op_data);
+	RETURN(rc);
 }
 
 static inline int obd_ping(const struct lu_env *env, struct obd_export *exp)

commit 1c92615dbd2c26796bd9a980c61cdde916848329

LU-5675 quota: correctly set II_FL_NONUNQ in dt_index_read()
Fix regression introduced by 60e07b972114df24105a3a1bfa7365892f72a4a7
II_FL_NONUNQ should be set in the index information only when the
backend index supports DT_IND_NONUNQ.
This mistake triggers the following warning in the qsd code:

Lustre: 25029:0:(qsd_reint.c:237:qsd_reint_index()) lustre-MDT0000:
II_FL_NONUNQ is set on index transfer for fid
[0x200000005:0x1011:0x0], it shouldn't be

Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>
Change-Id: I42a9c70666c741064b356ffd253b49e5ed7b3e93

lustre/obdclass/dt_object.c
@@ -926,7 +926,7 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 	if (!(feat->dif_flags & DT_IND_VARREC))
 		ii->ii_recsize = feat->dif_recsize_max;
 
-	if (!(feat->dif_flags & DT_IND_NONUNQ))
+	if (feat->dif_flags & DT_IND_NONUNQ)
 		/* key isn't necessarily unique */
 		ii->ii_flags |= II_FL_NONUNQ;
 