de51cb7c4221e65ef591678679289d51d8c90d29
Frank Zago <fzago@cray.com>
2014-09-29 22:10:50 -0500

    LU-5396 mdc: (and lmv, mgc, osc) make some functions static
    Some functions and variables are only used in their C file, so reduce
    their scope. This reduces the code size, and fixes sparse warnings
    such as:
    
      warning: symbol 'proc_lnet_routes' was not declared.
          Should it be static?
      warning: symbol 'proc_lnet_routers' was not declared.
          Should it be static?
    
    Some prototypes were removed from C files and added to the proper
    header.
    
    Signed-off-by: Frank Zago <fzago@cray.com>
    Change-Id: I8dcc5224c1da75cfb5ef7afb1fdb0f72422a3ac0

--- lustre/mdc/mdc_request.c
+++ lustre/mdc/mdc_request.c
@@ -64,8 +64,9 @@ struct mdc_renew_capa_args {
 
 static int mdc_cleanup(struct obd_device *obd);
 
-int mdc_unpack_capa(struct obd_export *exp, struct ptlrpc_request *req,
-                    const struct req_msg_field *field, struct obd_capa **oc)
+static int mdc_unpack_capa(struct obd_export *exp, struct ptlrpc_request *req,
+			   const struct req_msg_field *field,
+			   struct obd_capa **oc)
 {
         struct lustre_capa *capa;
         struct obd_capa *c;
@@ -150,8 +151,8 @@ out:
 }
 
 /* This should be mdc_get_info("rootfid") */
-int mdc_getstatus(struct obd_export *exp, struct lu_fid *rootfid,
-                  struct obd_capa **pc)
+static int mdc_getstatus(struct obd_export *exp, struct lu_fid *rootfid,
+			 struct obd_capa **pc)
 {
         return send_getstatus(class_exp2cliimp(exp), rootfid, pc,
                               LUSTRE_IMP_FULL, 0);
@@ -216,8 +217,8 @@ static int mdc_getattr_common(struct obd_export *exp,
         RETURN(0);
 }
 
-int mdc_getattr(struct obd_export *exp, struct md_op_data *op_data,
-                struct ptlrpc_request **request)
+static int mdc_getattr(struct obd_export *exp, struct md_op_data *op_data,
+		       struct ptlrpc_request **request)
 {
         struct ptlrpc_request *req;
         int                    rc;
@@ -261,8 +262,8 @@ int mdc_getattr(struct obd_export *exp, struct md_op_data *op_data,
         RETURN(rc);
 }
 
-int mdc_getattr_name(struct obd_export *exp, struct md_op_data *op_data,
-                     struct ptlrpc_request **request)
+static int mdc_getattr_name(struct obd_export *exp, struct md_op_data *op_data,
+			    struct ptlrpc_request **request)
 {
         struct ptlrpc_request *req;
         int                    rc;
@@ -418,10 +419,12 @@ static int mdc_xattr_common(struct obd_export *exp,const struct req_format *fmt,
         RETURN(rc);
 }
 
-int mdc_setxattr(struct obd_export *exp, const struct lu_fid *fid,
-                 struct obd_capa *oc, obd_valid valid, const char *xattr_name,
-                 const char *input, int input_size, int output_size,
-                 int flags, __u32 suppgid, struct ptlrpc_request **request)
+static int mdc_setxattr(struct obd_export *exp, const struct lu_fid *fid,
+			struct obd_capa *oc, obd_valid valid,
+			const char *xattr_name,
+			const char *input, int input_size, int output_size,
+			int flags, __u32 suppgid,
+			struct ptlrpc_request **request)
 {
         return mdc_xattr_common(exp, &RQF_MDS_REINT_SETXATTR,
                                 fid, oc, MDS_REINT, valid, xattr_name,
@@ -429,10 +432,11 @@ int mdc_setxattr(struct obd_export *exp, const struct lu_fid *fid,
                                 suppgid, request);
 }
 
-int mdc_getxattr(struct obd_export *exp, const struct lu_fid *fid,
-                 struct obd_capa *oc, obd_valid valid, const char *xattr_name,
-                 const char *input, int input_size, int output_size,
-                 int flags, struct ptlrpc_request **request)
+static int mdc_getxattr(struct obd_export *exp, const struct lu_fid *fid,
+			struct obd_capa *oc, obd_valid valid,
+			const char *xattr_name,
+			const char *input, int input_size, int output_size,
+			int flags, struct ptlrpc_request **request)
 {
         return mdc_xattr_common(exp, &RQF_MDS_GETXATTR,
                                 fid, oc, MDS_GETXATTR, valid, xattr_name,
@@ -833,8 +837,8 @@ static void mdc_close_handle_reply(struct ptlrpc_request *req,
         }
 }
 
-int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
-              struct md_open_data *mod, struct ptlrpc_request **request)
+static int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
+		     struct md_open_data *mod, struct ptlrpc_request **request)
 {
 	struct obd_device     *obd = class_exp2obd(exp);
 	struct ptlrpc_request *req;
@@ -951,8 +955,8 @@ int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
         RETURN(rc < 0 ? rc : saved_rc);
 }
 
-int mdc_done_writing(struct obd_export *exp, struct md_op_data *op_data,
-                     struct md_open_data *mod)
+static int mdc_done_writing(struct obd_export *exp, struct md_op_data *op_data,
+			    struct md_open_data *mod)
 {
         struct obd_device     *obd = class_exp2obd(exp);
         struct ptlrpc_request *req;
@@ -2417,9 +2421,9 @@ out:
 	return rc;
 }
 
-int mdc_get_info_rpc(struct obd_export *exp,
-                     obd_count keylen, void *key,
-                     int vallen, void *val)
+static int mdc_get_info_rpc(struct obd_export *exp,
+			    obd_count keylen, void *key,
+			    int vallen, void *val)
 {
         struct obd_import      *imp = class_exp2cliimp(exp);
         struct ptlrpc_request  *req;
@@ -2599,11 +2603,11 @@ static int mdc_kuc_reregister(struct obd_import *imp)
 					 (void *)imp);
 }
 
-int mdc_set_info_async(const struct lu_env *env,
-		       struct obd_export *exp,
-		       obd_count keylen, void *key,
-		       obd_count vallen, void *val,
-		       struct ptlrpc_request_set *set)
+static int mdc_set_info_async(const struct lu_env *env,
+			      struct obd_export *exp,
+			      obd_count keylen, void *key,
+			      obd_count vallen, void *val,
+			      struct ptlrpc_request_set *set)
 {
 	struct obd_import	*imp = class_exp2cliimp(exp);
 	int			 rc;
@@ -2651,9 +2655,9 @@ int mdc_set_info_async(const struct lu_env *env,
 	RETURN(-EINVAL);
 }
 
-int mdc_get_info(const struct lu_env *env, struct obd_export *exp,
-		 __u32 keylen, void *key, __u32 *vallen, void *val,
-		 struct lov_stripe_md *lsm)
+static int mdc_get_info(const struct lu_env *env, struct obd_export *exp,
+			__u32 keylen, void *key, __u32 *vallen, void *val,
+			struct lov_stripe_md *lsm)
 {
 	int rc = -EINVAL;
 
@@ -2715,8 +2719,8 @@ int mdc_get_info(const struct lu_env *env, struct obd_export *exp,
         RETURN(rc);
 }
 
-int mdc_fsync(struct obd_export *exp, const struct lu_fid *fid,
-	      struct obd_capa *oc, struct ptlrpc_request **request)
+static int mdc_fsync(struct obd_export *exp, const struct lu_fid *fid,
+		     struct obd_capa *oc, struct ptlrpc_request **request)
 {
         struct ptlrpc_request *req;
         int                    rc;
@@ -2809,7 +2813,8 @@ int mdc_fid_alloc(const struct lu_env *env, struct obd_export *exp,
 	RETURN(seq_client_alloc_fid(env, seq, fid));
 }
 
-struct obd_uuid *mdc_get_uuid(struct obd_export *exp) {
+static struct obd_uuid *mdc_get_uuid(struct obd_export *exp)
+{
         struct client_obd *cli = &exp->exp_obd->u.cli;
         return &cli->cl_target_uuid;
 }
@@ -2841,7 +2846,7 @@ static int mdc_resource_inode_free(struct ldlm_resource *res)
 	return 0;
 }
 
-struct ldlm_valblock_ops inode_lvbo = {
+static struct ldlm_valblock_ops inode_lvbo = {
 	.lvbo_free = mdc_resource_inode_free
 };
 
@@ -3010,9 +3015,9 @@ static int mdc_process_config(struct obd_device *obd, obd_count len, void *buf)
 
 
 /* get remote permission for current user on fid */
-int mdc_get_remote_perm(struct obd_export *exp, const struct lu_fid *fid,
-                        struct obd_capa *oc, __u32 suppgid,
-                        struct ptlrpc_request **request)
+static int mdc_get_remote_perm(struct obd_export *exp, const struct lu_fid *fid,
+			       struct obd_capa *oc, __u32 suppgid,
+			       struct ptlrpc_request **request)
 {
         struct ptlrpc_request  *req;
         int                    rc;
@@ -3103,7 +3108,7 @@ static int mdc_renew_capa(struct obd_export *exp, struct obd_capa *oc,
         RETURN(0);
 }
 
-struct obd_ops mdc_obd_ops = {
+static struct obd_ops mdc_obd_ops = {
         .o_owner            = THIS_MODULE,
         .o_setup            = mdc_setup,
         .o_precleanup       = mdc_precleanup,
@@ -3128,7 +3133,7 @@ struct obd_ops mdc_obd_ops = {
         .o_quotacheck       = mdc_quotacheck
 };
 
-struct md_ops mdc_md_ops = {
+static struct md_ops mdc_md_ops = {
         .m_getstatus        = mdc_getstatus,
         .m_null_inode	    = mdc_null_inode,
         .m_find_cbdata      = mdc_find_cbdata,
@@ -3162,7 +3167,7 @@ struct md_ops mdc_md_ops = {
         .m_revalidate_lock      = mdc_revalidate_lock
 };
 
-int __init mdc_init(void)
+static int __init mdc_init(void)
 {
 	return class_register_type(&mdc_obd_ops, &mdc_md_ops, true, NULL,
 				   LUSTRE_MDC_NAME, NULL);
--- lustre/osp/osp_object.c
+++ lustre/osp/osp_object.c
@@ -1473,7 +1473,7 @@ again0:
 	RETURN(rc);
 }
 
-int osp_orphan_it_next(const struct lu_env *env, struct dt_it *di)
+static int osp_orphan_it_next(const struct lu_env *env, struct dt_it *di)
 {
 	struct osp_it		*it = (struct osp_it *)di;
 	struct lu_idxpage	*idxpage;
@@ -1514,8 +1514,8 @@ void osp_it_put(const struct lu_env *env, struct dt_it *di)
 {
 }
 
-struct dt_key *osp_orphan_it_key(const struct lu_env *env,
-				 const struct dt_it *di)
+static struct dt_key *osp_orphan_it_key(const struct lu_env *env,
+					const struct dt_it *di)
 {
 	struct osp_it	*it  = (struct osp_it *)di;
 	struct lu_orphan_ent	*ent = (struct lu_orphan_ent *)it->ooi_ent;
@@ -1526,13 +1526,14 @@ struct dt_key *osp_orphan_it_key(const struct lu_env *env,
 	return NULL;
 }
 
-int osp_orphan_it_key_size(const struct lu_env *env, const struct dt_it *di)
+static int osp_orphan_it_key_size(const struct lu_env *env,
+				  const struct dt_it *di)
 {
 	return sizeof(struct lu_fid);
 }
 
-int osp_orphan_it_rec(const struct lu_env *env, const struct dt_it *di,
-		      struct dt_rec *rec, __u32 attr)
+static int osp_orphan_it_rec(const struct lu_env *env, const struct dt_it *di,
+			     struct dt_rec *rec, __u32 attr)
 {
 	struct osp_it	*it  = (struct osp_it *)di;
 	struct lu_orphan_ent	*ent = (struct lu_orphan_ent *)it->ooi_ent;
@@ -1615,7 +1616,7 @@ static int osp_index_try(const struct lu_env *env,
 	return 0;
 }
 
-struct dt_object_operations osp_obj_ops = {
+static struct dt_object_operations osp_obj_ops = {
 	.do_declare_attr_get	= osp_declare_attr_get,
 	.do_attr_get		= osp_attr_get,
 	.do_declare_attr_set	= osp_declare_attr_set,
