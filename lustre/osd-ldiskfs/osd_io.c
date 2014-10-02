3f68dc7a13f791dfa9eb870406ae3e2eab447970
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2014-10-02 14:10:44 +0400

    LU-5794 osd: additional checks to verify credits calculation
    every operation counts credits consumed, so we can check when
    credits are exceeded. now the number of declarations doesn't
    matter for the checks, it's only credits. i.e. it's OK to
    declare a single write for a specific range and then make many
    writes to that range within a single transaction.
    
    the patch fixes few issues on osd-zfs making it impossible to
    declare change to an object being created in a same transaction.
    
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Change-Id: I714ccae7ce957e639938df3bed0c4e8fb49d3454

--- lustre/osd-ldiskfs/osd_io.c
+++ lustre/osd-ldiskfs/osd_io.c
@@ -1069,6 +1069,7 @@ static int osd_declare_write_commit(const struct lu_env *env,
         int                      newblocks;
 	int			 rc = 0;
 	int			 flags = 0;
+	int			 credits = 0;
 	bool			 ignore_quota = false;
 	long long		 quota_space = 0;
 	ENTRY;
@@ -1116,13 +1117,13 @@ static int osd_declare_write_commit(const struct lu_env *env,
                 depth = ext_depth(inode);
                 depth = max(depth, 1) + 1;
                 newblocks += depth;
-                oh->ot_credits++; /* inode */
-                oh->ot_credits += depth * 2 * extents;
-        } else {
-                depth = 3;
-                newblocks += depth;
-                oh->ot_credits++; /* inode */
-                oh->ot_credits += depth * extents;
+		credits++; /* inode */
+		credits += depth * 2 * extents;
+	} else {
+		depth = 3;
+		newblocks += depth;
+		credits++; /* inode */
+		credits += depth * extents;
         }
 
 	/* quota space for metadata blocks */
@@ -1135,15 +1136,17 @@ static int osd_declare_write_commit(const struct lu_env *env,
 
         /* we can't dirty more bitmap blocks than exist */
         if (newblocks > LDISKFS_SB(osd_sb(osd))->s_groups_count)
-                oh->ot_credits += LDISKFS_SB(osd_sb(osd))->s_groups_count;
+		credits += LDISKFS_SB(osd_sb(osd))->s_groups_count;
         else
-                oh->ot_credits += newblocks;
+		credits += newblocks;
 
         /* we can't dirty more gd blocks than exist */
         if (newblocks > LDISKFS_SB(osd_sb(osd))->s_gdb_count)
-                oh->ot_credits += LDISKFS_SB(osd_sb(osd))->s_gdb_count;
+		credits += LDISKFS_SB(osd_sb(osd))->s_gdb_count;
         else
-                oh->ot_credits += newblocks;
+		credits += newblocks;
+
+	osd_trans_declare_op(env, oh, OSD_OT_WRITE, credits);
 
 	/* make sure the over quota flags were not set */
 	lnb[0].lnb_flags &= ~(OBD_BRW_OVER_USRQUOTA | OBD_BRW_OVER_GRPQUOTA);
@@ -1219,6 +1222,8 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 		osd_iobuf_add_page(iobuf, lnb[i].lnb_page);
         }
 
+	osd_trans_exec_op(env, thandle, OSD_OT_WRITE);
+
         if (OBD_FAIL_CHECK(OBD_FAIL_OST_MAPBLK_ENOSPC)) {
                 rc = -ENOSPC;
         } else if (iobuf->dr_npages > 0) {
@@ -1243,6 +1248,8 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 	} else {
 		osd_fini_iobuf(osd, iobuf);
 	}
+
+	osd_trans_exec_check(env, thandle, OSD_OT_WRITE);
 
         if (unlikely(rc != 0)) {
                 /* if write fails, we should drop pages from the cache */
@@ -1698,6 +1705,8 @@ static ssize_t osd_write(const struct lu_env *env, struct dt_object *dt,
 
         oh = container_of(handle, struct osd_thandle, ot_super);
         LASSERT(oh->ot_handle->h_transaction != NULL);
+	osd_trans_exec_op(env, handle, OSD_OT_WRITE);
+
 	/* Write small symlink to inode body as we need to maintain correct
 	 * on-disk symlinks for ldiskfs.
 	 * Note: the buf->lb_buf contains a NUL terminator while buf->lb_len
@@ -1710,9 +1719,12 @@ static ssize_t osd_write(const struct lu_env *env, struct dt_object *dt,
 		result = osd_ldiskfs_write_record(inode, buf->lb_buf,
 						  buf->lb_len, is_link, pos,
 						  oh->ot_handle);
-        if (result == 0)
-                result = buf->lb_len;
-        return result;
+	if (result == 0)
+		result = buf->lb_len;
+
+	osd_trans_exec_check(env, handle, OSD_OT_WRITE);
+
+	return result;
 }
 
 static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
@@ -1790,6 +1802,10 @@ static int osd_punch(const struct lu_env *env, struct dt_object *dt,
         h = journal_current_handle();
         LASSERT(h != NULL);
         LASSERT(h == oh->ot_handle);
+
+	/* do not check credits with osd_trans_exec_check() as the truncate
+	 * can restart the transaction internally and we restart the
+	 * transaction in this case */
 
         if (tid != h->h_transaction->t_tid) {
                 int credits = oh->ot_credits;
