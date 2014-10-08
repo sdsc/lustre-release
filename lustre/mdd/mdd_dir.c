4a5a7ed7ae9676b47365b5780270f40280ad3254
Wang Di <di.wang@intel.com>
2014-10-07 20:42:47 -0700

    LU-3537 mdt: allow cross-MDT rename and link
    Remove checks for cross-MDT operation, so cross-MDT
    rename and link will be allowed.
    
    Remove obsolete locality parameters in MDT lock API after all of
    cross-MDT operations are allowed.
    
    Change-Id: I29874d929f98593d00f5cbd836c7ce681d51add7
    Signed-off-by: Wang Di <di.wang@intel.com>

--- lustre/mdd/mdd_dir.c
+++ lustre/mdd/mdd_dir.c
@@ -181,18 +181,22 @@ static int mdd_is_parent(const struct lu_env *env,
 			GOTO(out, rc = 0);
                 if (lu_fid_eq(pfid, lf))
                         GOTO(out, rc = 1);
+
+		/* Note: we do not enqueue the parent lock, so remove
+		 * the remote object from the cache immediately,
+		 * otherwise if the parent is striped directory and
+		 * being left in memory, it might hold some sub_stripe
+		 * object ref_count, which can not be released, then
+		 * caused some deadlock there */
+		if (parent && mdd_object_remote(parent))
+			set_bit(LU_OBJECT_HEARD_BANSHEE,
+				&parent->mod_obj.mo_lu.lo_header->loh_flags);
                 if (parent)
                         mdd_object_put(env, parent);
 
 		parent = mdd_object_find(env, mdd, pfid);
-		if (IS_ERR(parent)) {
+		if (IS_ERR(parent))
 			GOTO(out, rc = PTR_ERR(parent));
-		} else if (mdd_object_remote(parent)) {
-			/*FIXME: Because of the restriction of rename in Phase I.
-			 * If the parent is remote, we just assumed lf is not the
-			 * parent of P1 for now */
-			GOTO(out, rc = 0);
-		}
 		p1 = parent;
         }
         EXIT;
@@ -2631,15 +2635,13 @@ static int mdd_declare_rename(const struct lu_env *env,
 	if (rc != 0)
 		return rc;
 
-        /* name from target dir (old name), we declare it unconditionally
-         * as mdd_rename() calls delete unconditionally as well. so just
-         * to balance declarations vs calls to change ... */
-        rc = mdo_declare_index_delete(env, mdd_tpobj, tname->ln_name, handle);
-        if (rc)
-                return rc;
-
         if (mdd_tobj && mdd_object_exists(mdd_tobj)) {
                 /* delete target child in target parent directory */
+		rc = mdo_declare_index_delete(env, mdd_tpobj, tname->ln_name,
+					      handle);
+		if (rc)
+			return rc;
+
                 rc = mdo_declare_ref_del(env, mdd_tobj, handle);
                 if (rc)
                         return rc;
@@ -2779,20 +2781,13 @@ static int mdd_rename(const struct lu_env *env,
                         GOTO(fixup_spobj, rc);
         }
 
-        /* Remove target name from target directory
-         * Here tobj can be remote one, so we do index_delete unconditionally
-         * and -ENOENT is allowed.
-         */
-        rc = __mdd_index_delete(env, mdd_tpobj, tname, is_dir, handle,
-                                mdd_object_capa(env, mdd_tpobj));
-        if (rc != 0) {
-                if (mdd_tobj) {
-                        /* tname might been renamed to something else */
-                        GOTO(fixup_spobj, rc);
-                }
-                if (rc != -ENOENT)
-                        GOTO(fixup_spobj, rc);
-        }
+	if (mdd_tobj != NULL && mdd_object_exists(mdd_tobj)) {
+		rc = __mdd_index_delete(env, mdd_tpobj, tname, is_dir, handle,
+					mdd_object_capa(env, mdd_tpobj));
+		if (rc != 0)
+			/* tname might been renamed to something else */
+			GOTO(fixup_spobj, rc);
+	}
 
         /* Insert new fid with target name into target dir */
 	rc = __mdd_index_insert(env, mdd_tpobj, lf, cattr->la_mode,
