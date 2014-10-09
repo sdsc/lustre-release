6f51854e5513e6d0f7bd7fb082eee6d6db27020a
Niu Yawei <yawei.niu@intel.com>
2014-10-09 04:11:31 -0400

    LU-5006 mdd: don't call attr_set on object create
    The object attr has been initialzed in OSD layer when create
    object, it's not necessary to initialize it again in MDD layer.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I6f4094d4384b2c153d4dad2666d64281c0450059

--- lustre/osp/osp_object.c
+++ lustre/osp/osp_object.c
@@ -422,10 +422,6 @@ static int __osp_attr_set(const struct lu_env *env, struct dt_object *dt,
 			RETURN(rc);
 	}
 
-	if (o->opo_new)
-		/* no need in logging for new objects being created */
-		RETURN(0);
-
 	if (!(attr->la_valid & (LA_UID | LA_GID)))
 		RETURN(0);
 
@@ -518,15 +514,6 @@ static int osp_attr_set(const struct lu_env *env, struct dt_object *dt,
 	/* we're interested in uid/gid changes only */
 	if (!(attr->la_valid & (LA_UID | LA_GID)))
 		RETURN(0);
-
-	/* new object, the very first ->attr_set()
-	 * initializing attributes needs no logging
-	 * all subsequent one are subject to the
-	 * logging and synchronization with OST */
-	if (o->opo_new) {
-		o->opo_new = 0;
-		RETURN(0);
-	}
 
 	if (!is_only_remote_trans(th))
 		/*
@@ -1142,10 +1129,6 @@ static int osp_object_create(const struct lu_env *env, struct dt_object *dt,
 			spin_unlock(&d->opd_pre_lock);
 		}
 	}
-
-	/* new object, the very first ->attr_set()
-	 * initializing attributes needs no logging */
-	o->opo_new = 1;
 
 	/* Only need update last_used oid file, seq file will only be update
 	 * during seq rollover */
