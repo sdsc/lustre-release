c68476349a28cfadc0cd52af0a53262f938a9d61
Wang Di <di.wang@intel.com>
2014-10-22 22:50:10 -0700

    LU-3534 update: change sync updates to async updates
    1. change sync updates to async updates.
    
    2. add tests(110,111,112,115) in replay-single to test
    async update recovery.
    
    3. Add replay-barrier for those old DNE replay tests,
    so all of MDTs can be recoveried correctly.
    
    4. enable DNE failover to dbench test (replay-single.sh 70b).
    
    5. Add test_26 in replay-dual for run_tar & run_dbench test.
    
    Change-Id: Ibe1ca42d60c7a0ec870b9cf4a70a71adc663b951
    Signed-off-by: Wang Di <di.wang@intel.com>

--- lustre/tests/run_dbench.sh
+++ lustre/tests/run_dbench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TMP=${TMP:-/tmp}
+TESTDIR=${TESTDIR:-$MOUNT/d0.dbench-$(hostname)}
 
 TESTLOG_PREFIX=${TESTLOG_PREFIX:-$TMP/recovery-mds-scale}
 TESTNAME=${TESTNAME:-""}
@@ -15,23 +16,31 @@ rm -f $LOG $DEBUGLOG
 exec 2>$DEBUGLOG
 set -x
 
+. $(dirname $0)/test-framework.sh
 . $(dirname $0)/functions.sh
 
+
 assert_env MOUNT END_RUN_FILE LOAD_PID_FILE
+assert_env MOUNT END_RUN_FILE LOAD_PID_FILE LFS CLIENT_COUNT MDTCOUNT
 
 trap signaled TERM
+rm -rf $TESTDIR
 
 # recovery-*-scale scripts use this to signal the client loads to die
 echo $$ >$LOAD_PID_FILE
-
-TESTDIR=$MOUNT/d0.dbench-$(hostname)
 
 CONTINUE=true
 
 while [ ! -e "$END_RUN_FILE" ] && $CONTINUE; do
 	echoerr "$(date +'%F %H:%M:%S'): dbench run starting"
 
-	mkdir -p $TESTDIR
+	MDT_IDX=$((RANDOM % MDTCOUNT))
+	test_mkdir -i $MDT_IDX -c$MDTCOUNT $TESTDIR || {
+		echoerr "create $TESTDIR fails"
+		echo $(hostname) >> $END_RUN_FILE
+		break
+	}
+	$LFS setdirstripe -D -c$MDTCOUNT $TESTDIR
 
 	sync
 	rundbench -D $TESTDIR 2 1>$LOG &
