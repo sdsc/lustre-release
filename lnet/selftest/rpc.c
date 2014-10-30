4a21e016bd6ece54dadab0eb840b6b47e3b3cf5a
Liang Zhen <liang.zhen@intel.com>
2014-10-30 13:33:04 +0800

    LU-5718 lnet: add offset for selftest brw
    In current lnet selftest, both client and server side bulk have
    no offset and we can only test page aligned IO, this patch changed
    this:
    
    - user can set brw offset by lst add_test ... brw off=OFFSET ...
    - offset is only effective on client side so far
    - to simply implementation, offset needs to be eight bytes aligned
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Change-Id: I34690d2c8fbb627b827291afcd46d7aa59831a3f

--- lnet/selftest/rpc.c
+++ lnet/selftest/rpc.c
@@ -88,14 +88,13 @@ void srpc_set_counters (const srpc_counters_t *cnt)
 }
 
 static int
-srpc_add_bulk_page(srpc_bulk_t *bk, struct page *pg, int i, int nob)
+srpc_add_bulk_page(srpc_bulk_t *bk, struct page *pg, int i, int off, int nob)
 {
-	nob = min(nob, (int)PAGE_CACHE_SIZE);
-
-	LASSERT(nob > 0);
+	LASSERT(off < PAGE_CACHE_SIZE);
+	LASSERT(nob > 0 && nob <= PAGE_CACHE_SIZE);
 	LASSERT(i >= 0 && i < bk->bk_niov);
 
-	bk->bk_iovs[i].kiov_offset = 0;
+	bk->bk_iovs[i].kiov_offset = off;
 	bk->bk_iovs[i].kiov_page   = pg;
 	bk->bk_iovs[i].kiov_len    = nob;
 	return nob;
@@ -121,7 +120,8 @@ srpc_free_bulk (srpc_bulk_t *bk)
 }
 
 srpc_bulk_t *
-srpc_alloc_bulk(int cpt, unsigned bulk_npg, unsigned bulk_len, int sink)
+srpc_alloc_bulk(int cpt, unsigned bulk_off, unsigned bulk_npg,
+		unsigned bulk_len, int sink)
 {
 	srpc_bulk_t  *bk;
 	int	      i;
@@ -151,8 +151,12 @@ srpc_alloc_bulk(int cpt, unsigned bulk_npg, unsigned bulk_len, int sink)
 			return NULL;
 		}
 
-		nob = srpc_add_bulk_page(bk, pg, i, bulk_len);
+		nob = min_t(unsigned, bulk_off + bulk_len, PAGE_CACHE_SIZE) -
+		      bulk_off;
+
+		srpc_add_bulk_page(bk, pg, i, bulk_off, nob);
 		bulk_len -= nob;
+		bulk_off = 0;
 	}
 
 	return bk;
