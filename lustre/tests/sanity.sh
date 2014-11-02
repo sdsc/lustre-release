e38183ca044c917946ae831ac995b92cd81eebac
Andreas Dilger <andreas.dilger@intel.com>
2014-11-02 13:46:59 -0500

    LU-3888 utils: print lmm_fid as part of getstripe
    Print the lmm_fid in the standard DFID format in addition to the older
    lmm_object_id and lmm_seq fields when using "lfs getstripe -v".  This
    is still a bit of a hack, since lmm_mds_md_{v1,v3} do not store a
    proper FID in the header, so the fields need to be parsed separately
    instead of using lmm_oi_seq() and lmm_oi_id(), but at least userspace
    can start using this output in scripts instead of needing to call
    path2fid separately.
    
    In the future, new layout formats should use proper FIDs instead of
    ost_id fields.
    
    Fix lfs_fid2path() to not complain on the MDS if an invalid FID is
    requested, since an error is returned to the application anyway.
    Don't print errors in llapi_fid2path(), there is already one being
    printed in lfs_fid2path().
    
    Signed-off-by: Lokesh Nagappa Jaliminche <lokesh.jaliminche@seagate.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I4e27797ec3905327ea0a88911b1c75193d500c1e

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -4382,6 +4382,23 @@ test_56a() {	# was test_56
 		rror "$GETSTRIPE $DIR/$tdir: showed lmm_magic"
 	echo "$GETSTRIPE --verbose passed."
 
+	#test lfs getstripe with --fid
+	[[ $($GETSTRIPE --fid $DIR/$tdir |
+		grep -c lmm_fid) -eq $NUMFILES ]] ||
+		error "$GETSTRIPE --fid $DIR/$tdir: want $NUMFILES"
+	[[ $($GETSTRIPE $DIR/$tdir | grep -c lmm_fid) -eq 0 ]] ||
+		rror "$GETSTRIPE $DIR/$tdir: showed lmm_fid"
+	echo "$GETSTRIPE --fid passed."
+
+#check for fid information
+	local fid_check1=$($GETSTRIPE --fid  $DIR/$tdir/file1 |
+			grep "lmm_fid" )
+	local fid_check2=$($GETSTRIPE --verbose  $DIR/$tdir/file1 |
+			grep "lmm_fid" )
+	if [ "$fid_check1" != "$fid_check2" ];then
+			error "wrong fid information"
+	fi
+
         #test lfs getstripe with --obd
         $GETSTRIPE --obd wrong_uuid $DIR/$tdir 2>&1 |
 		grep -q "unknown obduuid" ||
