a66a7e47dd7b551a602a7a13d377a8cd7b356e71
Fan Yong <fan.yong@intel.com>
2014-11-04 07:08:40 +0800

    LU-6146 tests: race condition for check/use cfs_fail_val
    There are some race conditions when check/use cfs_fail_val.
    For example: when inject failure stub for LFSCK test as following:
    
    764   if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_DELAY2) &&
    765       cfs_fail_val > 0) {
    766           struct l_wait_info lwi;
    767
    768           lwi = LWI_TIMEOUT(cfs_time_seconds(cfs_fail_val),
    769                             NULL, NULL);
    770           l_wait_event(thread->t_ctl_waitq,
    771                        !thread_is_running(thread),
    772                        &lwi);
    773
    774           if (unlikely(!thread_is_running(thread))) {
    775                   CDEBUG(D_LFSCK, "%s: scan dir exit for engine "
    776                          "stop, parent "DFID", cookie "LPX64"n",
    777                          lfsck_lfsck2name(lfsck),
    778                          PFID(lfsck_dto2fid(dir)),
    779                          lfsck->li_cookie_dir);
    780                   RETURN(0);
    781           }
    782   }
    
    The "cfs_fail_val" may be changed as zero by others after the check
    at the line 765 but before using it at the line 768. Then the LFSCK
    engine will fall into "wait" until someone run "lfsck_stop".
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I418621faaf6a1f42ba1d541b37374c1dc21831be

--- lustre/osd-ldiskfs/osd_scrub.c
+++ lustre/osd-ldiskfs/osd_scrub.c
@@ -969,13 +969,14 @@ static int osd_scrub_next(struct osd_thread_info *info, struct osd_device *dev,
 	int		      rc;
 
 	if (OBD_FAIL_CHECK(OBD_FAIL_OSD_SCRUB_DELAY) && cfs_fail_val > 0) {
-		struct l_wait_info lwi;
+		struct l_wait_info lwi =
+			LWI_TIMEOUT(cfs_time_seconds(cfs_fail_val), NULL, NULL);
 
-		lwi = LWI_TIMEOUT(cfs_time_seconds(cfs_fail_val), NULL, NULL);
-		l_wait_event(thread->t_ctl_waitq,
-			     !list_empty(&scrub->os_inconsistent_items) ||
-			     !thread_is_running(thread),
-			     &lwi);
+		if (likely(lwi.lwi_timeout > 0))
+			l_wait_event(thread->t_ctl_waitq,
+				!list_empty(&scrub->os_inconsistent_items) ||
+				!thread_is_running(thread),
+				&lwi);
 	}
 
 	if (OBD_FAIL_CHECK(OBD_FAIL_OSD_SCRUB_CRASH)) {
