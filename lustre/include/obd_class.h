596335da97f39e9e0371ba1e5373edc678248846
Andriy Skulysh <andriy.skulysh@seagate.com>
2014-11-13 10:33:25 +0200

    LU-2525 ldlm: add asynchronous flocks
    Add support of asynchronous flocks.
    They are used only by Linux nfsd for now.
    
    Xyratex-bug-id: MRP-656
    Change-Id: Iefafaf014fd06d569dc5d1dd22ebb3518d04e99a
    Signed-off-by: Cliff White <cliffwhite@intel.com>
    Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>

--- lustre/include/obd_class.h
+++ lustre/include/obd_class.h
@@ -1435,6 +1435,22 @@ static inline int md_enqueue(struct obd_export *exp,
         RETURN(rc);
 }
 
+static inline int md_enqueue_async(struct obd_export *exp,
+				   struct ldlm_enqueue_info *einfo,
+				   obd_enqueue_update_f upcall,
+				   struct md_op_data *op_data,
+				   const ldlm_policy_data_t *policy,
+				   __u64 lock_flags)
+{
+	int rc;
+	ENTRY;
+	EXP_CHECK_MD_OP(exp, enqueue_async);
+	EXP_MD_COUNTER_INCREMENT(exp, enqueue_async);
+	rc = MDP(exp->exp_obd, enqueue_async)(exp, einfo, upcall, op_data,
+					      policy, lock_flags);
+	RETURN(rc);
+}
+
 static inline int md_getattr_name(struct obd_export *exp,
                                   struct md_op_data *op_data,
                                   struct ptlrpc_request **request)
