b8dc123680af044cfc48f81edbf9923c58c1c886
Liang Zhen <liang.zhen@intel.com>
2014-11-17 23:35:54 +0800

    LU-5579 test: fixes for OBD_FAIL_LDLM_REPLY etc
    OBD_FAIL_LDLM_REPLY is missing from tgt_enqueue, and it's actually
    not suitable for tgt_enqueue anymore because tgt_enqueue() is a
    common handler now.
    
    This patch includes a few changes:
    - tgt_enqueue sets tgt_session_info::tsi_reply_fail_id to
      OBD_FAIL_MGS/MDS/OST_LDLM_REPLY_NET based on type of target.
    
    - rewrite test_52 of replay-single, the only reason that test_52
      can pass is because there is a typo:
    
      $CHECKSTAT -t file $DIR/$tfile-* which should be $DIR/$tfile
    
    - add definitions for OBD_FAIL_LDLM_SRV_CP/BL/GL_AST and resolve
      OBD_FAIL conflictions
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Change-Id: I754dc990e3ea9396af717a597b9c169ef74700f2

--- lustre/tests/replay-single.sh
+++ lustre/tests/replay-single.sh
@@ -1066,17 +1066,29 @@ run_test 50 "Double OSC recovery, don't LASSERT (3812)"
 
 # b3764 timed out lock replay
 test_52() {
+    if [ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.6.90) ]; then
+	echo "ignored"
+	return 0
+    fi
+
     touch $DIR/$tfile
     cancel_lru_locks mdc
 
-    multiop $DIR/$tfile s || return 1
-    replay_barrier $SINGLEMDS
-#define OBD_FAIL_LDLM_REPLY              0x30c
-    do_facet $SINGLEMDS "lctl set_param fail_loc=0x8000030c"
-    fail $SINGLEMDS || return 2
-    do_facet $SINGLEMDS "lctl set_param fail_loc=0x0"
+    multiop_bg_pause $DIR/$tfile s_s || return 1
+    mpid=$!
 
-    $CHECKSTAT -t file $DIR/$tfile-* && return 3 || true
+    #define OBD_FAIL_MDS_LDLM_REPLY_NET	0x157
+    lctl set_param -n ldlm.cancel_unused_locks_before_replay "0"
+    do_facet $SINGLEMDS "lctl set_param fail_loc=0x80000157"
+
+    fail $SINGLEMDS || return 2
+    kill -USR1 $mpid
+    wait $mpid || return 3
+
+    do_facet $SINGLEMDS "lctl set_param fail_loc=0x0"
+    lctl set_param fail_loc=0x0
+    lctl set_param -n ldlm.cancel_unused_locks_before_replay "1"
+    rm -f $DIR/$tfile
 }
 run_test 52 "time out lock replay (3764)"
 
