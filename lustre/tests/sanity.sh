467a9cc1c58ff93f56e992bef8baa3f7b52fbaaa
Li Wei <wei.g.li@intel.com>
2014-11-29 11:21:19 +0800

    LU-0000 lustre: For build only
    This combines the following patches:
    
      http://review.whamcloud.com/12944 patch set 1
      http://review.whamcloud.com/12945 patch set 1
      http://review.whamcloud.com/12946 patch set 1
      http://review.whamcloud.com/12435 patch set 3
      http://review.whamcloud.com/12828 patch set 6
    
    Test-Parameters: fortestonly
    Change-Id: Icb1dbebeaf39eba095add6d7bb97966742ae0086
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -6575,6 +6575,19 @@ test_102n() { # LU-4101 mdt: protect internal xattrs
 }
 run_test 102n "silently ignore setxattr on internal trusted xattrs"
 
+test_102q() {
+	orphan_linkea_check $DIR/$tfile || error "orphan_linkea_check"
+}
+run_test 102q "flistxattr should not return trusted.link EAs for orphans"
+
+test_102r() {
+	touch $DIR/$tfile || error "touch"
+	setfattr -n user.$(basename $tfile) $DIR/$tfile || error "setfattr"
+	getfattr -n user.$(basename $tfile) $DIR/$tfile || error "getfattr"
+	rm $DIR/$tfile || error "rm"
+}
+run_test 102r "set EAs with empty values"
+
 run_acl_subtest()
 {
     $LUSTRE/tests/acl/run $LUSTRE/tests/acl/$1.test
