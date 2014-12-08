a7f36b214348947cefa1a0f42cb8d6f024d023da
Frank Zago <fzago@cray.com>
2014-12-08 13:17:53 -0600

    LU-6081 user: Introducing llapi_create_volatile_idx2
    This new function is similar to the existing
    llapi_create_volatile_idx, but take 2 additional parameters: a stripe
    information, and a file mode.
    
    llapi_create_volatile_idx is changed to use
    llapi_create_volatile_idx2.
    
    Signed-off-by: frank zago <fzago@cray.com>
    Change-Id: Ia50750d68b8cfa335b53b114a20fccaed49c75cd

--- lustre/utils/liblustreapi.c
+++ lustre/utils/liblustreapi.c
@@ -4674,11 +4674,15 @@ int llapi_get_data_version(int fd, __u64 *data_version, __u64 flags)
  * \param[in]	idx		MDT index on which the file is created,
  *				\a idx == -1 means no specific MDT is requested
  * \param[in]	open_flags	standard open(2) flags
+ * \param[in]	mode		standard open(2) mode
+ * \param[in]	stripe_param	stripe parameters. May be NULL.
  *
  * \retval	0 on success.
  * \retval	-errno on error.
  */
-int llapi_create_volatile_idx(char *directory, int idx, int open_flags)
+int llapi_create_volatile_idx2(char *directory, int idx, int open_flags,
+			       mode_t mode,
+			       const struct llapi_stripe_param *stripe_param)
 {
 	char	file_path[PATH_MAX];
 	char	filename[PATH_MAX];
@@ -4712,13 +4716,24 @@ int llapi_create_volatile_idx(char *directory, int idx, int open_flags)
 	if (rc >= sizeof(file_path))
 		return -E2BIG;
 
-	fd = open(file_path, O_RDWR | O_CREAT | open_flags, S_IRUSR | S_IWUSR);
+	open_flags |= O_RDWR | O_CREAT;
+	if (stripe_param) {
+		fd = llapi_file_open_param(file_path, open_flags,
+					   mode, stripe_param);
+		if (fd < 0)
+			rc = -fd;
+	} else {
+		fd = open(file_path, open_flags, mode);
+		if (fd < 0)
+			rc = errno;
+	}
+
 	if (fd < 0) {
-		llapi_error(LLAPI_MSG_ERROR, errno,
+		llapi_error(LLAPI_MSG_ERROR, rc,
 			    "Cannot create volatile file '%s' in '%s'",
 			    filename + LUSTRE_VOLATILE_HDR_LEN,
 			    directory);
-		return -errno;
+		return -rc;
 	}
 	/* unlink file in case this wasn't a Lustre filesystem, and the
 	 * magic volatile filename wasn't handled as intended.  The effect
@@ -4726,6 +4741,32 @@ int llapi_create_volatile_idx(char *directory, int idx, int open_flags)
 	unlink(file_path);
 
 	return fd;
+}
+
+/*
+ * Create a file without any name open it for read/write
+ *
+ * - file is created as if it were a standard file in the given \a directory
+ * - file does not appear in \a directory and mtime does not change because
+ *   the filename is handled specially by the Lustre MDS.
+ * - file is removed at final close
+ * - file modes are rw------- since it doesn't make sense to have a read-only
+ *   or write-only file that cannot be opened again.
+ * - if user wants another mode it must use fchmod() on the open file, no
+ *   security problems arise because it cannot be opened by another process.
+ *
+ * \param[in]	directory	directory from which to inherit layout/MDT idx
+ * \param[in]	idx		MDT index on which the file is created,
+ *				\a idx == -1 means no specific MDT is requested
+ * \param[in]	open_flags	standard open(2) flags
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ */
+int llapi_create_volatile_idx(char *directory, int idx, int open_flags)
+{
+	return llapi_create_volatile_idx2(directory, idx, open_flags,
+					  S_IRUSR | S_IWUSR, NULL);
 }
 
 /**
