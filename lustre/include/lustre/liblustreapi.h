b084f7720af23146cd6bdb2b4e3953e743b1ca69
Andreas Dilger <andreas.dilger@intel.com>
2014-12-12 16:37:50 -0700

    LU-5969 lustreapi: change license from GPL to LGPL
    This is an patch to update the copyright license of the liblustreapi
    library from GPL to LGPL. This will simplify the use of liblustreapi
    by third party applications (e.g. non-GPL HSM copytools, Lustre-aware
    user applications or libraries, etc) and distribution of same in both
    source and binary form.  In particular, liblustreapi.c, lustreapi.h
    and supporting files and headers libcfs_kernelcomm.h, l_ioctl.c,
    lustre_user.h, and lustre_ioctl.h will have their license changed.
    
    Other files in the library (liblustreapi_hsm.c, liblustreapi_json.c,
    liblustreapi_layout.c, liblustreapi_lease.c, lustreapi_internal.h)
    already have explicit LGPL headers.
    
    This patch requires signoff from all organizations that have copyright
    on these files by virtue of making changes to the code, as recorded by
    the commit records after the Lustre 2.0 release.
    
    Copyright assignment was previously required for all contributions to
    Cluster File Systems, Sun Microsystems, and Oracle for Lustre 2.0 and
    earlier.  The Oracle Lustre copyright of the liblustreapi.c code was
    later assumed by Xyratex/Seagate, so a signoff by Seagate is enough
    to cover all earlier contributions.
    
    Authors of patches to these files since Lustre 2.0:
    
    Bull
    - Sebastien Buisson <sebastien.buisson@bull.net>
    - Gregoire Pichon <gregoire.pichon@bull.net>
    Commissariat a l'Energie Atomique et aux Energies Alternatives
    - Aurelien Degremont <aurelien.degremont@cea.fr>
    - Jacques-Charles Lafoucriere <jc.lafoucriere@cea.fr>
    - Henri Doreau <henri.doreau@cea.fr>
    Cray
    - Frank Zago <fzago@cray.com>
    Data Direct Networks
    - Li Xi <lixi@ddn.com>
    - Swapnil Pimpale <spimpale@ddn.com>
    Indiana University
    - Joshua Walgenbach <jjw@iu.edu>
    Intel/Whamcloud
    - Alex Zhuravlev <alexey.zhuravlev@intel.com>
    - Andreas Dilger <andreas.dilger@intel.com>
    - Bob Glossman <bob.glossman@intel.com>
    - Bobi Jam <bobijam@whamcloud.com>
    - Dmitry Eremin <dmitry.eremin@intel.com>
    - Fan Yong <fan.yong@intel.com>
    - Hongchao Zhang <hongchao.zhang@whamcloud.com>
    - James Nunez <james.a.nunez@intel.com>
    - Jinshan Xiong <jinshan.xiong@intel.com>
    - Johann Lombardi <johann@whamcloud.com>
    - John L. Hammond <john.hammond@intel.com>
    - Keith Mannthey <keith.mannthey@intel.com>
    - Lai Siyao <lai.siyao@intel.com>
    - Liang Zhen <liang@whamcloud.com>
    - Michael MacDonald <michael.macdonald@intel.com>
    - Minh Diep <mdiep@whamcloud.com>
    - Niu Yawei <yawei.niu@intel.com>
    - Richard Henwood <rhenwood@whamcloud.com>
    - Wang Di <di.wang@intel.com>
    - Yang Sheng <ys@whamcloud.com>
    Lawrence Livermore National Laboratory
    - Christopher J. Morrone <morrone2@llnl.gov>
    - Ned Bass <bass6@llnl.gov>
    - Prakash Surya <surya1@llnl.gov>
    Oak Ridge National Laboratory
    - James Simmons <uja.ornl@gmail.com>
    - Matt Ezell <ezellma@ornl.gov>
    Seagate/Xyratex
    - Vitaly Fertman <vitaly_fertman@xyratex.com>
    Texas Advance Computing Center
    - John L. Hammond <jhammond@tacc.utexas.edu>
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: If40fb44d57c91a9c35882eeee7ec6f2772586631

--- lustre/include/lustre/liblustreapi.h
+++ lustre/include/lustre/liblustreapi.h
@@ -1,27 +1,26 @@
 /*
- * GPL HEADER START
+ * LGPL HEADER START
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library in the LCOPYING file; if not, a copy
+ * is available at http://www.gnu.org/licenses/lgpl-2.1.html
  *
- * GPL HEADER END
+ * LGPL HEADER END
  */
 /*
  * This file is part of Lustre, http://www.lustre.org/
- * Lustre is a trademark of Sun Microsystems, Inc.
  */
 
 /*
--- lustre/utils/liblustreapi.c
+++ lustre/utils/liblustreapi.c
@@ -1,23 +1,23 @@
 /*
- * GPL HEADER START
+ * LGPL HEADER START
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library in the LCOPYING file; if not, see:
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
- * GPL HEADER END
+ * LGPL HEADER END
  */
 /*
  * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
@@ -27,9 +27,13 @@
  */
 /*
  * This file is part of Lustre, http://www.lustre.org/
- * Lustre is a trademark of Sun Microsystems, Inc.
  *
  * lustre/utils/liblustreapi.c
+ *
+ * This file includes helper functions for accessing Lustre-specific
+ * functionality from a client or server.  This abstracts usage of Lustre-
+ * specific ioctls, /proc files, etc. from appilcations and reduces the
+ * incidence of application incompatibility if changes are made to Lustre.
  *
  * Author: Peter J. Braam <braam@clusterfs.com>
  * Author: Phil Schwan <phil@clusterfs.com>
