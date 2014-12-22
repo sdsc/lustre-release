c6042bd94670ea071a0bf0763295e79ab0566d72
John L. Hammond <john.hammond@intel.com>
2014-12-22 12:40:21 -0600

    LU-6047 obd: remove client Size on MDS support
    Remove the unused OBD MD API method md_done_writing(). Remove the
    unused logcookie and struct md_open_data ** parameters from
    md_setattr(). Remove the unused functions iattr_from_obdo(),
    md_from_obdo(), and obdo_refresh_inode().
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: I59bf2b101807f5b582eb7ab27e5a742284800979

--- lustre/include/obd.h
+++ lustre/include/obd.h
@@ -813,8 +813,6 @@ struct md_op_data {
 	unsigned int		op_attr_flags; /* LUSTRE_{SYNC,..}_FL */
 	__u64                   op_valid; /* OBD_MD_* */
 
-	/* Size-on-MDS epoch and flags. */
-	__u64                   op_ioepoch;
 	enum md_op_flags	op_flags;
 
 	/* Capa fields */
@@ -1053,8 +1051,7 @@ struct md_ops {
 			struct ptlrpc_request **);
 
 	int (*m_setattr)(struct obd_export *, struct md_op_data *, void *,
-			size_t , void *, size_t, struct ptlrpc_request **,
-			 struct md_open_data **mod);
+			 size_t , struct ptlrpc_request **);
 
 	int (*m_fsync)(struct obd_export *, const struct lu_fid *,
 		       struct obd_capa *, struct ptlrpc_request **);
@@ -1092,9 +1089,6 @@ struct md_ops {
 
 	int (*m_find_cbdata)(struct obd_export *, const struct lu_fid *,
 			     ldlm_iterator_t, void *);
-
-	int (*m_done_writing)(struct obd_export *, struct md_op_data  *,
-			      struct md_open_data *);
 
 	int (*m_getattr_name)(struct obd_export *, struct md_op_data *,
 			      struct ptlrpc_request **);
