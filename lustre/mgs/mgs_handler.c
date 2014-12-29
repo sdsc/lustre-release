005fbae8480d6ae1d870ee3302c1886ff79e76ed
Vitaly Fertman <vitaly_fertman@xyratex.com>
2014-12-29 10:10:10 -0500

    LU-3031 ldlm: disconnect speedup
    disconnect takes too long time if there are many locks to cancel.
    besides the amount of time spent on each lock cancel, there is a
    resched() in cfs_hash_for_each_relax(), i.e. disconnect or eviction
    may take unexpectedly long time.
    - do not cancel locks on disconnect_export;
    - export will be left in obd_unlinked_exports list pinned by live
      locks;
    - new re-connects will created other non-conflicting exports;
    - new locks will cancel obsolete locks on conflicts;
    - once all the locks on the disconnected export will be cancelled,
      the export will be destroyed on the last ref put;
    - do not cancel in small portions, cancel all together in just 1
      dedicated thread - use server side blocking thread for that;
    - cancel blocked locks first so that waiting locks could proceed;
    - take care about blocked waiting locks, so that they would get
      cancelled quickly too;
    - do not remove lock from waiting list on AST error before moving
      it to elt_expired_locks list, because it removes it from export
      list too; otherwise this blocked lock will not be cancelled
      immediately on failed export;
    - cancel lock instead of just destroy for failed export, to make
      full cleanup, i.e. remove it from export list.
    
    also make the proper order of events on umount:
    - disconnect export;
    - cleanup namespace, to cancel all the locks before export barrier;
    - exports barrier;
    - lprocfs_free_per_client_stats (requires nid_exp_ref_count == 0);
    - namespace_free_post is left in cleanup ensure will not get and
      segfault on an absent namespace.
    
    Signed-off-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Change-Id: Ia39b09ce967237ed5078c8a71e760b1e103c6f55
    Xyratex-bug-id: MRP-395 MRP-1366 MRP-1366
    Reviewed-by: Andriy Skulysh <Andriy_Skulysh@xyratex.com>
    Reviewed-by: Alexey Lyashkov <Alexey_Lyashkov@xyratex.com>
    Tested-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>

--- lustre/mgs/mgs_handler.c
+++ lustre/mgs/mgs_handler.c
@@ -1381,6 +1381,7 @@ static struct lu_device *mgs_device_fini(const struct lu_env *env,
 
 	ptlrpc_unregister_service(mgs->mgs_service);
 
+	ldlm_namespace_free_prior(obd->obd_namespace, NULL, 1);
 	obd_exports_barrier(obd);
 	obd_zombie_barrier();
 
@@ -1397,7 +1398,7 @@ static struct lu_device *mgs_device_fini(const struct lu_env *env,
 
 	mgs_fs_cleanup(env, mgs);
 
-	ldlm_namespace_free(obd->obd_namespace, NULL, 1);
+	ldlm_namespace_free_post(obd->obd_namespace);
 	obd->obd_namespace = NULL;
 
 	lu_site_purge(env, d->ld_site, ~0);
