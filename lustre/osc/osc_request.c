a4c42c206a6b9198a296c262ee27da92907214a6
John L. Hammond <john.hammond@intel.com>
2015-01-15 15:40:00 -0600

    LU-5814 obd: remove unused LSM parameters
    Remove unused struct lov_stripe_md * parameters from obd_get_info(),
    mgc_enqueue(), and osc_brw_prep_request().
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: Iee0be72393c609967ee8dad06f878026ca09821d

--- lustre/osc/osc_request.c
+++ lustre/osc/osc_request.c
@@ -1075,8 +1075,9 @@ static obd_count osc_checksum_bulk(int nob, obd_count pg_count,
 	return cksum;
 }
 
-static int osc_brw_prep_request(int cmd, struct client_obd *cli,struct obdo *oa,
-                                struct lov_stripe_md *lsm, obd_count page_count,
+static int osc_brw_prep_request(int cmd, struct client_obd *cli,
+				struct obdo *oa,
+				obd_count page_count,
                                 struct brw_page **pga,
                                 struct ptlrpc_request **reqp,
                                 struct obd_capa *ocapa, int reserve,
@@ -1493,7 +1494,6 @@ static int osc_brw_redo_request(struct ptlrpc_request *request,
         rc = osc_brw_prep_request(lustre_msg_get_opc(request->rq_reqmsg) ==
                                         OST_WRITE ? OBD_BRW_WRITE :OBD_BRW_READ,
                                   aa->aa_cli, aa->aa_oa,
-                                  NULL /* lsm unused by osc currently */,
                                   aa->aa_page_count, aa->aa_ppga,
                                   &new_req, aa->aa_ocapa, 0, 1);
         if (rc)
@@ -1834,8 +1834,8 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 	}
 
 	sort_brw_pages(pga, page_count);
-	rc = osc_brw_prep_request(cmd, cli, oa, NULL, page_count,
-			pga, &req, crattr->cra_capa, 1, 0);
+	rc = osc_brw_prep_request(cmd, cli, oa, page_count, pga, &req,
+				  crattr->cra_capa, 1, 0);
 	if (rc != 0) {
 		CERROR("prep_req failed: %d\n", rc);
 		GOTO(out, rc);
