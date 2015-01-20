21cbb7659117b1740899e73749e6ddf15be5ebcb
Andreas Dilger <andreas.dilger@intel.com>
2015-01-20 02:51:20 -0700

    LU-5969 lustreapi: replace llapi_get_version()
    The existing llapi_get_version() helper has an awkward calling
    style, requiring a temporary input buffer, as well as an output
    pointer.  It also typically returns an unsightly string for
    the build, instead of a simple version that would be expected.
    
    Replace this with two separate functions:
    llapi_get_build_version() which returns the current ugly build string
    llapi_get_release_version() which returns the simple version string
    Mark the old llapi_get_version() function deprecated.
    
    These versions are extracted from /proc/fs/lustre/version instead
    of from an ioctl, to avoid the messiness that is Lustre ioctls.
    
    Update "lctl lustre_build_version" to use llapi_build_version() and
    add "lctl lustre_release_version" to use llapi_release_version().
    Print out the build version at the start of each test so it is logged
    in the test results for convenience.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I681a353e6f65fd1ec2d1434fe38dfa55ed3ebbe5

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -6695,29 +6695,36 @@ check_write_access() {
 }
 
 init_logging() {
-    if [[ -n $YAML_LOG ]]; then
-        return
-    fi
-    local SAVE_UMASK=`umask`
-    umask 0000
+	[[ -n $YAML_LOG ]] && return
+	local SAVE_UMASK=$(umask)
+	umask 0000
 
-    export YAML_LOG=${LOGDIR}/results.yml
-    mkdir -p $LOGDIR
-    init_clients_lists
+	export YAML_LOG=${LOGDIR}/results.yml
+	mkdir -p $LOGDIR
+	init_clients_lists
 
-    if [ ! -f $YAML_LOG ]; then       # If the yaml log already exists then we will just append to it
-      if check_shared_dir $LOGDIR; then
-          touch $LOGDIR/shared
-          echo "Logging to shared log directory: $LOGDIR"
-      else
-          echo "Logging to local directory: $LOGDIR"
-      fi
+	# If the yaml log already exists then we will just append to it
+	if [ ! -f $YAML_LOG ]; then
+		if check_shared_dir $LOGDIR; then
+			touch $LOGDIR/shared
+			echo "Logging to shared log directory: $LOGDIR"
+		else
+			echo "Logging to local directory: $LOGDIR"
+		fi
 
-      yml_nodes_file $LOGDIR >> $YAML_LOG
-      yml_results_file >> $YAML_LOG
-    fi
+		yml_nodes_file $LOGDIR >> $YAML_LOG
+		yml_results_file >> $YAML_LOG
+	fi
 
-    umask $SAVE_UMASK
+	umask $SAVE_UMASK
+
+	# If modules are not yet loaded then older "lctl lustre_build_version"
+	# will fail.  Use lctl build version instead.
+	log "Client: $($LCTL lustre_build_version)"
+	log "MDS: $(do_facet $SINGLEMDS $LCTL lustre_build_version 2>/dev/null||
+		    do_facet $SINGLEMDS $LCTL --version)"
+	log "OSS: $(do_facet ost1 $LCTL lustre_build_version 2> /dev/null ||
+		    do_facet ost1 $LCTL --version)"
 }
 
 log_test() {
