4f5c465abd5fcecc45de5831d00260cbc729995a
Andreas Dilger <andreas.dilger@intel.com>
2015-01-20 02:51:20 -0700

    LU-5969 lustreapi: replace llapi_get_version()
    The existing llapi_get_version() helper has an awkward calling
    style, requiring a temporary input buffer, as well as an output
    pointer.  It also typically returns an unsightly string for
    the build, instead of a simple version that would be expected.
    
    Replace this with two separate functions:
    llapi_get_build_version() which returns the current ugly build string
    llapi_get_release_version() which returns the simple version string
    Mark the old llapi_get_version() function deprecated.
    
    These versions are extracted from /proc/fs/lustre/version instead
    of from an ioctl, to avoid the messiness that is Lustre ioctls.
    
    Update "lctl lustre_build_version" to use llapi_build_version() and
    add "lctl lustre_release_version" to use llapi_release_version().
    Print out the build version at the start of each test so it is logged
    in the test results for convenience.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I681a353e6f65fd1ec2d1434fe38dfa55ed3ebbe5

--- lustre/utils/liblustreapi_util.c
+++ lustre/utils/liblustreapi_util.c
@@ -5,6 +5,8 @@
  *
  * (C) Copyright (c) 2015, Cray Inc, all rights reserved.
  *
+ * Copyright (c) 2015, Intel Corporation.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * LGPL version 2.1 or (at your discretion) any later version.
@@ -26,26 +28,39 @@
  * Author: Frank Zago <fzago@cray.com>
  */
 
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 #include <stdbool.h>
+#include <errno.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/syscall.h>
+#include <lustre/lustreapi.h>
+#include <libcfs/util/string.h>	/* only needed for compat strlcpy() */
+#include <lustre_ver.h>		/* only until LUSTRE_BUILD_VERSION is gone */
+#include "lustreapi_internal.h"
 
 /*
  * Indicate whether the liblustreapi_init() constructor below has run or not.
  *
- * This can be used by external programs to ensure if the initialization
+ * This can be used by external programs to ensure that the initialization
  * mechanism has actually worked.
  */
 bool liblustreapi_initialized;
 
 
-/*
- * Initializes the library
+/**
+ * Initialize the library once at startup.
+ *
+ * Initializes the random number generator (random()). Get
+ * data from different places in case one of them fails. This
+ * is enough to get reasonably random numbers, but is not
+ * strong enough to be used for cryptography.
  */
 static __attribute__ ((constructor)) void liblustreapi_init(void)
 {
@@ -53,10 +68,6 @@ static __attribute__ ((constructor)) void liblustreapi_init(void)
 	struct timeval	tv;
 	int		fd;
 
-	/* Initializes the random number generator (random()). Get
-	 * data from different places in case one of them fails. This
-	 * is enough to get reasonably random numbers, but is not
-	 * strong enough to be used for cryptography. */
 	seed = syscall(SYS_gettid);
 
 	if (gettimeofday(&tv, NULL) == 0) {
@@ -76,4 +87,117 @@ static __attribute__ ((constructor)) void liblustreapi_init(void)
 
 	srandom(seed);
 	liblustreapi_initialized = true;
+}
+
+/* The "version" file in /proc currently returns something like:
+ * lustre: 2.6.92
+ * kernel: patchless_client
+ * build: v2_6_92_0-gadb3ee4-2.6.32-431.29.2.el6_lustre.g36cd22b.x86_64
+ *
+ * Extract only the named \a string from \a version from this file.
+ */
+static int get_version_string(const char *string, char *version,
+			      int version_size)
+{
+	char buffer[4096];
+	char *ptr;
+	int rc;
+
+	if (version == NULL) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	rc = get_param("version", buffer, sizeof(buffer));
+
+	ptr = strstr(buffer, string);
+	if (ptr != NULL) {
+		llapi_chomp_string(ptr);
+		ptr += strlen(string);
+		while (*ptr == ' ')
+			ptr++;
+
+		if (strlcpy(version, ptr, version_size) >= version_size) {
+			errno = EOVERFLOW;
+			return -1;
+		}
+	} else {
+		errno = ENODATA;
+		return -1;
+	}
+
+	return 0;
+}
+
+/**
+ * Return the release version for Lustre.
+ *
+ * This returns the tagged release version of Lustre, e.g. 2.6.92.
+ *
+ * \param version[in,out]	buffer to store build version string
+ * \param version_size[in]	size of \a version
+ *
+ * \retval			0 on success
+ * \retval			-1 on failure, errno set
+ */
+int llapi_get_release_version(char *version, int version_size)
+{
+	return get_version_string("lustre:", version, version_size);
+}
+
+/**
+ * Return the build version for Lustre.
+ *
+ * This returns the specific version of Lustre that includes the Lustre
+ * release version, commit hash, kernel version, and architecture, e.g.
+ * v2_6_92_0-gadb3ee4-CHANGED-2.6.32-431.29.2.el6_lustre.g36cd22b.x86_64.
+ *
+ * \param version[in,out]	buffer to store build version string
+ * \param version_size[in]	size of \a version
+ *
+ * \retval			0 on success
+ * \retval			-1 on failure, errno set
+ */
+int llapi_get_build_version(char *version, int version_size)
+{
+	return get_version_string("build:", version, version_size);
+}
+
+/**
+ * Return the build version of the Lustre code.
+ *
+ * For historical reasons this returns the build version of the code from
+ * llapi_get_build_version(), which is typically a long ugly string, rather
+ * than the release version, which is a nice short version.  That probably
+ * isn't what people wanted, but they may depend on the old output format.
+ *
+ * Callers should use one of the two newer functions instead, but give them
+ * a few versions to make the switch.
+ *
+ * \param buffer[in]		temporary buffer to hold version string
+ * \param buffer_size[in]	length of the \a buffer
+ * \param version[out]		pointer to the start of build version string
+ *
+ * \retval			0 on success
+ * \retval			-ve errno on failure
+ */
+int llapi_get_version(char *buffer, int buffer_size, char **version)
+{
+	int rc;
+#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 9, 53, 0)
+	static bool printed;
+	if (!printed) {
+		fprintf(stderr, "%s deprecated, use llapi_get_release_version()"
+			" or llapi_get_build_version() instead\n", __func__);
+		printed = true;
+	}
+#endif
+
+	*version = buffer;
+	rc = get_version_string("build:", buffer, buffer_size);
+	/* keep old return style for this legacy function */
+	if (rc == -1)
+		rc = -errno;
+
+	return rc;
 }
