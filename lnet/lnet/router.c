71fc9d5ea68eb60e14081c8dc4c175ab656c8afc
Amir Shehata <amir.shehata@intel.com>
2015-01-23 15:27:22 -0800

    LU-6122 lnet: Allocate the correct number of rtr buffers
    This patch ensures that the correct number of router buffers are
    allocated.  It keeps a new count that keeps track of the number of
    buffers allocated.  Another count keeps the number of buffers
    requested. The number of buffers allocated is set when creating
    new buffers and reduced when buffers are freed.
    
    The number of requested buffer is set when the buffers are
    allocated and is checked when credits are returned to determine
    whether the buffer should be freed or kept.
    
    When the above check is performed a mutex is locked to ensure that
    no other buffers are added while the buffer is being freed.  After
    the mutex is locked the condition is checked again to deal with
    the scenario where the user requests an increase in buffers, that
    thread grabs the mutex and starts to allocate buffers, during this
    process a buffer is being returned and is about to be freed.  At
    this point the condition could be true, but after the buffers
    are created, the condition is no longer true.  In order to avoid
    this the condition is checked again once the mutex is acquired.
    
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Change-Id: I96627cc8ba3d3d70a0bf581b21ccd3c9b2de327f

--- lnet/lnet/router.c
+++ lnet/lnet/router.c
@@ -1358,12 +1358,17 @@ lnet_rtrpool_free_bufs(lnet_rtrbufpool_t *rbp, int cpt)
 
 	INIT_LIST_HEAD(&tmp);
 
+
 	lnet_net_lock(cpt);
 	lnet_drop_routed_msgs_locked(&rbp->rbp_msgs, cpt);
 	list_splice_init(&rbp->rbp_bufs, &tmp);
+	mutex_lock(&rbp->rbp_nbuf_mutex);
+	rbp->rbp_req_nbuffers = 0;
 	rbp->rbp_nbuffers = rbp->rbp_credits = 0;
 	rbp->rbp_mincredits = 0;
+	mutex_unlock(&rbp->rbp_nbuf_mutex);
 	lnet_net_unlock(cpt);
+
 
 	/* Free buffers on the free list. */
 	while (!list_empty(&tmp)) {
@@ -1383,17 +1388,22 @@ lnet_rtrpool_adjust_bufs(lnet_rtrbufpool_t *rbp, int nbufs, int cpt)
 	int		npages = rbp->rbp_npages;
 
 	/* If we are called for less buffers than already in the pool, we
-	 * just lower the nbuffers number and excess buffers will be
+	 * just lower the req_nbuffers number and excess buffers will be
 	 * thrown away as they are returned to the free list.  Credits
 	 * then get adjusted as well. */
-	if (nbufs <= rbp->rbp_nbuffers) {
+	if (nbufs <= rbp->rbp_req_nbuffers) {
 		lnet_net_lock(cpt);
-		rbp->rbp_nbuffers = nbufs;
+		rbp->rbp_req_nbuffers = nbufs;
 		lnet_net_unlock(cpt);
 		return 0;
 	}
 
 	INIT_LIST_HEAD(&rb_list);
+
+	/* make sure that rbp_nbuffers can not change while we are
+	 * allocating the new set of buffers, to ensure we allocate
+	 * exactly the correct number needed */
+	mutex_lock(&rbp->rbp_nbuf_mutex);
 
 	/* allocate the buffers on a local list first.  If all buffers are
 	 * allocated successfully then join this list to the rbp buffer
@@ -1403,6 +1413,7 @@ lnet_rtrpool_adjust_bufs(lnet_rtrbufpool_t *rbp, int nbufs, int cpt)
 	while (num_rb < nbufs) {
 		rb = lnet_new_rtrbuf(rbp, cpt);
 		if (rb == NULL) {
+			mutex_unlock(&rbp->rbp_nbuf_mutex);
 			CERROR("Failed to allocate %d route bufs of %d pages\n",
 			       nbufs, npages);
 			goto failed;
@@ -1418,7 +1429,11 @@ lnet_rtrpool_adjust_bufs(lnet_rtrbufpool_t *rbp, int nbufs, int cpt)
 	list_splice_tail(&rb_list, &rbp->rbp_bufs);
 	rbp->rbp_nbuffers += num_buffers;
 	rbp->rbp_credits += num_buffers;
+	rbp->rbp_req_nbuffers = nbufs;
 	rbp->rbp_mincredits = rbp->rbp_credits;
+
+	mutex_unlock(&rbp->rbp_nbuf_mutex);
+
 	/* We need to schedule blocked msg using the newly
 	 * added buffers. */
 	while (!list_empty(&rbp->rbp_bufs) &&
@@ -1444,10 +1459,11 @@ lnet_rtrpool_init(lnet_rtrbufpool_t *rbp, int npages)
 {
 	INIT_LIST_HEAD(&rbp->rbp_msgs);
 	INIT_LIST_HEAD(&rbp->rbp_bufs);
+	mutex_init(&rbp->rbp_nbuf_mutex);
 
-        rbp->rbp_npages = npages;
-        rbp->rbp_credits = 0;
-        rbp->rbp_mincredits = 0;
+	rbp->rbp_npages = npages;
+	rbp->rbp_credits = 0;
+	rbp->rbp_mincredits = 0;
 }
 
 void
