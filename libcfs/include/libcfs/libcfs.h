726992d2beb690c8e4bb2290239b448bdf8ab344
Isaac Huang <he.huang@intel.com>
2015-01-27 14:03:32 -0700

    LU-6155 osd-zfs: dbuf_hold_impl() called without the lock
    The osd-zfs osd_count_not_mapped() calls dbuf_hold_impl() without
    the required lock. In addition, dbuf_hold_impl() is an internal
    function and has the expensive side effect of reading the block
    from disk which would convert a full-block write into a
    read-modify-write.
    
    Since space estimation with ZFS is complicated any way, just use
    the worst case as a rough estimate where a snapshot holds all current
    blocks, i.e. no old space can be freed after the COW.
    
    Change-Id: Idf6f2ff80ff185ca8c0f38e1002ff90e457c3ca0
    Signed-off-by: Isaac Huang <he.huang@intel.com>
    Test-Parameters: mdtfilesystemtype=zfs ostfilesystemtype=zfs

--- libcfs/include/libcfs/libcfs.h
+++ libcfs/include/libcfs/libcfs.h
@@ -96,9 +96,9 @@ static inline int __is_po2(unsigned long long val)
         return !(val & (val - 1));
 }
 
-#define IS_PO2(val) __is_po2((unsigned long long)(val))
-
-#define LOWEST_BIT_SET(x)       ((x) & ~((x) - 1))
+#define IS_PO2(val)	    __is_po2((unsigned long long)(val))
+#define PO2_ROUNDUP(x, po2) (-(-(x) & -(po2)))
+#define LOWEST_BIT_SET(x)   ((x) & ~((x) - 1))
 
 /* Sparse annotations */
 #ifdef __KERNEL__
