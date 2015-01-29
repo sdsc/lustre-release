9b26453b775ca95f8727ad97468ec35c0b5939dc
Patrick Farrell <paf@cray.com>
2015-01-29 04:18:35 -0600

    LU-6179 llite: Implement lock ahead
    Lock ahead is a new feature allowing userspace to request
    extent locks in advance of the IO which will use them.
    These locks are not expanded beyond the size requested by
    userspace.  Their use is intended to address lock
    contention between multiple clients resulting from lock
    expansion.  They should allow optimizing various IO
    patterns.
    
    The lock ahead code re-uses the upper layers of the
    glimpse lock implementation.
    
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Change-Id: I1e80286f54946a0df08b19b1339829fcfd1117e7

--- lustre/llite/glimpse.c
+++ lustre/llite/glimpse.c
@@ -81,40 +81,86 @@ blkcnt_t dirty_cnt(struct inode *inode)
         return (cnt > 0) ? 1 : 0;
 }
 
-int cl_glimpse_lock(const struct lu_env *env, struct cl_io *io,
-		    struct inode *inode, struct cl_object *clob, int agl)
+/* FIXME: Wondering about these as an implementation choice:
+ * Is this a good choice?  Should they be here, or elsewhere? */
+static enum cl_lock_mode cl_mode_user_to_kernel(lock_mode_user mode)
+{
+	switch (mode) {
+	case READ_USER:
+		return CLM_READ;
+	case WRITE_USER:
+		return CLM_WRITE;
+	default:
+		return -EINVAL;
+	}
+}
+
+static enum cl_io_type user_mode_to_type(enum cl_lock_mode mode)
+{
+	switch (mode) {
+	case READ_USER:
+		return CIT_READ;
+	case WRITE_USER:
+		return CIT_WRITE;
+	default:
+		return -EINVAL;
+	}
+}
+
+/* Used to allow the upper layers of the client to request an LDLM lock
+ * when not doing a standard IO operation.
+ * Used both for glimpse locks and the lock ahead IOCTL.
+ * lock_mode_user of 0 indicates a glimpse lock request.
+ *
+ * \param[in] env	thread specific execution environment
+ * \param[in] io	client io description
+ * \param[in] inode	inode for the file this lock request is on
+ * \param[in] clob	cl_object for the file this lock is on
+ * \param[in] start	start of the lock extent (pages)
+ * \param[in] end	end of the lock extent (pages)
+ * \param[in] mode	lock mode (read, write), 0 for glimpse locks
+ * \param[in] flags	flags (currently only used for AGL for glimpse locks)
+ *
+ * \retval 0		on success
+ * \retval negative	negative errno on error
+ * */
+int cl_request_lock0(const struct lu_env *env, struct cl_io *io,
+		    struct inode *inode, struct cl_object *clob,
+		    pgoff_t start, pgoff_t end, lock_mode_user mode,
+		    __u32 flags)
 {
 	struct ll_inode_info *lli   = ll_i2info(inode);
-        const struct lu_fid  *fid   = lu_object_fid(&clob->co_lu);
-        int result;
+	const struct lu_fid  *fid   = lu_object_fid(&clob->co_lu);
+	enum cl_lock_mode	cl_mode = 0;
+	int result;
 
-        ENTRY;
-        result = 0;
-        if (!(lli->lli_flags & LLIF_MDS_SIZE_LOCK)) {
-                CDEBUG(D_DLMTRACE, "Glimpsing inode "DFID"\n", PFID(fid));
+	ENTRY;
+	result = 0;
+	/* Glimpse lock request */
+	if (!(lli->lli_flags & LLIF_MDS_SIZE_LOCK) && mode == 0) {
+		CDEBUG(D_DLMTRACE, "Glimpsing inode "DFID"\n", PFID(fid));
 		if (lli->lli_has_smd) {
 			struct cl_lock *lock = ccc_env_lock(env);
 			struct cl_lock_descr *descr = &lock->cll_descr;
 
-                        /* NOTE: this looks like DLM lock request, but it may
-                         *       not be one. Due to CEF_ASYNC flag (translated
-                         *       to LDLM_FL_HAS_INTENT by osc), this is
-                         *       glimpse request, that won't revoke any
-                         *       conflicting DLM locks held. Instead,
-                         *       ll_glimpse_callback() will be called on each
-                         *       client holding a DLM lock against this file,
-                         *       and resulting size will be returned for each
-                         *       stripe. DLM lock on [0, EOF] is acquired only
-                         *       if there were no conflicting locks. If there
-                         *       were conflicting locks, enqueuing or waiting
-                         *       fails with -ENAVAIL, but valid inode
-                         *       attributes are returned anyway. */
-                        *descr = whole_file;
-                        descr->cld_obj   = clob;
+			/* NOTE: this looks like DLM lock request, but it may
+			 *       not be one. Due to CEF_ASYNC flag (translated
+			 *       to LDLM_FL_HAS_INTENT by osc), this is
+			 *       glimpse request, that won't revoke any
+			 *       conflicting DLM locks held. Instead,
+			 *       ll_glimpse_callback() will be called on each
+			 *       client holding a DLM lock against this file,
+			 *       and resulting size will be returned for each
+			 *       stripe. DLM lock on [0, EOF] is acquired only
+			 *       if there were no conflicting locks. If there
+			 *       were conflicting locks, enqueuing or waiting
+			 *       fails with -ENAVAIL, but valid inode
+			 *       attributes are returned anyway. */
+			*descr = whole_file;
+			descr->cld_obj   = clob;
 			descr->cld_mode  = CLM_READ;
 			descr->cld_enq_flags = CEF_ASYNC | CEF_MUST;
-			if (agl)
-				descr->cld_enq_flags |= CEF_AGL;
+			descr->cld_enq_flags |= flags;
 			/*
 			 * CEF_ASYNC is used because glimpse sub-locks cannot
 			 * deadlock (because they never conflict with other
@@ -127,7 +173,7 @@ int cl_glimpse_lock(const struct lu_env *env, struct cl_io *io,
 			if (result < 0)
 				RETURN(result);
 
-			if (!agl) {
+			if (!(flags & CEF_AGL)) {
 				ll_merge_attr(env, inode);
 				if (i_size_read(inode) > 0 &&
 				    inode->i_blocks == 0) {
@@ -145,8 +191,36 @@ int cl_glimpse_lock(const struct lu_env *env, struct cl_io *io,
 			CDEBUG(D_DLMTRACE, "No objects for inode\n");
 			ll_merge_attr(env, inode);
 		}
-	}
+	} else if (mode != 0) {
+		/* Non-glimpse lock. */
+		struct cl_lock *lock = ccc_env_lock(env);
+		struct cl_lock_descr *descr = &lock->cll_descr;
 
+		/* Convert mode */
+		cl_mode = cl_mode_user_to_kernel(mode);
+		if (cl_mode < 0)
+			RETURN(cl_mode);
+
+		descr->cld_obj   = clob;
+		descr->cld_start = start;
+		descr->cld_end   = end;
+		descr->cld_mode  = cl_mode;
+		/* CEF_MUST is used because we do not want to convert a lock
+		 * ahead request to a lockless lock */
+		descr->cld_enq_flags = CEF_MUST | CEF_NO_EXPAND | CEF_NONBLOCK;
+		descr->cld_enq_flags |= flags;
+
+		// FIXME: mode translation
+		CDEBUG(D_VFSTRACE, "Non-glimpse request, inode=%p,"
+		       "start=%lu,end=%lu,mode=%s\n",inode,start,end,
+		       cl_lock_mode_name(mode));
+
+		result = cl_lock_request(env, io, lock);
+		if (result < 0)
+			RETURN(result);
+
+		cl_lock_release(env, lock);
+	}
 	RETURN(result);
 }
 
@@ -174,39 +248,88 @@ static int cl_io_get(struct inode *inode, struct lu_env **envout,
         return result;
 }
 
-int cl_glimpse_size0(struct inode *inode, int agl)
+/* Preps IO environment and arguments for cl_request_lock0, which does the
+ * actual lock request.
+ *
+ * Used for lock ahead and glimpse locking.
+ *
+ * \param[in] inode	client io description
+ * \param[in] start	start of the lock extent (bytes)
+ * \param[in] end	end of the lock extent (bytes)
+ * \param[in] mode	lock mode (read, write), 0 for glimpse locks
+ * \param[in] flags	flags (currently only used for AGL for glimpse locks)
+ *
+ * \retval 0		success
+ * \retval negative	negative errno on error
+ */
+int cl_request_lock(struct inode *inode, off_t start, off_t end,
+		     lock_mode_user mode, __u32 flags)
 {
-        /*
-         * We don't need ast_flags argument to cl_glimpse_size(), because
-         * osc_lock_enqueue() takes care of the possible deadlock that said
-         * argument was introduced to avoid.
-         */
-        /*
-         * XXX but note that ll_file_seek() passes LDLM_FL_BLOCK_NOWAIT to
-         * cl_glimpse_size(), which doesn't make sense: glimpse locks are not
-         * blocking anyway.
-         */
-        struct lu_env          *env = NULL;
-        struct cl_io           *io  = NULL;
-        int                     result;
-        int                     refcheck;
+	/* FIXME: Not sure what to do with this comment?
+	 * We don't need ast_flags argument to cl_glimpse_size(), because
+	 * osc_lock_enqueue() takes care of the possible deadlock that said
+	 * argument was introduced to avoid.
+	 */
+	/* FIXME: This comment seems to be outdated.
+	 * ll/cl_glimpse_size take only an inode.  Remove?
+	 *
+	 * XXX but note that ll_file_seek() passes LDLM_FL_BLOCK_NOWAIT to
+	 * cl_glimpse_size(), which doesn't make sense: glimpse locks are not
+	 * blocking anyway.
+	 */
+	struct lu_env          *env = NULL;
+	struct cl_io           *io  = NULL;
+	pgoff_t			start_pg = 0;
+	pgoff_t			end_pg = 0;
+	enum cl_io_type		io_type;
+	int                     result;
+	int                     refcheck;
 
-        ENTRY;
+	ENTRY;
 
-        result = cl_io_get(inode, &env, &io, &refcheck);
-        if (result > 0) {
+	if (mode != 0) {
+		/* Lock ahead locks set IO type to CIT_{READ,WRITE} */
+		io_type = user_mode_to_type(mode);
+		if (io_type < 0)
+			RETURN(io_type);
+	} else {
+		/* Glimpse locks use CIT_MISC */
+		io_type = CIT_MISC;
+	}
+
+	result = cl_io_get(inode, &env, &io, &refcheck);
+	if (result > 0) {
 	again:
-		io->ci_verify_layout = 1;
-                result = cl_io_init(env, io, CIT_MISC, io->ci_obj);
-                if (result > 0)
-                        /*
-                         * nothing to do for this io. This currently happens
-                         * when stripe sub-object's are not yet created.
-                         */
-                        result = io->ci_result;
-                else if (result == 0)
-                        result = cl_glimpse_lock(env, io, inode, io->ci_obj,
-                                                 agl);
+		if (mode == 0)
+			/* For glimpse locks */
+			io->ci_verify_layout = 1;
+		else
+			io->ci_lock_ahead = 1;
+
+		result = cl_io_init(env, io, io_type, io->ci_obj);
+
+		if (result > 0) {
+			/*
+			 * nothing to do for this io. This currently happens
+			 * when stripe sub-object's are not yet created.
+			 */
+			result = io->ci_result;
+		} else if (result == 0) {
+			if (mode == 0) {
+				/* Glimpse lock request, doesn't use mode */
+				result = cl_request_lock0(env, io, inode,
+							  io->ci_obj, 0, 0, 0,
+							  flags);
+			} else {
+				/* Extent lock request */
+				start_pg = cl_index(io->ci_obj, start);
+				end_pg = cl_index(io->ci_obj, end);
+
+				result = cl_request_lock0(env, io, inode,
+							  io->ci_obj, start_pg,
+							  end_pg, mode, flags);
+			}
+		}
 
 		OBD_FAIL_TIMEOUT(OBD_FAIL_GLIMPSE_DELAY, 2);
                 cl_io_fini(env, io);
