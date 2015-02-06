148f4d63f5283f05cef2838e5f2f5f658603562c
Henri Doreau <henri.doreau@cea.fr>
2015-02-06 10:01:36 +0100

    LU-4840 tests: Add file migration to racer
    Make racer run both blocking and non-blocking "lfs migrate" commands.
    
    Test-Parameters: testlist=racer,racer,racer
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I51b3f19c78029ff47102e96a71ec4a0fc472183a

---
+++ lustre/tests/racer/file_migrate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$1
+MAX=$2
+
+OSTCOUNT=${OSTCOUNT:-$($LFS df $DIR 2> /dev/null | grep -c OST)}
+[ $OSTCOUNT -gt 0 ] || exit 1
+
+while true; do
+	file=$((RANDOM % MAX))
+	stripecount=$((RANDOM % (OSTCOUNT + 1)))
+	blk_arg=""
+	[ $(($file % 8)) -eq 0 ] && blk_arg="--block"
+	$LFS migrate ${blk_arg} -c $stripecount $DIR/$file 2> /dev/null
+done
