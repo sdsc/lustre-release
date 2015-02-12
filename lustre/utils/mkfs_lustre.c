1556c1a6c7070d6f3ced7550528b5d0a93cfa81f
Bobi Jam <bobijam.xu@intel.com>
2015-02-12 11:58:26 +0800

    LU-6234 util: check fsname and pool name
    The pool name length check should not include its fsname. This patch
    get rid of the possibly added fsname in the pool name argument.
    
    Check lustre filesystem name (fsname) in mkfs.lustre, it can only
    contains alphabetic, digital char and '-'/'_'.
    
    Check pool name in lctl/lfs, its character also abides by the
    constraint of the fsname.
    
    Add test cases to check the pool names for length, with and without
    a filesystem name prefix, and with invalid names.
    
    Also fix pool_add in the test-framework so that it greps for the
    poolname plus the filesystem name, so it doesn't match more lines than
    necessary.
    
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Signed-off-by: frank zago <fzago@cray.com>
    Change-Id: I4f542e1bc7b3ea97b35ec60e51aa971e73da91eb

--- lustre/utils/mkfs_lustre.c
+++ lustre/utils/mkfs_lustre.c
@@ -422,7 +422,6 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
                                 sizeof(mop->mo_mkfsopts));
                         break;
                 case 'L': {
-                        char *tmp;
                         if (!(mop->mo_flags & MO_FORCEFORMAT) &&
                             (!(mop->mo_ldd.ldd_flags &
                                (LDD_F_UPGRADE14 | LDD_F_VIRGIN |
@@ -431,16 +430,18 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
                                         " a registered target\n", progname);
                                 return 1;
                         }
-                        if ((strlen(optarg) < 1) || (strlen(optarg) > 8)) {
-                                fprintf(stderr, "%s: filesystem name must be "
-                                        "1-8 chars\n", progname);
-                                return 1;
-                        }
-                        if ((tmp = strpbrk(optarg, "/:"))) {
-                                fprintf(stderr, "%s: char '%c' not allowed in "
-                                        "filesystem name\n", progname, *tmp);
-                                return 1;
-                        }
+
+			rc = is_fsname_valid(optarg, 1, 8);
+			if (rc < 0) {
+				fprintf(stderr, "%s: filesystem name must be "
+					"1-8 chars\n", progname);
+				return 1;
+			} else if (rc > 0) {
+				fprintf(stderr, "%s: char '%c' not allowed in "
+					"filesystem name\n", progname, rc);
+				return 1;
+			}
+
                         strscpy(mop->mo_ldd.ldd_fsname, optarg,
                                 sizeof(mop->mo_ldd.ldd_fsname));
                         break;
