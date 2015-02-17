9a93b1370bbbeae989cf016249ac9d61cf45a3d0
wang di <di.wang@intel.com>
2015-02-17 11:34:37 -0800

    LU-0000 dne: other temporary fixes.
    These fixes are already being pushed as other tickets,
    and can be landed independently.
    
    Duplicate of http://review.whamcloud.com/#/c/14313/
    
    Duplicate of http://review.whamcloud.com/14444
    
    Disable oic_cache, until LU-6465 is fixed.
    
    http://review.whamcloud.com/#/c/14497/
    
    http://review.whamcloud.com/#/c/14495/
    
    Signed-off-by: Wang Di <di.wang@intel.com>
    Change-Id: I6456b7fc5c28b852464e41a6611f8c5fb0c79779

--- lustre/mdt/mdt_reint.c
+++ lustre/mdt/mdt_reint.c
@@ -778,6 +778,7 @@ static int mdt_reint_unlink(struct mdt_thread_info *info,
         struct mdt_object       *mc;
         struct mdt_lock_handle  *parent_lh;
         struct mdt_lock_handle  *child_lh;
+	__u64			lock_ibits;
 	struct ldlm_enqueue_info *einfo = &info->mti_einfo;
 	struct mdt_lock_handle  *s0_lh = NULL;
 	struct mdt_object	*s0_obj = NULL;
@@ -913,11 +914,6 @@ static int mdt_reint_unlink(struct mdt_thread_info *info,
 	/* We used to acquire MDS_INODELOCK_FULL here but we can't do
 	 * this now because a running HSM restore on the child (unlink
 	 * victim) will hold the layout lock. See LU-4002. */
-<<<<<<< HEAD
-	rc = mdt_object_lock(info, mc, child_lh,
-			     MDS_INODELOCK_LOOKUP | MDS_INODELOCK_UPDATE,
-			     MDT_CROSS_LOCK);
-=======
 	lock_ibits = MDS_INODELOCK_LOOKUP | MDS_INODELOCK_UPDATE;
 	if (mdt_object_remote(mp)) {
 		/* Enqueue lookup lock from parent MDT */
@@ -932,7 +928,6 @@ static int mdt_reint_unlink(struct mdt_thread_info *info,
 	}
 
 	rc = mdt_object_lock(info, mc, child_lh, lock_ibits);
->>>>>>> b8fc9fe... LU-3537 mdt: allow cross-MDT rename and link
 	if (rc != 0)
 		GOTO(put_child, rc);
 	/*
@@ -1357,6 +1352,7 @@ static int mdt_reint_migrate_internal(struct mdt_thread_info *info,
 	struct mdt_lock_handle  *lh_tgtp = NULL;
 	struct lu_fid           *old_fid = &info->mti_tmp_fid1;
 	struct list_head	lock_list;
+	__u64			lock_ibits;
 	int			rc;
 	ENTRY;
 
@@ -1445,7 +1441,7 @@ static int mdt_reint_migrate_internal(struct mdt_thread_info *info,
 
 	rc = mdt_object_lock(info, mold, lh_childp, lock_ibits);
 	if (rc != 0)
-		GOTO(out_unlock_list, rc);
+		GOTO(out_unlock_child, rc);
 
 	ma->ma_need = MA_LMV;
 	ma->ma_valid = 0;
@@ -1453,7 +1449,7 @@ static int mdt_reint_migrate_internal(struct mdt_thread_info *info,
 	ma->ma_lmv_size = sizeof(info->mti_xattr_buf);
 	rc = mdt_stripe_get(info, mold, ma, XATTR_NAME_LMV);
 	if (rc != 0)
-		GOTO(out_unlock_list, rc);
+		GOTO(out_unlock_child, rc);
 
 	if ((ma->ma_valid & MA_LMV)) {
 		struct lmv_mds_md_v1 *lmm1;
