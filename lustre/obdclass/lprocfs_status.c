cd24e489a29d6aef2a81fc6517812c2ba72ef28f
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2015-03-02 11:49:26 +0300

    LU-5319 protocol: define reserved OBD_CONNECT bits
    to prevent overuse.
    
    Change-Id: I135cdc4fa535920735a7400b4f9e5370b134a1ac
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -887,6 +887,11 @@ static const char *obd_connect_names[] = {
 	"pingless",
 	"flock_deadlock",
 	"disp_stripe",
+	"open_by_fid",
+	"lfsck",
+	"unlink_close",
+	"multi_mod_rpcs",
+	"dir_stripe",
 	"unknown",
 	NULL
 };
