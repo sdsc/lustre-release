0c091a3c0dd57003e37e63f175936e5b3fddce39
Yang Sheng <yang.sheng@intel.com>
2015-03-06 02:59:51 +0800

    LU-6109 osd-ldiskfs: fix previous patch issue
    The commit: 4d408c9aed9adaf1f4e2ea87851728a1cf662594
    bring a issue in '..' no fid-in-dirent case. Since
    it just deal with '..' for remote case. So if '..'
    is local object and no fid-in-dirent, then it may
    cause '..' cannot be display when 'ls -la'.
    
    Signed-off-by: Yang Sheng <yang.sheng@intel.com>
    Change-Id: Ib2b0be1c43986472cb674dd86d32f023a4b26a34

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -5674,17 +5674,18 @@ static inline int osd_it_ea_rec(const struct lu_env *env,
 	} else {
 		attr &= ~LU_DIRENT_ATTRS_MASK;
 		if (!fid_is_sane(fid)) {
+			int dd = 0;
 			if (it->oie_dirent->oied_namelen == 2 &&
 			    it->oie_dirent->oied_name[0] == '.' &&
-			    it->oie_dirent->oied_name[1] == '.') {
-				/* If the parent is on remote MDT, and there
-				 * is no FID-in-dirent, then we have to get
-				 * the parent FID from the linkEA.  */
-				if (ino == osd_remote_parent_ino(dev))
-					rc = osd_get_pfid_from_linkea(env, obj,
-								      fid);
+			    it->oie_dirent->oied_name[1] == '.')
+				dd = 1;
+			/* If the parent is on remote MDT, and there
+			 * is no FID-in-dirent, then we have to get
+			 * the parent FID from the linkEA.  */
+			if (ino == osd_remote_parent_ino(dev) && dd) {
+				rc = osd_get_pfid_from_linkea(env, obj, fid);
 			} else {
-				if (OBD_FAIL_CHECK(OBD_FAIL_FID_LOOKUP))
+				if (!dd && OBD_FAIL_CHECK(OBD_FAIL_FID_LOOKUP))
 					RETURN(-ENOENT);
 
 				rc = osd_ea_fid_get(env, obj, ino, fid, id);
