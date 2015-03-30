37c23313eebb2a3b31c7a016c876a8a4e6f774f6
Gregoire Pichon <gregoire.pichon@bull.net>
2015-03-30 17:06:54 +0200

    LU-5319 mdc: manage number of modify RPCs in flight
    This patch is the main client part of a new feature that supports
    multiple modify metadata RPCs in parallel. Its goal is to improve
    metadata operations performance of a single client, while maintening
    the consistency of MDT reply reconstruction and MDT recovery
    mecanisms.
    
    It allows to manage the number of modify RPCs in flight within
    the client obd structure and to assign a virtual index (the tag) to
    each modify RPC to help server side cleaning of reply data.
    
    The mdc component uses this feature to send multiple modify RPCs
    in parallel.
    
    Signed-off-by: Gregoire Pichon <gregoire.pichon@bull.net>
    Change-Id: Ia707e39770e479648627611a99d0724e7070baec

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -10901,16 +10901,18 @@ run_test 181 "Test open-unlinked dir ========================"
 
 test_182() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
-	# disable MDC RPC lock wouldn't crash client
 	local fcount=1000
-	local tcount=4
+	local tcount=10
 
 	mkdir -p $DIR/$tdir || error "creating dir $DIR/$tdir"
-#define OBD_FAIL_MDC_RPCS_SEM		0x804
-	$LCTL set_param fail_loc=0x804
+
+	$LCTL set_param mdc.*.rpc_stats="clear"
 
 	for (( i=0; i < $tcount; i++ )) ; do
 		mkdir $DIR/$tdir/$i
+	done
+
+	for (( i=0; i < $tcount; i++ )) ; do
 		createmany -o $DIR/$tdir/$i/f- $fcount &
 	done
 	wait
@@ -10920,11 +10922,11 @@ test_182() {
 	done
 	wait
 
-	rm -rf $DIR/$tdir
+	$LCTL get_param mdc.*.rpc_stats
 
-	$LCTL set_param fail_loc=0
+	rm -rf $DIR/$tdir
 }
-run_test 182 "Disable MDC RPCs semaphore wouldn't crash client ================"
+run_test 182 "Test parallel modify metadata operations ================"
 
 test_183() { # LU-2275
 	remote_mds_nodsh && skip "remote MDS with nodsh" && return
