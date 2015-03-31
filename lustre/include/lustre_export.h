79e1a7bebc85d05db911d2bd471d2bfb2e056c47
Gregoire Pichon <gregoire.pichon@bull.net>
2015-03-31 14:47:53 +0200

    LU-5319 mdt: support multiple modify RCPs in parallel
    This patch implements the server part of the feature that allows
    support of multiple modify RPCs in parallel on MDT targets.
    
    Each target export is able to store several in-memory reply
    data so that it can reconstruct several requests issued by
    the client in parallel.
    
    Additionally, a new internal file REPLY_DATA is created on
    the target to store on-disk reply data. The reply data slots
    in that file are managed by a bitmap (lut_reply_bitmap) and
    can be used to store reply data of any client.
    
    When target recovers, the on-disk reply data is used to
    restore the in-memory reply data and ensure reconstruction
    of committed operations.
    
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Signed-off-by: Gregoire Pichon <gregoire.pichon@bull.net>
    Change-Id: I8f91666f5b0b4f7b9445a01c520d73f56d059ff3

--- lustre/include/lustre_export.h
+++ lustre/include/lustre_export.h
@@ -71,6 +71,18 @@ struct tg_export_data {
 	/** nodemap this export is a member of */
 	struct lu_nodemap	*ted_nodemap;
 	struct hlist_node	ted_nodemap_member;
+
+	/* Every reply data fields below are
+	 * protected by ted_lcd_lock */
+	/** List of reply data */
+	struct list_head	ted_reply_list;
+	int			ted_reply_cnt;
+	/** Reply data with highest transno is retained */
+	struct tg_reply_data	*ted_last_reply;
+	/* Statistics */
+	int			ted_reply_hwm;
+	int			ted_release_xid;
+	int			ted_release_tag;
 };
 
 /**
