ad73b6d3807fe2a4c31274295f396b1d4642a406
Gregoire Pichon <gregoire.pichon@bull.net>
2015-03-31 14:47:53 +0200

    LU-5319 mdt: support multiple modify RCPs in parallel
    This patch implements the server part of the feature that allows
    support of multiple modify RPCs in parallel on MDT targets.
    
    Each target export is able to store several in-memory reply
    data so that it can reconstruct several requests issued by
    the client in parallel.
    
    Additionally, a new internal file REPLY_DATA is created on
    the target to store on-disk reply data. The reply data slots
    in that file are managed by a bitmap (lut_reply_bitmap) and
    can be used to store reply data of any client.
    
    When target recovers, the on-disk reply data is used to
    restore the in-memory reply data and ensure reconstruction
    of committed operations.
    
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Signed-off-by: Gregoire Pichon <gregoire.pichon@bull.net>
    Change-Id: I8f91666f5b0b4f7b9445a01c520d73f56d059ff3

--- lustre/mdt/mdt_internal.h
+++ lustre/mdt/mdt_internal.h
@@ -65,14 +65,6 @@
 #include <lustre_quota.h>
 #include <lustre_linkea.h>
 
-/* check if request's xid is equal to last one or not*/
-static inline int req_xid_is_last(struct ptlrpc_request *req)
-{
-        struct lsd_client_data *lcd = req->rq_export->exp_target_data.ted_lcd;
-        return (req->rq_xid == lcd->lcd_last_xid ||
-                req->rq_xid == lcd->lcd_last_close_xid);
-}
-
 struct mdt_object;
 
 /* file data for open files on MDS */
@@ -431,6 +423,7 @@ struct mdt_thread_info {
 	/* should be enough to fit lustre_mdt_attrs */
 	char			   mti_xattr_buf[128];
 	struct ldlm_enqueue_info   mti_einfo;
+	struct tg_reply_data	  *mti_reply_data;
 };
 
 extern struct lu_context_key mdt_thread_key;
@@ -776,7 +769,7 @@ __u32 mdt_identity_get_perm(struct md_identity *, __u32, lnet_nid_t);
 int mdt_pack_remote_perm(struct mdt_thread_info *, struct mdt_object *, void *);
 
 /* mdt/mdt_recovery.c */
-void mdt_req_from_lcd(struct ptlrpc_request *req, struct lsd_client_data *lcd);
+__u64 mdt_req_from_lrd(struct ptlrpc_request *req, struct tg_reply_data *trd);
 
 /* mdt/mdt_hsm.c */
 int mdt_hsm_state_get(struct tgt_session_info *tsi);
@@ -914,13 +907,12 @@ static inline int mdt_check_resent(struct mdt_thread_info *info,
         struct ptlrpc_request *req = mdt_info_req(info);
         ENTRY;
 
-        if (lustre_msg_get_flags(req->rq_reqmsg) & MSG_RESENT) {
-                if (req_xid_is_last(req)) {
+	if (lustre_msg_get_flags(req->rq_reqmsg) & MSG_RESENT) {
+		if (req_can_reconstruct(req, &info->mti_reply_data)) {
                         reconstruct(info, lhc);
                         RETURN(1);
                 }
-                DEBUG_REQ(D_HA, req, "no reply for RESENT req (have "LPD64")",
-                          req->rq_export->exp_target_data.ted_lcd->lcd_last_xid);
+		DEBUG_REQ(D_HA, req, "no reply data found for RESENT req");
         }
         RETURN(0);
 }
