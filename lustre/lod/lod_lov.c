72666597d8a37f17ca0bbb7a5a82b62845edc012
Gregoire Pichon <gregoire.pichon@bull.net>
2015-04-01 16:45:42 +0200

    LU-5319 osp: manage number of modify RPCs in flight
    This patch allows the osp component to send multiple modify RPCs
    in parallel to the MDT. This will improve metadata performance of
    cross MDT operations.
    
    Signed-off-by: Gregoire Pichon <gregoire.pichon@bull.net>
    Change-Id: Icb601afabd6767463634a4c7943ec4206bc758ec

--- lustre/lod/lod_lov.c
+++ lustre/lod/lod_lov.c
@@ -274,7 +274,8 @@ int lod_add_device(const struct lu_env *env, struct lod_device *lod,
 					   OBD_CONNECT_FID |
 					   OBD_CONNECT_AT |
 					   OBD_CONNECT_FULL20 |
-					   OBD_CONNECT_LFSCK;
+					   OBD_CONNECT_LFSCK |
+					   OBD_CONNECT_MULTIMODRPCS;
 		spin_lock(&imp->imp_lock);
 		imp->imp_server_timeout = 1;
 		spin_unlock(&imp->imp_lock);
