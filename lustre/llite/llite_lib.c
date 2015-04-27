f72097d6ee3edf6820a5814b1dd25206e38ec491
Bruno Faccini <bruno.faccini@intel.com>
2015-04-27 11:08:34 +0200

    LU-6460 llite: clear LLIF_FILE_RESTORING when done
    Clear LLIF_FILE_RESTORING if restore done to ensure to start again
    to glimpse new attrs.
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Change-Id: I0f2f9532e06965273efa10e3f69e26d00676b8a6

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -2007,6 +2007,12 @@ int ll_update_inode(struct inode *inode, struct lustre_md *md)
 	if (body->mbo_valid & OBD_MD_TSTATE) {
 		if (body->mbo_t_state & MS_RESTORE)
 			lli->lli_flags |= LLIF_FILE_RESTORING;
+	} else {
+		/* Clear LLIF_FILE_RESTORING if restore done
+		 * to ensure to start again to glimpse new attrs
+		 */
+		if (lli->lli_flags & LLIF_FILE_RESTORING)
+			lli->lli_flags &= ~LLIF_FILE_RESTORING;
 	}
 
 	return 0;
