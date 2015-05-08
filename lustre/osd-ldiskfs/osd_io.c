a7ee6e2c623a7d17d394254896750cc51e15e6f2
Yang Sheng <yang.sheng@intel.com>
2015-05-08 23:17:33 +0800

    LU-6489 osd: check max_page_index correctly
    This patch fixes a issue that original code check
    max_page_index against incorrect variable.
    
    Signed-off-by: Yang Sheng <yang.sheng@intel.com>
    Change-Id: I4b164d6187ef4a1c343d4b5803b8915283394037

--- lustre/osd-ldiskfs/osd_io.c
+++ lustre/osd-ldiskfs/osd_io.c
@@ -845,7 +845,7 @@ static int osd_ldiskfs_map_ext_inode_pages(struct inode *inode,
 			continue;
 		}
 
-		if (fp->index + i >= extent_max_page_index)
+		if (fp->index + clen >= extent_max_page_index)
 			GOTO(cleanup, rc = -EFBIG);
 
 		/* process found extent */
@@ -860,10 +860,14 @@ static int osd_ldiskfs_map_ext_inode_pages(struct inode *inode,
 		blocks += blocks_per_page * clen;
 	}
 
-	if (fp)
+	if (fp) {
+		if (fp->index + clen >= extent_max_page_index)
+			GOTO(cleanup, rc = -EFBIG);
 		rc = osd_ldiskfs_map_nblocks(inode, fp->index * blocks_per_page,
 					     clen * blocks_per_page, blocks,
 					     create);
+	}
+
 cleanup:
 	return rc;
 }
@@ -918,7 +922,7 @@ static int osd_ldiskfs_map_inode_pages(struct inode *inode, struct page **page,
 			if (++i != pages)
 				continue;
 		}
-		if (fp->index + i >= max_page_index)
+		if (fp->index + clen >= max_page_index)
 			GOTO(cleanup, rc = -EFBIG);
 		/* process found extent */
 		map.m_lblk = fp->index * blocks_per_page;
