1e02901b5e066f044438bc1aee16317cf80b648a
Niu Yawei <yawei.niu@intel.com>
2015-05-18 22:31:00 -0400

    LU-6617 quota: remove quota migration code
    The quota migration code was used to migrate old quota admin
    files into new format when upgrading system from old 2.1 system.
    
    It can be removed now, anyone want to upgrade from such an old
    system, and want to keep the quota limits settings at the same
    time, one has to upgrade to an intermediate release to do the
    migration first.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I41699157f07eba94453a86fe77a02189807ecbe8

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -3119,10 +3119,6 @@ static int osd_index_try(const struct lu_env *env, struct dt_object *dt,
         }
         LINVRNT(osd_invariant(obj));
 
-	if (result == 0 && is_quota_glb_feat(feat) &&
-	    fid_seq(lu_object_fid(&dt->do_lu)) == FID_SEQ_QUOTA_GLB)
-		result = osd_quota_migration(env, dt, feat);
-
         return result;
 }
 
