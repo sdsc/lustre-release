362adf7283021439eb1ffa5262e469527d3c207c
Kit Westneat <kit.westneat@gmail.com>
2015-05-20 16:28:19 -0400

    LU-5092 nodemap: convert member hash to a list
    The members of a nodemap were stored in a hash, but they were only
    iterated against, not accessed by key. This patch converts the hash
    to a list in order to simplifying things.
    
    Signed-off-by: Kit Westneat <kit.westneat@gmail.com>
    Change-Id: Ic1df4035d1c53a7c7966c4c528d20cefc7258d0e

--- lustre/include/lustre_export.h
+++ lustre/include/lustre_export.h
@@ -70,7 +70,7 @@ struct tg_export_data {
 
 	/** nodemap this export is a member of */
 	struct lu_nodemap	*ted_nodemap;
-	struct hlist_node	ted_nodemap_member;
+	struct list_head	ted_nodemap_member;
 };
 
 /**
