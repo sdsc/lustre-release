64efc6a53a8fd34a89234d223f7844506ccfeaaf
Niu Yawei <yawei.niu@intel.com>
2015-05-21 11:07:54 -0400

    LU-6529 ldlm: reclaim granted locks defensively
    To avoid ldlm lock exhausting server memory, two global parameters:
    ldlm_watermark_low & ldlm_watermark_high are used for reclaiming
    granted locks and rejecting incoming enqueue requests defensively.
    
    ldlm_watermark_low: When the amount of granted locks reaching this
    threshold, server start to revoke locks gradually.
    
    ldlm_watermark_high: When the amount of granted locks reaching this
    threshold, server will return -EINPROGRESS to any incoming enqueue
    request until the lock count is shrunk below the threshold again.
    
    ldlm_watermark_low & ldlm_watermark_high can be set to a ratio of
    the total memory (1 ~ 99) or kbytes of memory (>= 100), when it's
    set to zero, the feature is disabled.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I2fab39ac0ab6f269b7f1a40f3e08b8a51807cc69

--- lustre/ldlm/ldlm_internal.h
+++ lustre/ldlm/ldlm_internal.h
@@ -339,3 +339,10 @@ void ldlm_flock_policy_wire_to_local(const ldlm_wire_policy_data_t *wpolicy,
 
 void ldlm_flock_policy_local_to_wire(const ldlm_policy_data_t *lpolicy,
                                      ldlm_wire_policy_data_t *wpolicy);
+
+/* ldlm_reclaim.c */
+int ldlm_reclaim_setup(void);
+void ldlm_reclaim_cleanup(void);
+void ldlm_reclaim_add(struct ldlm_lock *lock);
+void ldlm_reclaim_del(struct ldlm_lock *lock);
+bool ldlm_granted_full(void);
--- lustre/obdclass/class_obd.c
+++ lustre/obdclass/class_obd.c
@@ -117,6 +117,11 @@ struct lprocfs_stats *obd_memory = NULL;
 EXPORT_SYMBOL(obd_memory);
 #endif
 
+__u64 ldlm_watermark_low;
+EXPORT_SYMBOL(ldlm_watermark_low);
+__u64 ldlm_watermark_high;
+EXPORT_SYMBOL(ldlm_watermark_high);
+
 /* Get jobid of current process by reading the environment variable
  * stored in between the "env_start" & "env_end" of task struct.
  *
