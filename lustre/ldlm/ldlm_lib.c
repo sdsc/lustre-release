8f65e26e67fdd5f02015f96c027e31b8d19103c6
Hongchao Zhang <hongchao.zhang@intel.com>
2015-05-29 02:12:29 +0800

    LU-6841 target: debug patch to collect more logs
    debug patch to collect the logs during recovery at MDT.
    
    Test-Parameters: fortestonly alwaysuploadlogs \
    envdefinitions=SLOW=yes \
    mdtfilesystemtype=ldiskfs mdsfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs \
    clientdistro=el6.6 ossdistro=el6.6 mdsdistro=el6.6 clientarch=x86_64 \
    ossarch=x86_64 mdsarch=x86_64 mdtcount=4 \
    austeroptions=-R failover=true \
    testlist=replay-single,replay-single,replay-single
    
    Change-Id: I2774af319a8686d8825fc4fc8aebdde5bbac5d57
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -2436,10 +2436,8 @@ static int target_recovery_thread(void *arg)
 	delta = jiffies_to_msecs(jiffies - delta) / MSEC_PER_SEC;
 	CDEBUG(D_INFO,"4: recovery completed in %lus - %d/%d reqs/locks\n",
 	      delta, obd->obd_replayed_requests, obd->obd_replayed_locks);
-	if (delta > OBD_RECOVERY_TIME_SOFT) {
+	if (delta > OBD_RECOVERY_TIME_SOFT)
 		CWARN("too long recovery - read logs\n");
-		libcfs_debug_dumplog();
-	}
 
 	target_finish_recovery(lut);
 
