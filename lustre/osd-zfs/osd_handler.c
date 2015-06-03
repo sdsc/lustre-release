7fc0e68df9f917fb0f590f3169867dfa082f6373
Jinshan Xiong <jinshan.xiong@intel.com>
2015-06-03 00:06:19 -0700

    LU-4865 zfs: grow block size by write pattern
    This patch grows the block size by write RPC. The osd-zfs blocksize
    used to be fixed at 128KB, which is too big for random write and
    too small for seqential write.
    
    This patch decides the block size by the first few RPCs. If the first
    few RPCs are sequential, mostly it will use bigger block size;
    otherwise, smaller block size will be used.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I6cf8fa8eca998e73bbb99f66eb1323c3abcf39f0

--- lustre/osd-zfs/osd_handler.c
+++ lustre/osd-zfs/osd_handler.c
@@ -938,7 +938,7 @@ static int osd_mount(const struct lu_env *env,
 		GOTO(err, rc);
 
 	o->od_xattr_in_sa = B_TRUE;
-	o->od_max_blksz = SPA_OLD_MAXBLOCKSIZE;
+	o->od_max_blksz = min_t(uint64_t, SPA_MAXBLOCKSIZE, ONE_MB_BRW_SIZE);
 
 	rc = osd_objset_register_callbacks(o);
 	if (rc)
--- lustre/osd-zfs/osd_object.c
+++ lustre/osd-zfs/osd_object.c
@@ -1292,16 +1292,9 @@ static dmu_buf_t *osd_mkreg(const struct lu_env *env, struct osd_object *obj,
 	if (rc)
 		return ERR_PTR(rc);
 
-	/*
-	 * XXX: This heuristic is non-optimal.  It would be better to
-	 * increase the blocksize up to osd->od_max_blksz during the write.
-	 * This is exactly how the ZPL behaves and it ensures that the right
-	 * blocksize is selected based on the file size rather than the
-	 * making broad assumptions based on the osd type.
-	 */
 	if (!lu_device_is_md(osd2lu_dev(osd))) {
 		rc = -dmu_object_set_blocksize(osd->od_os, db->db_object,
-					       osd->od_max_blksz, 0, oh->ot_tx);
+					       PAGE_CACHE_SIZE, 0, oh->ot_tx);
 		if (unlikely(rc)) {
 			CERROR("%s: can't change blocksize: %d\n",
 			       osd->od_svname, rc);
