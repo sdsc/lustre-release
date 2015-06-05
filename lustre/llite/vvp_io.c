d8e5e0cbcc0900c29adaf43f23e2eb7649e5a7b1
Al Viro <viro@zeniv.linux.org.uk>
2015-06-05 11:16:26 -0400

    LU-6260 llite: add support for new iter functionality
    For the 3.16+ kernels struct file_operations added new read
    and write methods; read_iter and write_iter; to use struct
    iov_iter as a parameter since it contains all the iovec
    data needed. This avoid having the file system managing
    iovec data like transversing the iovec page list. Now we
    can use supplied iov_iter helper functions that does this
    work for us. In later kernels only this back end is
    supported. Backported from the upstream lustre client.
    
    -----------------------------------------------------------------------
    Linux-commit: b42b15fdad3ebb790250041d1517acebb9bd56d9
    
    lustre: get rid of messing with iovecs
    
    * switch to ->read_iter/->write_iter
    * keep a pointer to iov_iter instead of iov/nr_segs
    * do not modify iovecs; use iov_iter_truncate()/iov_iter_advance() and
      a new primitive - iov_iter_reexpand() (expand previously truncated
      iterator) istead.
    * (racy) check for lustre VMAs intersecting with iovecs kept for now as
      for_each_iov() loop.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    -----------------------------------------------------------------------
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Change-Id: I71928beaa7b1c87f3e2c689e1dee96052eaef872

--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -394,7 +394,12 @@ static int vvp_mmap_locks(const struct lu_env *env,
 	struct cl_lock_descr   *descr = &vti->vti_descr;
         ldlm_policy_data_t      policy;
         unsigned long           addr;
+#ifdef HAVE_FILE_ITER
+	struct iov_iter i;
+	struct iovec iov;
+#else
         unsigned long           seg;
+#endif
         ssize_t                 count;
 	int                     result = 0;
         ENTRY;
@@ -404,18 +409,29 @@ static int vvp_mmap_locks(const struct lu_env *env,
         if (!cl_is_normalio(env, io))
                 RETURN(0);
 
-	if (vio->vui_iov == NULL) /* nfs or loop back device write */
+	/* nfs or loop back device write */
+#ifdef HAVE_FILE_ITER
+	if (vio->vui_iter == NULL)
+#else
+	if (vio->vui_iov == NULL)
+#endif
 		RETURN(0);
 
 	/* No MM (e.g. NFS)? No vmas too. */
 	if (mm == NULL)
 		RETURN(0);
 
+#ifdef HAVE_FILE_ITER
+	iov_for_each(iov, i, *(vio->vui_iter)) {
+		addr = (unsigned long)iov.iov_base;
+		count = iov.iov_len;
+#else /* HAVE_FILE_ITER */
 	for (seg = 0; seg < vio->vui_nrsegs; seg++) {
 		const struct iovec *iv = &vio->vui_iov[seg];
 
                 addr = (unsigned long)iv->iov_base;
                 count = iv->iov_len;
+#endif /* !HAVE_FILE_ITER */
                 if (count == 0)
                         continue;
 
@@ -477,13 +493,17 @@ static void vvp_io_advance(const struct lu_env *env,
 	struct vvp_io    *vio = cl2vvp_io(env, ios);
 	struct cl_io     *io  = ios->cis_io;
 	struct cl_object *obj = ios->cis_io->ci_obj;
+#ifndef HAVE_FILE_ITER
 	struct iovec     *iov;
-
+#endif
 	CLOBINVRNT(env, obj, vvp_object_invariant(obj));
 
 	if (!cl_is_normalio(env, io))
 		return;
 
+#ifdef HAVE_FILE_ITER
+	iov_iter_reexpand(vio->vui_iter, vio->vui_tot_count -= nob);
+#else /* HAVE_FILE_ITER */
 	LASSERT(vio->vui_tot_nrsegs >= vio->vui_nrsegs);
 	LASSERT(vio->vui_tot_count  >= nob);
 
@@ -509,13 +529,20 @@ static void vvp_io_advance(const struct lu_env *env,
 
 	vio->vui_iov = iov;
 	vio->vui_tot_count -= nob;
+#endif /* !HAVE_FILE_ITER */
 }
 
 static void vvp_io_update_iov(const struct lu_env *env,
 			      struct vvp_io *vio, struct cl_io *io)
 {
-	int i;
 	size_t size = io->u.ci_rw.crw_count;
+#ifdef HAVE_FILE_ITER
+	if (!cl_is_normalio(env, io) || vio->vui_iter == NULL)
+		return;
+
+	iov_iter_truncate(vio->vui_iter, size);
+#else /* HAVE_FILE_ITER */
+	int i;
 
 	vio->vui_iov_olen = 0;
 	if (!cl_is_normalio(env, io) || vio->vui_tot_nrsegs == 0)
@@ -539,6 +566,7 @@ static void vvp_io_update_iov(const struct lu_env *env,
 	LASSERTF(vio->vui_tot_nrsegs >= vio->vui_nrsegs,
 		 "tot_nrsegs: %lu, nrsegs: %lu\n",
 		 vio->vui_tot_nrsegs, vio->vui_nrsegs);
+#endif /* !HAVE_FILE_ITER */
 }
 
 static int vvp_io_rw_lock(const struct lu_env *env, struct cl_io *io,
@@ -789,9 +817,13 @@ static int vvp_io_read_start(const struct lu_env *env,
 	switch (vio->vui_io_subtype) {
 	case IO_NORMAL:
 		LASSERT(vio->vui_iocb->ki_pos == pos);
+#ifdef HAVE_FILE_ITER
+		result = generic_file_read_iter(vio->vui_iocb, vio->vui_iter);
+#else
 		result = generic_file_aio_read(vio->vui_iocb,
 					       vio->vui_iov, vio->vui_nrsegs,
 					       vio->vui_iocb->ki_pos);
+#endif
 		break;
 	case IO_SPLICE:
 		result = generic_file_splice_read(file, &pos,
@@ -1040,7 +1072,11 @@ static int vvp_io_write_start(const struct lu_env *env,
 		RETURN(-EFBIG);
 	}
 
+#ifdef HAVE_FILE_ITER
+	if (vio->vui_iter == NULL) {
+#else
 	if (vio->vui_iov == NULL) {
+#endif
 		/* from a temp io in ll_cl_init(). */
 		result = 0;
 	} else {
@@ -1052,9 +1088,13 @@ static int vvp_io_write_start(const struct lu_env *env,
 		 * consistency, proper locking to protect against writes,
 		 * trucates, etc. is handled in the higher layers of lustre.
 		 */
+#ifdef HAVE_FILE_ITER
+		result = generic_file_write_iter(vio->vui_iocb, vio->vui_iter);
+#else
 		result = __generic_file_aio_write(vio->vui_iocb,
 						  vio->vui_iov, vio->vui_nrsegs,
 						  &vio->vui_iocb->ki_pos);
+#endif
 		if (result > 0 || result == -EIOCBQUEUED) {
 			ssize_t err;
 
@@ -1404,11 +1444,15 @@ int vvp_io_init(const struct lu_env *env, struct cl_object *obj,
                  *  results."  -- Single Unix Spec */
                 if (count == 0)
                         result = 1;
-		else {
+		else
+#ifdef HAVE_FILE_ITER
+			vio->vui_tot_count = count;
+#else
+		{
 			vio->vui_tot_count = count;
 			vio->vui_tot_nrsegs = 0;
 		}
-
+#endif
 		/* for read/write, we store the jobid in the inode, and
 		 * it'll be fetched by osc when building RPC.
 		 *
