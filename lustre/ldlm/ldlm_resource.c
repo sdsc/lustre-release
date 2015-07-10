a0f0aebe6c7b4c311d1e0a975e8833cdf1b184f3
Vitaly Fertman <vitaly_fertman@xyratex.com>
2015-07-10 14:31:10 -0400

    LU-3031 ldlm: disconnect speedup
    disconnect takes too long time if there are many locks to cancel.
    besides the amount of time spent on each lock cancel, there is a
    resched() in cfs_hash_for_each_relax(), i.e. disconnect or eviction
    may take unexpectedly long time.
    - do not cancel locks on disconnect_export;
    - export will be left in obd_unlinked_exports list pinned by live
      locks;
    - new re-connects will created other non-conflicting exports;
    - new locks will cancel obsolete locks on conflicts;
    - once all the locks on the disconnected export will be cancelled,
      the export will be destroyed on the last ref put;
    - do not cancel in small portions, cancel all together in just 1
      dedicated thread - use server side blocking thread for that;
    - cancel blocked locks first so that waiting locks could proceed;
    - take care about blocked waiting locks, so that they would get
      cancelled quickly too;
    - do not remove lock from waiting list on AST error before moving
      it to elt_expired_locks list, because it removes it from export
      list too; otherwise this blocked lock will not be cancelled
      immediately on failed export;
    - cancel lock instead of just destroy for failed export, to make
      full cleanup, i.e. remove it from export list.
    
    also make the proper order of events on umount:
    - disconnect export;
    - cleanup namespace, to cancel all the locks before export barrier;
    - exports barrier;
    - lprocfs_free_per_client_stats (requires nid_exp_ref_count == 0);
    - namespace_free_post is left in cleanup ensure will not get and
      segfault on an absent namespace.
    
    Signed-off-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Change-Id: Ia39b09ce967237ed5078c8a71e760b1e103c6f55
    Xyratex-bug-id: MRP-395 MRP-1366 MRP-1366
    Reviewed-by: Andriy Skulysh <Andriy_Skulysh@xyratex.com>
    Reviewed-by: Alexey Lyashkov <Alexey_Lyashkov@xyratex.com>
    Tested-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>

--- lustre/ldlm/ldlm_resource.c
+++ lustre/ldlm/ldlm_resource.c
@@ -744,11 +744,10 @@ static void cleanup_resource(struct ldlm_resource *res, struct list_head *q,
                         if (rc)
                                 CERROR("ldlm_cli_cancel: %d\n", rc);
                 } else {
-                        ldlm_resource_unlink_lock(lock);
                         unlock_res(res);
                         LDLM_DEBUG(lock, "Freeing a lock still held by a "
                                    "client node");
-                        ldlm_lock_destroy(lock);
+			ldlm_lock_cancel(lock);
                 }
                 LDLM_LOCK_RELEASE(lock);
         } while (1);
@@ -896,6 +895,7 @@ void ldlm_namespace_free_prior(struct ldlm_namespace *ns,
         }
         EXIT;
 }
+EXPORT_SYMBOL(ldlm_namespace_free_prior);
 
 /**
  * Performs freeing memory structures related to \a ns. This is only done
@@ -927,6 +927,7 @@ void ldlm_namespace_free_post(struct ldlm_namespace *ns)
 	ldlm_put_ref();
 	EXIT;
 }
+EXPORT_SYMBOL(ldlm_namespace_free_post);
 
 /**
  * Cleanup the resource, and free namespace.
