3c842c0cd831a58634bf1eb3746278f10c46072a
Elena Gryaznova <elena.gryaznova@seagate.com>
2015-07-23 20:46:11 +0300

    LU-6119 test: Add testing for Lustre exported via Samba
    Create a parallel-scale-cifs.sh test which exports
    Lustre via Samba and runs concurrent load compilebench, dbench,
    fsx, iozone on Samba clients.
    
    The test assumes that Samba server and clients have necessary
    packages installed.
    
    Signed-off-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Xyratex-bug-id: MRP-1374
    Reviewed-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Change-Id: Iace3b786bd34e42198a2ffdb35c436595fe2b8a5

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -229,6 +229,7 @@ init_test_env() {
     # Ubuntu, at least, has a truncate command in /usr/bin
     # so fully path our truncate command.
     export TRUNCATE=${TRUNCATE:-$LUSTRE/tests/truncate}
+	export FSX=${FSX:-$LUSTRE/tests/fsx}
     export MDSRATE=${MDSRATE:-"$LUSTRE/tests/mpi/mdsrate"}
     [ ! -f "$MDSRATE" ] && export MDSRATE=$(which mdsrate 2> /dev/null)
     if ! echo $PATH | grep -q $LUSTRE/tests/racer; then
@@ -3321,6 +3322,7 @@ cleanup_echo_devs () {
 
 cleanupall() {
     nfs_client_mode && return
+	cifs_client_mode && return
 
     stopall $*
     cleanup_echo_devs
@@ -3557,6 +3559,7 @@ writeconf_all () {
 
 setupall() {
     nfs_client_mode && return
+	cifs_client_mode && return
 
     sanity_mount_check ||
         error "environments are insane!"
@@ -3822,6 +3825,11 @@ nfs_client_mode () {
     return 1
 }
 
+cifs_client_mode () {
+	[ x$CIFSCLIENT = xyes ] &&
+		echo "CIFSCLIENT=$CIFSCLIENT mode: setup, cleanup, check config skipped"
+}
+
 check_config_client () {
     local mntpt=$1
 
@@ -3869,6 +3877,7 @@ check_config_clients () {
 	local mntpt=$1
 
 	nfs_client_mode && return
+	cifs_client_mode && return
 
 	do_rpc_nodes "$clients" check_config_client $mntpt
 
@@ -3915,6 +3924,7 @@ is_empty_fs() {
 
 check_and_setup_lustre() {
     nfs_client_mode && return
+	cifs_client_mode && return
 
     local MOUNTED=$(mounted_lustre_filesystems)
 
@@ -7322,4 +7332,13 @@ check_start_ost_idx() {
 	[[ $start_ost_idx = $expected ]] ||
 		error "OST index of the first stripe on $file is" \
 		      "$start_ost_idx, should be $expected"
+}
+
+killall_process () {
+	local clients=${1:-$(hostname)}
+	local name=$2
+	local signal=$3
+	local rc=0
+
+	do_nodes $clients "killall $signal $name"
 }
