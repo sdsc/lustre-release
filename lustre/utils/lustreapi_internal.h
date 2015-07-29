4c254b75092cc622222eb7091e96155c6f0ce4c4
James Simmons <uja.ornl@yahoo.com>
2015-07-29 16:32:45 -0400

    LU-5030 utils: create a libcfs function to handle tunable paths
    With lustre going to sysfs in the future but at the same time
    many people using older versions of Lustre adds a requirement
    of suporting tunables for both the procfs and sysfs backends.
    Here we create a new library function that autoexpands the
    path and test for the presence of one or the other and sets
    or gets the tunable.
    
    Change-Id: I7fbb7348e009cb9f77ba9847aeb4879dc302e4ee
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Signed-off-by: Wang Chao <chao.ornl@gmail.com>

--- lustre/utils/lustreapi_internal.h
+++ lustre/utils/lustreapi_internal.h
@@ -35,6 +35,7 @@
 #ifndef _LUSTREAPI_INTERNAL_H_
 #define _LUSTREAPI_INTERNAL_H_
 
+#include <glob.h>
 #include <uapi_kernelcomm.h>
 
 #define WANT_PATH   0x1
@@ -45,8 +46,28 @@
 int get_root_path(int want, char *fsname, int *outfd, char *path, int index);
 int root_ioctl(const char *mdtname, int opc, void *data, int *mdtidxp,
 	       int want_error);
-int get_param(const char *param_path, char *result,
-	      unsigned int result_size);
+
+/* Lustre parameter helpers */
+enum param_filter {
+	FILTER_BY_EXACT,
+	FILTER_BY_FS_NAME,
+	FILTER_BY_PATH
+};
+
+int get_lustre_param_path(glob_t *param, const char *obd_type,
+			  const char *filter, enum param_filter type,
+			  const char *param_name);
+int get_lustre_clilov_path(glob_t *path, char *fsname, char *file_path,
+			   char *param_name);
+int get_lustre_param_value(const char *obd_type, const char *filter,
+			   enum param_filter type, const char *param_name,
+			   char *value, size_t val_len);
+
+static inline
+int poolpath(glob_t *pool_path, char *fsname, char *pathname)
+{
+	return get_lustre_clilov_path(pool_path, fsname, pathname, "pools");
+}
 
 #define LLAPI_LAYOUT_MAGIC 0x11AD1107 /* LLAPILOT */
 
