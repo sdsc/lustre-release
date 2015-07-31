fd97d8d760da84daadeec0ad3543d59ae60909fe
Sebastien Buisson <sebastien.buisson@bull.net>
2015-07-31 16:02:06 +0200

    LU-5560 tests: add sanity-selinux.sh
    New test sanity-selinux.sh aims at exercing SELinux support
    on the client side, as implemented according to LU-5560.
    This patch also adds new fail_locs in CLIO.
    
    Test-Parameters: trivial testlist=sanity-selinux
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Change-Id: I222a82329142705345837e1235dd01e1d85e7a28
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -3244,7 +3244,7 @@ facet_mntpt () {
 mount_ldiskfs() {
 	local facet=$1
 	local dev=$(facet_device $facet)
-	local mnt=$(facet_mntpt $facet)
+	local mnt=${2:-$(facet_mntpt $facet)}
 	local opts
 
 	if ! do_facet $facet test -b $dev; then
@@ -3256,7 +3256,7 @@ mount_ldiskfs() {
 unmount_ldiskfs() {
 	local facet=$1
 	local dev=$(facet_device $facet)
-	local mnt=$(facet_mntpt $facet)
+	local mnt=${2:-$(facet_mntpt $facet)}
 
 	do_facet $facet $UMOUNT $mnt
 }
@@ -3268,7 +3268,7 @@ var_name() {
 mount_zfs() {
 	local facet=$1
 	local ds=$(facet_device $facet)
-	local mnt=$(facet_mntpt $facet)
+	local mnt=${2:-$(facet_mntpt $facet)}
 	local canmnt
 	local mntpt
 
@@ -3291,7 +3291,7 @@ mount_zfs() {
 unmount_zfs() {
 	local facet=$1
 	local ds=$(facet_device $facet)
-	local mnt=$(facet_mntpt $facet)
+	local mnt=${2:-$(facet_mntpt $facet)}
 	local var_mntpt=mz_$(var_name ${facet}_$ds)_mountpoint
 	local var_canmnt=mz_$(var_name ${facet}_$ds)_canmount
 	local mntpt=${!var_mntpt}
@@ -3307,16 +3307,18 @@ unmount_zfs() {
 
 mount_fstype() {
 	local facet=$1
+	local mnt=$2
 	local fstype=$(facet_fstype $facet)
 
-	mount_$fstype $facet
+	mount_$fstype $facet $mnt
 }
 
 unmount_fstype() {
 	local facet=$1
+	local mnt=$2
 	local fstype=$(facet_fstype $facet)
 
-	unmount_$fstype $facet
+	unmount_$fstype $facet $mnt
 }
 
 ########
