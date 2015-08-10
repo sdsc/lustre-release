f891ed7b2ca76a3502de4fdbf9688a3965d5017a
Alexander Boyko <alexander.boyko@seagate.com>
2015-08-10 14:40:28 +0300

    LU-6910 osp: add procfs values for OST reserved size
    osp_pre_status=-ENOSPC is used to skip OST from object allocation.
    The error was set when OST available space is less than 0.1% of total
    OST size. This value is not configurable, so procfs files was
    added:
    reserved_mb_low - low watermark, if available space is less
    		  than it, object allocation is stopped.
    reserved_mb_high - highw watermark, if available space is more
    		   than it, object allocation is enabled.
    
    By default ~0.1% is reserved as low watermark. The high watermark
    is twice bigger than the low by default.
    High and low watermark could be changed by:
    lctl set_param osp.lustre-OST0000-osc-MDT0000.reserved_mb_high=1024
    
    When object allocation is disabled, a clients could appened to
    existing files. And 0.1% is too low for them. For example, OST size
    is 8TB, 0.1% is 8GB, if cluster has 1k clients, reserved space is
    ~8MB per client. The main reason of the patch is ability to increase
    reserved space.
    
    Signed-off-by: Alexander Boyko <alexander.boyko@seagate.com>
    Seagate-bug-id: MRP-2606
    Test-Parameters: testlist=sanity,sanity,sanity,sanity,sanity,sanity
    Test-Parameters: testlist=sanity,sanity,sanity,sanity,sanity,sanity
    Change-Id: Idd759352cec30a6039c228695f753465fbccc75f

--- lustre/include/lustre/lustre_user.h
+++ lustre/include/lustre/lustre_user.h
@@ -102,6 +102,8 @@ enum obd_statfs_state {
         OS_STATE_RDONLY_1       = 0x00000004, /**< obsolete 1.6, was EROFS=30 */
         OS_STATE_RDONLY_2       = 0x00000008, /**< obsolete 1.6, was EROFS=30 */
         OS_STATE_RDONLY_3       = 0x00000010, /**< obsolete 1.6, was EROFS=30 */
+	OS_STATE_ENOSPC		= 0x00000020, /**< not enough free space */
+	OS_STATE_ENOINO		= 0x00000040, /**< not enough inodes */
 };
 
 struct obd_statfs {
