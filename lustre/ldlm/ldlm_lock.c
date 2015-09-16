30295c85794e91650a908b6ebbc346a668c83a35
Jinshan Xiong <jinshan.xiong@intel.com>
2015-09-16 11:47:20 -0700

    LU-6271 osc: further OSC cleanup after eviction
    A few problems are fixed in this patch:
    1. a ldlm lock could be canceled simutaneously by ldlm bl thread and
      cleanup_resource(). In this case, only one side will win the race
      and the other side should wait for the work to complete;
    2. in lov_io_iter_init(), if cl_io_iter_init() against sub io fails,
      it should call cl_io_iter_fini() to cleanup leftover information;
    3. define osc_lru_reserve() and osc_lru_unreserve() to reserve LRU
      slots in osc_io_write_iter_init() and unreserve them in fini();
    4. cleanups
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I293770b62e177a9ecefe0b4e05f3a8f44b1c831d

--- lustre/ldlm/ldlm_lock.c
+++ lustre/ldlm/ldlm_lock.c
@@ -2169,6 +2169,19 @@ restart:
 }
 EXPORT_SYMBOL(ldlm_reprocess_all);
 
+static bool is_bl_done(struct ldlm_lock *lock)
+{
+	bool bl_done = true;
+
+	if (!ldlm_is_bl_done(lock)) {
+		lock_res_and_lock(lock);
+		bl_done = ldlm_is_bl_done(lock);
+		unlock_res_and_lock(lock);
+	}
+
+	return bl_done;
+}
+
 /**
  * Helper function to call blocking AST for LDLM lock \a lock in a
  * "cancelling" mode.
@@ -2186,8 +2199,19 @@ void ldlm_cancel_callback(struct ldlm_lock *lock)
                 } else {
                         LDLM_DEBUG(lock, "no blocking ast");
                 }
-        }
-	ldlm_set_bl_done(lock);
+
+		/* only canceller can set bl_done bit */
+		ldlm_set_bl_done(lock);
+		wake_up_all(&lock->l_waitq);
+        } else if (!ldlm_is_bl_done(lock)) {
+		struct l_wait_info lwi = { 0 };
+
+		/* The lock is guaranteed to have been canceled once
+		 * returning from this function. */
+		unlock_res_and_lock(lock);
+		l_wait_event(lock->l_waitq, is_bl_done(lock), &lwi);
+		lock_res_and_lock(lock);
+	}
 }
 
 /**
