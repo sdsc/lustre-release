a63ed45cbdec5bec726b75d0593cc0163b2e9152
Di Wang <di.wang@intel.com>
2015-10-04 20:34:41 -0700

    LU-0000 dne: dne llog fixes
    Mix fixes from update llog.
    
    Test-Parameters: fortestonly
    Signed-off-by: Di Wang <di.wang@intel.com>
    Change-Id: Id2415ebf51e496559b8c0326287eaa279eab160c

--- lustre/tests/cfg/local.sh
+++ lustre/tests/cfg/local.sh
@@ -13,7 +13,7 @@ TMP=${TMP:-/tmp}
 DAEMONSIZE=${DAEMONSIZE:-500}
 MDSCOUNT=${MDSCOUNT:-1}
 MDSDEVBASE=${MDSDEVBASE:-$TMP/${FSNAME}-mdt}
-MDSSIZE=${MDSSIZE:-200000}
+MDSSIZE=${MDSSIZE:-2000000}
 #
 # Format options of facets can be specified with these variables:
 #
@@ -89,8 +89,9 @@ STRIPE_BYTES=${STRIPE_BYTES:-1048576}
 STRIPES_PER_OBJ=${STRIPES_PER_OBJ:-0}
 SINGLEMDS=${SINGLEMDS:-"mds1"}
 TIMEOUT=${TIMEOUT:-20}
-PTLDEBUG=${PTLDEBUG:-"vfstrace rpctrace dlmtrace neterror ha config \
-		      ioctl super lfsck"}
+#PTLDEBUG=${PTLDEBUG:-"vfstrace rpctrace dlmtrace neterror ha config \
+#		      ioctl super lfsck"}
+PTLDEBUG=${PTLDEBUG:-"neterror ha config ioctl super lfsck"}
 SUBSYSTEM=${SUBSYSTEM:-"all -lnet -lnd -pinger"}
 
 # promise 2MB for every cpu
