9c2ad7a2594667d63841aee46921604858d2310f
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2015-10-14 20:46:08 +0300

    LU-5041 ofd: precreate to use LOC_F_NEW
    the idea is to skip OI lookup at object create declaration,
    instead issue prefetch request to ZFS. so having few creates
    declared we'd get few I/Os in fligh. this should improve
    creation rate as the precreation thread doesn't need to lookup
    OI mappings one by one issuing syncrhnous I/Os.
    
    OSDs should be able to deal with the case when an object
    exists, initialize the object using IO and return -EEXIST.
    
    the patch isn't complete yet.
    
    TODO:
     - fzap_prefetch()
     - a test causing dt_create() to return -EEXIST
    
    Test-Parameters: fortestonly
    
    Change-Id: I696b2f4ef3878696347d2fa17d0d94449cd3938f
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

--- lustre/osd-zfs/osd_object.c
+++ lustre/osd-zfs/osd_object.c
@@ -345,7 +345,7 @@ static int osd_check_lma(const struct lu_env *env, struct osd_object *obj)
 {
 	struct osd_thread_info	*info = osd_oti_get(env);
 	struct lu_buf		buf;
-	int			rc;
+	int			rc, size = 0;
 	struct lustre_mdt_attrs	*lma;
 	ENTRY;
 
@@ -354,9 +354,8 @@ static int osd_check_lma(const struct lu_env *env, struct osd_object *obj)
 	buf.lb_buf = lma;
 	buf.lb_len = sizeof(info->oti_buf);
 
-	rc = osd_xattr_get(env, &obj->oo_dt, &buf, XATTR_NAME_LMA);
-	if (rc > 0) {
-		rc = 0;
+	rc = __osd_xattr_get(env, obj, &buf, XATTR_NAME_LMA, &size);
+	if (rc == 0) {
 		lustre_lma_swab(lma);
 		if (unlikely((lma->lma_incompat & ~LMA_INCOMPAT_SUPP) ||
 			     CFS_FAIL_CHECK(OBD_FAIL_OSD_LMA_INCOMPAT))) {
@@ -366,7 +365,7 @@ static int osd_check_lma(const struct lu_env *env, struct osd_object *obj)
 			      PFID(lu_object_fid(&obj->oo_dt.do_lu)));
 			rc = -EOPNOTSUPP;
 		}
-	} else if (rc == -ENODATA) {
+	} else if (rc == -ENOENT) {
 		/* haven't initialize LMA xattr */
 		rc = 0;
 	}
@@ -394,6 +393,9 @@ static int osd_object_init(const struct lu_env *env, struct lu_object *l,
 		l->lo_header->loh_attr |= LOHA_EXISTS;
 		RETURN(0);
 	}
+
+	if (conf && conf->loc_flags & LOC_F_NEW)
+		RETURN(0);
 
 	rc = osd_fid_lookup(env, osd, lu_object_fid(l), &oid);
 	if (rc == 0) {
@@ -1045,6 +1047,11 @@ static int osd_declare_object_create(const struct lu_env *env,
 
 	LASSERT(dof);
 
+	down(&obj->oo_guard);
+
+	if (unlikely(lu_object_exists(&dt->do_lu)))
+		GOTO(out, rc = -EEXIST);
+
 	switch (dof->dof_type) {
 		case DFT_REGULAR:
 		case DFT_SYM:
@@ -1082,6 +1089,9 @@ static int osd_declare_object_create(const struct lu_env *env,
 	/* and we'll add it to some mapping */
 	zapid = osd_get_name_n_idx(env, osd, fid, buf);
 	dmu_tx_hold_bonus(oh->ot_tx, zapid);
+	/* XXX: with name specified it will be doing full lookup
+	 *	probably this is too expensive. instead we should
+	 *	be prefetching ZAP */
 	dmu_tx_hold_zap(oh->ot_tx, zapid, TRUE, buf);
 
 	/* we will also update inode accounting ZAPs */
@@ -1097,6 +1107,8 @@ static int osd_declare_object_create(const struct lu_env *env,
 
 	rc = osd_declare_quota(env, osd, attr->la_uid, attr->la_gid, 1, oh,
 			       false, NULL, false);
+out:
+	up(&obj->oo_guard);
 	RETURN(rc);
 }
 
@@ -1435,8 +1447,10 @@ static int osd_object_create(const struct lu_env *env, struct dt_object *dt,
 	 * in regular cases acquisition should be cheap */
 	down(&obj->oo_guard);
 
+	if (unlikely(dt_object_exists(dt)))
+		GOTO(out, rc = -EEXIST);
+
 	LASSERT(osd_invariant(obj));
-	LASSERT(!dt_object_exists(dt));
 	LASSERT(dof != NULL);
 
 	LASSERT(th != NULL);
@@ -1466,6 +1480,19 @@ static int osd_object_create(const struct lu_env *env, struct dt_object *dt,
 	zapid = osd_get_name_n_idx(env, osd, fid, buf);
 
 	rc = -zap_add(osd->od_os, zapid, buf, 8, 1, zde, oh->ot_tx);
+	if (unlikely(rc == -EEXIST)) {
+		/* another thread may have a reference to the
+		 * object, so it's better to initialize it
+		 * and let others to use the object as it
+		 * was successfully created */
+		dmu_object_free(osd->od_os, db->db_object, oh->ot_tx);
+		sa_buf_rele(db, osd_obj_tag);
+		rc = osd_object_init(env, &dt->do_lu, NULL);
+		if (rc < 0)
+			set_bit(LU_OBJECT_HEARD_BANSHEE,
+				&dt->do_lu.lo_header->loh_flags);
+		GOTO(out, rc = -EEXIST);
+	}
 	if (rc)
 		GOTO(out, rc);
 
