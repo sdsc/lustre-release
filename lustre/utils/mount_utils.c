9fd6157b0d665f065d0e93e0f2bb9d0079977897
Fan Yong <fan.yong@intel.com>
2015-10-21 10:24:58 +0800

    LU-5070 utils: mkfs.lustre code cleanup
    Re-order the options to increase the code readability.
    Remove unused code, redundant logic, white spaces,
    blank lines, and so on.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I88ee52fb2425e9e256b7751bad4b6cfcd88011ec

--- lustre/utils/mount_utils.c
+++ lustre/utils/mount_utils.c
@@ -590,7 +590,6 @@ int osd_write_ldd(struct mkfs_opts *mop)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->write_ldd(mop);
-
 	else
 		ret = EINVAL;
 
@@ -604,7 +603,6 @@ int osd_read_ldd(char *dev, struct lustre_disk_data *ldd)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->read_ldd(dev, ldd);
-
 	else
 		ret = EINVAL;
 
@@ -638,7 +636,6 @@ int osd_make_lustre(struct mkfs_opts *mop)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->make_lustre(mop);
-
 	else
 		ret = EINVAL;
 
@@ -654,7 +651,6 @@ int osd_prepare_lustre(struct mkfs_opts *mop,
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->prepare_lustre(mop,
 							wanted_mountopts, len);
-
 	else
 		ret = EINVAL;
 
@@ -682,7 +678,6 @@ int osd_tune_lustre(char *dev, struct mount_opts *mop)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->tune_lustre(dev, mop);
-
 	else
 		ret = EINVAL;
 
@@ -696,7 +691,6 @@ int osd_label_lustre(struct mount_opts *mop)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->label_lustre(mop);
-
 	else
 		ret = EINVAL;
 
@@ -711,7 +705,6 @@ int osd_enable_quota(struct mkfs_opts *mop)
 
 	if (backfs_mount_type_okay(ldd->ldd_mount_type))
 		ret = backfs_ops[ldd->ldd_mount_type]->enable_quota(mop);
-
 	else
 		ret = EINVAL;
 
