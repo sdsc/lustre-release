3be7363cd21abd6458389a6f5b80ea8d9f42e906
Nikitas Angelinas <nikitas.angelinas@seagate.com>
2015-10-30 09:38:41 +0530

    LU-7131 utils: add erase-param change param in tunefs.lustre
    A new option is added to tunefs.lustre, "--erase-param", which allows
    to remove all instances of a specific parameter.
    
    This patch should come with a warning, as it also introduces a change
    in current behaviour: option "--param" is modified to erase
    all previously stored instances of the parameter, and store all new
    instances of the parameter specified in the command line.
    
    Seagate-bug-id: MRP-153
    
    Change-Id: Ibc8d81227045471b8e5c56c7faf1275ad8bbbf86
    Signed-off-by: Nikitas Angelinas <nikitas.angelinas@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/4330
    Tested-by: Jenkins
    Reviewed-by: Artem Blagodarenko <artem.blagodarenko@seagate.com>
    Reviewed-by: Alexey Lyashkov <alexey.lyashkov@seagate.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -5745,6 +5745,51 @@ test_87() { #LU-6544
 }
 run_test 87 "check if MDT inode can hold EAs with N stripes properly"
 
+test_88() { # LU-7131
+	local key=failover.node
+	local val1=192.0.2.254@tcp0 # Reserved IPs, see RFC 5735
+	local val2=192.0.2.255@tcp0
+	local mdsdev=$(mdsdevname 1)
+	local params
+
+	stopall
+
+	do_facet mds "$TUNEFS --erase-params $mdsdev >/dev/null" ||
+		error "tunefs failed"
+
+	# Check that parameters are added correctly
+	do_facet mds "$TUNEFS --param $key=$val1 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val1) = "1" ] ||
+		error "on-disk parameter not added correctly via tunefs"
+
+	# Check that parameters replace existing instances when added
+	do_facet mds "$TUNEFS --param $key=$val2 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "1" ] ||
+		error "on-disk parameter not replaced via tunefs"
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val2) = "1" ] ||
+		error "on-disk parameter not replaced correctly via tunefs"
+
+	# Check that a parameter is erased properly
+	do_facet mds "$TUNEFS --erase-param $key $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "0" ] ||
+		error "on-disk parameter not erased correctly via tunefs"
+
+	reformat
+}
+run_test 88 "check tunefs correctly handles parameter addition and removal"
+
 # $1 test directory
 # $2 (optional) value of max_mod_rpcs_in_flight to set
 check_max_mod_rpcs_in_flight() {
