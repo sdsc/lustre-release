59b78b75029975f768317e8902d8f39b99bf76b7
Jian Yu <jian.yu@intel.com>
2015-11-05 15:10:51 -0800

    LU-0000 tests: reproduce LU-6271
    Revert "LU-4870 lfsck: lock old MDT-object in migrating"
    This reverts commit 19ae9b3605bd41348b2bd64accc50a34eaaa35a1.
    
    This is a for-test-only patch to reproduce LU-6271.
    
    Test-Parameters: fortestonly alwaysuploadlogs \
    envdefinitions=SLOW=yes,ENABLE_QUOTA=yes,REQFAIL=100,PTLDEBUG=-1,SUBSYSTEM=-1,DEBUG_SIZE=300,TIMEOUT=10,SERVER_FAILOVER_PERIOD=600 \
    mdtfilesystemtype=ldiskfs mdsfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs \
    clientdistro=el6.6 ossdistro=el6.6 mdsdistro=el6.6 \
    clientcount=4 osscount=2 mdscount=2 mdtcount=1 \
    austeroptions=-R failover=true iscsi=1 \
    testlist=recovery-mds-scale
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: I9eee70a25e0382b44b9ee55ce501752bf2227de8

--- lustre/mdd/mdd_dir.c
+++ lustre/mdd/mdd_dir.c
@@ -4095,8 +4095,6 @@ static int mdd_migrate(const struct lu_env *env, struct md_object *pobj,
 		if (unlikely(OBD_FAIL_CHECK_RESET(OBD_FAIL_MIGRATE_NET_REP,
 						  OBD_FAIL_MDS_REINT_NET_REP)))
 			GOTO(put, rc = 0);
-	} else {
-		OBD_FAIL_TIMEOUT(OBD_FAIL_MIGRATE_DELAY, cfs_fail_val);
 	}
 
 	/* step 4: update name entry to the new object */
