f504a1f06b234186225ab97a65d6f9b49629207b
Di Wang <di.wang@intel.com>
2015-11-10 07:31:55 -0800

    LU-7419 llog: lock new llog object creation
    Lock the new llog object creation to avoid two
    process create the same object at the same time.
    
    Test-Parameters: fortestonly
    Signed-off-by: Di Wang <di.wang@intel.com>
    Change-Id: Ieab77ed2cc4a3f696a737575b6d430420ecae46c
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>

--- lustre/obdclass/llog_cat.c
+++ lustre/obdclass/llog_cat.c
@@ -465,11 +465,19 @@ int llog_cat_declare_add_rec(const struct lu_env *env,
 
 	if (!llog_exist(cathandle->u.chd.chd_current_log)) {
 		if (dt_object_remote(cathandle->lgh_obj)) {
+
+			loghandle = cathandle->u.chd.chd_current_log;
 			/* If it is remote cat-llog here, let's create the
 			 * remote llog object synchronously, so other threads
 			 * can use it correctly. */
-			rc = llog_cat_new_log(env, cathandle,
-					cathandle->u.chd.chd_current_log, NULL);
+			down_write_nested(&loghandle->lgh_lock, LLOGH_LOG);
+			if (!llog_exist(loghandle))
+				rc = llog_cat_new_log(env, cathandle, loghandle,
+						      NULL);
+			up_write(&loghandle->lgh_lock);
+			if (rc < 0)
+				GOTO(out, rc);
+
 		} else {
 			rc = llog_declare_create(env,
 					cathandle->u.chd.chd_current_log, th);
@@ -492,8 +500,13 @@ int llog_cat_declare_add_rec(const struct lu_env *env,
 				/* If it is remote cat-llog here, let's create
 				 * the remote remote llog object synchronously,
 				 * so other threads can use it correctly. */
-				rc = llog_cat_new_log(env, cathandle, next,
-						      NULL);
+				down_write_nested(&next->lgh_lock, LLOGH_LOG);
+				if (!llog_exist(next))
+					rc = llog_cat_new_log(env, cathandle,
+							      next, NULL);
+				up_write(&next->lgh_lock);
+				if (rc < 0)
+					GOTO(out, rc);
 			} else {
 				rc = llog_declare_create(env, next, th);
 				llog_declare_write_rec(env, cathandle,
