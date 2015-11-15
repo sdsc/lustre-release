f94e740856388cb6ab3c23d9f9a6c27a61c52b39
James Simmons <uja.ornl@yahoo.com>
2015-11-15 14:31:26 -0500

    LU-6245 libcfs: remove typedefs in libcfs source code
    Convert most of the typdefs used in libcfs source code
    to a standard struct. Only a few left which will be
    completely removeed in later pacthes. The typedef
    function pointers will be the only ones remaining.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Change-Id: I5c9efa10d23904c456154d5afbb80c4197e91641

--- libcfs/libcfs/tracefile.h
+++ libcfs/libcfs/tracefile.h
@@ -39,12 +39,12 @@
 
 #include <libcfs/libcfs.h>
 
-typedef enum {
+enum cfs_trace_buf_type {
 	CFS_TCD_TYPE_PROC = 0,
 	CFS_TCD_TYPE_SOFTIRQ,
 	CFS_TCD_TYPE_IRQ,
 	CFS_TCD_TYPE_MAX
-} cfs_trace_buf_type_t;
+};
 
 /* trace file lock routines */
 
@@ -267,12 +267,12 @@ extern int cfs_trace_lock_tcd(struct cfs_trace_cpu_data *tcd, int walking);
 extern void cfs_trace_unlock_tcd(struct cfs_trace_cpu_data *tcd, int walking);
 
 extern char *cfs_trace_console_buffers[NR_CPUS][CFS_TCD_TYPE_MAX];
-extern cfs_trace_buf_type_t cfs_trace_buf_idx_get(void);
+extern enum cfs_trace_buf_type cfs_trace_buf_idx_get(void);
 
 static inline char *cfs_trace_get_console_buffer(void)
 {
 	unsigned int i = get_cpu();
-	unsigned int j = cfs_trace_buf_idx_get();
+	unsigned int j = (unsigned int) cfs_trace_buf_idx_get();
 
 	return cfs_trace_console_buffers[i][j];
 }
