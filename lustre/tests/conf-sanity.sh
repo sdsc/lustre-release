548e632f30e5cbd3c36b1c40e7dec18ddad2290d
HemaHarish <hema.yarramilli@seagate.com>
2015-11-27 10:25:52 +0530

    LU-7442 test: Unexpected concurent MDT mounts in conf-sanity 41c
    Lustre-modules are not available on MDS, so loaded the modules.
    
    Seagate-bug-id: MRP-2711
    Signed-off-by: Hema Yarramilli <hema.yarramilli@seagate.com>
    Change-Id: Icad9410765121bd17f00761477066f9cebf8f686

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -2763,6 +2763,11 @@ test_41c() {
 
 	cleanup
 	# MDT concurrent start
+	load_modules
+	do_facet $SINGLEMDS "lsmod | grep -q lustre" ||
+		error "MDS does not have lustre module loaded"
+	do_facet $SINGLEMDS "lsmod | grep -q libcfs" ||
+		error "libcfs module not loaded"
 	#define OBD_FAIL_TGT_DELAY_CONNECT 0x703
 	do_facet $SINGLEMDS "$LCTL set_param fail_loc=0x703"
 	start mds1 $(mdsdevname 1) $MDS_MOUNT_OPTS &
