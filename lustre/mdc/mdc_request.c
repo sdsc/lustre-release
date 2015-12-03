df0a8671c56773967cef99471744d4bcdf08d305
Alexander Boyko <alexander.boyko@seagate.com>
2015-12-03 09:57:36 +0300

    LU-5282 mdc: fix panic at mdc_free_open()
    Assertion was happened for open request when rq_replay is set
    to 1.
        ASSERTION(mod->mod_open_req->rq_replay == 0)
    But this situation is not fatal for client, and could happened
    when mdc_close() failed.
    The fix allow to free such requests. If mdc_close fail, MDS doesn`t
    receive close request from client. And in a worst case client would
    be evicted.U-5282 test: check error during mdc_close
    
    The test recreates issue when mdc_close failed and
    client asserts:
       ASSERTION( mod->mod_open_req->rq_replay == 0 ) failed
    
    Signed-off-by: Alexander Boyko <alexander.boyko@seagate.com>
    Seagate-bug-id: MRP-3156
    Change-Id: I5f98901f633355849fc107149eadc9cf171819af

--- lustre/mdc/mdc_request.c
+++ lustre/mdc/mdc_request.c
@@ -688,7 +688,14 @@ static void mdc_free_open(struct md_open_data *mod)
 	    imp_connect_disp_stripe(mod->mod_open_req->rq_import))
 		committed = 1;
 
-	LASSERT(mod->mod_open_req->rq_replay == 0);
+	/**
+	 * No reason to asssert here if the open request has
+	 * rq_replay == 1. It means that mdc_close failed, and
+	 * close request wasn`t sent. It is not fatal to client.
+	 * The worst thing is eviction if the client gets open lock
+	 **/
+	if (mod->mod_open_req->rq_replay != 0)
+		CWARN("Close open request with rq_replay == 1\n");
 
 	DEBUG_REQ(D_RPCTRACE, mod->mod_open_req, "free open request\n");
 
@@ -749,6 +756,8 @@ static int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
 	}
 
 	*request = NULL;
+	if (OBD_FAIL_CHECK(OBD_FAIL_MDC_CLOSE))
+		RETURN(-ENOMEM);
 	req = ptlrpc_request_alloc(class_exp2cliimp(exp), req_fmt);
         if (req == NULL)
                 RETURN(-ENOMEM);
