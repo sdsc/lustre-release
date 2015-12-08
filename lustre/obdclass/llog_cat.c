ae3203b29932bf112200754714f28e5eb44d6804
Di Wang <di.wang@intel.com>
2015-12-08 09:51:48 -0800

    LU-0000 dne: dne llog fixes
    Mix fixes from update llog.
    
    Test-Parameters: fortestonly
    Signed-off-by: Di Wang <di.wang@intel.com>
    Change-Id: Id2415ebf51e496559b8c0326287eaa279eab160c

--- lustre/obdclass/llog_cat.c
+++ lustre/obdclass/llog_cat.c
@@ -176,10 +176,31 @@ static int llog_cat_new_log(const struct lu_env *env,
 
 	loghandle->lgh_hdr->llh_cat_idx = rec->lid_hdr.lrh_index;
 out:
-	if (handle != NULL)
-		dt_trans_stop(env, dt, handle);
+	if (handle != NULL) {
+		int rc1;
 
-	RETURN(0);
+		rc1 = dt_trans_stop(env, dt, handle);
+		if (rc1 != 0) {
+			/* cleanup llog for error case */
+			mutex_lock(&cathandle->lgh_hdr_mutex);
+			ext2_clear_bit(rec->lid_hdr.lrh_index,
+				       LLOG_HDR_BITMAP(cathandle->lgh_hdr));
+			cathandle->lgh_hdr->llh_count--;
+			mutex_unlock(&cathandle->lgh_hdr_mutex);
+
+			/* restore llog last_idx */
+			cathandle->lgh_last_idx--;
+			LLOG_HDR_TAIL(cathandle->lgh_hdr)->lrt_index =
+						cathandle->lgh_last_idx;
+			if (rc == 0)
+				rc = rc1;
+		}
+	}
+
+	if (rc > 0)
+		rc = 0;
+
+	RETURN(rc);
 
 out_destroy:
 	/* to signal llog_cat_close() it shouldn't try to destroy the llog,
@@ -382,6 +403,29 @@ next:
 	RETURN(loghandle);
 }
 
+int llog_cat_update_header(const struct lu_env *env,
+			   struct llog_handle *cathandle)
+{
+	struct llog_handle	*loghandle;
+	int rc;
+	ENTRY;
+
+	list_for_each_entry(loghandle, &cathandle->u.chd.chd_head,
+			    u.phd.phd_entry) {
+		if (!llog_exist(loghandle))
+			continue;
+
+		rc = llog_read_header(env, loghandle, NULL);
+		if (rc != 0)
+			GOTO(out, rc);
+	}
+
+	rc = llog_read_header(env, cathandle, NULL);
+out:
+	RETURN(rc);
+}
+EXPORT_SYMBOL(llog_cat_update_header);
+
 /* Add a single record to the recovery log(s) using a catalog
  * Returns as llog_write_record
  *
@@ -486,11 +530,26 @@ int llog_cat_declare_add_rec(const struct lu_env *env,
 
 	if (!llog_exist(cathandle->u.chd.chd_current_log)) {
 		if (dt_object_remote(cathandle->lgh_obj)) {
-			/* If it is remote cat-llog here, let's create the
-			 * remote llog object synchronously, so other threads
-			 * can use it correctly. */
-			rc = llog_cat_new_log(env, cathandle,
-					cathandle->u.chd.chd_current_log, NULL);
+			/* For remote operation, if we put the llog object
+			 * creation in the current transaction, then the
+			 * llog object will not be created on the remote
+			 * target until the transaction stop, if other
+			 * operations start before the transaction stop,
+			 * and use the same llog object, will be dependent
+			 * on the success of this transaction. So let's
+			 * create the llog object synchronously here to
+			 * remove the dependency. */
+			down_read_nested(&cathandle->lgh_lock, LLOGH_CAT);
+			loghandle = cathandle->u.chd.chd_current_log;
+			down_write_nested(&loghandle->lgh_lock, LLOGH_LOG);
+			if (!llog_exist(loghandle))
+				rc = llog_cat_new_log(env, cathandle, loghandle,
+						      NULL);
+			up_write(&loghandle->lgh_lock);
+			up_read(&cathandle->lgh_lock);
+			if (rc < 0)
+				GOTO(out, rc);
+
 		} else {
 			rc = llog_declare_create(env,
 					cathandle->u.chd.chd_current_log, th);
@@ -510,11 +569,27 @@ int llog_cat_declare_add_rec(const struct lu_env *env,
 	if (next) {
 		if (!llog_exist(next)) {
 			if (dt_object_remote(cathandle->lgh_obj)) {
-				/* If it is remote cat-llog here, let's create
-				 * the remote remote llog object synchronously,
-				 * so other threads can use it correctly. */
-				rc = llog_cat_new_log(env, cathandle, next,
-						      NULL);
+				/* For remote operation, if we put the llog
+				 * object creation in the current transaction,
+				 * then the llog object will not be created on
+				 * the remote target until the transaction stop,
+				 * if other operations start before the
+				 * transaction stop, and use the same llog
+				 * object, will be dependent on the success of
+				 * this transaction. So let's create the llog
+				 * object synchronously here to remove the
+				 * dependency. */
+				down_read_nested(&cathandle->lgh_lock,
+						 LLOGH_CAT);
+				next = cathandle->u.chd.chd_next_log;
+				down_write_nested(&next->lgh_lock, LLOGH_LOG);
+				if (!llog_exist(next))
+					rc = llog_cat_new_log(env, cathandle,
+							      next, NULL);
+				up_write(&next->lgh_lock);
+				up_read(&cathandle->lgh_lock);
+				if (rc < 0)
+					GOTO(out, rc);
 			} else {
 				rc = llog_declare_create(env, next, th);
 				llog_declare_write_rec(env, cathandle,
