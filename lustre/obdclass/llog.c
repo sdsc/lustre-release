b66f9d956c27b7f3b1ee7289ef2e230de93b71e4
Jinshan Xiong <jinshan.xiong@intel.com>
2015-12-14 09:05:16 -0800

    LU-6846 llog: create remote llog synchronously
    Create remote llog synchronously, because llog_create
    for remote object only pack the RPC in the buffer,
    instead the real llog object will be created until
    transaction stop. If other thread happens to use
    this llog object and send RPC before the creation,
    which might cause the failure.
    
    Lustre-patch: http://review.whamcloud.com/16333
    
    Test-Parameters: fortestonly
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: I905554330744b3692e74874696a9a4c7f85149b3

--- lustre/obdclass/llog.c
+++ lustre/obdclass/llog.c
@@ -111,6 +111,8 @@ static int llog_cancel_rec_internal(const struct lu_env *env,
 	struct llog_log_hdr	*llh = loghandle->lgh_hdr;
 	struct thandle		*th;
 	int			 rc;
+	int rc1;
+	bool subtract_count = false;
 
 	ENTRY;
 
@@ -143,15 +145,24 @@ static int llog_cancel_rec_internal(const struct lu_env *env,
 	/* update header */
 	rc = llog_write_rec(env, loghandle, &llh->llh_hdr, NULL,
 			    LLOG_HEADER_IDX, th);
-	if (rc == 0)
-		loghandle->lgh_hdr->llh_count--;
-	else
-		ext2_set_bit(index, LLOG_HDR_BITMAP(llh));
+	if (rc != 0)
+		GOTO(out_unlock, rc);
+
+	loghandle->lgh_hdr->llh_count--;
+	subtract_count = true;
 out_unlock:
 	mutex_unlock(&loghandle->lgh_hdr_mutex);
 	up_write(&loghandle->lgh_lock);
 out_trans:
-	dt_trans_stop(env, dt, th);
+	rc1 = dt_trans_stop(env, dt, th);
+	if (rc == 0)
+		rc = rc1;
+	if (rc < 0 && subtract_count) {
+		mutex_lock(&loghandle->lgh_hdr_mutex);
+		loghandle->lgh_hdr->llh_count++;
+		ext2_set_bit(index, LLOG_HDR_BITMAP(llh));
+		mutex_unlock(&loghandle->lgh_hdr_mutex);
+	}
 	RETURN(rc);
 }
 
