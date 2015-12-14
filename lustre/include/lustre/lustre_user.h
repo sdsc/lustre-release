4f497d5e251b1b41efa4f2d5de608797def01327
Jeremy Filizetti <jeremy.filizetti@gmail.com>
2015-12-13 22:17:38 -0500

    LU-3289 gss: Add Shared key and GSS Null functionality
    Update the GSS null and SK mechanisms framework with the actual
    implementation for wrapping and signing of RPCs.
    
    Signed-off-by: Jeremy Filizetti <jeremy.filizetti@gmail.com>
    Change-Id: I050944ccc2413d398442351d8615c08ef48e3500

--- lustre/include/lustre/lustre_user.h
+++ lustre/include/lustre/lustre_user.h
@@ -1391,6 +1391,27 @@ struct llapi_json_item_list {
 	struct llapi_json_item	*ljil_items;
 };
 
-/** @} lustreuser */
+/* Shared key */
+enum sk_crypt_alg {
+	SK_CRYPT_AES_CTR        = 0,
+	SK_CRYPT_MAX            = 1,
+};
 
+enum sk_hmac_alg {
+	SK_HMAC_SHA256  = 0,
+	SK_HMAC_SHA512  = 1,
+	SK_HMAC_MAX     = 2,
+};
+
+struct sk_crypt_type {
+	char    *sct_name;
+	size_t   sct_bytes;
+};
+
+struct sk_hmac_type {
+	char    *sht_name;
+	size_t   sht_bytes;
+};
+
+/** @} lustreuser */
 #endif /* _LUSTRE_USER_H */
