ed6c390b1bc6301b08dc30df0dbda524f648b48f
Andreas Dilger <andreas.dilger@intel.com>
2015-12-15 02:44:45 -0700

    LU-7555 tests: add load_module to mount_facet
    Since commit 2a223541d299bc8 there has not been automated loading
    of lustre modules in the log() command, since this command only
    needs libcfs loaded to use the "lctl mark" functionality.
    
    When testing from a local build tree, conf-sanity.sh was failing
    because it unloads the modules during cleanup and is not able to
    automatically load the modules on demand if they are not installed,
    and does not always manually load the modules for each running test.
    
    Add a call to load_modules() in mount_facet() where the modules are
    actually needed for the filesystem to mount.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: Ie7655c04a7bf42ec9969cb5c11fab3ac92f3dc0b

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -2713,7 +2713,6 @@ test_41a() { #bug 14134
 	stop ost1 -f || error "unable to stop OST1"
 	stop_mds || error "Unable to stop MDS"
 	stop_mds || error "Unable to stop MDS on second try"
-	unload_modules_conf || error "unload_modules_conf failed"
 }
 run_test 41a "mount mds with --nosvc and --nomgs"
 
@@ -2865,7 +2864,6 @@ test_42() { #bug 14693
 
 	do_facet mgs $LCTL conf_param $FSNAME.sys.some_wrong_param=20
 	cleanup || error "stopping $FSNAME failed with invalid sys param"
-	load_modules
 	setup
 	check_mount || error "client was not mounted with invalid sys param"
 	cleanup || error "stopping $FSNAME failed with invalid sys param"
@@ -3510,7 +3508,6 @@ test_50i() {
 	[ "$MDSCOUNT" -lt "2" ] && skip_env "$MDSCOUNT < 2, skipping" && return
 
 	[ $(facet_fstype ost1) == zfs ] && import_zpool ost1
-	load_modules
 	do_facet mds2 "$TUNEFS --param mdc.active=0 $(mdsdevname 2)" ||
 		error "tunefs MDT2 failed"
 	start_mds  || error "Unable to start MDT"
@@ -3809,7 +3806,6 @@ thread_sanity() {
 	local newvalue="${opts}=$(expr $basethr \* $ncpts)"
 	setmodopts -a $modname "$newvalue" oldvalue
 
-	load_modules
 	setup
 	check_mount || return 41
 
@@ -3825,7 +3821,6 @@ thread_sanity() {
 		return $?
 	cleanup
 
-	load_modules
 	setup
 }
 
@@ -4806,7 +4801,6 @@ test_72() { #LU-2634
 run_test 72 "test fast symlink with extents flag enabled"
 
 test_73() { #LU-3006
-	load_modules
 	[ $(facet_fstype ost1) == zfs ] && import_zpool ost1
 	do_facet ost1 "$TUNEFS --failnode=1.2.3.4@$NETTYPE $(ostdevname 1)" ||
 		error "1st tunefs failed"
@@ -5556,7 +5550,6 @@ test_84() {
 	local correct_clients
 	local wrap_up=5
 
-	load_modules
 	echo "start mds service on $(facet_active_host $facet)"
 	start_mds \
 	"-o recovery_time_hard=$time_min,recovery_time_soft=$time_min" $@ ||
@@ -6043,8 +6036,6 @@ test_91() {
 	local uuid
 	local nid
 	local found
-
-	load_modules
 
 	[[ $(lustre_version_code ost1) -ge $(version_code 2.7.63) ]] ||
 		{ skip "Need OST version at least 2.7.63" && return 0; }
