8431fec8ba5434b76aa994d93d5fa44b850be689
Andriy Skulysh <andriy.skulysh@seagate.com>
2015-12-16 16:05:31 +0200

    LU-7382 llite: vvp_io_update_iov() ASSERTION failure
    lti_local_iov is used to store iovec in case of 1 segment.
    
    It is needed to hold reference on lu_env during
    call of ll_file_write_iter().
    
    Change-Id: Iaff4c81c6ced9ac0e1557dd0eb1fab5205b48e28
    Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -1342,6 +1342,8 @@ static ssize_t ll_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct iov_iter *from;
 	size_t iov_count;
 	ssize_t result;
+	struct lu_env *env = NULL;
+	int refcheck;
 	ENTRY;
 
 	result = ll_file_get_iov_count(iov, &nr_segs, &iov_count);
@@ -1349,17 +1351,12 @@ static ssize_t ll_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 		RETURN(result);
 
 	if (nr_segs == 1) {
-		struct lu_env *env;
-		int refcheck;
-
 		env = cl_env_get(&refcheck);
 		if (IS_ERR(env))
 			RETURN(PTR_ERR(env));
 
 		local_iov = &ll_env_info(env)->lti_local_iov;
 		*local_iov = *iov;
-
-		cl_env_put(env, &refcheck);
 	} else {
 		OBD_ALLOC(local_iov, sizeof(*iov) * nr_segs);
 		if (local_iov == NULL)
@@ -1383,7 +1380,9 @@ static ssize_t ll_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 
 	OBD_FREE_PTR(from);
 out:
-	if (nr_segs > 1)
+	if (nr_segs == 1)
+		cl_env_put(env, &refcheck);
+	else
 		OBD_FREE(local_iov, sizeof(*iov) * nr_segs);
 
 	RETURN(result);
