956835fcc71530e187295603f36cedd9d00b2e24
Di Wang <di.wang@intel.com>
2015-12-21 00:51:05 -0800

    LU-7579 osd: move ORPHAN/DEAD flag to OSD
    If a directory is unlinked while it is opened,
    then it will set flags LUSTRE_DEAD_FL and
    LUSTRE_ORPHAN_FL to osd object, so the remote
    target can aware this status by OSP attr_get().
    
    And also LUSTRE_ORPHAN_FL will be stored inside
    LMA (converted to LMAI_ORPHAN). In the mean time,
    it will remove LUSTRE_SLAVE_DEAD_FL flag for dead
    stripes of striped directory, i.e. all of
    dead directories (stripes) will use LMAI_ORPHAN
    in LMA to indicate the status.
    
    Also in osp_xattr, it should retrieve the return
    value from the reply, which is the real length of
    EA, instead of using reply buffer length.
    
    Signed-off-by: Di Wang <di.wang@intel.com>
    Change-Id: I32f864cdcc72b460b4c2169d8e34211522b9c266

--- lustre/osp/osp_object.c
+++ lustre/osp/osp_object.c
@@ -1033,7 +1033,6 @@ unlock:
 		GOTO(out, rc = -ERANGE);
 
 	memcpy(buf->lb_buf, rbuf->lb_buf, rbuf->lb_len);
-	rc = rbuf->lb_len;
 	if (obj->opo_ooa == NULL || osp->opd_connect_mdt)
 		GOTO(out, rc);
 
