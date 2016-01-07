d87b0f991d04e5f257abc6d6301a0265d929392b
James Simmons <uja.ornl@yahoo.com>
2016-01-07 11:52:50 -0500

    LU-5030 snmp: migrate lustre SNMP utilites to use cfs_get_paths
    Move the SNMP lustre tools to use cfs_get_paths so
    the tools function with the upstream client as well.
    
    Change-Id: I18d76e10c45a9c8c582e77917a33bb2afa86aac4
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Signed-off-by: Wang Chao <chao.ornl@gmail.com>

--- snmp/lustre-snmp-trap.c
+++ snmp/lustre-snmp-trap.c
@@ -232,10 +232,15 @@ void health_poll_worker(unsigned int registration_number, void *clientarg)
     FILE    *fptr = NULL;
     char string[MAX_LINE_SIZE];
     int b_seen_portals_catastrophe = 0;
-    const char *filename =  g_health_check_test_file == 0 ? 
-            LUSTRE_PATH FILENAME_SYSHEALTHCHECK : 
-            g_health_check_test_file;
-    
+    char *filename;
+    glob_t path;
+
+    if (cfs_get_param_paths(&path, "health_check") != 0)
+        return;
+
+    filename = g_health_check_test_file == 0 ? path.gl_pathv[0] : g_health_check_test_file;
+    cfs_free_param_data(&path);
+
     /*DEBUGMSGTL(("lsnmpd","health_entry_parser(%s)\n",filename));*/
 
     /* Open the file.  Use the test file env variable if
