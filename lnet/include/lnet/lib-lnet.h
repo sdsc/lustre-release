f7c2ba45e035e9dfe5890710e670feaf05a7773b
Amir Shehata <amir.shehata@intel.com>
2016-01-12 17:09:31 -0800

    LU-7734 lnet: configure peers from DLC
    This patch adds the ability to configure peers from the DLC
    interface.
    
    When a peer is added a primary NID must be provided. If none is
    provided then the first NID in the list of NIDs shall be used
    as the primary NID.
    
    Basic error checking is done at the DLC level, to ensure properly
    formatted NIDs, however, if a NID is a duplicate, this will be
    detected when adding it in the kernel. Operation is halted, which
    means some peer NIDs might have already been added, but not the
    entire set. It's the role of the caller to backtrack and remove that
    peer that failed to add.
    
    When deleting a peer the primary NID must be provided. If the primary
    NID is not provided the operation fails and no changes occur in the
    system. If only the primary NID is provided the entire peer is
    removed.  If a set of NIDs are provided each one will be removed
    from the peer identified by the peer NID in turn.
    
    The existing show peer credits API can be used to show peer
    information.
    
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Change-Id: Iaf588a062b44d74305aa9aa7d31c7341c6c384b9

--- lnet/include/lnet/lib-lnet.h
+++ lnet/include/lnet/lib-lnet.h
@@ -786,12 +786,15 @@ struct lnet_peer_net *lnet_peer_get_net_locked(struct lnet_peer *peer,
 					       __u32 net_id);
 bool lnet_peer_is_ni_pref_locked(struct lnet_peer_ni *lpni,
 				 struct lnet_ni *ni);
-int lnet_get_peer_info(__u32 peer_index, __u64 *nid,
-		       char alivness[LNET_MAX_STR_LEN],
-		       __u32 *cpt_iter, __u32 *refcount,
-		       __u32 *ni_peer_tx_credits, __u32 *peer_tx_credits,
-		       __u32 *peer_rtr_credits, __u32 *peer_min_rtr_credtis,
-		       __u32 *peer_tx_qnob);
+int lnet_del_peer_ni_from_peer(lnet_nid_t key_nid, lnet_nid_t nid);
+int lnet_get_peer_info(__u32 idx, lnet_nid_t *primary_nid, lnet_nid_t *nid,
+		       struct lnet_peer_ni_credit_info *peer_ni_info);
+int lnet_get_peer_ni_info(__u32 peer_index, __u64 *nid,
+			  char alivness[LNET_MAX_STR_LEN],
+			  __u32 *cpt_iter, __u32 *refcount,
+			  __u32 *ni_peer_tx_credits, __u32 *peer_tx_credits,
+			  __u32 *peer_rtr_credits, __u32 *peer_min_rtr_credtis,
+			  __u32 *peer_tx_qnob);
 
 static inline bool
 lnet_is_peer_ni_healthy_locked(struct lnet_peer_ni *lpni)
