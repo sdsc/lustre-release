c76b5c22a339ca70f3d00576e5999b9699312cfc
Sebastien Buisson <sbuisson@ddn.com>
2016-01-14 17:57:10 +0100

    LU-7845 gss: support namespace in lgss_keyring
    When we want Kerberos authentication for a Lustre client run from
    a Docker container, it is necessary that request_key forwards
    to lgss_keyring userland helper sufficient information regarding
    the namespace from which mount command was launched. That way,
    helper function can bind to caller's namespace, and retrieve
    Kerberos credentials of the container.
    
    To achieve this:
    - add a lpi_reftask field to struct llog_process_info: this is the
      task_struct of the process initiating the mount;
    - use lpi_reftask to put llog_process_thread_daemonize thread in
      same namespace as calling llog_process_or_fork();
    - add a reference PID to struct obd_import: this is the PID of
      the init process of the namespace from which the import
      creation was originated;
    - use reference PID from struct obd_import as a new 'pid' parameter
      to call-out information sent from kernel space to userspace
      via request-key;
    - in lgss_keyring, if necessary perform credentials retrieval
      in caller's namespace, thanks to a call to 'setns'.
    
    For user's credentials, use current PID instead of import's
    reference PID to get the reference namespace.
    
    Change-Id: I934ed857fdf323e55a2a5acf06e8799c3a6421f0
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>

--- lustre/ptlrpc/gss/gss_keyring.c
+++ lustre/ptlrpc/gss/gss_keyring.c
@@ -820,19 +820,25 @@ struct ptlrpc_cli_ctx * gss_sec_lookup_ctx_kr(struct ptlrpc_sec *sec,
 	construct_key_desc(desc, sizeof(desc), sec, vcred->vc_uid);
 
 	/* callout info format:
-	 * secid:mech:uid:gid:sec_flags:svc_flag:svc_type:peer_nid:target_uuid
+	 * secid:mech:uid:gid:sec_flags:svc_flag:svc_type:peer_nid:target_uuid:
+	 * self_nid:pid
 	 */
-        coinfo_size = sizeof(struct obd_uuid) + MAX_OBD_NAME + 64;
-        OBD_ALLOC(coinfo, coinfo_size);
-        if (coinfo == NULL)
-                goto out;
+	coinfo_size = sizeof(struct obd_uuid) + MAX_OBD_NAME + 64;
+	OBD_ALLOC(coinfo, coinfo_size);
+	if (coinfo == NULL)
+		goto out;
 
-	snprintf(coinfo, coinfo_size, "%d:%s:%u:%u:%s:%c:%d:"LPX64":%s:"LPX64,
+	/* Last callout parameter is pid of process whose namespace will be used
+	 * for credentials' retrieval.
+	 * For user's credentials (in which case co_flags is empty), use current
+	 * PID instead of import's reference PID to get reference namespace. */
+	snprintf(coinfo, coinfo_size, "%d:%s:%u:%u:%s:%c:%d:"LPX64":%s:"LPX64":%d",
 		 sec->ps_id, sec2gsec(sec)->gs_mech->gm_name,
 		 vcred->vc_uid, vcred->vc_gid,
 		 sec_part_flags, svc_flag, import_to_gss_svc(imp),
 		 imp->imp_connection->c_peer.nid, imp->imp_obd->obd_name,
-		 imp->imp_connection->c_self);
+		 imp->imp_connection->c_self,
+		 co_flags[0] == '\0' ? current_pid() : imp->imp_sec_refpid);
 
         CDEBUG(D_SEC, "requesting key for %s\n", desc);
 
