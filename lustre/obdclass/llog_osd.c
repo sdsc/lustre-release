1098122a0f8e332e87d234d94038cd22462a253e
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2016-01-18 09:24:19 +0300

    LU-6838 llog: limit file size of plain logs
    on small filesystems plain log can grow dramatically. especially
    given large record sizes produced by DNE and extended chunksize.
    I saw >50% of space consumed by a single llog file which was still
    in use. this leads to test failures (sanityn, etc).
    the patch introduces additional limit on plain llog size, which
    is calculated as <free space>/64 (128MB at most) at llog creation
    time.
    
    Change-Id: I0eab8177d4e416a32a6aab56d47e4142c81d13de
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

--- lustre/obdclass/llog_osd.c
+++ lustre/obdclass/llog_osd.c
@@ -552,6 +552,18 @@ static int llog_osd_write_rec(const struct lu_env *env,
 
 	LASSERT(lgi->lgi_attr.la_valid & LA_SIZE);
 	lgi->lgi_off = lgi->lgi_attr.la_size;
+
+	if (loghandle->lgh_max_size > 0 &&
+	    lgi->lgi_off >= loghandle->lgh_max_size) {
+		CDEBUG(D_OTHER, "llog is getting too large (%u > %u) at %u "
+		       DOSTID"\n", (unsigned)lgi->lgi_off,
+		       loghandle->lgh_max_size,
+		       (int)loghandle->lgh_last_idx,
+		       POSTID(&loghandle->lgh_id.lgl_oi));
+		loghandle->lgh_last_idx = LLOG_HDR_BITMAP_SIZE(llh) - 1;
+		RETURN(-ENOSPC);
+	}
+
 	left = chunk_size - (lgi->lgi_off & (chunk_size - 1));
 	/* NOTE: padding is a record, but no bit is set */
 	if (left != 0 && left != reclen &&
