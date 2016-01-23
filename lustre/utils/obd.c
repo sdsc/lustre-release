ea75f6ed8f11290567643389539ea582e91f8e65
Christopher J. Morrone <morrone2@llnl.gov>
2016-01-23 13:16:40 -0800

    LU-7699 build: Eliminate lustre_build_version.h
    The lustre_build_version.h is no longer necessary.  With the new
    versioning system the lustre version can now be retrieved from
    standard autotools defines and/or lustre_ver.h.
    
    Change-Id: I08233d8547bc4af7bf2fb0c15bee8e306f6fa0f0
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>

--- lustre/utils/obd.c
+++ lustre/utils/obd.c
@@ -73,7 +73,6 @@
 #include <lustre/lustre_idl.h>
 #include <lustre_cfg.h>
 #include <lustre_ioctl.h>
-#include <lustre/lustre_build_version.h>
 
 #include <lnet/lnetctl.h>
 #include <lustre/lustreapi.h>
