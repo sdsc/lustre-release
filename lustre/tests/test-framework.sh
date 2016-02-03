05ceab6d41227b1fc5657f2e2d5ed2eb52fbbc5c
Hongchao Zhang <hongchao.zhang@intel.com>
2016-02-03 22:34:39 +0800

    LU-7428 test: remove test 84 from ALWAYS_EXCEPT
    Debug patch to remove the test 84 from ALWAYS_EXCEPT in conf-sanity
    temporarily to verify the problem is really fixed.
    
    
    Test-Parameters: trivial testlist=conf-sanity,conf-sanity,conf-sanity,conf-sanity
    Test-Parameters: testlist=conf-sanity,conf-sanity,conf-sanity,conf-sanity
    Change-Id: I1d2dd16f33f0713a5e11d243708865b45ec283cb
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -1333,7 +1333,9 @@ mount_facet() {
 
 	# commit the device label change to disk
 	if [[ $devicelabel =~ (:[a-zA-Z]{3}[0-9]{4}) ]]; then
+		echo "Commit the device label on ${!dev}"
 		do_facet $facet "sync; sync; sync"
+		sleep 5
 	fi
 
 
