241e0466d333a798c08aeba097318b49f71e084c
Andreas Dilger <andreas.dilger@intel.com>
2016-03-12 02:38:25 -0700

    LU-7813 lod: inherit pool from parent if unspecified
    If the OST pool is not set in a specific file layout, but the pool
    is set on the parent directory, inherit the pool from the parent.
    
    This makes OST pools much more usable, since the expectation is that
    files created in a directory with a pool set will be part of the pool,
    even if there was a need to set explicit striping for the file.
    
    If the specified stripe size is not a multiple of LOV_MIN_STRIPE_SIZE,
    instead of truncating it to LOV_MIN_STRIPE_SIZE, round it to the
    nearest multiple instead.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I4747ec3aa829fa9beec50e4c85c5238b2f4f5ff7

--- lustre/tests/ost-pools.sh
+++ lustre/tests/ost-pools.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# -*- mode: Bash; tab-width: 4; indent-tabs-mode: t; -*-
-# vim:shiftwidth=4:softtabstop=4:tabstop=4:
 #
 # Run select tests by setting ONLY, or as arguments to the script.
 # Skip specific tests by setting EXCEPT.
@@ -1176,30 +1174,35 @@ test_20() {
           printf "$FSNAME-OST%04x_UUID " $i; done)
     add_pool $POOL2 "$FSNAME-OST[$start-$TGT_MAX/2]" "$TGT"
 
-    create_dir $dir1 $POOL
-    create_file $dir1/file1 $POOL2
-    create_dir $dir2 $POOL2
-    touch $dir2/file2
-    mkdir $dir3
-    $SETSTRIPE -c 1 $dir3 # No pool assignment
-    touch $dir3/file3
-    $SETSTRIPE -c 1 $dir2/file4 # No pool assignment
+	start_full_debug_logging
+	create_dir $dir1 $POOL
+	create_file $dir1/file1 $POOL2
+	create_dir $dir2 $POOL2
+	touch $dir2/file2		# should inherit $POOL2 from $dir2
+	lctl mark "===== start mkdir ====="
+	mkdir $dir3			# should inherit $POOL from parent $dir1
+	lctl mark "===== finish mkdir ====="
+	$LFS setstripe -c 1 $dir3	# should inherit $POOL from parent $dir1
+	lctl mark "===== start create ====="
+	touch $dir3/file3		# should inherit $POOL from $dir3
+	lctl mark "===== finish create ====="
+	stop_full_debug_logging
+	$LFS setstripe -c 1 $dir2/file4	# should inherit $POOL2 from $dir2
+	$LFS setstripe -S 1M $dir2/file5 # should inherit $POOL2 from $dir2
 
-    check_file_in_pool $dir1/file1 $POOL2
-    check_file_in_pool $dir2/file2 $POOL2
+	check_file_in_pool $dir2/file2 $POOL2
 
-    check_dir_not_in_pool $dir3 $POOL
-    check_dir_not_in_pool $dir3 $POOL2
+	if [ $(lustre_version_code mds1) -ge $(version_code 2.8.50) ]; then
+		check_dir_in_pool $dir3 $POOL
 
-    check_file_not_in_pool $dir3/file3 $POOL
-    check_file_not_in_pool $dir3/file3 $POOL2
+		check_file_in_pool $dir3/file3 $POOL
+		check_file_in_pool $dir2/file4 $POOL
+		check_file_in_pool $dir2/file5 $POOL
+	fi
 
-    check_file_not_in_pool $dir2/file4 $POOL
-    check_file_not_in_pool $dir2/file4 $POOL2
+	rm -rf $dir1
 
-    rm -rf $dir1
-
-    return 0
+	return 0
 }
 run_test 20 "Different pools in a directory hierarchy."
 
