ed5d89fcd3f6528d49ce2feb06ff40ebcc4b3e26
Nathaniel Clark <nathaniel.l.clark@intel.com>
2016-03-22 12:52:53 -0400

    LU-7890 lov: Ensure correct operation for large object sizes
    If a backing filesystem (ZFS) returns that it supports very large
    (LLONG_MAX) object sizes, that should be correctly supported.  This
    fixes the check for unitialized stripe_maxbytes in
    lsm_unpackmd_common(), so that ZFS can return LLONG_MAX and it will be
    okay.  This issue is excersized by writing to or past the 2TB boundry
    of a singly stripped file.  A regression test is added to check for
    this.
    
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: I669096281bd826d69b5e86ada50077740412d268

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -13481,6 +13481,15 @@ test_246() { # LU-7371
 }
 run_test 246 "Read file of size 4095 should return right length"
 
+test_249() { # LU-7890
+	rm -f $DIR/$tfile
+	$SETSTRIPE -c 1 $DIR/$tfile
+
+	dd if=/dev/zero of=$DIR/$tfile bs=4k count=1 seek=2T oflag=seek_bytes ||
+		error "dd to 2T offset failed"
+}
+run_test 249 "Write above 2T file size"
+
 test_250() {
 	[ "$(facet_fstype ost$(($($GETSTRIPE -i $DIR/$tfile) + 1)))" = "zfs" ] \
 	 && skip "no 16TB file size limit on ZFS" && return
