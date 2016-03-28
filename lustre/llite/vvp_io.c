cc09f595a64d79b17a7d5ac8f110ec94f0843c17
Andriy Skulysh <andriy.skulysh@seagate.com>
2016-03-28 12:15:54 +0300

    LU-7927 llite: Deadlock between ll_setattr and write/ll_fsync
    commit 85bd36cc69563d7a79e3ed34f8fadb4ed1a72b7c
    Author: Henri Doreau <henri.doreau@cea.fr>
    Date: Fri Apr 18 16:17:01 2014 +0200
    
    LU-4840 lfs: Use file lease to implement migration
    
    moves lli_trunc_sem into vvp layer.
    It violates lli_trunc_sem/i_mutex locking order.
    
    So i_mutex should be taken after lli_trunc_sem now.
    
    Change-Id: I2ecd52b7ae6eca74c6db7d94b1de1333560bc45d
    Seagate-bug-id: MRP-3372
    Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>

--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -661,10 +661,12 @@ static int vvp_io_setattr_start(const struct lu_env *env,
 	struct inode		*inode = vvp_object_inode(io->ci_obj);
 	struct ll_inode_info	*lli   = ll_i2info(inode);
 
-	mutex_lock(&inode->i_mutex);
 	if (cl_io_is_trunc(io)) {
 		down_write(&lli->lli_trunc_sem);
+		mutex_lock(&inode->i_mutex);
 		inode_dio_wait(inode);
+	} else {
+		mutex_lock(&inode->i_mutex);
 	}
 
 	if (io->u.ci_setattr.sa_valid & TIMES_SET_FLAGS)
@@ -685,9 +687,11 @@ static void vvp_io_setattr_end(const struct lu_env *env,
 		 * because osc has already notified to destroy osc_extents. */
 		vvp_do_vmtruncate(inode, io->u.ci_setattr.sa_attr.lvb_size);
 		inode_dio_write_done(inode);
+		mutex_unlock(&inode->i_mutex);
 		up_write(&lli->lli_trunc_sem);
+	} else {
+		mutex_unlock(&inode->i_mutex);
 	}
-	mutex_unlock(&inode->i_mutex);
 }
 
 static void vvp_io_setattr_fini(const struct lu_env *env,
