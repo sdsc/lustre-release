2f762283dc68804c8ff6439ee196bf3a8dd306f4
Andreas Dilger <andreas.dilger@intel.com>
2016-03-29 11:29:57 -0600

    LU-7759 llite: handle inactive OSTs better in statfs
    Change the order of checks for inactive OSCs in lov_prep_statfs_set()
    so that administratively disabled OSTs do not generate any output in
    "lfs df" at all, to avoid needlessly cluttering the output.
    
    Enable the lazystatfs mount option by default, so that "df" does not
    hang when an OST is temporarily offline.
    
    Fix test-framework.sh to use "stat $MOUNT" instead of "df $MOUNT" to
    determine when recovery is complete, now that "df" does not block.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I993761a7eb120a36a1b80c2822cb6d8011ccab07

--- lustre/ofd/ofd_dev.c
+++ lustre/ofd/ofd_dev.c
@@ -1925,9 +1925,6 @@ static int ofd_statfs_hdl(struct tgt_session_info *tsi)
 		CERROR("%s: statfs failed: rc = %d\n",
 		       tgt_name(tsi->tsi_tgt), rc);
 
-	if (OBD_FAIL_CHECK(OBD_FAIL_OST_STATFS_EINPROGRESS))
-		rc = -EINPROGRESS;
-
 	ofd_counter_incr(tsi->tsi_exp, LPROC_OFD_STATS_STATFS,
 			 tsi->tsi_jobid, 1);
 
@@ -1955,6 +1952,9 @@ static int ofd_sync_hdl(struct tgt_session_info *tsi)
 	int			 rc = 0;
 
 	ENTRY;
+
+	if (OBD_FAIL_CHECK(OBD_FAIL_OST_SYNC_EINPROGRESS))
+		rc = -EINPROGRESS;
 
 	repbody = req_capsule_server_get(tsi->tsi_pill, &RMF_OST_BODY);
 
