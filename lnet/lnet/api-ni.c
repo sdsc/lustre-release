3f6a11f128939895b079ab5462c960e56cf8885e
Amir Shehata <amir.shehata@intel.com>
2016-03-31 16:53:02 -0700

    LU-7734 lnet: handle non-MR peers
    Add the ability to declare a peer to be non-MR from the DLC
    interface. By default if a peer is configured from DLC it is
    assumed to be MR capable, except when the non-mr flag is set.
    
    For non-MR peers always use the same NI to communicate with it.
    If multiple NIs are used to communicate with a non-MR peer the
    peer will consider that it's talking to different peers which could
    cause upper layers to be confused.
    
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Change-Id: Ie3ec45f5f44fa7d72e3e0335b1383f9c3cc92627

--- lnet/lnet/api-ni.c
+++ lnet/lnet/api-ni.c
@@ -2664,7 +2664,8 @@ LNetCtl(unsigned int cmd, void *arg)
 
 		lnet_incr_dlc_seq();
 		return lnet_add_peer_ni_to_peer(cfg->prcfg_key_nid,
-						cfg->prcfg_cfg_nid);
+						cfg->prcfg_cfg_nid,
+						cfg->prcfg_mr);
 	}
 
 	case IOC_LIBCFS_DEL_PEER_NI: {
