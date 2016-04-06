1b09e049bc6abcb87c05639aa9f1a0a2da8095a2
Frank Zago <fzago@cray.com>
2016-04-06 16:03:14 -0500

    LU-7988 hsm: execute HSM coordinator once per second at most
    When there is heavy HSM usage, each new HSM request can trigger the
    HSM coordinator, which may run many times per seconds. When it is
    running it locks the HSM catalog (cdt_llog_lock) preventing any other
    HSM operation to happen, such as insertion, removal or dumping of the
    requests.
    
    Limit the coordinator to run once per second, and only if there is
    work to do. It will still execute the loop once every 10 seconds (or
    as defined by the loop_period parameter) to do housekeeping.
    
    Test-Parameters: testlist=sanity-hsm
    Signed-off-by: frank zago <fzago@cray.com>
    Change-Id: Ide3f061f8943a3088ea713993521897fb74e5d99

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -428,6 +428,8 @@ static int mdt_coordinator(void *data)
 	struct mdt_device	*mdt = mti->mti_mdt;
 	struct coordinator	*cdt = &mdt->mdt_coordinator;
 	struct hsm_scan_data	 hsd = { NULL };
+	const unsigned int	 wait_event_time = 1;
+	unsigned long		 next_housekeeping = 0;
 	int			 rc = 0;
 	ENTRY;
 
@@ -454,11 +456,15 @@ static int mdt_coordinator(void *data)
 		struct l_wait_info lwi;
 		int i;
 
-		lwi = LWI_TIMEOUT(cfs_time_seconds(cdt->cdt_loop_period),
+		/* Limit execution of the expensive requests traversal
+		 * to at most every "wait_event_time" seconds. But we
+		 * also want to start or exit the coordinator as soon
+		 * as it is signaled, so use an event with timeout. */
+		lwi = LWI_TIMEOUT(cfs_time_seconds(wait_event_time),
 				  NULL, NULL);
 		l_wait_event(cdt->cdt_thread.t_ctl_waitq,
 			     (cdt->cdt_thread.t_flags &
-			      (SVC_STOPPING|SVC_EVENT)),
+			      (SVC_STOPPING | SVC_EVENT)),
 			     &lwi);
 
 		CDEBUG(D_HSM, "coordinator resumes\n");
@@ -470,15 +476,20 @@ static int mdt_coordinator(void *data)
 			break;
 		}
 
-		/* wake up before timeout, new work arrives */
-		if (cdt->cdt_thread.t_flags & SVC_EVENT)
-			cdt->cdt_thread.t_flags &= ~SVC_EVENT;
-
 		/* if coordinator is suspended continue to wait */
 		if (cdt->cdt_state == CDT_DISABLE) {
 			CDEBUG(D_HSM, "disable state, coordinator sleeps\n");
 			continue;
 		}
+
+		/* If no event, and no housekeeping to do, continue to
+		 * wait. */
+		if (next_housekeeping <= get_seconds())
+			next_housekeeping = get_seconds() + cdt->cdt_loop_period;
+		else if ((cdt->cdt_thread.t_flags & SVC_EVENT) == 0)
+			continue;
+
+		cdt->cdt_thread.t_flags &= ~SVC_EVENT;
 
 		CDEBUG(D_HSM, "coordinator starts reading llog\n");
 
@@ -751,27 +762,6 @@ static int hsm_init_ucred(struct lu_ucred *uc)
 	uc->uc_umask = 0777;
 	uc->uc_ginfo = NULL;
 	uc->uc_identity = NULL;
-
-	RETURN(0);
-}
-
-/**
- * wake up coordinator thread
- * \param mdt [IN] device
- * \retval 0 success
- * \retval -ve failure
- */
-int mdt_hsm_cdt_wakeup(struct mdt_device *mdt)
-{
-	struct coordinator	*cdt = &mdt->mdt_coordinator;
-	ENTRY;
-
-	if (cdt->cdt_state == CDT_STOPPED)
-		RETURN(-ESRCH);
-
-	/* wake up coordinator */
-	cdt->cdt_thread.t_flags = SVC_EVENT;
-	wake_up(&cdt->cdt_thread.t_ctl_waitq);
 
 	RETURN(0);
 }
@@ -1468,9 +1458,9 @@ int mdt_hsm_update_request_state(struct mdt_thread_info *mti,
 				       pgs->hpk_cookie);
 			rc = (rc != 0 ? rc : rc1);
 		}
-		/* ct has completed a request, so a slot is available, wakeup
-		 * cdt to find new work */
-		mdt_hsm_cdt_wakeup(mdt);
+		/* ct has completed a request, so a slot is available,
+		 * signal the coordinator to find new work */
+		mdt_hsm_cdt_work(cdt);
 	} else {
 		/* if copytool send a progress on a canceled request
 		 * we inform copytool it should stop
@@ -1912,7 +1902,8 @@ mdt_hsm_cdt_control_seq_write(struct file *file, const char __user *buffer,
 	if (strcmp(kernbuf, CDT_ENABLE_CMD) == 0) {
 		if (cdt->cdt_state == CDT_DISABLE) {
 			cdt->cdt_state = CDT_RUNNING;
-			mdt_hsm_cdt_wakeup(mdt);
+			mdt_hsm_cdt_work(cdt);
+			wake_up(&cdt->cdt_thread.t_ctl_waitq);
 		} else {
 			rc = mdt_hsm_cdt_start(mdt);
 		}
