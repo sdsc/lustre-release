9bde996d57c6215ec73eb2b31659bf9c88147dea
Kirtankumar Krishna Shetty <kirtan.shetty@seagate.com>
2016-04-21 14:18:01 +0530

    LU-8051 tests: Skipping sanity tests 300[a-d] for old servers.
    The tests 300[a-i] were added in version 2.7.0 for the newer
    version of the code to test striped directory related features
    and hence the tests 300[a,b,c,d] used to fail for the v2.7.0
    and above client with older server versions. Added skip
    statement for the tests if server is of an older versions.
    
    Seagate-bug-id: MRP-3393
    Change-Id: I26f271c1a4aa62c8f41c391e0c595f1ccaf18f8b
    Signed-off-by: Kirtankumar Krishna Shetty <kirtan.shetty@seagate.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -13822,6 +13822,8 @@ test_striped_dir() {
 }
 
 test_300a() {
+	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.7.0) ] &&
+		skip "skipped for lustre < 2.7.0" && return
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 
@@ -13831,6 +13833,8 @@ test_300a() {
 run_test 300a "basic striped dir sanity test"
 
 test_300b() {
+	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.7.0) ] &&
+		skip "skipped for lustre < 2.7.0" && return
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 	local i
@@ -13861,6 +13865,8 @@ test_300b() {
 run_test 300b "check ctime/mtime for striped dir"
 
 test_300c() {
+	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.7.0) ] &&
+		skip "skipped for lustre < 2.7.0" && return
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 	local file_count
@@ -13884,6 +13890,8 @@ test_300c() {
 run_test 300c "chown && check ls under striped directory"
 
 test_300d() {
+	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.7.0) ] &&
+		skip "skipped for lustre < 2.7.0" && return
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 	local stripe_count
