c58fa1a8da4c43b26abefda54dbe46ec79cadc3f
Sebastien Buisson <sbuisson@ddn.com>
2016-04-26 11:01:00 +0200

    LU-7845 obd: add 'network' client mount option
    Add a 'network' mount option on client side. All connections made by
    the client must be on the LNet network specified in the 'network'
    option.
    This is achieved by tampering with 2 config commands:
    - setup: add a fourth parameter, which is the net to restrict
      connections to. This parameter will be passed down to
      ptlrpc_uuid_to_peer() so that client only connects to peers on the
      restricted network.
    - add_conn: skip this command if uuid to connect to is not on
      restricted network.
    
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Change-Id: Ica7ab6ebe7c2c9e7b0409b615e90d9271c31e92f

--- lustre/obdclass/obd_config.c
+++ lustre/obdclass/obd_config.c
@@ -1582,7 +1582,8 @@ int class_config_llog_handler(const struct lu_env *env,
 				lcfg->lcfg_command = LCFG_LOV_ADD_INA;
 		}
 
-                lustre_cfg_bufs_init(&bufs, lcfg);
+		lustre_cfg_bufs_reset(&bufs, NULL);
+		lustre_cfg_bufs_init(&bufs, lcfg);
 
                 if (clli && clli->cfg_instance &&
                     LUSTRE_CFG_BUFLEN(lcfg, 0) > 0){
@@ -1623,6 +1624,46 @@ int class_config_llog_handler(const struct lu_env *env,
                         lustre_cfg_bufs_set_string(&bufs, 0,
                                                    clli->cfg_obdname);
                 }
+
+		/* Add net info to setup command
+		 * if given on command line.
+		 * So config log will be:
+		 * [0]: client name
+		 * [1]: client UUID
+		 * [2]: server UUID
+		 * [3]: inactive-on-startup
+		 * [4]: restrictive net
+		 */
+		if (clli && clli->cfg_sb && s2lsi(clli->cfg_sb) &&
+			!IS_SERVER(s2lsi(clli->cfg_sb))) {
+			struct lustre_sb_info *lsi = s2lsi(clli->cfg_sb);
+			char *nidnet = lsi->lsi_lmd->lmd_nidnet;
+			if (lcfg->lcfg_command == LCFG_SETUP &&
+			    lcfg->lcfg_bufcount != 2 && nidnet) {
+				CDEBUG(D_CONFIG, "Adding net %s info to setup "
+				       "command for client %s\n", nidnet,
+				       lustre_cfg_string(lcfg, 0));
+				lustre_cfg_bufs_set_string(&bufs, 4, nidnet);
+			}
+		}
+
+		/* Skip add_conn command if uuid is
+		 * not on restricted net */
+		if (clli && clli->cfg_sb && s2lsi(clli->cfg_sb) &&
+			!IS_SERVER(s2lsi(clli->cfg_sb))) {
+			struct lustre_sb_info *lsi = s2lsi(clli->cfg_sb);
+			char *uuid_str = lustre_cfg_string(lcfg, 1);
+			if (lcfg->lcfg_command == LCFG_ADD_CONN &&
+			    lsi->lsi_lmd->lmd_nidnet &&
+			    LNET_NIDNET(libcfs_str2nid(uuid_str)) !=
+			    libcfs_str2net(lsi->lsi_lmd->lmd_nidnet)) {
+				CDEBUG(D_CONFIG, "skipping add_conn for %s\n",
+				       uuid_str);
+				rc = 0;
+				/* No processing! */
+				break;
+			}
+		}
 
                 lcfg_new = lustre_cfg_new(lcfg->lcfg_command, &bufs);
 		if (lcfg_new == NULL)
