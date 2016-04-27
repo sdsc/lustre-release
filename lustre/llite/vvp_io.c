373e454b841550312f61c8df183be11cc92526de
Ben Evans <bevans@cray.com>
2016-04-27 16:03:16 -0500

    CRAY-3 llite: create private copy of __generic_file_aio_write
    In SLES12 __generic_file_aio_write assumes i_mutex is locked.
    In Lustre it is not, as protections are handled elsewhere.
    This causes a stack dump the first time file_remove_suid() is called.
    
    Signed-off-by: Ben Evans <bevans@cray.com>
    Change-Id: If3770913a670cb17219ff0a262dbb6ba2be75dec

--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -1045,6 +1045,127 @@ int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io)
 	RETURN(rc);
 }
 
+/**
+ * lustre_file_aio_write - write data to a file
+ *
+ * @iocb:       IO state structure (file, offset, etc.)
+ * @iov:        vector with data to write
+ * @nr_segs:    number of segments in the vector
+ * @ppos:       position where to write
+ *
+ * This function does all the work needed for actually writing data to a
+ * file. It does all basic checks, removes SUID from the file, updates
+ * modification times and calls proper subroutines depending on whether we
+ * do direct IO or a standard buffered write.
+ *
+ * Duplicate/change of__generic file_aio_write. We need to take out i_mutex
+ * to safely change SUID bits
+ *
+ * This function does *not* take care of syncing data in case of O_SYNC write.
+ * A caller has to handle it. This is mainly due to the fact that we want to
+ * avoid syncing under i_mutex.
+ */
+ssize_t lustre_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
+				 unsigned long nr_segs, loff_t *ppos)
+{
+	struct file *file = iocb->ki_filp;
+	struct address_space *mapping = file->f_mapping;
+	size_t ocount;          /* original count */
+	size_t count;           /* after file limit checks */
+	struct inode    *inode = mapping->host;
+	loff_t          pos;
+	ssize_t         written;
+	ssize_t         err;
+
+	ocount = 0;
+	err = generic_segment_checks(iov, &nr_segs, &ocount, VERIFY_READ);
+	if (err)
+		return err;
+
+	count = ocount;
+	pos = *ppos;
+
+	/* We can write back this queue in page reclaim */
+	current->backing_dev_info = mapping->backing_dev_info;
+	written = 0;
+
+	err = generic_write_checks(file, &pos, &count, S_ISBLK(inode->i_mode));
+	if (err)
+		goto out;
+
+	if (count == 0)
+		goto out;
+
+	mutex_lock(&inode->i_mutex);
+	err = file_remove_suid(file);
+	mutex_unlock(&inode->i_mutex);
+	if (err)
+		goto out;
+
+	err = file_update_time(file);
+	if (err)
+		goto out;
+
+	/* coalesce the iovecs and go direct-to-BIO for O_DIRECT */
+	if (unlikely(file->f_flags & O_DIRECT)) {
+		loff_t endbyte;
+		ssize_t written_buffered;
+
+		written = generic_file_direct_write(iocb, iov, &nr_segs, pos,
+						    ppos, count, ocount);
+		if (written < 0 || written == count)
+			goto out;
+		/*
+		 * direct-io write to a hole: fall through to buffered I/O
+		 * for completing the rest of the request.
+		 */
+		pos += written;
+		count -= written;
+		written_buffered = generic_file_buffered_write(iocb, iov,
+							       nr_segs, pos,
+							       ppos, count,
+							       written);
+		/*
+		 * If generic_file_buffered_write() retuned a synchronous error
+		 * then we want to return the number of bytes which were
+		 * direct-written, or the error code if that was zero.  Note
+		 * that this differs from normal direct-io semantics, which
+		 * will return -EFOO even if some bytes were written.
+		 */
+		if (written_buffered < 0) {
+			err = written_buffered;
+			goto out;
+		}
+
+		/*
+		 * We need to ensure that the page cache pages are written to
+		 * disk and invalidated to preserve the expected O_DIRECT
+		 * semantics.
+		 */
+		endbyte = pos + written_buffered - written - 1;
+		err = filemap_write_and_wait_range(file->f_mapping, pos,
+						   endbyte);
+		if (err == 0) {
+			written = written_buffered;
+			invalidate_mapping_pages(mapping,
+						 pos >> PAGE_CACHE_SHIFT,
+						 endbyte >> PAGE_CACHE_SHIFT);
+		} else {
+			/*
+			 * We don't know how much we wrote, so just return
+			 * the number of bytes which were direct-written
+			 */
+		}
+	} else {
+		written = generic_file_buffered_write(iocb, iov, nr_segs,
+						      pos, ppos, count,
+						      written);
+	}
+out:
+	current->backing_dev_info = NULL;
+	return written ? written : err;
+}
+
 static int vvp_io_write_start(const struct lu_env *env,
                               const struct cl_io_slice *ios)
 {
@@ -1106,10 +1227,10 @@ static int vvp_io_write_start(const struct lu_env *env,
 #ifdef HAVE_FILE_OPERATIONS_READ_WRITE_ITER
 		result = generic_file_write_iter(vio->vui_iocb, vio->vui_iter);
 #else
-		result = __generic_file_aio_write(vio->vui_iocb,
-						  vio->vui_iter->iov,
-						  vio->vui_iter->nr_segs,
-						  &vio->vui_iocb->ki_pos);
+		result = lustre_file_aio_write(vio->vui_iocb,
+					       vio->vui_iter->iov,
+					       vio->vui_iter->nr_segs,
+					       &vio->vui_iocb->ki_pos);
 #endif
 		if (result > 0 || result == -EIOCBQUEUED) {
 			ssize_t err;
