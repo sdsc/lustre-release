d6a6dd5eae5fdcd140531b686ccda48dec7f81d4
Giuseppe Di Natale <dinatale2@llnl.gov>
2016-05-05 15:43:28 -0700

    LU-8055 ldev: Add %H and %b to command sub
    Introduced %H and %b substitutions for hostname
    and backend file system, respectively.
    
    Signed-off-by: Giuseppe Di Natale <dinatale2@llnl.gov>
    Change-Id: I9b50defee43307f614106de4ad75e1fa9267e23b

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -6297,9 +6297,10 @@ generate_ldev_conf() {
 	# generate an ldev.conf file
 	local ldevconfpath=$1
 	touch $ldevconfpath
-	printf "%s\t-\t%s-MGS0000\t%s\n" \
+	printf "%s\t-\t%s-MGS0000\t%s:%s\n" \
 		$mgs_HOST \
 		$FSNAME \
+		$(facet_fstype mgs) \
 		$(mgsdevname) > $ldevconfpath
 
 	local mdsfo_host=$mdsfailover_HOST;
@@ -6308,11 +6309,12 @@ generate_ldev_conf() {
 	fi
 
 	for num in $(seq $MDSCOUNT); do
-		printf "%s\t%s\t%s-MDT%04d\t%s\n" \
+		printf "%s\t%s\t%s-MDT%04d\t%s:%s\n" \
 			$mds_HOST \
 			$mdsfo_host \
 			$FSNAME \
 			$num \
+			$(facet_fstype mds$num) \
 			$(mdsdevname $num) >> $ldevconfpath
 	done
 
@@ -6322,11 +6324,12 @@ generate_ldev_conf() {
 	fi
 
 	for num in $(seq $OSTCOUNT); do
-		printf "%s\t%s\t%s-OST%04d\t%s\n" \
+		printf "%s\t%s\t%s-OST%04d\t%s:%s\n" \
 			$ost_HOST \
 			$ostfo_host \
 			$FSNAME \
 			$num \
+			$(facet_fstype ost$num) \
 			$(ostdevname $num) >> $ldevconfpath
 	done
 
@@ -6538,6 +6541,59 @@ test_94() {
 	rm $LDEVCONFPATH $NIDSPATH
 }
 run_test 94 "ldev should only allow one label filter"
+
+test_95() {
+	if [ -z "$LDEV" ]; then
+		error "ldev is missing!"
+	fi
+
+	local LDEVCONFPATH=$TMP/ldev.conf
+	local NIDSPATH=$TMP/nids
+
+	generate_ldev_conf $LDEVCONFPATH
+	generate_nids $NIDSPATH
+
+	local LDEV_OUTPUT=$TMP/ldev-output.txt
+	$LDEV -c $LDEVCONFPATH -n $NIDSPATH -H $mgs_HOST \
+		echo %H-%b | \
+		awk '{print $2}' > $LDEV_OUTPUT
+
+	# ldev failed, error
+	if [ $? -ne 0 ]; then
+		rm $LDEVCONFPATH $NIDSPATH $LDEV_OUTPUT
+		error "ldev failed to execute!"
+	fi
+
+	# expected output
+	local EXPECTED_OUTPUT=$TMP/ldev-expected-output.txt
+
+	echo "$mgs_HOST-$(facet_fstype mgs)" > $EXPECTED_OUTPUT
+
+	if [ "$mgs_HOST" == "$mds_HOST" ]; then
+		for num in $(seq $MDSCOUNT); do
+			echo "$mds_HOST-$(facet_fstype mds$num)" \
+			>> $EXPECTED_OUTPUT
+		done
+	fi
+
+	if [ "$mgs_HOST" == "$ost_HOST" ]; then
+		for num in $(seq $OSTCOUNT); do
+			echo "$ost_HOST-$(facet_fstype ost$num)" \
+			>> $EXPECTED_OUTPUT
+		done
+	fi
+
+	compare_ldev_output $LDEV_OUTPUT $EXPECTED_OUTPUT
+
+	if [ $? -ne 0 ]; then
+		rm $LDEVCONFPATH $NIDSPATH $EXPECTED_OUTPUT $LDEV_OUTPUT
+		error "ldev failed to produce the correct output!"
+	fi
+
+	rm $LDEVCONFPATH $NIDSPATH $EXPECTED_OUTPUT $LDEV_OUTPUT
+}
+run_test 95 "ldev returns hostname and backend fs correctly in command sub"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi
