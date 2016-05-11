c28158f7be2ab1eb8cefa5331e8b1fbe1aeca569
Quentin Bouget <quentin.bouget.ocre@cea.fr>
2016-05-11 09:21:11 +0200

    LU-7659 mdc: expose hsm requests through char devices
    Register one character device per filesystem to allow non-llapi to
    read them and to make delivery more efficient.
    
    HSM requests (actually "struct hsm_action_list") can be published
    from the kernel with the "ct_cdev_hal_publish" function.
    
    Opening the char device in reading mode is similar to subscribing to
    incoming requests. Reading from the char device will copy requests
    published after the open in the order they were published. Requests
    published prior to the open operation are simply not visible. The
    reading offset is used to provide the archive_mask of the copytool
    and only retrieve the right requests (the other requests are ignored)
    
    Requests are stored in a linked list and are not dropped until every
    reader who can read it actually does. The close operation releases
    allocated resources. Requests are dropped accordingly.
    
    (Writing in the char device fails with errno set to EOPNOTSUPP)
    
    Signed-off-by: Quentin Bouget <quentin.bouget.ocre@cea.fr>
    Change-Id: I32091aa6221e48e2fc2cc0bcc3cb3d013df0b663

--- lustre/include/lustre/lustre_user.h
+++ lustre/include/lustre/lustre_user.h
@@ -1278,17 +1278,18 @@ static inline char *hai_dump_data_field(const struct hsm_action_item *hai,
 
 /* Copytool action list */
 #define HAL_VERSION 1
+#define HAL_MAGIC 0x48616c6d
 #define HAL_MAXSIZE LNET_MTU /* bytes, used in userspace only */
 struct hsm_action_list {
 	__u32 hal_version;
-	__u32 hal_count;       /* number of hai's to follow */
-	__u64 hal_compound_id; /* returned by coordinator */
+	__u32 hal_count;	/* number of hai's to follow */
+	__u64 hal_compound_id;	/* returned by coordinator */
 	__u64 hal_flags;
-	__u32 hal_archive_id; /* which archive backend */
-	__u32 padding1;
-	char  hal_fsname[0];   /* null-terminated */
+	__u32 hal_archive_id;	/* which archive backend */
+	__u32 hal_magic;	/* Only needed if sent across network */
+	char  hal_fsname[0];	/* null-terminated */
 	/* struct hsm_action_item[hal_count] follows, aligned on 8-byte
-	   boundaries. See hai_zero */
+	 * boundaries. See hai_zero */
 } __attribute__((packed));
 
 #ifndef HAVE_CFS_SIZE_ROUND
