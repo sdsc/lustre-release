cb330e6dead5ee0eb8481f22d0e68b76ea75d638
Emoly Liu <emoly.liu@intel.com>
2016-05-17 12:00:39 +0800

    LU-8125 nrs: pol_arg should be copied after the policy starts
    NRS policy->pol_arg should be copied to a given policy argument
    after the policy starts, otherwise a wrong argument will be saved
    and it will cause LBUG when we run "lctl get_param" to get NRS
    policy.
    
    sanityn.sh test_77h is added to verify this patch.
    
    Signed-off-by: Emoly Liu <emoly.liu@intel.com>
    Change-Id: I5a015bb44fa88f69a4d5347cc2721eb72a57c8ad

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -3190,6 +3190,41 @@ test_77g() {
 }
 run_test 77g "Change TBF type directly"
 
+test_77h() {
+	local old_policy=$(do_facet ost"$i" \
+		lctl get_param ost.OSS.ost_io.nrs_policies)
+	local new_policy
+
+	do_facet ost"$i" lctl set_param \
+		ost.OSS.ost_io.nrs_policies="abc"
+	[ $? -eq 0 ] && error "should return error"
+
+	do_facet ost"$i" lctl set_param \
+		ost.OSS.ost_io.nrs_policies="tbf\ abc"
+	[ $? -eq 0 ] && error "should return error"
+
+	do_facet ost"$i" lctl set_param \
+		ost.OSS.ost_io.nrs_policies="tbf\ reg"
+	[ $? -eq 0 ] && error "should return error"
+
+	do_facet ost"$i" lctl set_param \
+		ost.OSS.ost_io.nrs_policies="tbf\ reg\ abc"
+	[ $? -eq 0 ] && error "should return error"
+
+	do_facet ost"$i" lctl set_param \
+		ost.OSS.ost_io.nrs_policies="tbf\ abc\ efg"
+	[ $? -eq 0 ] && error "should return error"
+
+	new_policy=$(do_facet ost"$i" \
+		lctl get_param ost.OSS.ost_io.nrs_policies)
+	[ $? -eq 0 ] || error "shouldn't LBUG"
+
+	[ "$old_policy" = "$new_policy" ] || error "NRS policy should be same"
+
+	return 0
+}
+run_test 77h "Wrong policy name should report error, not LBUG"
+
 test_78() { #LU-6673
 	local rc
 
