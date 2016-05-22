3f22022e2908328044952e857761b5095aa870dd
Oleg Drokin <oleg.drokin@intel.com>
2016-05-22 10:46:15 -0400

    LU-TEST-00 none: Test build failures detection
    Trying to see if my new build failure detection for
    gatekeeper helper works.
    
    Change-Id: I9571accf63ba888ea166e4bc40ce3e6f296f5735
    Test-Parameters: forbuildonly
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/llite/dir.c
+++ lustre/llite/dir.c
@@ -153,6 +153,8 @@ struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
 	struct page		*page;
 	int			rc;
 
+	test = page;
+
 	cb_op.md_blocking_ast = ll_md_blocking_ast;
 	rc = md_read_page(ll_i2mdexp(dir), op_data, &cb_op, offset, &page);
 	if (rc != 0)
