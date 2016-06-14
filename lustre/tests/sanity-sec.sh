d824ac4c2f10056551093d44050b537e90922184
Chris Hanna <hannac@indiana.edu>
2016-06-14 11:41:40 -0400

    LU-8275 tests: add flag to enable secret shared key for tests
    When the SHARED_KEY environment variable is set to true,
    test-framework will set up a shared key between the nodes and start
    Lustre with shared key enabled. Tests can then proceed as per usual.
    
    Test-Parameters: fortestonly envdefinitions=SHARED_KEY=true
    Signed-off-by: Kit Westneat <kit.westneat@gmail.com>
    Signed-off-by: Chris Hanna <hannac@iu.edu>
    Change-Id: I2abff35fbb7dd4f227c6cd12a66cb0b77f610157

--- lustre/tests/sanity-sec.sh
+++ lustre/tests/sanity-sec.sh
@@ -125,6 +125,8 @@ sec_login() {
 	local user=$1
 	local group=$2
 
+	$GSS_KRB5 || return
+
 	if ! $RUNAS_CMD -u $user krb5_login.sh; then
 		error "$user login kerberos failed."
 		exit 1
@@ -1673,6 +1675,16 @@ test_25() {
 
 	nodemap_version_check || return 0
 
+	# Because of how the test is stopping, cleaning up, and restarting
+	# nodemap to save configuration, the testing script is not keeping
+	# the needed SK info to restart properly. This should be fixable
+	# by adjusting the test or framework to not completely clean up
+	# in cleanupall() when SK_UNIQUE_NM is set.
+	if $SHARED_KEY; then
+		skip "this test currently not compat with shared_key, see code"
+		return
+	fi
+
 	# stop clients for this test
 	zconf_umount_clients $CLIENTS $MOUNT ||
 	    error "unable to umount clients $CLIENTS"
@@ -1680,6 +1692,11 @@ test_25() {
 	nodemap_test_setup
 
 	trap nodemap_test_cleanup EXIT
+
+	# Need to know in advance if SK and nodemap will be used
+	if [ $GSS_SK ]; then
+		export SK_UNIQUE_NM=true
+	fi
 
 	# create a new, empty nodemap, and add fileset info to it
 	do_facet mgs $LCTL nodemap_add test26 ||
@@ -1693,6 +1710,9 @@ test_25() {
 	do_facet mds $LCTL nodemap_info > $tmpfile2
 
 	cleanup_and_setup_lustre
+	if [ $GSS_SK ]; then
+		export SK_UNIQUE_NM=false
+	fi
 	# stop clients for this test
 	zconf_umount_clients $CLIENTS $MOUNT ||
 	    error "unable to umount clients $CLIENTS"
