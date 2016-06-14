5a5ebff33884e76728a972764093fd55be5a13a7
Patrick Farrell <paf@cray.com>
2016-06-14 13:52:44 -0500

    LU-7225 llite: ladvise protocol changes
    This patch makes some changes to the ladvise API and
    protocol to support lock ahead and possible future users.
    
    Primarily, it separates the userspace API arguments from
    the structures which go out on the network, and adds a
    number of 'value' fields without a predefined use.
    
    The meaning of each value field can be different for
    different advice types, allowing some extensibility.
    
    We also use the ladvise magic passed in from userspace
    to recognize different versions; this requires a separate
    magic for the userspace argument and the wire protocol.
    
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Change-Id: I7ac18e546f16a20c3c6bc6849becb0d45e3d5dc9

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -2627,6 +2627,7 @@ out:
 	}
 	case LL_IOC_LADVISE: {
 		struct ladvise_hdr *ladvise_hdr;
+		__u32 magic;
 		int i;
 		int num_advise;
 		int alloc_size = sizeof(*ladvise_hdr);
@@ -2635,6 +2636,12 @@ out:
 		OBD_ALLOC_PTR(ladvise_hdr);
 		if (ladvise_hdr == NULL)
 			RETURN(-ENOMEM);
+
+		if (copy_from_user(&magic, (void __user *)arg, sizeof(magic)))
+			GOTO(out_ladvise, rc = -EFAULT);
+
+		if (magic != LADVISE_MAGIC)
+			GOTO(out_ladvise, rc = -EINVAL);
 
 		if (copy_from_user(ladvise_hdr,
 				   (const struct ladvise_hdr __user *)arg,
