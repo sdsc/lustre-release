bc20f77150dc63e55e69100ebda208d3cf931263
Nathaniel Clark <nathaniel.l.clark@intel.com>
2016-07-13 17:30:37 -0400

    LU-8330 wiretest: Add IOCTL checking to wiretest
    Add IOCTL checking (to ensure they don't change) to wiretest, but
    don't add to ptlrpc ASSERTS.  Doing this, showed that the
    OBD_IOC_GET_MNTOPT doesn't work in user-space as mntopt_t isn't
    defined in user-space and throws an error if the appropriate header is
    included.  This type is an enum, so should be the same as a long.
    
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: I6edf4c8bdd501a62cc7214ad14d3bb92822ffb99

---
+++ lustre/utils/const_check.h
@@ -0,0 +1,173 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2016, Intel Corporation.
+ */
+
+#ifndef _CONST_CHECK_H_
+#define _CONST_CHECK_H_
+
+#include <stdio.h>
+
+#define BLANK_LINE()						\
+do {								\
+	printf("\n");						\
+} while (0)
+
+#define COMMENT(c)						\
+do {								\
+	printf("	/* "c" */\n");				\
+} while (0)
+
+#define STRINGIFY(a) #a
+
+#define CHECK_CDEFINE(a)					\
+	printf("	CLASSERT("#a" == "STRINGIFY(a) ");\n")
+
+#define CHECK_CVALUE(a)					 \
+	printf("	CLASSERT("#a" == %lld);\n", (long long)a)
+
+#define CHECK_CVALUE_X(a)					\
+	printf("	CLASSERT("#a" == 0x%.8x);\n", a)
+
+#define CHECK_DEFINE(a)						\
+do {								\
+	printf("	LASSERTF("#a" == "STRINGIFY(a)		\
+		", \"found %%lld\\n\",\n			"\
+		"(long long)"#a");\n");				\
+} while (0)
+
+#define CHECK_DEFINE_X(a)					\
+do {								\
+	printf("	LASSERTF("#a" == "STRINGIFY(a)		\
+		", \"found 0x%%.8x\\n\",\n		"#a	\
+		");\n");					\
+} while (0)
+
+#define CHECK_DEFINE_64X(a)					\
+do {								\
+	printf("	LASSERTF("#a" == "STRINGIFY(a)		\
+		", \"found 0x%%.16llxULL\\n\",\n		"\
+		" "#a");\n");					\
+} while (0)
+
+#define CHECK_VALUE(a)						\
+do {								\
+	printf("	LASSERTF("#a				\
+		" == %lld, \"found %%lld\\n\",\n		"\
+		" (long long)"#a");\n", (long long)a);		\
+} while (0)
+
+#define CHECK_VALUE_X(a)					\
+do {								\
+	printf("	LASSERTF("#a				\
+		" == 0x%.8xUL, \"found 0x%%.8xUL\\n\",\n	"\
+		"	(unsigned)"#a");\n", (unsigned)a);	\
+} while (0)
+
+#define CHECK_VALUE_O(a)					\
+do {								\
+	printf("	LASSERTF("#a				\
+		" == 0%.11oUL, \"found 0%%.11oUL\\n\",\n	"\
+		"	"#a");\n", a);				\
+} while (0)
+
+#define CHECK_VALUE_64X(a)					\
+do {								\
+	printf("	LASSERTF("#a" == 0x%.16llxULL, "	\
+		"\"found 0x%%.16llxULL\\n\",\n			"\
+		"(long long)"#a");\n", (long long)a);		\
+} while (0)
+
+#define CHECK_VALUE_64O(a)					\
+do {								\
+	printf("	LASSERTF("#a" == 0%.22lloULL, "		\
+		"\"found 0%%.22lloULL\\n\",\n			"\
+		"(long long)"#a");\n", (long long)a);		\
+} while (0)
+
+#define CHECK_MEMBER_OFFSET(s, m)				\
+do {								\
+	CHECK_VALUE((int)offsetof(struct s, m));		\
+} while (0)
+
+#define CHECK_MEMBER_OFFSET_TYPEDEF(s, m)			\
+do {								\
+	CHECK_VALUE((int)offsetof(s, m));			\
+} while (0)
+
+#define CHECK_MEMBER_SIZEOF(s, m)				\
+do {								\
+	CHECK_VALUE((int)sizeof(((struct s *)0)->m));		\
+} while (0)
+
+#define CHECK_MEMBER_SIZEOF_TYPEDEF(s, m)			\
+do {								\
+	CHECK_VALUE((int)sizeof(((s *)0)->m));			\
+} while (0)
+
+#define CHECK_MEMBER(s, m)					\
+do {								\
+	CHECK_MEMBER_OFFSET(s, m);				\
+	CHECK_MEMBER_SIZEOF(s, m);				\
+} while (0)
+
+#define CHECK_MEMBER_TYPEDEF(s, m)				\
+do {								\
+	CHECK_MEMBER_OFFSET_TYPEDEF(s, m);			\
+	CHECK_MEMBER_SIZEOF_TYPEDEF(s, m);			\
+} while (0)
+
+#define CHECK_STRUCT(s)						\
+do {								\
+	COMMENT("Checks for struct "#s);			\
+		CHECK_VALUE((int)sizeof(struct s));		\
+} while (0)
+
+#define CHECK_STRUCT_TYPEDEF(s)					\
+do {								\
+	COMMENT("Checks for type "#s);				\
+		CHECK_VALUE((int)sizeof(s));			\
+} while (0)
+
+#define CHECK_UNION(s)						\
+do {								\
+	COMMENT("Checks for union "#s);				\
+	CHECK_VALUE((int)sizeof(union s));			\
+} while (0)
+
+#define CHECK_VALUE_SAME(v1, v2)				\
+do {								\
+	printf("\tLASSERTF("#v1" == "#v2", "			\
+		"\"%%d != %%d\\n\",\n"				\
+		"\t\t "#v1", "#v2");\n");			\
+} while (0)
+
+#define CHECK_MEMBER_SAME(s1, s2, m)				\
+do {								\
+	CHECK_VALUE_SAME((int)offsetof(struct s1, m),		\
+			 (int)offsetof(struct s2, m));		\
+	CHECK_VALUE_SAME((int)sizeof(((struct s1 *)0)->m),	\
+			 (int)sizeof(((struct s2 *)0)->m));	\
+} while (0)
+
+#endif
