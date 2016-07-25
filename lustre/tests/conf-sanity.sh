e2c06c1d514b117a88cbb094fc13be107d88638f
Lokesh Nagappa Jaliminche <lokesh.jaliminche@seagate.com>
2016-07-25 10:55:24 +0530

    LU-7919 mount: Buffer overflow issue while parsing mount
    check is added in parse_options, parse_ldd, add_mgsnids,
    parse_ldd and main function to avoid buffer overflow
    while parsing mount options.
    
    Seagate-bug-id: MRP-2384
    Change-Id: I1af9db4221ccdf1fd64a9a565063e5948cd5ba16
    Signed-off-by: Lokesh Nagappa Jaliminche <lokesh.jaliminche@seagate.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -6779,6 +6779,28 @@ test_97() {
 }
 run_test 97 "ldev returns correct ouput when querying based on role"
 
+test_98()
+{
+	local mountopt
+	local temp=$MDS_MOUNT_OPTS
+
+	setup
+	check_mount || return 41
+	mountopt="user_xattr"
+	for x in $(seq 1 400); do
+		mountopt="$mountopt,user_xattr"
+	done
+	stop_mds
+	MDS_MOUNT_OPTS="-o $mountopt"
+	out_str=$(start_mds 2>&1)
+	[[ "$out_str" =~ "mount options exceeds page size of kernel" ]] ||
+	error "Buffer overflow check failed"
+	MDS_MOUNT_OPTS=$temp
+	start_mds
+	cleanup || return $?
+}
+run_test 98 "Buffer-overflow check while parsing mount_opts"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi
