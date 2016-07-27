1bd1366ecaffa7145b836be4491bb0cf9f4d4352
Olaf Weber <olaf@sgi.com>
2016-07-27 13:18:54 +0200

    LU-7734 lnet: reference counts on lnet_peer/lnet_peer_net
    Peer discovery will be keeping track of lnet_peer structures,
    so there will be references to an lnet_peer independent of
    the references implied by lnet_peer_ni structures. Manage
    this by adding explicit reference counts to lnet_peer_net and
    lnet_peer.
    
    Each lnet_peer_net has a hold on the lnet_peer it links to
    with its lpn_peer pointer. Note that removing an lnet_peer_net
    from the lp_peer_nets list does not release this hold, it just
    prevents new lookups of the lnet_peer_net via the lnet_peer.
    
    Each lnet_peer_ni has a hold on the lnet_net it links to with
    its lpni_peer_net pointer. Note that removing an lnet_peer_ni
    from the lpn_peer_nis list does not release this hold, it just
    prevents new lookups of the lnet_peer_ni via the lnet_peer_net.
    
    Keep a count of the total number of lnet_peer_ni attached to
    an lnet_peer in lp_nnis.
    
    Split the global ln_peers list into per-lnet_peer_table lists.
    The CPT of the peer table in which the lnet_peer is linked is
    stored in lp_cpt.
    
    Test-Parameters: trivial
    Signed-off-by: Olaf Weber <olaf@sgi.com>
    Change-Id: Ica78e903126d67eb2c677ce69e634b9f72f6e4d1

--- lnet/lnet/module.c
+++ lnet/lnet/module.c
@@ -211,6 +211,7 @@ static int __init lnet_init(void)
 	printk(KERN_INFO "LNET_PEER_MULTI_RAIL\n");
 	printk(KERN_INFO "preferred NI for non-MR\n");
 	printk(KERN_INFO "LNET_PEER_CONFIGURED\n");
+	printk(KERN_INFO "peer refcount\n");
 	mutex_init(&lnet_config_mutex);
 
 	rc = lnet_lib_init();
