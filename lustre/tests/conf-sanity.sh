9eedbb7f03ad891ac6ad4d43296d2bccdc92e4d1
Jian Yu <jian.yu@intel.com>
2016-08-03 00:20:35 -0700

    LU-8474 tests: stop MGS before setup_noconfig in conf-sanity.sh
    In conf-sanity.sh, some sub-tests will leave MGS mounted
    under separate MGT and MDT configuration, which will cause
    setup_noconfig() fail. This patch fixes the issue by stopping
    MGS before running setup_noconfig().
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: I53f5549c392d300a2c76bbc4ce68e9a8198ba559

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -4038,6 +4038,10 @@ test_55() {
 
 	for i in 1023 2048
 	do
+		if ! combined_mgs_mds; then
+			stop_mgs || error "stopping MGS service failed"
+			format_mgs || error "formatting MGT failed"
+		fi
 		add mds1 $(mkfs_opts mds1 ${mdsdev}) --reformat $mdsdev \
 			$mdsvdev || exit 10
 		add ost1 $(mkfs_opts ost1 $(ostdevname 1)) --index=$i \
@@ -4134,6 +4138,7 @@ count_osts() {
 }
 
 test_58() { # bug 22658
+	combined_mgs_mds || stop_mgs || error "stopping MGS service failed"
 	setup_noconfig
 	mkdir $DIR/$tdir || error "mkdir $DIR/$tdir failed"
 	createmany -o $DIR/$tdir/$tfile-%d 100
@@ -4237,6 +4242,7 @@ test_61() { # LU-80
 		done
 	fi
 
+	combined_mgs_mds || stop_mgs || error "stopping MGS service failed"
 	setup_noconfig || error "setting up the filesystem failed"
 	client_up || error "starting client failed"
 
