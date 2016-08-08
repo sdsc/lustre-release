d0f82be70ef01af4ccea83d870ddb4d9178690ac
Nathaniel Clark <nathaniel.l.clark@intel.com>
2016-08-08 16:35:00 -0400

    LU-5473 tests: Add debug to sanity/51b
    Do lfs df and lfs df -i on completion of sanity/test_51b.  This will
    help determine the correct inode sizing for ZFS.
    
    Test-Parameters: mdsfilesystemtype=zfs mdtfilesystemtype=zfs ostfilesystemtype=zfs testlist=sanity envdefinitions=SLOW=yes
    Test-Parameters: mdsfilesystemtype=zfs mdtfilesystemtype=zfs ostfilesystemtype=zfs testlist=sanity envdefinitions=SLOW=yes
    Test-Parameters: mdsfilesystemtype=zfs mdtfilesystemtype=zfs ostfilesystemtype=zfs testlist=sanity envdefinitions=SLOW=yes
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: Icd8e0d55a89d8e3d22bbb1b2ff206e238a7262ac

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -4097,6 +4097,12 @@ test_51a() {	# was test_51
 }
 run_test 51a "special situations: split htree with empty entry =="
 
+cleanup_print_lfs_df () {
+	trap 0
+	$LFS df
+	$LFS df -i
+}
+
 test_51b() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
 	local dir=$DIR/$tdir
@@ -4121,6 +4127,8 @@ test_51b() {
 	numfree=$((blkfree / 4))
 	[[ $numfree -lt $nrdirs ]] && skip "not enough blocks ($numfree)" &&
 		return
+
+	trap cleanup_print_lfsdf EXIT
 
 	# create files
 	createmany -d $dir/d $nrdirs ||
@@ -4151,6 +4159,8 @@ test_51b() {
 	local after=$(stat -c %h $dir)
 	[[ $after -gt 2 ]] && error "nlink after: $after > 2" ||
 		echo "nlink after: $after"
+
+	cleanup_print_lfs_df
 }
 run_test 51b "exceed 64k subdirectory nlink limit on create, verify unlink"
 
