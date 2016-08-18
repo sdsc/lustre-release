efdd1bb9d427943a61dbd06e54c289948b5b8260
Fan Yong <fan.yong@intel.com>
2016-08-19 04:32:34 +0800

    LU-8840 osp: handle EA cache properly
    This patch contains several changes:
    
    1) For success case, the osp_xattr_get() should return the EA
       size instead of zero. Otherwise, it will misguide the caller
       that such EA does not exist.
    
    2) Currently, not cache MDT-object's EAs in OSP. That is almost
       useless (for namespace LFSCK) and may be NOT safe. Different
       from the layout LFSCK, the namespace LFSCK does not need to
       pre-fetch MDT-object's EA since remote MDT-object is relative
       rare case.
    
    3) More code cleanup for OSP EA cache to avoid reference leak,
       buffer overflow, and so on.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I352b99b1ed08f1b15bdb8da2bf28689ae2d61c23

--- lustre/target/out_handler.c
+++ lustre/target/out_handler.c
@@ -283,15 +283,13 @@ static int out_xattr_get(struct tgt_session_info *tsi)
 	dt_read_unlock(env, obj);
 	if (rc < 0)
 		lbuf->lb_len = 0;
+
 	CDEBUG(D_INFO, "%s: "DFID" get xattr %s len %d\n",
 	       tgt_name(tsi->tsi_tgt), PFID(lu_object_fid(&obj->do_lu)),
 	       name, (int)lbuf->lb_len);
 
-	GOTO(out, rc);
-
-out:
 	object_update_result_insert(reply, lbuf->lb_buf, lbuf->lb_len, idx, rc);
-	RETURN(0);
+	RETURN(rc);
 }
 
 static int out_index_lookup(struct tgt_session_info *tsi)
