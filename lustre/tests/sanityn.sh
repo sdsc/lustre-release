ab98ce805ab544cadec367fb3a330f0d289a962f
Li Xi <lixi@ddn.com>
2016-08-22 18:17:18 +0800

    LU-8551 test: Use mds1 rather than mds to operate on MDT0000
    Use mds1 rather than mds to operate on MDT0000, fixed following cases:
    conf-sanity.sh: test_43, test_58, test_62
    sanityn.sh:	test_54, test_55.
    
    Test-Parameters: testlist=conf-sanity
    Test-Parameters: testlist=sanityn
    
    Change-Id: Ibe0b419f837e4478610d61b8efb54af93662a45e
    Signed-off-by: Gu Zheng <gzheng@ddn.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -2496,7 +2496,7 @@ test_54_part1()
 	echo "==> rename vs getattr vs setxattr should not deadlock"
 	mkdir -p $DIR/d1/d2/d3 || error "(1) mkdir failed"
 
-	do_facet mds $LCTL set_param fail_loc=$1
+	do_facet mds1 $LCTL set_param fail_loc=$1
 
 	mv -T $DIR/d1/d2/d3 $DIR/d1/d3 &
 	PID1=$!
@@ -2518,7 +2518,7 @@ test_54_part2() {
 	echo "==> rename vs getattr vs open vs getattr should not deadlock"
 	mkdir -p $DIR/d1/d2/d3 || error "(1) mkdir failed"
 
-	do_facet mds $LCTL set_param fail_loc=$1
+	do_facet mds1 $LCTL set_param fail_loc=$1
 
 	mv -T $DIR/d1/d2/d3 $DIR/d1/d3 &
 	PID1=$!
@@ -2563,7 +2563,7 @@ test_55a() {
 	mkdir -p $DIR/d1/d2 $DIR/d3 || error "(1) mkdir failed"
 
 #define OBD_FAIL_MDS_RENAME4              0x156
-	do_facet mds $LCTL set_param fail_loc=0x80000156
+	do_facet mds1 $LCTL set_param fail_loc=0x80000156
 
 	mv -T $DIR/d1/d2 $DIR/d3/d2 &
 	PID1=$!
@@ -2581,7 +2581,7 @@ test_55b()
 	mkdir -p $DIR/d1/d2 $DIR/d3 || error "(1) mkdir failed"
 
 #define OBD_FAIL_MDS_RENAME4             0x156
-	do_facet mds $LCTL set_param fail_loc=0x80000156
+	do_facet mds1 $LCTL set_param fail_loc=0x80000156
 
 	mv -T $DIR/d1/d2 $DIR/d3/d2 &
 	PID1=$!
@@ -2599,7 +2599,7 @@ test_55c()
 	mkdir -p $DIR/d1/d2 $DIR/d3 || error "(1) mkdir failed"
 
 #define OBD_FAIL_MDS_RENAME4              0x156
-	do_facet mds $LCTL set_param fail_loc=0x156
+	do_facet mds1 $LCTL set_param fail_loc=0x156
 
 	mv -T $DIR/d1/d2 $DIR/d3/d2 &
 	PID1=$!
@@ -2627,7 +2627,7 @@ test_55d()
 	touch $DIR/f1
 
 #define OBD_FAIL_MDS_RENAME3              0x155
-	do_facet mds $LCTL set_param fail_loc=0x155
+	do_facet mds1 $LCTL set_param fail_loc=0x155
 	mv $DIR/f1 $DIR/$tdir &
 	PID1=$!
 	sleep 2
