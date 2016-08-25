470bfb100a456a8ff439e6e3cf671cb22abd41ed
Vitaly Fertman <vitaly.fertman@seagate.com>
2016-08-25 23:54:11 +0300

    LU-7433 ldlm: xattr locks are lost on mdt
    mdt_intent_getxattr() can return EFAULT if a buffer cannot be found,
    it is returned after lock_replace, where a new lock is installed into
    lockp. An error forces ldlm_lock_enqueue() to destroy the original
    lock, but ldlm_handle_enqueue0() drops the reference on the new lock.
    
    xattr client code implied intent error is returned under a lock,
    which is immediately cancelled. Check if a lock obtained and cancel
    it properly for error cases. Note: we should support both cases for
    interop needs, an intent error under a lock and with a lock abort.
    
    Keep returning a lock with an intent error for interop purposes for
    now, to be dropped later when client will get old enough.
    
    make all intent ops to work through md_intent_lock: getxattr
    and layout, which should extract the intent error.
    
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Change-Id: I7b628b50448c4bdb26a3a8758fc16a44212ad9ac
    Seagate-big-id: MRP-3072 MRP-3137
    Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Reviewed-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -14996,6 +14996,29 @@ test_311() {
 }
 run_test 311 "disable OSP precreate, and unlink should destroy objs"
 
+test_257() {
+	remote_mds_nodsh && skip "remote MDS with nodsh" && return
+	[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.8.55) ]] &&
+		skip "Need MDS version at least 2.8.55" && return
+
+	test_mkdir -p $DIR/$tdir
+
+	setfattr -n trusted.name1 -v value1 $DIR/$tdir ||
+		error "setfattr -n trusted.name1=value1 $DIR/$tdir failed"
+	stat $DIR/$tdir
+
+#define OBD_FAIL_MDS_XATTR_REP			0x161
+	local mdtidx=$($LFS getstripe -M $DIR/$tdir)
+	local facet=mds$((mdtidx + 1))
+	set_nodes_failloc $(facet_active_host $facet) 0x80000161
+	getfattr -n trusted.name1 $DIR/$tdir 2> /dev/null
+
+	stop $facet || error "stop MDS failed"
+	start $facet $(mdsdevname $((mdtidx + 1))) $MDS_MOUNT_OPTS ||
+		error "start MDS fail"
+}
+run_test 257 "xattr locks are not lost"
+
 test_400a() { # LU-1606, was conf-sanity test_74
 	local extra_flags=''
 	local out=$TMP/$tfile
