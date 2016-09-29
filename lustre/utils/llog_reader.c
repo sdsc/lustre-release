dfa445b00304ea290aa04dbb4a7c78b1153a0637
Ben Evans <bevans@cray.com>
2016-09-29 16:20:59 -0400

    LU-6401 headers: move lu_fid, ost_id funcs out of lustre_idl
    Move lu_fid functions into lustre/lustre_fid.h
    Move ost_id functions into lustre/lustre_ostid.h
    Fix indenting, include new headers as needed
    
    Signed-off-by: Ben Evans <bevans@cray.com>
    Change-Id: Idcbb95f614829b434c48a476172eedb421fe488f

--- lustre/utils/llog_reader.c
+++ lustre/utils/llog_reader.c
@@ -57,7 +57,9 @@
 #include <errno.h>
 #include <time.h>
 #include <lnet/nidstr.h>
+#include <lustre/lustre_fid.h>
 #include <lustre/lustre_idl.h>
+#include <lustre/lustre_ostid.h>
 #include <lustre/lustreapi.h>
 #include <lustre_log_user.h>
 #include <lustre_cfg.h>
