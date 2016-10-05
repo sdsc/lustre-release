147554d36fd955d98119d717e57968c1344d8582
Jadhav Vikram <jadhav.vikram@seagate.com>
2016-10-06 01:02:42 +0530

    LU-8687 tests: list pool on mds when mgs is separate
    In case of separate MGS/MDS setup list pool on mgs
    will show nothing, so listing pool from mds instead of
    mgs.
    
    Seagate-bug-id: MRP-3327
    Signed-off-by: Jadhav Vikram <jadhav.vikram@seagate.com>
    Change-Id: If5e9e6a7303059ab79e14967d2ea86b6d61c8aba
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/11895
    Reviewed-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Reviewed-by: Rahul Deshmukh <rahul.deshmukh@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -6215,8 +6215,18 @@ oos_full() {
 	return $OSCFULL
 }
 
-pool_list () {
-   do_facet mgs lctl pool_list $1
+list_pool() {
+	echo -e "$(do_facet $SINGLEMDS $LCTL pool_list $1 2>/dev/null |
+			sed '1d')"
+}
+
+check_pool_not_exist() {
+	local fsname=${1%%.*}
+	local poolname=${1##$fsname.}
+	[[ $# -ne 1 ]] && return 0
+	[[ x$poolname = x ]] &&  return 0
+	list_pool $fsname | grep -w $1 && return 1
+	return 0
 }
 
 create_pool() {
@@ -6258,8 +6268,7 @@ remove_pool_from_list () {
 
 destroy_pool_int() {
     local ost
-    local OSTS=$(do_facet $SINGLEMDS lctl pool_list $1 | \
-        awk '$1 !~ /^Pool:/ {print $1}')
+	local OSTS=$(list_pool $1)
     for ost in $OSTS; do
         do_facet mgs lctl pool_remove $1 $ost
     done
@@ -6275,7 +6284,8 @@ destroy_pool() {
 
     local RC
 
-    pool_list $fsname.$poolname || return $?
+	check_pool_not_exist $fsname.$poolname
+	[[ $? -eq 0 ]] && return 0
 
     destroy_pool_int $fsname.$poolname
     RC=$?
@@ -6296,8 +6306,6 @@ destroy_pools () {
     local fsname=${1:-$FSNAME}
     local poolname
     local listvar=${fsname}_CREATED_POOLS
-
-    pool_list $fsname
 
     [ x${!listvar} = x ] && return 0
 
