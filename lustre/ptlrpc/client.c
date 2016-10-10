487586d8d40506f1e9980ecd8f8aad5b5277f519
Niu Yawei <yawei.niu@intel.com>
2016-10-10 07:08:54 -0400

    LU-6808 ptlrpc: no need to reassign mbits for replay
    It's not necessary reassgin & re-adjust rq_mbits for replay
    request in ptlrpc_set_bulk_mbits(), they all must have already
    been correctly assigned before.
    
    Such unecessary reassign could make the first matchbit not
    PTLRPC_BULK_OPS_MASK aligned, that'll trigger LASSERT in
    ptlrpc_register_bulk():
    
    - ptlrpc_set_bulk_mbits() is called when first time sending
      request, rq_mbits is set as xid, which is BULK_OPS aligned;
    
    - ptlrpc_set_bulk_mbits() continue to adjust the mbits for
      multi-bulk RPC, rq_mbits is not aligned anymore, then rq_xid
      is changed accordingly if client is connecting to an old
      server, so rq_xid became unaligned too;
    
    - The request is replayed, ptlrpc_set_bulk_mbits() reassign
      the rq_mbits as rq_xid, which isn't aligned already, but
      ptlrpc_register_bulk() still assumes this value as the
      first matchbits and LASSERT it's BULK_OPS aligned.
    
    Test-Parameters: alwaysuploadlogs envdefinitions=SLOW=yes, ENABLE_QUOTA=yes mdtfilesystemtype=ldiskfs mdsfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs clientdistro=el7 ossdistro=el6.8 mdsdistro=el6.8 clientarch=x86_64 ossarch=x86_64 mdsarch=x86_64 mdsjob=lustre-b_ieel2_0 ossjob=lustre-b_ieel2_0 ossbuildno=274 mdsbuildno=274 mdtcount=1 ostcount=2 clientcount=2 testlist=replay-single, replay-single,replay-single,replay-single
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: Ib5d5a969702d3b621fb44643586cc19bf931c365

--- lustre/ptlrpc/client.c
+++ lustre/ptlrpc/client.c
@@ -3242,24 +3242,39 @@ void ptlrpc_set_bulk_mbits(struct ptlrpc_request *req)
 
 	LASSERT(bd != NULL);
 
-	if (!req->rq_resend) {
-		/* this request has a new xid, just use it as bulk matchbits */
-		req->rq_mbits = req->rq_xid;
-
-	} else { /* needs to generate a new matchbits for resend */
-		__u64	old_mbits = req->rq_mbits;
-
-		if (OCD_HAS_FLAG(&bd->bd_import->imp_connect_data, BULK_MBITS)){
-			req->rq_mbits = ptlrpc_next_xid();
-		} else {/* old version transfers rq_xid to peer as matchbits */
-			spin_lock(&req->rq_import->imp_lock);
-			list_del_init(&req->rq_unreplied_list);
-			ptlrpc_assign_next_xid_nolock(req);
+	/* Generate new matchbits for all resend requests, including
+	 * resend replay. */
+	if (req->rq_resend) {
+		/* Resend for -EINPROGRESS is different from normal resend,
+		 * it already got a new xid, so we can use it directly as
+		 * matchbits. */
+		if (req->rq_nr_resend) {
 			req->rq_mbits = req->rq_xid;
-			spin_unlock(&req->rq_import->imp_lock);
+		} else {
+			__u64 old_mbits = req->rq_mbits;
+
+			if (OCD_HAS_FLAG(&bd->bd_import->imp_connect_data,
+					 BULK_MBITS)){
+				req->rq_mbits = ptlrpc_next_xid();
+			} else {
+				/* Old version transfers rq_xid to peer as
+				 * matchbits. */
+				spin_lock(&req->rq_import->imp_lock);
+				list_del_init(&req->rq_unreplied_list);
+				ptlrpc_assign_next_xid_nolock(req);
+				spin_unlock(&req->rq_import->imp_lock);
+				req->rq_mbits = req->rq_xid;
+			}
+			CDEBUG(D_HA, "resend bulk old x%llu new x%llu\n",
+			       old_mbits, req->rq_mbits);
 		}
-		CDEBUG(D_HA, "resend bulk old x%llu new x%llu\n",
-		       old_mbits, req->rq_mbits);
+	} else if (!(lustre_msg_get_flags(req->rq_reqmsg) & MSG_REPLAY)) {
+		/* Request being sent first time, use xid as matchbits. */
+		req->rq_mbits = req->rq_xid;
+	} else {
+		/* Replay request, xid and matchbits have already been
+		 * correctly assigned. */
+		return;
 	}
 
 	/* For multi-bulk RPCs, rq_mbits is the last mbits needed for bulks so
@@ -3269,7 +3284,10 @@ void ptlrpc_set_bulk_mbits(struct ptlrpc_request *req)
 			  LNET_MAX_IOV) - 1;
 
 	/* Set rq_xid as rq_mbits to indicate the final bulk for the old
-	 * server which does not support OBD_CONNECT_BULK_MBITS. LU-6808 */
+	 * server which does not support OBD_CONNECT_BULK_MBITS. LU-6808.
+	 *
+	 * It's ok to directly set the rq_xid here, since this xid bump
+	 * won't affect the request position in unreplied list. */
 	if (!OCD_HAS_FLAG(&bd->bd_import->imp_connect_data, BULK_MBITS))
 		req->rq_xid = req->rq_mbits;
 }
