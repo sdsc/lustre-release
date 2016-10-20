b863241a65baebcab779c5ea7c7481b622336b02
James Simmons <uja.ornl@yahoo.com>
2016-10-20 12:46:03 -0400

    LU-6245 libcfs: use libcfs_private.h only for kernel space
    Several pieces of code in libcfs_private.h were wrappers to
    be used in userspace. Those bits are no longer used so we
    can remove them. With the removal very little is left for
    userspace so cleanup the user land code so libcfs_private.h
    is no longer needed.
    
    Change-Id: Iaa54bdcfb6104d13f2aabae63335e041481244a5
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>

--- lustre/utils/liblustreapi.c
+++ lustre/utils/liblustreapi.c
@@ -41,6 +41,7 @@
 #define _GNU_SOURCE
 #endif
 
+#include <assert.h>
 #include <mntent.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -2693,9 +2694,10 @@ static int print_failed_tgt(struct find_param *param, char *path, int type)
 {
         struct obd_statfs stat_buf;
         struct obd_uuid uuid_buf;
-        int ret;
+	int ret = -EINVAL;
 
-        LASSERT(type == LL_STATFS_LOV || type == LL_STATFS_LMV);
+	if (type != LL_STATFS_LOV && type != LL_STATFS_LMV)
+		return ret;
 
         memset(&stat_buf, 0, sizeof(struct obd_statfs));
         memset(&uuid_buf, 0, sizeof(struct obd_uuid));
@@ -2723,7 +2725,7 @@ static int cb_find_init(char *path, DIR *parent, DIR **dirp,
         int checked_type = 0;
         int ret = 0;
 
-        LASSERT(parent != NULL || dir != NULL);
+	assert(parent != NULL || dir != NULL);
 
 	param->fp_lmd->lmd_lmm.lmm_stripe_count = 0;
 
@@ -3059,7 +3061,9 @@ static int cb_migrate_mdt_init(char *path, DIR *parent, DIR **dirp,
 	char			*filename;
 	bool			retry = false;
 
-	LASSERT(parent != NULL || dirp != NULL);
+	if (parent == NULL && dirp == NULL)
+		return -EINVAL;
+
 	if (dirp != NULL)
 		closedir(*dirp);
 
@@ -3178,7 +3182,8 @@ static int cb_get_mdt_index(char *path, DIR *parent, DIR **dirp, void *data,
 	int ret;
 	int mdtidx;
 
-	LASSERT(parent != NULL || d != NULL);
+	if (parent == NULL && d == NULL)
+		return -EINVAL;
 
 	if (d != NULL) {
 		ret = llapi_file_fget_mdtidx(dirfd(d), &mdtidx);
@@ -3240,7 +3245,8 @@ static int cb_getstripe(char *path, DIR *parent, DIR **dirp, void *data,
 	DIR *d = dirp == NULL ? NULL : *dirp;
         int ret = 0;
 
-        LASSERT(parent != NULL || d != NULL);
+	if (parent == NULL && d == NULL)
+		return -EINVAL;
 
 	if (param->fp_obd_uuid) {
 		param->fp_quiet = 1;
