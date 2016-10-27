f09f17661323a0f134d9ef02044b863693ee0a9c
Niu Yawei <yawei.niu@intel.com>
2016-10-27 02:38:58 -0400

    LU-8765 ptlrpc: update replay cursor when close during replay
    The replay cursor should be updated properly when close happened
    during replay, otherwise, ptlrpc_replay_next() could run into a
    dead loop due to an invalid replay cursor:
    
    - replay cursor is moved to an open request during replay;
    - application close that open file, so the rq_replay of the open
      request is cleared;
    - ptlrpc_replay_next() calls ptlrpc_free_committed() to free
      committed/closed requests, the open request is removed from
      the committed list, so the replay cursor is changed to an
      empty list_head now. The open request won't be freed now since
      it's still held by the pending close request;
    - ptlrpc_replay_next() continue to move the replay cursor to
      next and run into a dead loop at the end;
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I358c4f55dbf38b0c9122c3aff40cf9ec96619d6a

--- lustre/ptlrpc/recover.c
+++ lustre/ptlrpc/recover.c
@@ -129,6 +129,8 @@ int ptlrpc_replay_next(struct obd_import *imp, int *inflight)
 					break;
 
 				req = NULL;
+				LASSERT(imp->imp_replay_cursor !=
+					imp->imp_replay_cursor->next);
 				imp->imp_replay_cursor =
 					imp->imp_replay_cursor->next;
 			}
